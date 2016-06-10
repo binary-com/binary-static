0d9b408e2dcc09eacadd7d17216df8d51f124f86
Tee Shuwn Yuan <shuwnyuan@binary.com>
2016-06-10 09:16:30 +0800

    translation related changes

---
+++ scripts/translation.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+DIR=$(cd "$(dirname "$0")"; pwd)
+cd "$DIR"
+echo "make translation under $DIR\n";
+
+carton exec perl local/bin/xgettext.pl  --verbose  -P haml=haml -P perl=pl,pm -P tt2=tt,tt2  \
+    --output=messages.pot   --output-dir=../src/config/locales/   --directory=../src/templates/  --directory=bin/
+
+PATH=/usr/local/opt/gettext/bin/:$PATH
+
+for i in $(ls ../src/config/locales/*.po); do \
+    msgmerge --previous --backup none --no-wrap --update $i ../src/config/locales/messages.pot ; \
+done
+
+msgmerge --previous --backup none --no-wrap --update ../src/config/en.po ../src/config/locales/messages.pot
+
+carton exec perl -pi -e 's/Content-Type: text\/plain; charset=CHARSET/Content-Type: text\/plain; charset=UTF-8/'  ../src/config/locales/messages.pot
+
+
+carton exec perl -ni -e  'print unless m/(^#:|^#\.)/'  ../src/config/en.po
+
+for i in $(ls ../src/config/locales/*.po*); do \
+    carton exec perl -ni -e  'print unless m/(^#:|^#\.)/'  $i ; \
+done
