b5a10056a29f8e8c30ed62f9dbf1796cb5b6de10
binary-hamedanchi <hamedanchi@gmail.com>
2016-06-15 11:15:18 +0800

    Add stylelint

---
+++ build/stylelint.js
@@ -0,0 +1,8 @@
+module.exports = {
+    all: {
+        // options: {
+        //     syntax: require('postcss-scss') // work with SCSS directly
+        // },
+        src: ['src/sass/**/*.scss', '!src/sass/external/**/*.scss']
+    }
+};
