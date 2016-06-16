46c7fd6e41d9cd1528bbb3ab6e175fdfa70abd4d
binary-hamedanchi <hamedanchi@gmail.com>
2016-06-16 09:37:07 +0800

    Fix styles

--- build/stylelint.js
+++ build/stylelint.js
@@ -1,8 +1,13 @@
 module.exports = {
     all: {
-        // options: {
-        //     syntax: require('postcss-scss') // work with SCSS directly
-        // },
-        src: ['src/sass/**/*.scss', '!src/sass/external/**/*.scss']
+        options: {
+            syntax: require('postcss-scss') // work with SCSS directly
+        },
+        src: [
+            'src/sass/**/*.scss',
+            '!src/sass/external/**/*.scss',
+            '!src/sass/_constants.scss',
+            '!src/sass/mixin.scss'
+        ]
     }
 };
