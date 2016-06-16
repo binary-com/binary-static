4ed98f34c7af583941a15ba735602be5f15f7087
ashkanx <ashkan.nemati@gmail.com>
2016-06-16 09:29:51 +0800

    update

--- build/stylelint.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    all: {
-        // options: {
-        //     syntax: require('postcss-scss') // work with SCSS directly
-        // },
-        src: ['src/sass/pages/cashier.scss', '!src/sass/external/**/*.scss']
-    }
-};
