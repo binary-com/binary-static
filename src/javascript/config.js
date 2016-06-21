20b21a52285bfccc2911a2dcc1844d9de3b24d13
Mahdi Mohammadi <mm@binary.com>
2016-06-21 11:19:08 +0800

    merge with master

--- src/javascript/config.js
+++ src/javascript/config.js
@@ -1,11 +1,19 @@
 /*
  * Configuration values needed in js codes
- * 
+ *
  * NOTE:
  * Please use the following command to avoid accidentally committing personal changes
  * git update-index --assume-unchanged src/javascript/config.js
  *
  */
 
-var app_id = /staging\.binary\.com/i.test(window.location.hostname) ? '1098' : '1';
-var socket_url = 'wss://ws.binaryws.com/websockets/v3';
+function getAppId() {
+  return localStorage.getItem('config.app_id') ? localStorage.getItem('config.app_id') :
+               /staging\.binary\.com/i.test(window.location.hostname) ? '1098' : '1';
+}
+
+function getSocketURL() {
+    var server_url = localStorage.getItem('config.server_url');
+    if(!server_url) server_url = (/staging\.binary\.com/i.test(window.location.hostname) ? 'www2' : 'ws') + '.binaryws.com';
+    return 'wss://' + server_url + '/websockets/v3';
+}
