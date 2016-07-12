92f7707acb6aca912ef2e5864be896f1276ddc6e
Eeo Jun <packwolf58@gmail.com>
2016-07-12 13:27:18 +0800

    Fix undefined on IE

--- src/javascript/binary/websocket_pages/user/account/settings/iphistory/iphistory.ui.js
+++ src/javascript/binary/websocket_pages/user/account/settings/iphistory/iphistory.ui.js
@@ -13,7 +13,8 @@ var IPHistoryUI = (function(){
             {name: 'Chromium',  regex: /(?:chromium|crios)\/([\d\w\.\-]+)/i},
             {name: 'Chrome',    regex: /chrome\/([\d\w\.\-]+)/i},
             {name: 'Safari',    regex: /version\/([\d\w\.\-]+)/i},
-            {name: 'IE',        regex: /msie\s([\d\.]+[\d])|trident\/\d+\.\d+;.*[rv:]+(\d+\.\d)/i},
+            {name: 'IE',        regex: /msie\s([\d\.]+[\d])/i},
+            {name: 'IE',        regex: /trident\/\d+\.\d+;.*[rv:]+(\d+\.\d)/i},
             {name: 'Firefox',   regex: /firefox\/([\d\w\.\-]+)/i},
         ];
         var name = 'Unknown';
