e5bd3be4f3df7ce13db7a78f24b9b515232da0c2
Eeo Jun <packwolf58@gmail.com>
2016-07-15 12:01:29 +0800

    Restructure code

--- src/javascript/binary/websocket_pages/user/account/settings/authorised_apps/authorised_apps.data.js
+++ src/javascript/binary/websocket_pages/user/account/settings/authorised_apps/authorised_apps.data.js
@@ -1,6 +1,25 @@
 var ApplicationsData = (function(){
     "use strict";
 
+    function calls(callback) {
+        return function(msg) {
+            var response = JSON.parse(msg.data);
+            if (!response || response.msg_type !== 'oauth_apps') {
+                return;
+            }
+            callback(response);
+        };
+    }
+
+    function list() {
+        BinarySocket.send({oauth_apps: 1});
+    }
+
+    function revoke(id) {
+        if (!id) return;
+        BinarySocket.send({oauth_apps: 1, revoke_app: id});
+    }
+
     function parse(app) {
         var last = app.last_used ? moment.utc(app.last_used) : null;
         return {
@@ -12,6 +31,9 @@ var ApplicationsData = (function(){
     }
 
     return {
-        parse: parse
+        parse: parse,
+        calls: calls,
+        revoke: revoke,
+        list: list,
     };
 }());
