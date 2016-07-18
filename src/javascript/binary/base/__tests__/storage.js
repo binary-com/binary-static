b6abcef1e466aa09ab920cf56bfe806e71f727ad
Eeo Jun <packwolf58@gmail.com>
2016-07-18 10:02:45 +0800

    Test with data from real localisation

--- src/javascript/binary/base/__tests__/storage.js
+++ src/javascript/binary/base/__tests__/storage.js
@@ -6,10 +6,13 @@ describe('text.localize', function() {
     var text = new storage.Localizable({
         key1: 'value',
         key2: 'value [_1]',
+        'You_can_view_your_[_1]trading_limits_here_': 'Ihre [_1] Handelslimits sind hier ersichtlich.',
     });
     it('should try to return a string from the localised texts', function() {
         expect(text.localize('key')).to.equal('key');
         expect(text.localize('key1')).to.equal('value');
+        expect(text.localize('You can view your [_1]trading limits here.'))
+            .to.equal('Ihre [_1] Handelslimits sind hier ersichtlich.');
     });
 
     it('should only template when required', function() {
@@ -17,6 +20,8 @@ describe('text.localize', function() {
         // inject global template function
         global.template = utility.template;
         expect(text.localize('key2', [1])).to.equal('value 1');
+        expect(text.localize('You can view your [_1]trading limits here.', ['something']))
+            .to.equal('Ihre something Handelslimits sind hier ersichtlich.');
         delete global.template;
     });
 });
