325d370f01cea3d8b3973cbb4989e37952810747
binary-hamedanchi <hamedanchi@gmail.com>
2016-07-27 13:35:31 +0800

    fix www2

--- src/javascript/config.js
+++ src/javascript/config.js
@@ -16,8 +16,8 @@ function getSocketURL() {
     var server_url = localStorage.getItem('config.server_url'),
         loginid = $.cookie('loginid');
     if(!server_url) server_url =
-        /staging\.binary\.com/i.test(window.location.hostname) ? 'www2' :
-            (loginid && !/^VRT/.test(loginid) ? 'green' : 'blue') +
-        '.binaryws.com';
+        (/staging\.binary\.com/i.test(window.location.hostname) ? 'www2' :
+            (loginid && !/^VRT/.test(loginid) ? 'green' : 'blue')
+        ) + '.binaryws.com';
     return 'wss://' + server_url + '/websockets/v3';
 }
