89a3003089bb04d6fc65037b8d707863613ba576
binary-hamedanchi <hamedanchi@gmail.com>
2016-08-01 18:22:44 +0800

    Read from cookie and add logged_out category

--- src/javascript/config.js
+++ src/javascript/config.js
@@ -16,18 +16,26 @@ function getSocketURL() {
     var server_url = localStorage.getItem('config.server_url');
     if(!server_url) {
         var loginid = $.cookie('loginid'),
-            isReal = loginid && !/^VRT/.test(loginid),
-            toGreenPercent = {'real': 100, 'other': 0},
+            isReal  = loginid && !/^VRT/.test(loginid),
+            toGreenPercent = {'real': 100, 'virtual': 0, 'logged_out': 0}, // default percentage
+            categoryMap    = ['real', 'virtual', 'logged_out'],
             randomPercent = Math.random() * 100,
-            percentValues = $.cookie('connection_setup');
+            percentValues = $.cookie('connection_setup'); // set by GTM
+
+        // override defaults by cookie values
         if(percentValues && percentValues.indexOf(',') > 0) {
-            var percents = percentValues.split(',');
-            toGreenPercent.real  = +percents[0];
-            toGreenPercent.other = +percents[1];
+            var cookiePercents = percentValues.split(',');
+            categoryMap.map(function(cat, idx) {
+                if(cookiePercents[idx] && !isNaN(cookiePercents[idx])) {
+                    toGreenPercent[cat] = +cookiePercents[idx].trim();
+                }
+            });
         }
+
         server_url = (/staging\.binary\.com/i.test(window.location.hostname) ? 'www2' :
-                (isReal ? (randomPercent < toGreenPercent.real  ? 'green' : 'blue') :
-                          (randomPercent < toGreenPercent.other ? 'green' : 'blue'))
+                (isReal  ? (randomPercent < toGreenPercent.real       ? 'green' : 'blue') :
+                 loginid ? (randomPercent < toGreenPercent.virtual    ? 'green' : 'blue') :
+                           (randomPercent < toGreenPercent.logged_out ? 'green' : 'blue'))
             ) + '.binaryws.com';
     }
     return 'wss://' + server_url + '/websockets/v3';
