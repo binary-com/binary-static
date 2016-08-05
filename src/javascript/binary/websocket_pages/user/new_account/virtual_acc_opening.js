a786f81d037ace195a946d26b6f21dce84c8fcab
Eeo Jun <141bytes@gmail.com>
2016-08-05 11:33:47 +0800

    Refactor

--- src/javascript/binary/websocket_pages/user/new_account/virtual_acc_opening.js
+++ src/javascript/binary/websocket_pages/user/new_account/virtual_acc_opening.js
@@ -1,98 +1,88 @@
 pjax_config_page("new_account/virtualws", function() {
-
-  // TODO: clean this up
-  function handler(msg) {
-    var response = JSON.parse(msg.data);
-    if (!response) return;
-
-    var errorAccount = $('#error-account-opening')[0];
-    var type  = response.msg_type;
-    var error = response.error;
-
-    if (type === 'new_account_virtual' && !error) {
-      var new_account = response.new_account_virtual;
-      page.client.set_cookie('residence', response.echo_req.residence);
-      page.client.process_new_account(
-        new_account.email,
-        new_account.client_id,
-        new_account.oauth_token,
-        true);
-      return;
-    }
-    if (type !== 'error' && !error) return;
-    if (error.code === 'InvalidToken' || error.code === 'duplicate email') {
-      $form.empty();
-      $('.notice-message').remove();
-      var noticeText;
-      if (error.code === 'InvalidToken') {
-        noticeText = '<p>' + Content.localize().textClickHereToRestart.replace('[_1]', page.url.url_for('')) + '</p>';
-      } else if (error.code === 'duplicate email') {
-        noticeText = '<p>' + Content.localize().textDuplicatedEmail.replace('[_1]', page.url.url_for('user/lost_passwordws')) + '</p>';
-      }
-      $form.html(noticeText);
-      return;
-    } else if (error.code === 'PasswordError') {
-      errorAccount.textContent = text.localize('Password is not strong enough.');
-    } else if (error.message) {
-      errorAccount.textContent = error.message;
-    }
-    Validate.displayErrorMessage(errorAccount);
-  }
-
-  function validate(data) {
-    var V2 = ValidateV2;
-    var err = Content.localize().textPasswordsNotMatching;
-    function matches(value) {
-      return value === data.password;
+    function onSuccess(res) {
+        var new_account = res.new_account_virtual;
+        page.client.set_cookie('residence', res.echo_req.residence);
+        page.client.process_new_account(
+            new_account.email,
+            new_account.client_id,
+            new_account.oauth_token,
+            true
+        );
     }
 
-    return validate_object(data, {
-      residence: [V2.required],
-      password:  [V2.password],
-      'verification-code': [V2.required, V2.token],
-      'r-password': [dv.check(matches, err)],
-    });
-  }
-
-  function init() {
-    Content.populate();
-    handle_residence_state_ws();
-    BinarySocket.send({residence_list: 1});
-
-    var form = $('#virtual-form')[0];
-
-    if (!isIE()) {
-      $('#password').on('input', function() {
-        $('#password-meter').attr('value', testPassword($('#password').val())[0]);
-      });
-    } else {
-      $('#password-meter').remove();
+    function onInvalidToken(res) {
+        $('.notice-message').remove();
+        var $form = $('#virtual-form');
+        $form.html($('<p/>', {
+            html: template(Content.localize().textClickHereToRestart, [page.url.url_for('')]),
+        }));
     }
 
-    if (!form) return;
-    bind_validation.simple(form, {
-      validate: validate,
-      submit: function(e, validation) {
-        e.preventDefault();
-        if (validation.errors.length > 0) return;
-        BinarySocket.init({onmessage: handler});
-        var values = validation.values;
-        VirtualAccOpeningData.getDetails({
-          password:  values.password,
-          residence: values.residence,
-          verification_code: values['verification-code']
+    function onDuplicateEmail(res) {
+        $('.notice-message').remove();
+        var $form = $('#virtual-form');
+        $form.html($('<p/>', {
+            html: template(Content.localize().textDuplicatedEmail, [page.url.url_for('user/lost_passwordws')]),
+        }));
+    }
+
+    function onPasswordError(res) {
+        var $error = $('#error-account-opening');
+        $error.css({display: 'block'})
+            .text(text.localize('Password is not strong enough.'));
+    }
+
+    function configureSocket() {
+        BinarySocket.init({
+            onmessage: VirtualAccOpeningData.handler({
+                success:        onSuccess,
+                invalidToken:   onInvalidToken,
+                duplicateEmail: onDuplicateEmail,
+                passwordError:  onPasswordError,
+            })
         });
-      }
-    });
-  }
-
-  return {
-    onLoad: function() {
-      if (getCookieItem('login')) {
-          window.location.href = page.url.url_for('user/my_accountws');
-          return;
-      }
-      init();
     }
-  };
+
+    function init() {
+        Content.populate();
+        handle_residence_state_ws();
+        BinarySocket.send({residence_list: 1});
+
+        var form = $('#virtual-form')[0];
+        if (!form) return;
+        if (!isIE()) {
+            $('#password').on('input', function() {
+                $('#password-meter').attr('value', testPassword($('#password').val())[0]);
+            });
+        } else {
+            $('#password-meter').remove();
+        }
+
+        bind_validation.simple(form, {
+            validate: VirtualAccOpeningData.validate,
+            submit: function(ev, info) {
+                ev.preventDefault();
+                if (info.errors.length > 0) {
+                    return;
+                }
+                configureSocket();
+                var data = info.values;
+                VirtualAccOpeningData.newAccount({
+                    password:  data.password,
+                    residence: data.residence,
+                    verification_code: data['verification-code'],
+                });
+            },
+        });
+    }
+
+    return {
+        onLoad: function() {
+            if (getCookieItem('login')) {
+                window.location.href = page.url.url_for('user/my_accountws');
+                return;
+            }
+            init();
+        }
+    };
 });
--- src/javascript/binary/websocket_pages/user/new_account/virtual_acc_opening/virtual_acc_opening.data.js
+++ src/javascript/binary/websocket_pages/user/new_account/virtual_acc_opening/virtual_acc_opening.data.js
@@ -1,7 +1,7 @@
 var VirtualAccOpeningData = (function(){
     "use strict";
 
-    function getDetails(config) {
+    function newAccount(config) {
         var req = {
             new_account_virtual: 1,
             client_password:     config.password,
@@ -17,7 +17,43 @@ var VirtualAccOpeningData = (function(){
         BinarySocket.send(req);
     }
 
+    function validate(data) {
+        var V2 = ValidateV2;
+        var err = Content.localize().textPasswordsNotMatching;
+        function matches(value) {
+            return value === data.password;
+        }
+        return validate_object(data, {
+            residence: [V2.required],
+            password:  [V2.password],
+            'verification-code': [V2.required, V2.token],
+            'r-password': [dv.check(matches, err)],
+        });
+    }
+
+    function handler(config) {
+        return function(msg) {
+            var response = JSON.parse(msg.data);
+            if (!response) return;
+
+            var type  = response.msg_type;
+            var error = response.error;
+
+            if (type === 'new_account_virtual' && !error) return config.success(response);
+            if (type !== 'error' && !error) return;
+
+            switch (error.code) {
+                case 'InvalidToken':    return config.invalidToken(response);
+                case 'duplicate email': return config.duplicateEmail(response);
+                case 'PasswordError':   return config.passwordError(response);
+                default: return;
+            }
+        };
+    }
+
     return {
-        getDetails: getDetails
+        newAccount: newAccount,
+        validate:   validate,
+        handler:    handler,
     };
 }());
