fa2a3f0c689a6cedc86452665c338935a5b95574
binary-hamedanchi <hamedanchi@gmail.com>
2016-08-05 12:23:27 +0800

    Send source information on virtual account opening

--- src/javascript/binary/websocket_pages/user/new_account/virtual_acc_opening/virtual_acc_opening.data.js
+++ src/javascript/binary/websocket_pages/user/new_account/virtual_acc_opening/virtual_acc_opening.data.js
@@ -9,6 +9,14 @@ var VirtualAccOpeningData = (function(){
                     verification_code: verificationCode
                 };
 
+        // Add AdWords parameters
+        // NOTE: following lines can be uncommented (Re-check property names)
+        // once these fields added to this ws call
+        // var utm_data = AdWords.getData();
+        // req.source = utm_data.utm_source || utm_data.referrer || 'direct';
+        // if(utm_data.utm_medium)   req.medium   = utm_data.utm_medium;
+        // if(utm_data.utm_campaign) req.campaign = utm_data.utm_campaign;
+
         if ($.cookie('affiliate_tracking')) {
             req.affiliate_token = JSON.parse($.cookie('affiliate_tracking')).t;
         }
