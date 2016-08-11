85c301c990d06beefb117c07959363a25ce29afe
Eeo Jun <141bytes@gmail.com>
2016-08-11 16:27:56 +0800

    Remove preamble for now

--- src/javascript/binary/common_functions/common_data.js
+++ src/javascript/binary/common_functions/common_data.js
@@ -1,10 +1,3 @@
-var CommonData = (function(){
-    //because getCookieItem('login') is confusing and does not looks like we are getting API token
-    function getApiToken() {
-        return Cookies.get('login');
-    }
-
-    return {
-        getApiToken: getApiToken
-    };
-}());
+var CommonData = {
+    getApiToken: function() { return Cookies.get('login'); }
+};
