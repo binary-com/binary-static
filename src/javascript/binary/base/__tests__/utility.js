f54655582a4f4c66151922e7207baecd4ba01033
Eeo Jun <141bytes@gmail.com>
2016-08-18 15:18:31 +0800

    add tests

--- src/javascript/binary/base/__tests__/utility.js
+++ src/javascript/binary/base/__tests__/utility.js
@@ -11,3 +11,19 @@ describe('template', function() {
         expect(utility.template('[_1] [_2]', ['[_2]', 'abc'])).to.eq('[_2] abc');
     });
 });
+
+describe('objectNotEmpty', function() {
+    it('returns true for empty objects', function() {
+        expect(utility.objectNotEmpty({})).to.eq(false);
+        expect(utility.objectNotEmpty({notEmpty: true})).to.eq(true);
+    });
+
+    it('returns false for non objects', function() {
+        expect(utility.objectNotEmpty(1)).to.eq(false);
+        expect(utility.objectNotEmpty(undefined)).to.eq(false);
+        expect(utility.objectNotEmpty(null)).to.eq(false);
+        expect(utility.objectNotEmpty(false)).to.eq(false);
+        expect(utility.objectNotEmpty(true)).to.eq(false);
+        expect(utility.objectNotEmpty('')).to.eq(false);
+    });
+});
