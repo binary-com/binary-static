aaa94da2f35865fa18bef68006ca6aa2598a049f
binary-hamedanchi <hamedanchi@gmail.com>
2016-09-01 11:43:52 +0800

    Add webpack

---
+++ build/webpack.js
@@ -0,0 +1,13 @@
+module.exports = function (grunt) {
+    return {
+        all: {
+            entry: {
+                app: './src/javascript/index.js',
+            },
+            output: {
+                path: './src/javascript',
+                filename: 'modules.js',
+            },
+        }
+    }
+};
