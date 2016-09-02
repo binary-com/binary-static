08e44a767ec1490b76b2c4caab334a012532b18a
Amin Marashi <marashi.s.amin@gmail.com>
2016-09-02 13:52:56 +0800

    Removed binary_pack from uglify

--- build/uglify.js
+++ build/uglify.js
@@ -5,8 +5,7 @@ module.exports = {
             sourceMapIncludeSources: true,
         },
         files: [
-            {dest: global.dist + '/js/binary.min.js', src: global.dist + '/js/binary.js'},
-            {dest: global.dist + '/js/binary_pack.min.js', src: global.dist + '/js/binary_pack.js'}
+            {dest: global.dist + '/js/binary.min.js', src: global.dist + '/js/binary.js'}
         ]
     }
 };
