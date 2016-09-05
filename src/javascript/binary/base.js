96e9cb1666eb6f9841047ce857f148c772f86ed7
negar <negar@binary.com>
2016-09-05 11:02:50 +0800

    remove markets.js

--- src/javascript/binary/base.js
+++ src/javascript/binary/base.js
@@ -250,13 +250,6 @@ var TUser = (function () {
     };
 })();
 
-/*
- * Make sure data js is loaded before this
- * else website will not work properly
- * objects texts_json, markets_list, markets_json
- * should be available
- */
-
 // make texts object as Localizable
 var texts = {};
 for (var key in texts_json) {
@@ -264,6 +257,3 @@ for (var key in texts_json) {
         texts[key] = new Localizable(texts_json[key]);
     }
 }
-
-// make markets object
-var markets = new Markets(markets_list, markets_json);
