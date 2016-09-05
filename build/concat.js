9ca3dcb8700209ce70740693962eb277fa82bfe2
Amin Marashi <marashi.s.amin@gmail.com>
2016-09-05 10:41:05 +0800

    Removed jquery.js and added all libs but enjoyhint to webpack

--- build/concat.js
+++ build/concat.js
@@ -7,12 +7,8 @@ module.exports = {
             {
                 dest: global.dist + '/js/binary.js',
                 src: [
-                    'src/javascript/lib/jquery.js',
-                    'src/javascript/lib/highstock/highstock.js',
-                    'src/javascript/lib/highstock/highstock-exporting.js',
-                    'src/javascript/lib/moment/moment.js',
-                    'src/javascript/lib/**/*.js',
                     global.dist + '/js/binary_pack.js',
+                    'src/javascript/lib/guide.enjoyhint.js',
                 ]
             }
         ]
