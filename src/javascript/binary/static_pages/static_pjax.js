94b2635da961994d40bcfa2545eb8004bad7bb7f
binary-hamedanchi <hamedanchi@gmail.com>
2016-09-07 10:46:31 +0800

    Fix home pjax and redirect if logged in

--- src/javascript/binary/static_pages/static_pjax.js
+++ src/javascript/binary/static_pages/static_pjax.js
@@ -1,11 +1,10 @@
-pjax_config_page('/\?.+|/home', function() {
+pjax_config_page('/home', function() {
     return {
         onLoad: function() {
-            if(/^(\/|\/home)$/i.test(window.location.pathname)) {
-                page.client.redirect_if_login();
+            if(!page.client.redirect_if_login()) {
+                check_login_hide_signup();
+                submit_email();
             }
-            check_login_hide_signup();
-            submit_email();
         }
     };
 });
