d1c6255617468c31d179103a0152b748e08820c8
binary-hamedanchi <hamedanchi@gmail.com>
2016-09-26 15:33:05 +0800

    Fix visibility and alignment of topbar message

--- src/javascript/binary/base/page.js
+++ src/javascript/binary/base/page.js
@@ -966,7 +966,7 @@ Contents.prototype = {
                         show_upgrade('new_account/realws', 'Upgrade to a Real Account');
                     }
                 } else if (show_virtual_msg) {
-                    $upgrade_msg.removeClass(hiddenClass).find('> span').removeClass(hiddenClass);
+                    $upgrade_msg.removeClass(hiddenClass).find('> span').removeClass(hiddenClass + ' gr-hide-m');
                 }
             } else {
                 var show_financial = false;
