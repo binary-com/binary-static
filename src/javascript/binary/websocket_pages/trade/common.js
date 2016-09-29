d5696bfb44ce95acffcd69729a16cf4ad0ea07f5
amin roosta <amin.roosta@outlook.com>
2016-09-29 12:53:21 +0330

    Prevent IE memory leak in trade page

--- src/javascript/binary/websocket_pages/trade/common.js
+++ src/javascript/binary/websocket_pages/trade/common.js
@@ -911,7 +911,10 @@ function showHighchart(){
   if (window.chartAllowed) {
     chartFrameSource();
   } else {
-    document.getElementById('chart_frame').src = '';
+    /*
+     * Prevent IE memory leak (http://stackoverflow.com/questions/8407946).
+     */
+    document.getElementById('chart_frame').src = 'about:blank';
     $('#trade_live_chart').hide();
     $('#chart-error').text(page.text.localize('Chart is not available for this underlying.'))
                      .show();
@@ -920,7 +923,7 @@ function showHighchart(){
 }
 
 function chartFrameSource() {
-  if ($('#tab_graph').hasClass('active') && (sessionStorage.getItem('old_underlying') !== sessionStorage.getItem('underlying') || $('#chart_frame').attr('src') === '')) {
+  if ($('#tab_graph').hasClass('active') && (sessionStorage.getItem('old_underlying') !== sessionStorage.getItem('underlying') || $('#chart_frame').attr('src') === '' || $('#chart_frame').attr('src') === 'about:blank')) {
       setChartSource();
       sessionStorage.setItem('old_underlying', document.getElementById('underlying').value);
   }
