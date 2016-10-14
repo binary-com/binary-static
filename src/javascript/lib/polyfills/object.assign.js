1630626d1f0f811a7588e48a146b59b5f35f3391
binary-hamedanchi <hamedanchi@gmail.com>
2016-10-14 16:22:49 +0800

    Move polyfills and remove cdn

---
+++ src/javascript/lib/polyfills/object.assign.js
@@ -0,0 +1,14 @@
+if (!('assign' in Object)) {
+    Object.assign = function assign(target, source) {
+        for (var index = 1, key, src; index < arguments.length; ++index) {
+            src = arguments[index];
+
+            for (key in src) {
+                if (Object.prototype.hasOwnProperty.call(src, key)) {
+                    target[key] = src[key];
+                }
+            }
+        }
+        return target;
+    };
+}
