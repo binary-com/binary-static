c07fb3519687e6e017a7f7770a4b419b278446e3
binary-hamedanchi <hamedanchi@gmail.com>
2016-10-26 09:02:09 +0800

    Multi barriers trading

---
+++ src/javascript/binary/websocket_pages/mb_trade/mb_tradepage.js
@@ -0,0 +1,65 @@
+var MBTradePage = (function(){
+
+  var trading_page = 0, events_initialized = 0;
+
+  var onLoad = function(){
+    trading_page = 1;
+    if(sessionStorage.getItem('currencies')){
+      displayCurrencies();
+    }
+    BinarySocket.init({
+      onmessage: function(msg){
+        MBMessage.process(msg);
+      }
+    });
+    Price.clearFormId();
+    if (events_initialized === 0) {
+        events_initialized = 1;
+        TradingEvents.init();
+    }
+    Content.populate();
+
+    if (sessionStorage.getItem('currencies')){
+      displayCurrencies();
+      Symbols.getSymbols(1);
+    } else {
+      BinarySocket.send({ payout_currencies: 1 });
+    }
+
+    if (document.getElementById('websocket_form')) {
+        addEventListenerForm();
+    }
+
+    TradingAnalysis.bindAnalysisTabEvent();
+    $('#tab_portfolio a').text(page.text.localize('Portfolio'));
+    $('#tab_graph a').text(page.text.localize('Chart'));
+    $('#tab_explanation a').text(page.text.localize('Explanation'));
+
+    window.chartAllowed = true;
+  };
+
+  var reload = function() {
+    window.location.reload();
+  };
+
+  var onUnload = function(){
+    chartFrameCleanup();
+    window.chartAllowed = false;
+    trading_page = 0;
+    events_initialized = 0;
+    JapanPortfolio.hide();
+    forgetTradingStreams();
+    BinarySocket.clear();
+  };
+
+  return {
+    onLoad: onLoad,
+    reload: reload,
+    onUnload : onUnload,
+    is_trading_page: function(){return trading_page;}
+  };
+})();
+
+module.exports = {
+    MBTradePage: MBTradePage,
+};
---
+++ src/javascript/binary/websocket_pages/mb_trade/symbols.js
@@ -0,0 +1,52 @@
+/*
+ * Symbols object parses the active_symbols json that we get from socket.send({active_symbols: 'brief'}
+ * and outputs in usable form, it gives markets, underlyings
+ *
+ *
+ * Usage:
+ *
+ * use `Symbols.details` to populate this object first
+ *
+ * then use
+ *
+ * `Symbols.markets` to get markets like Forex, Random etc
+ * `Symbols.underlyings` to get underlyings
+ *
+ */
+
+var Symbols = (function () {
+    'use strict';
+
+    var tradeMarkets = {}, tradeMarketsList = {}, tradeUnderlyings = {}, need_page_update = 1, names = {};
+
+    var details = function (data) {
+        var allSymbols = data['active_symbols'];
+        tradeMarkets = ActiveSymbols.getMarkets(allSymbols);
+        tradeMarketsList = ActiveSymbols.getMarketsList(allSymbols);
+        tradeUnderlyings = ActiveSymbols.getTradeUnderlyings(allSymbols);
+        names = ActiveSymbols.getSymbolNames(allSymbols);
+    };
+
+    var getSymbols = function (update) {
+        BinarySocket.send({
+            active_symbols : 'brief',
+            landing_company: 'japan',
+        });
+        need_page_update = update;
+    };
+
+    return {
+        details: details,
+        getSymbols: getSymbols,
+        markets: function (list) { return list ? tradeMarketsList : tradeMarkets; },
+        underlyings: function () { return tradeUnderlyings; },
+        getName: function(symbol){ return names[symbol]; },
+        need_page_update: function () { return need_page_update; },
+        getAllSymbols: function(){return names;}
+    };
+
+})();
+
+module.exports = {
+    Symbols: Symbols,
+};
