a7cf959029a33507670ee9baaca17b47d91b5384
negar <negar@binary.com>
2016-11-21 16:08:16 +0800

    change decimal points

--- src/javascript/binary/common_functions/__tests__/currency_to_symbol.js
+++ src/javascript/binary/common_functions/__tests__/currency_to_symbol.js
@@ -4,15 +4,15 @@ var format_money = require('../currency_to_symbol').format_money;
 
 describe('format_money', function() {
     it('works as expected', function() {
-        expect(format_money('USD', '123.00')).to.eq('$123.00');
-        expect(format_money('GBP', '123.00')).to.eq('£123.00');
-        expect(format_money('EUR', '123.00')).to.eq('€123.00');
-        expect(format_money('AUD', '123.00')).to.eq('A$123.00');
-        expect(format_money('JPY', '123.00')).to.eq('¥123');
-        expect(format_money('JPY', '1234.00')).to.eq('¥1,234');
+        expect(format_money('USD', '123.55')).to.eq('$123.55');
+        expect(format_money('GBP', '123.55')).to.eq('£123.55');
+        expect(format_money('EUR', '123.55')).to.eq('€123.55');
+        expect(format_money('AUD', '123.55')).to.eq('A$123.55');
+        expect(format_money('JPY', '123.55')).to.eq('¥124');
+        expect(format_money('JPY', '1234.55')).to.eq('¥1,235');
     });
 
     it('works for unexpected currencies', function() {
-        expect(format_money('WTV', '123.00')).to.eq('WTV123.00');
+        expect(format_money('WTV', '123.55')).to.eq('WTV123.55');
     });
 });
