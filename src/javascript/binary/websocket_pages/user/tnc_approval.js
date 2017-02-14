9a82d106990dcffe6b2efdcd1106ab35f4b17f3d
binary-hamedanchi <hamedanchi@gmail.com>
2017-02-14 14:03:59 +0800

    Add accept button to tnc page

--- src/javascript/binary/websocket_pages/user/tnc_approval.js
+++ src/javascript/binary/websocket_pages/user/tnc_approval.js
@@ -7,62 +7,67 @@ const template             = require('../../base/utility').template;
 const TNCApproval = (function() {
     'use strict';
 
-    let redirect_url;
     const hidden_class = 'invisible';
 
     const init = function() {
-        redirect_url = sessionStorage.getItem('tnc_redirect');
-        sessionStorage.removeItem('tnc_redirect');
-
-        BinarySocket.wait('website_status', 'get_settings').then(() => {
-            showTNC();
-        });
+        requiresTNCApproval($('#btn_accept'), display, null, true);
     };
 
-    const showTNC = function() {
-        const terms_conditions_version = State.get(['response', 'website_status', 'website_status', 'terms_conditions_version']);
-        const client_tnc_status        = State.get(['response', 'get_settings', 'get_settings', 'client_tnc_status']);
-        const landing_company          = Client.get('landing_company_fullname');
-
-        if (!terms_conditions_version || !client_tnc_status || !landing_company) {
-            return;
-        }
-
-        if (terms_conditions_version === client_tnc_status) {
-            redirectBack();
-            return;
-        }
+    const display = () => {
+        const landing_company = Client.get('landing_company_fullname');
+        if (!landing_company) return;
 
         const $container = $('#tnc_container');
-        const $tnc_msg = $container.find('#tnc_message');
+        const $tnc_msg   = $container.find('#tnc_message');
         $tnc_msg.html(template($tnc_msg.html(), [
             landing_company,
             url_for(Client.get('residence') === 'jp' ? 'terms-and-conditions-jp' : 'terms-and-conditions'),
         ]));
         $container.find('#tnc_loading').remove();
         $container.find('#tnc_approval').removeClass(hidden_class);
+    };
 
-        $container.find('#btn_accept').click(function(e) {
-            e.preventDefault();
-            e.stopPropagation();
-            BinarySocket.send({ tnc_approval: '1' }, true).then((response) => {
-                if (response.error) {
-                    $('#err_message').html(response.error.message).removeClass(hidden_class);
-                } else {
-                    sessionStorage.setItem('check_tnc', 'checked');
-                    redirectBack();
-                }
+    const requiresTNCApproval = ($btn, funcDisplay, onSuccess, redirect_anyway) => {
+        BinarySocket.wait('website_status', 'get_settings').then(() => {
+            const terms_conditions_version = State.get(['response', 'website_status', 'website_status', 'terms_conditions_version']);
+            const client_tnc_status        = State.get(['response', 'get_settings', 'get_settings', 'client_tnc_status']);
+
+            if (!terms_conditions_version || terms_conditions_version === client_tnc_status) {
+                // redirectBack();
+                return;
+            }
+
+            funcDisplay();
+
+            $btn.click(function (e) {
+                e.preventDefault();
+                e.stopPropagation();
+                BinarySocket.send({ tnc_approval: '1' }, true).then((response) => {
+                    if (response.error) {
+                        $('#err_message').html(response.error.message).removeClass(hidden_class);
+                    } else {
+                        sessionStorage.setItem('check_tnc', 'checked');
+                        redirectBack(redirect_anyway);
+                        if (typeof onSuccess === 'function') {
+                            onSuccess();
+                        }
+                    }
+                });
             });
         });
     };
 
-    const redirectBack = function() {
-        window.location.href = redirect_url || default_redirect_url();
+    const redirectBack = function(redirect_anyway) {
+        const redirect_url = sessionStorage.getItem('tnc_redirect');
+        sessionStorage.removeItem('tnc_redirect');
+        if (redirect_url || redirect_anyway) {
+            window.location.href = redirect_url || default_redirect_url();
+        }
     };
 
     return {
-        init   : init,
-        showTNC: showTNC,
+        init               : init,
+        requiresTNCApproval: requiresTNCApproval,
     };
 })();
 
