b682018c53b69e42e9b0c8357a31c2f65eacf7f1
binary-hamedanchi <hamedanchi@gmail.com>
2017-02-21 17:21:09 +0800

    Fix risk classification

--- src/javascript/binary/common_functions/check_risk_classification.js
+++ src/javascript/binary/common_functions/check_risk_classification.js
@@ -1,10 +1,11 @@
 const RiskClassification  = require('../common_functions/risk_classification').RiskClassification;
 const FinancialAssessment = require('../websocket_pages/user/account/settings/financial_assessment');
 const Client  = require('../base/client').Client;
+const State   = require('../base/storage').State;
 const url_for = require('../base/url').url_for;
 
 function check_risk_classification() {
-    if (localStorage.getItem('risk_classification.response') === 'high' &&
+    if (State.get(['response', 'get_account_status', 'get_account_status', 'risk_classification']) === 'high' &&
         localStorage.getItem('risk_classification') === 'high' &&
         qualify_for_risk_classification()) {
         renderRiskClassificationPopUp();
@@ -12,23 +13,23 @@ function check_risk_classification() {
 }
 
 function renderRiskClassificationPopUp() {
-    if (window.location.pathname === '/user/settings/assessmentws') {
+    if (/user\/settings\/assessmentws/i.test(window.location.pathname)) {
         window.location.href = url_for('user/settingsws');
         return;
     }
+    if ($('#frm_assessment').length) return;
+
     $.ajax({
         url     : url_for('user/settings/assessmentws'),
         dataType: 'html',
         method  : 'GET',
         success : function(riskClassificationText) {
-            if (riskClassificationText.includes('assessment_form')) {
-                const payload = $(riskClassificationText);
-                RiskClassification.showRiskClassificationPopUp(payload.find('#assessment_form'));
+            const $frm_assessment = $(riskClassificationText).find('#frm_assessment');
+            if ($frm_assessment.length) {
+                $frm_assessment.removeClass('invisible').attr('style', 'text-align: left;');
+                RiskClassification.showRiskClassificationPopUp($frm_assessment);
                 const $risk_classification = $('#risk_classification');
-                $risk_classification.find('#assessment_form').removeClass('invisible')
-                    .attr('style', 'text-align: left;');
-                $risk_classification.find('#high_risk_classification').removeClass('invisible');
-                $risk_classification.find('#heading_risk').removeClass('invisible');
+                $risk_classification.find('#high_risk_classification, #heading_risk').removeClass('invisible');
                 handleForm($risk_classification);
             }
         },
@@ -40,17 +41,18 @@ function renderRiskClassificationPopUp() {
 }
 
 function handleForm($risk_classification) {
-    FinancialAssessment.handleForm();
-    $risk_classification.find('#assessment_form').on('submit', function(event) {
-        event.preventDefault();
-        FinancialAssessment.submitForm();
-        return false;
+    BinarySocket.wait('get_financial_assessment').then(() => {
+        FinancialAssessment.handleForm();
+        $risk_classification.find('#frm_assessment').on('submit', function(event) {
+            event.preventDefault();
+            FinancialAssessment.submitForm();
+            return false;
+        });
     });
 }
 
 function qualify_for_risk_classification() {
-    return (Client.is_logged_in() && !Client.get('is_virtual') &&
-            Client.get('residence') !== 'jp' && $('#assessment_form').length === 0 &&
+    return (Client.is_logged_in() && !Client.get('is_virtual') && Client.get('residence') !== 'jp' &&
             (localStorage.getItem('reality_check.ack') === '1' || !localStorage.getItem('reality_check.interval')));
 }
 
