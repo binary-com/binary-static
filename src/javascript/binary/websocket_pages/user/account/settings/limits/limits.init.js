cb66040a4cf0e14b2cde6d3d2ad8b748d3e54409
kelly <kelly@binary.com>
2017-02-24 15:27:21 +0800

    Renamed variables

--- src/javascript/binary/websocket_pages/user/account/settings/limits/limits.init.js
+++ src/javascript/binary/websocket_pages/user/account/settings/limits/limits.init.js
@@ -14,39 +14,40 @@ const Limits = (() => {
         const limits = response.get_limits;
         LimitsUI.fillLimitsTable(limits);
 
-        const elWithdrawLimit    = document.getElementById('withdrawal-limit');
-        const elWithdrawn        = document.getElementById('already-withdraw');
-        const elWithdrawLimitAgg = document.getElementById('withdrawal-limit-aggregate');
+        const el_withdraw_limit     = document.getElementById('withdrawal-limit');
+        const el_withdrawn          = document.getElementById('already-withdraw');
+        const el_withdraw_limit_agg = document.getElementById('withdrawal-limit-aggregate');
 
         if (limits.lifetime_limit === 99999999 && limits.num_of_days_limit === 99999999) {
-            elementTextContent(elWithdrawLimit, localize('Your account is fully authenticated and your withdrawal limits have been lifted.'));
+            elementTextContent(el_withdraw_limit, localize('Your account is fully authenticated and your withdrawal limits have been lifted.'));
         } else {
-            let txtWithdrawLim             = localize('Your withdrawal limit is [_1] [_2] (or equivalent in other currency).'),
-                txtWithdrawAmt             = localize('You have already withdrawn the equivalent of [_1] [_2].'),
-                text_CurrentMaxWithdrawal  = localize('Therefore your current immediate maximum withdrawal (subject to your account having sufficient funds) is [_1] [_2] (or equivalent in other currency).'),
+            let txt_withdraw_lim           = localize('Your withdrawal limit is [_1] [_2] (or equivalent in other currency).'),
+                txt_withdraw_amt           = localize('You have already withdrawn the equivalent of [_1] [_2].'),
+                txt_current_max_withdrawal = localize('Therefore your current immediate maximum withdrawal (subject to your account having sufficient funds) is [_1] [_2] (or equivalent in other currency).'),
                 currency                   = 'EUR';
-            const daysLimit                = addComma(limits.num_of_days_limit).split('.')[1] === '00' ? addComma(limits.num_of_days_limit).split('.')[0] : addComma(limits.num_of_days_limit);
+            const days_limit               = addComma(limits.num_of_days_limit).split('.')[1] === '00' ? addComma(limits.num_of_days_limit).split('.')[0] : addComma(limits.num_of_days_limit);
             // no need for addComma since it is already string like "1,000"
             const withdrawn                = limits.withdrawal_since_inception_monetary;
             const remainder                = addComma(limits.remainder).split('.')[1] === '00' ? addComma(limits.remainder).split('.')[0] : addComma(limits.remainder);
 
             if ((/^(iom)$/i).test(Client.get('landing_company_name'))) { // MX
-                txtWithdrawLim = localize('Your [_1] day withdrawal limit is currently [_2] [_3] (or equivalent in other currency).');
-                txtWithdrawAmt = localize('You have already withdrawn the equivalent of [_1] [_2] in aggregate over the last [_3] days.');
-                elementTextContent(elWithdrawLimit,
-                   template(txtWithdrawLim, [limits.num_of_days, currency, daysLimit]));
-                elementTextContent(elWithdrawn, template(txtWithdrawAmt,  [currency, withdrawn, limits.num_of_days]));
+                txt_withdraw_lim  = localize('Your [_1] day withdrawal limit is currently [_2] [_3] (or equivalent in other currency).');
+                txt_withdraw_amt = localize('You have already withdrawn the equivalent of [_1] [_2] in aggregate over the last [_3] days.');
+                elementTextContent(el_withdraw_limit,
+                    template(txt_withdraw_lim, [limits.num_of_days, currency, days_limit]));
+                elementTextContent(el_withdrawn,
+                    template(txt_withdraw_amt,  [currency, withdrawn, limits.num_of_days]));
             } else {
                 if ((/^(costarica|japan)$/i).test(Client.get('landing_company_name'))) { // CR , JP
-                    txtWithdrawLim            = localize('Your withdrawal limit is [_1] [_2].');
-                    txtWithdrawAmt            = localize('You have already withdrawn [_1] [_2].');
-                    text_CurrentMaxWithdrawal = localize('Therefore your current immediate maximum withdrawal (subject to your account having sufficient funds) is [_1] [_2].');
-                    currency                  = Client.get('currency') || Client.get('default_currency');
+                    txt_withdraw_lim           = localize('Your withdrawal limit is [_1] [_2].');
+                    txt_withdraw_amt           = localize('You have already withdrawn [_1] [_2].');
+                    txt_current_max_withdrawal = localize('Therefore your current immediate maximum withdrawal (subject to your account having sufficient funds) is [_1] [_2].');
+                    currency                   = Client.get('currency') || Client.get('default_currency');
                 }
-                elementTextContent(elWithdrawLimit, template(txtWithdrawLim, [currency, daysLimit]));
-                elementTextContent(elWithdrawn, template(txtWithdrawAmt,  [currency, withdrawn]));
+                elementTextContent(el_withdraw_limit, template(txt_withdraw_lim, [currency, days_limit]));
+                elementTextContent(el_withdrawn, template(txt_withdraw_amt,  [currency, withdrawn]));
             }
-            elementTextContent(elWithdrawLimitAgg, template(text_CurrentMaxWithdrawal, [currency, remainder]));
+            elementTextContent(el_withdraw_limit_agg, template(txt_current_max_withdrawal, [currency, remainder]));
         }
     };
 
--- src/javascript/binary/websocket_pages/user/account/settings/limits/limits.ui.js
+++ src/javascript/binary/websocket_pages/user/account/settings/limits/limits.ui.js
@@ -7,10 +7,10 @@ const elementTextContent  = require('../../../../../common_functions/common_func
 const LimitsUI = (() => {
     'use strict';
 
-    let clientLimits = '';
+    let client_limits = '';
 
     const appendRowTable = (name, turnover_limit, padding, font_weight) => {
-        clientLimits.append('<tr class="flex-tr">' +
+        client_limits.append('<tr class="flex-tr">' +
             '<td class="flex-tr-child" style="padding-left: ' + padding + '; font-weight: ' + font_weight + ';">' +
             localize(name) +
             '</td>' +
@@ -27,20 +27,20 @@ const LimitsUI = (() => {
             $('.limit').append(' (' + currency + ')');
         }
 
-        const openPosition = document.getElementById('open-positions'),
-            accountBalance = document.getElementById('account-balance'),
-            payout         = document.getElementById('payout'),
-            payoutPer      = document.getElementById('payout-per-symbol-and-contract-type');
+        const open_position = document.getElementById('open-positions'),
+            account_balance = document.getElementById('account-balance'),
+            payout          = document.getElementById('payout'),
+            payout_per      = document.getElementById('payout-per-symbol-and-contract-type');
 
-        elementTextContent(openPosition, addComma(limits.open_positions).split('.')[0]);
-        elementTextContent(accountBalance, addComma(limits.account_balance).split('.')[0]);
+        elementTextContent(open_position, addComma(limits.open_positions).split('.')[0]);
+        elementTextContent(account_balance, addComma(limits.account_balance).split('.')[0]);
         elementTextContent(payout, addComma(limits.payout).split('.')[0]);
-        elementTextContent(payoutPer, addComma(limits.payout_per_symbol_and_contract_type).split('.')[0]);
+        elementTextContent(payout_per, addComma(limits.payout_per_symbol_and_contract_type).split('.')[0]);
 
-        const marketSpecific = limits.market_specific;
-        clientLimits = $('#client-limits');
-        Object.keys(marketSpecific).forEach(function (key) {
-            const object = marketSpecific[key];
+        const market_specific = limits.market_specific;
+        client_limits = $('#client-limits');
+        Object.keys(market_specific).forEach(function (key) {
+            const object = market_specific[key];
             if (object.length && object.length > 0) {
                 appendRowTable(localize(key.charAt(0).toUpperCase() + key.slice(1)), '', 'auto', 'bold');
                 Object.keys(object).forEach(function (c) {
@@ -52,10 +52,10 @@ const LimitsUI = (() => {
                 appendRowTable(object.name, object.turnover_limit !== 'null' ? addComma(object.turnover_limit).split('.')[0] : 0, 'auto', 'bold');
             }
         });
-        const loginId =  Client.get('loginid');
-        if (loginId) {
-            $('#trading-limits').prepend(loginId + ' - ');
-            $('#withdrawal-title').prepend(loginId + ' - ');
+        const login_id =  Client.get('login_id');
+        if (login_id) {
+            $('#trading-limits').prepend(login_id + ' - ');
+            $('#withdrawal-title').prepend(login_id + ' - ');
         }
         $('#withdrawal-limits, #limits-title').removeClass('invisible');
     };
