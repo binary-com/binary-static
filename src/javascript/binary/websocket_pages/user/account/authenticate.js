a800f9f2845c52435488d0e666ef92331177d1a2
kelly <kelly@binary.com>
2017-02-24 14:54:44 +0800

    Authenticate

--- src/javascript/binary/websocket_pages/user/account/authenticate.js
+++ src/javascript/binary/websocket_pages/user/account/authenticate.js
@@ -3,40 +3,35 @@ const japanese_client = require('../../../common_functions/country_base').japane
 const Client   = require('../../../base/client').Client;
 const url_for  = require('../../../base/url').url_for;
 
-const AuthenticateWS = (function() {
-    const init = function() {
+const Authenticate = (() => {
+    const init = () => {
         if (japanese_client()) {
             window.location.href = url_for('trading');
         }
         Content.populate();
 
-        const show_error = function(error) {
+        const show_error = (error) => {
             $('#error_message').removeClass('invisible').text(error);
             return true;
         };
 
-        const check_virtual = function() {
-            return Client.get('is_virtual') && show_error(Content.localize().featureNotRelevantToVirtual);
-        };
+        const check_virtual = () => Client.get('is_virtual') && show_error(Content.localize().featureNotRelevantToVirtual);
+
         if (!check_virtual()) {
-            BinarySocket.init({
-                onmessage: function(msg) {
-                    const response = JSON.parse(msg.data);
-                    if (response) {
-                        const error = response.error;
-                        if (response.msg_type === 'get_account_status' && !check_virtual() && !error) {
-                            if ($.inArray('authenticated', response.get_account_status.status) > -1) {
-                                $('#fully-authenticated').removeClass('invisible');
-                            } else {
-                                $('#not-authenticated').removeClass('invisible');
-                            }
-                        } else if (error) {
-                            show_error(error.message);
+            BinarySocket.send({ get_account_status: 1 }).then((response) => {
+                if (response) {
+                    const error = response.error;
+                    if (response.msg_type === 'get_account_status' && !check_virtual() && !error) {
+                        if ($.inArray('authenticated', response.get_account_status.status) > -1) {
+                            $('#fully-authenticated').removeClass('invisible');
+                        } else {
+                            $('#not-authenticated').removeClass('invisible');
                         }
+                    } else if (error) {
+                        show_error(error.message);
                     }
-                },
+                }
             });
-            BinarySocket.send({ get_account_status: 1 });
         }
     };
     return {
@@ -44,6 +39,4 @@ const AuthenticateWS = (function() {
     };
 })();
 
-module.exports = {
-    AuthenticateWS: AuthenticateWS,
-};
+module.exports = Authenticate;
