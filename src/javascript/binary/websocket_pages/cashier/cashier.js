04ae92109cb0a3a87402ccebd475d0c2267a139a
negar <negar254000@gmail.com>
2017-02-27 16:01:46 +0800

    fixes and cleanup

--- src/javascript/binary/websocket_pages/cashier/cashier.js
+++ src/javascript/binary/websocket_pages/cashier/cashier.js
@@ -1,21 +1,15 @@
-const japanese_client      = require('../../common_functions/country_base').japanese_client;
-const japanese_residence   = require('../../common_functions/country_base').japanese_residence;
 const Client               = require('../../base/client').Client;
 const Header               = require('../../base/header').Header;
 const default_redirect_url = require('../../base/url').default_redirect_url;
+const japanese_client      = require('../../common_functions/country_base').japanese_client;
+const japanese_residence   = require('../../common_functions/country_base').japanese_residence;
 
 const Cashier = (function() {
     'use strict';
 
-    let withdrawal_locked;
-
-    const lockUnlockCashier = function(action, lock_type) {
-        const toggle = action === 'lock' ? 'disable' : 'enable';
-        if (/withdraw/.test(lock_type) && withdrawal_locked) {
-            return;
-        }
+    const lock = function(lock_type) {
         $.each($('.' + lock_type), function() {
-            replaceButton(toggle, $(this).parent());
+            disableButton($(this).parent());
         });
     };
 
@@ -30,13 +24,11 @@ const Cashier = (function() {
             checkTopUpWithdraw();
             BinarySocket.wait('get_account_status').then(() => {
                 if (Client.status_detected('cashier_locked')) {
-                    lockUnlockCashier('lock', 'deposit, .withdraw');
-                    withdrawal_locked = true;
+                    lock('deposit, .withdraw');
                 } else if (Client.status_detected('withdrawal_locked')) {
-                    lockUnlockCashier('lock', 'withdraw');
-                    withdrawal_locked = true;
+                    lock('withdraw');
                 } else if (Client.status_detected('unwelcome')) {
-                    lockUnlockCashier('lock', 'deposit');
+                    lock('deposit');
                 }
             });
         });
@@ -49,41 +41,24 @@ const Cashier = (function() {
             if (Client.get('is_virtual')) {
                 if ((currency !== 'JPY' && balance > 1000) ||
                     (currency === 'JPY' && balance > 100000)) {
-                    replaceButton('disable', '#VRT_topup_link');
+                    disableButton('#VRT_topup_link');
                 }
             } else if (!currency || +balance === 0) {
-                lockUnlockCashier('lock', 'withdraw');
-            } else {
-                lockUnlockCashier('unlock', 'withdraw');
+                lock('withdraw');
             }
         });
     };
 
-    const replaceButton = function(action, el_to_replace) {
+    const disableButton = function(el_to_replace) {
         const $a = $(el_to_replace);
         if ($a.length === 0) return;
-        const replace = ['button-disabled', 'pjaxload'];
-        const disable = action === 'disable';
-        const id = $a.attr('id');
-        const href = $a.attr('href');
-        const data_href = $a.attr('data-href');
 
         // use replaceWith, to disable previously caught pjax event
-        const new_element = {
-            class      : $a.attr('class').replace(replace[+disable], replace[+!disable]),
-            id         : id,
-            html       : $a.html(),
-            href       : href || data_href,
-            'data-href': href,
-        };
+        const new_element = { class: $a.attr('class').replace('pjaxload', 'button-disabled'), html: $a.html() };
 
-        if (disable) {
-            delete new_element.href;
-        } else {
-            delete new_element['data-href'];
-        }
-        if (!id) {
-            delete new_element.id;
+        const id = $a.attr('id');
+        if (id) {
+            new_element.id = id;
         }
 
         $a.replaceWith($('<a/>', new_element));
@@ -91,7 +66,6 @@ const Cashier = (function() {
 
     const onLoad = function() {
         if (Client.is_logged_in()) {
-            withdrawal_locked = false;
             checkLocked();
             Header.topbar_message_visibility(Client.landing_company());
         }
