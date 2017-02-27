3a218ea236b614f50e5ed45924a3ee57e1119e14
kelly <kelly@binary.com>
2017-02-27 14:47:11 +0800

    Remove onesignal

--- build/copy.js
+++ build/copy.js
@@ -16,7 +16,6 @@ module.exports = {
             { expand: true, cwd: 'src/download/', src: ['**'], dest: global.dist + '/download/' },
             { expand: true, cwd: 'src/css/external/jquery-ui-custom-theme/images/', src: ['**'], dest: global.dist + '/css/images' },
             { expand: true, cwd: 'src/css/external/jquery-ui-custom-theme/', src: ['*.css'], dest: global.dist + '/css/' },
-            { expand: true, cwd: 'src/javascript/lib/onesignal/', src: ['**'], dest: global.dist }, /* serves OneSignal sdks from root */
         ]
     }
 };
