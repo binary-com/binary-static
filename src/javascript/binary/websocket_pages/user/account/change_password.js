bfd4f29851d32e4036e85014a63d15f92a53b998
negar <negar254000@gmail.com>
2017-02-27 10:59:42 +0800

    use new validation for change password

--- src/javascript/binary/websocket_pages/user/account/change_password.js
+++ src/javascript/binary/websocket_pages/user/account/change_password.js
@@ -1,76 +1,24 @@
-const Content         = require('../../../common_functions/content').Content;
-const ValidateV2      = require('../../../common_functions/validation_v2').ValidateV2;
-const ValidationUI    = require('../../../validator').ValidationUI;
-const customError     = require('../../../validator').customError;
-const bind_validation = require('../../../validator').bind_validation;
-const dv       = require('../../../../lib/validation');
-const localize = require('../../../base/localize').localize;
-const Client   = require('../../../base/client').Client;
-const url_for  = require('../../../base/url').url_for;
+const Client          = require('../../../base/client').Client;
+const localize        = require('../../../base/localize').localize;
+const url_for         = require('../../../base/url').url_for;
+const FormManager     = require('../../../common_functions/form_manager');
 
 const PasswordWS = (function() {
     let $form,
         $result;
 
     const init = function() {
-        const $container = $('#change-password');
+        const formID = '#change-password';
+        const $container = $(formID);
         $container.removeClass('invisible');
         $form = $container.find(' > form');
         $result = $container.find(' > div[data-id="success-result"]');
-        bind_validation.simple($form[0], {
-            stop  : displayErrors,
-            schema: getSchema(),
-            submit: function(ev, info) {
-                ev.preventDefault();
-                ev.stopPropagation();
-                if (info.errors.length > 0) return;
-                sendRequest(info.values);
-            },
-        });
-    };
-
-    const IS_EMPTY = { q: 'old-blank' };
-    const MATCHES_OLD = { q: 'same-as-old' };
-
-    const displayErrors = function(info) {
-        ValidationUI.clear();
-        $form.find('p[data-error]').addClass('hidden');
-        info.errors.forEach(function(err) {
-            switch (err.err) {
-                case MATCHES_OLD:
-                case IS_EMPTY:
-                    $form.find('p[data-error="' + err.err.q + '"]').removeClass('hidden');
-                    break;
-                default:
-                    ValidationUI.draw('input[name=' + err.ctx + ']', err.err);
-            }
-        });
-    };
-
-    const getSchema = function() {
-        const V2 = ValidateV2;
-        const err = Content.localize().textPasswordsNotMatching;
-
-        const notMatchingOld = function(value, data) {
-            return value !== data.old_password;
-        };
-
-        const match = function(value, data) {
-            return value === data.new_password;
-        };
-        return {
-            old_password   : [customError(V2.required, IS_EMPTY)],
-            new_password   : [V2.required, dv.check(notMatchingOld, MATCHES_OLD), V2.password],
-            repeat_password: [V2.required, dv.check(match, err)],
-        };
-    };
-
-    const sendRequest = function(data) {
-        BinarySocket.send({
-            change_password: '1',
-            old_password   : data.old_password,
-            new_password   : data.new_password,
-        });
+        FormManager.init(formID, [
+            { selector: '#old_password',   validations: ['req', 'password'] },
+            { selector: '#new_password',   validations: ['req', 'password', ['not_equal', { to: '#old_password', name1: 'Current password', name2: 'New password' }]] },
+            { selector: 'repeat_password', validations: ['req', ['compare', { to: '#new_password' }]], exclude_request: 1 },
+        ]);
+        FormManager.handleSubmit(formID, { change_password: 1 }, handler);
     };
 
     const handler = function(response) {
@@ -93,25 +41,12 @@ const PasswordWS = (function() {
     };
 
     const onLoad = function() {
-        Content.populate();
-
         BinarySocket.wait('get_account_status').then((response) => {
             if (/has_password/.test(response.get_account_status.status)) {
                 init();
             } else {
                 window.location.href = url_for('user/settingsws');
             }
-        });
-
-        BinarySocket.init({
-            onmessage: function(msg) {
-                const response = JSON.parse(msg.data);
-                if (!response) return;
-                const type = response.msg_type;
-                if (type === 'change_password' || (type === 'error' && 'change_password' in response.echo_req)) {
-                    handler(response);
-                }
-            },
         });
     };
 
