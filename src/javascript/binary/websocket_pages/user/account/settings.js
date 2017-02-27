a04cff799866a3a2fef3f79526512cd5601ad4c9
negar <negar254000@gmail.com>
2017-02-27 10:37:31 +0800

    make settings & security page promise based

--- src/javascript/binary/websocket_pages/user/account/settings.js
+++ src/javascript/binary/websocket_pages/user/account/settings.js
@@ -4,41 +4,26 @@ const Client          = require('../../../base/client').Client;
 const SettingsWS = (function() {
     'use strict';
 
-    const init = function() {
-        const classHidden = 'invisible',
-            classReal   = '.real';
-
-        if (!Client.get('is_virtual')) {
-            // control-class is a fake class, only used to counteract ja-hide class
-            $(classReal).not((japanese_client() ? '.ja-hide' : '.control-class')).removeClass(classHidden);
-        } else {
-            $(classReal).addClass(classHidden);
-        }
-
-        if (Client.get('has_password')) {
-            $('#change_password').removeClass(classHidden);
-        }
-
-        $('#settingsContainer').removeClass(classHidden);
-    };
-
     const onLoad = function() {
-        if (!Client.get('values_set')) {
-            BinarySocket.init({
-                onmessage: function(msg) {
-                    const response = JSON.parse(msg.data);
-                    if (response && response.msg_type === 'authorize') {
-                        SettingsWS.init();
-                    }
-                },
-            });
-        } else {
-            SettingsWS.init();
-        }
+        BinarySocket.wait('authorize', 'get_account_status').then(() => {
+            const classHidden = 'invisible';
+            const classReal   = '.real';
+
+            if (Client.get('is_virtual')) {
+                $(classReal).addClass(classHidden);
+            } else {
+                $(classReal).not((japanese_client() ? '.ja-hide' : '')).removeClass(classHidden);
+            }
+
+            if (Client.get('has_password')) {
+                $('#change_password').removeClass(classHidden);
+            }
+
+            $('#settingsContainer').removeClass(classHidden);
+        });
     };
 
     return {
-        init  : init,
         onLoad: onLoad,
     };
 })();
