ab0035157b49dda53b057b4e7a575992d1b2774f
negar <negar254000@gmail.com>
2017-03-08 15:25:13 +0800

    refactor account transfer

---
+++ src/javascript/binary/websocket_pages/cashier/account_transfer.js
@@ -0,0 +1,100 @@
+const localize    = require('../../base/localize').localize;
+const FormManager = require('../../common_functions/form_manager');
+
+const AccountTransfer = (() => {
+    'use strict';
+
+    const form_id = '#frm_account_transfer';
+    const hidden_class = 'invisible';
+
+    let accounts,
+        $transfer;
+
+    const populateAccounts = (response) => {
+        accounts = response.accounts;
+        const $form = $(form_id);
+        $transfer = $form.find('#transfer');
+        let text,
+            from_loginid,
+            to_loginid;
+
+        accounts.forEach((account, idx) => {
+            if (+account.balance) {
+                from_loginid = accounts[idx].loginid;
+                to_loginid = accounts[1 - idx].loginid;
+                text = localize('from [_1] to [_2]', [from_loginid, to_loginid]);
+                $transfer.append($('<option/>', {
+                    text           : text,
+                    'data-from'    : from_loginid,
+                    'data-to'      : to_loginid,
+                    'data-currency': accounts[idx].currency,
+                    'data-balance' : accounts[idx].balance,
+                }));
+            }
+        });
+
+        if (from_loginid) {
+            showForm($form);
+        } else {
+            $('#client_message').removeClass(hidden_class);
+        }
+    };
+
+    const showForm = ($form) => {
+        const $currency = $form.find('#currency');
+        $transfer.on('change', function() {
+            updateCurrency($currency, $(this));
+            bindValidation();
+        });
+        updateCurrency($currency);
+        $form.removeClass(hidden_class);
+        bindValidation();
+        FormManager.handleSubmit({
+            form_selector       : form_id,
+            fnc_response_handler: responseHandler,
+        });
+    };
+
+    const updateCurrency = ($currency) => { $currency.text(getTransferAttr('data-currency')); };
+
+    const getTransferAttr = attribute => $transfer.find('option:selected').attr(attribute);
+
+    const bindValidation = () => {
+        FormManager.init(form_id, [
+            { selector: '#amount', validations: ['req', ['number', { type: 'float', decimals: '1, 2', min: 0.1, max: getTransferAttr('data-balance') }]] },
+
+            { request_field: 'transfer_between_accounts', value: 1 },
+            { request_field: 'account_from',              value: () => getTransferAttr('data-from') },
+            { request_field: 'account_to',                value: () => getTransferAttr('data-to') },
+            { request_field: 'currency',                  value: () => getTransferAttr('data-currency') },
+        ]);
+    };
+
+    const responseHandler = (response) => {
+        if (response.error) {
+            $('#form_error').text(response.error.message).removeClass(hidden_class);
+        } else {
+            BinarySocket.send({ transfer_between_accounts: 1 }).then(data => populateReceipt(data));
+        }
+    };
+
+    const populateReceipt = (response) => {
+        $(form_id).addClass(hidden_class);
+        accounts = response.accounts;
+        accounts.forEach((account, idx) => {
+            $('#loginid_' + (idx + 1)).text(account.loginid);
+            $('#balance_' + (idx + 1)).text(account.currency + ' ' + account.balance);
+        });
+        $('#success_form').removeClass(hidden_class);
+    };
+
+    const onLoad = () => {
+        BinarySocket.send({ transfer_between_accounts: 1 }).then(response => populateAccounts(response));
+    };
+
+    return {
+        onLoad: onLoad,
+    };
+})();
+
+module.exports = AccountTransfer;
