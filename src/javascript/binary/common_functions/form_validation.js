ca986d985f0ab8eea13aaf87cb878f70098eb35b
kelly <kelly@binary.com>
2017-03-13 10:10:32 +0800

    Fix validations

--- src/javascript/binary/common_functions/form_validation.js
+++ src/javascript/binary/common_functions/form_validation.js
@@ -65,7 +65,7 @@ const Validation = (function() {
     const validEmail        = value => /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}$/.test(value);
     const validPassword     = value => /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]+/.test(value);
     const validLetterSymbol = value => !/[`~!@#$%^&*)(_=+\[}{\]\\\/";:\?><,|\d]+/.test(value);
-    const validGeneral      = value => !/[`~!@#$%^&*)(_=+\[}{\]\\\/";:\?><,|]+/.test(value);
+    const validGeneral      = value => !/[`~!@#$%^&*)(_=+\[}{\]\\\/";:\?><|]+/.test(value);
     const validAddress      = value => !/[`~!#$%^&*)(_=+\[}{\]\\";:\?><|]+/.test(value);
     const validPostCode     = value => /^[a-zA-Z\d-]*$/.test(value);
     const validPhone        = value => /^\+?[0-9\s]*$/.test(value);
