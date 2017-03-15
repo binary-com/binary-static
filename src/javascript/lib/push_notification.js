54634c17280385b6cc72c0f91025050dcf2fd589
kelly <kelly@binary.com>
2017-03-15 14:40:26 +0800

    Exclude all except staging and production

--- src/javascript/lib/push_notification.js
+++ src/javascript/lib/push_notification.js
@@ -9,6 +9,9 @@ const BinaryPushwoosh = (() => {
     let initialised = false;
 
     const init = () => {
+        const hostname = window.location.hostname;
+        if (!(hostname.match(/^(www.binary.com|staging.binary.com)$/))) return;
+
         if (!initialised) {
             pw.push(['init', {
                 logLevel                : 'none', // or debug
