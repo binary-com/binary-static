30b1e9d15ddb728ebc7f08ae45854a100c0428fa
negar <negar254000@gmail.com>
2017-05-02 17:04:56 +0800

    move global functions to their own file

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -19,6 +19,7 @@ exportAllFunctions(require('./binary/common_functions/check_new_release'));
 
 require('event-source-polyfill');
 require('./lib/jquery.sparkline.js');
+require('./lib/plugins');
 require('jquery.scrollto');
 
 require('./binary/components/trackjs_onerror');
@@ -26,30 +27,3 @@ require('./binary/components/trackjs_onerror');
 const BinaryLoader = require('./binary/base/binary_loader');
 
 $(window).on('load', BinaryLoader.init);
-
-Element.prototype.hide = function() {
-    this.style.display = 'none';
-};
-
-Element.prototype.show = function() {
-    this.style.display = '';
-};
-
-if (!('remove' in Element.prototype)) {
-    Element.prototype.remove = function() {
-        if (this.parentNode) {
-            this.parentNode.removeChild(this);
-        }
-    };
-}
-
-(function ($) {
-    $.fn.setVisibility = function(make_visible) {
-        if (make_visible) {
-            this.removeClass('invisible');
-        } else {
-            this.addClass('invisible');
-        }
-        return this;
-    };
-})(jQuery);
