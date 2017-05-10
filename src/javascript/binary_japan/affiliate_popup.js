1c03e229ed8355887c480579f5daea238af430e5
binary-hamedanchi <hamedanchi@gmail.com>
2017-05-10 10:55:59 +0800

    Display popup every time affiliate token exists in URL

--- src/javascript/binary_japan/affiliate_popup.js
+++ src/javascript/binary_japan/affiliate_popup.js
@@ -5,12 +5,12 @@ const jpClient = require('../binary/common_functions/country_base').jpClient;
 const AffiliatePopup = (() => {
     'use strict';
 
-    const confirm_key  = 'affiliate_disclaimer_confirmed';
     const container_id = 'affiliate_disclaimer_popup';
 
     const show = () => {
+        if (Client.isLoggedIn()) return;
         BinarySocket.wait('website_status').then((response) => {
-            if (!Client.get(confirm_key) && (jpClient() || response.website_status.clients_country === 'jp')) {
+            if ((jpClient() || response.website_status.clients_country === 'jp')) {
                 $.ajax({
                     url     : urlFor('affiliate_disclaimer'),
                     dataType: 'html',
@@ -27,7 +27,6 @@ const AffiliatePopup = (() => {
 
     const close = () => {
         $(`#${container_id}`).remove();
-        Client.set(confirm_key, 1);
     };
 
     return {
