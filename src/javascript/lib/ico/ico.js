efb5214324f1df58b625ac4a07ae9ff5d577126a
kelly <kelly@binary.com>
2017-05-22 15:13:31 +0800

    Scroll to on subscription message

--- src/javascript/lib/ico/ico.js
+++ src/javascript/lib/ico/ico.js
@@ -1,15 +1,53 @@
-// Handle form submission
-if (window.location.hash === '#done') {
-    for (let i = 0; i < 2; i++) {
-        document.querySelectorAll('.notice-msg')[i].classList.remove('invisible');
-        document.getElementsByTagName('form')[i].classList.add('invisible');
+// Handler when the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", function(){
+
+    // Handle form submission
+    if (window.location.hash === '#done') {
+        for (let i = 0; i < 2; i++) {
+            document.querySelectorAll('.notice-msg')[i].classList.remove('invisible');
+            document.getElementsByTagName('form')[i].classList.add('invisible');
+        }
+        if(window.history.pushState) {
+            window.history.pushState('', '/', window.location.pathname)
+        } else {
+            window.location.hash = '';
+        }
+        let navbarHeight = checkWidth();
+        const to = document.getElementById('coming-soon').offsetTop - navbarHeight;
+        scrollTo(document.body, to, 1500);
     }
-    if(window.history.pushState) {
-        window.history.pushState('', '/', window.location.pathname)
-    } else {
-        window.location.hash = '';
-    }
-}
+
+    // Toggle mobile menu
+    const toggleButton = document.getElementById('toggle-menu');
+    toggleButton.addEventListener('click', function (e) {
+        const navbar = document.getElementsByClassName('navbar-fixed-top')[0];
+        if (navbar.classList.contains('expand')) {
+            navbar.classList.remove('expand');
+        } else {
+            navbar.classList.add('expand');
+        }
+        e.stopPropagation();
+    });
+
+    // Scroll to section
+    document.addEventListener('click', function(e) {
+        if (e.target.classList.contains('page-scroll')) {
+            const target = e.target.getAttribute('href').substr(1);
+            let offset = 0;
+            if (target === 'who-we-are' || target === 'page-top') {
+                offset = 55;
+            }
+            let navbarHeight = checkWidth();
+            const to = document.getElementById(target).offsetTop - navbarHeight - offset;
+            scrollTo(document.body, to, 1500);
+            e.preventDefault();
+        }
+    });
+
+    window.onresize = checkWidth;
+    window.onscroll = collapseNavbar;
+    document.ready = collapseNavbar;
+});
 
 // Collapse navbar on scroll
 function collapseNavbar() {
@@ -21,46 +59,27 @@ function collapseNavbar() {
     }
 }
 
-window.onscroll = collapseNavbar;
-document.ready = collapseNavbar;
-
-// Toggle mobile menu
-const toggleButton = document.getElementById('toggle-menu');
-toggleButton.addEventListener('click', function (e) {
-    const navbar = document.getElementsByClassName('navbar-fixed-top')[0];
-    if (navbar.classList.contains('expand')) {
-        navbar.classList.remove('expand');
+function checkWidth() {
+    let navbarHeight;
+    if (window.innerWidth <= 320) {
+        navbarHeight = 0;
     } else {
-        navbar.classList.add('expand');
+        navbarHeight = document.getElementsByClassName('navbar-fixed-top')[0].scrollHeight;
     }
-    e.stopPropagation();
-});
-
-// Scroll to section
-document.addEventListener('click', function(e) {
-    if (e.target.classList.contains('page-scroll')) {
-        const target = e.target.getAttribute('href').substr(1);
-        let offset = 0;
-        if (target === 'who-we-are' || target === 'page-top') {
-            offset = 80;
-        }
-        const to = document.getElementById(target).offsetTop - 50 - offset;
-        scrollTo(document.body, to, 1500);
-        e.preventDefault();
-    }
-});
+    return navbarHeight;
+}
 
 // scrollTo function with animation
 // - Gist reference: https://gist.github.com/andjosh/6764939
 function scrollTo(element, to, duration) {
-    var start = element.scrollTop,
+    let start = element.scrollTop,
         change = to - start,
         currentTime = 0,
         increment = 20;
 
-    var animateScroll = function(){
+    const animateScroll = function(){
         currentTime += increment;
-        var val = Math.easeInOutQuad(currentTime, start, change, duration);
+        let val = Math.easeInOutQuad(currentTime, start, change, duration);
         element.scrollTop = val;
         if(currentTime < duration) {
             setTimeout(animateScroll, increment);
