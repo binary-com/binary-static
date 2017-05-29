806021b859e76048412e7200ba06d98716ebd908
negar <negar254000@gmail.com>
2017-05-29 13:41:59 +0800

    simplify

--- src/javascript/binary/websocket_pages/cashier/cashier.js
+++ src/javascript/binary/websocket_pages/cashier/cashier.js
@@ -2,7 +2,6 @@ const BinarySocket       = require('../socket');
 const BinaryPjax         = require('../../base/binary_pjax');
 const Client             = require('../../base/client');
 const Header             = require('../../base/header');
-const State              = require('../../base/storage').State;
 const defaultRedirectUrl = require('../../base/url').defaultRedirectUrl;
 const urlFor             = require('../../base/url').urlFor;
 const jpClient           = require('../../common_functions/country_base').jpClient;
@@ -18,18 +17,22 @@ const Cashier = (() => {
         Client.activateByClientType();
     };
 
-    const displayTopUpButton = (currency, balance) => {
-        const can_topup = (currency !== 'JPY' && balance <= 1000) || (currency === 'JPY' && balance <= 100000);
-        const top_up_id = '#VRT_topup_link';
-        const $a = $(top_up_id);
-        const classes = ['toggle', 'button-disabled'];
-        const new_el = { class: $a.attr('class').replace(classes[+can_topup], classes[1 - +can_topup]), html: $a.html(), id: $a.attr('id') };
-        if (can_topup) {
-            href = href || urlFor('/cashier/top_up_virtualws');
-            new_el.href = href;
-        }
-        $a.replaceWith($('<a/>', new_el));
-        $(top_up_id).parent().setVisibility(1);
+    const displayTopUpButton = () => {
+        BinarySocket.wait('balance').then((response) => {
+            const currency = response.balance.currency;
+            const balance = +response.balance.balance;
+            const can_topup = (currency !== 'JPY' && balance <= 1000) || (currency === 'JPY' && balance <= 100000);
+            const top_up_id = '#VRT_topup_link';
+            const $a = $(top_up_id);
+            const classes = ['toggle', 'button-disabled'];
+            const new_el = { class: $a.attr('class').replace(classes[+can_topup], classes[1 - +can_topup]), html: $a.html(), id: $a.attr('id') };
+            if (can_topup) {
+                href = href || urlFor('/cashier/top_up_virtualws');
+                new_el.href = href;
+            }
+            $a.replaceWith($('<a/>', new_el));
+            $(top_up_id).parent().setVisibility(1);
+        });
     };
 
     const onLoad = () => {
@@ -37,14 +40,12 @@ const Cashier = (() => {
             BinaryPjax.load(defaultRedirectUrl());
         }
         if (Client.isLoggedIn()) {
-            BinarySocket.wait('authorize', 'balance').then(() => {
+            BinarySocket.wait('authorize').then(() => {
                 Header.upgradeMessageVisibility(); // To handle the upgrade buttons visibility
-                const balance_response = State.get(['response', 'balance', 'balance']) || {};
-                const balance          = +balance_response.balance;
                 const is_virtual = Client.get('is_virtual');
-                const is_crypto  = isCryptocurrency(balance_response.currency);
+                const is_crypto = isCryptocurrency(Client.get('currency'));
                 if (is_virtual) {
-                    displayTopUpButton(balance_response.currency, balance);
+                    displayTopUpButton();
                 }
                 if (is_virtual || (/CR/.test(Client.get('loginid')) && !is_crypto)) {
                     $('#payment-agent-section').setVisibility(1);
