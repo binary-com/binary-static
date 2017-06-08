f9e6440d0468faed42bcae2f67c66469c8a95657
binary-hamedanchi <hamedanchi@gmail.com>
2017-06-08 15:54:06 +0800

    Grouping titles

--- src/javascript/binary/base/__tests__/utility.js
+++ src/javascript/binary/base/__tests__/utility.js
@@ -1,30 +1,32 @@
 const expect  = require('chai').expect;
 const utility = require('../utility');
 
-describe('template', () => {
-    it('works as expected', () => {
-        expect(utility.template('abc [_1] abc', ['2'])).to.eq('abc 2 abc');
-        expect(utility.template('[_1] [_2]', ['1', '2'])).to.eq('1 2');
-        expect(utility.template('[_1] [_1]', ['1'])).to.eq('1 1');
+describe('Utility', () => {
+    describe('.template()', () => {
+        it('works as expected', () => {
+            expect(utility.template('abc [_1] abc', ['2'])).to.eq('abc 2 abc');
+            expect(utility.template('[_1] [_2]', ['1', '2'])).to.eq('1 2');
+            expect(utility.template('[_1] [_1]', ['1'])).to.eq('1 1');
+        });
+
+        it('does not replace twice', () => {
+            expect(utility.template('[_1] [_2]', ['[_2]', 'abc'])).to.eq('[_2] abc');
+        });
     });
 
-    it('does not replace twice', () => {
-        expect(utility.template('[_1] [_2]', ['[_2]', 'abc'])).to.eq('[_2] abc');
-    });
-});
+    describe('.isEmptyObject()', () => {
+        it('returns true for empty objects', () => {
+            expect(utility.isEmptyObject({})).to.eq(true);
+            expect(utility.isEmptyObject({ notEmpty: true })).to.eq(false);
+        });
 
-describe('isEmptyObject', () => {
-    it('returns true for empty objects', () => {
-        expect(utility.isEmptyObject({})).to.eq(true);
-        expect(utility.isEmptyObject({ notEmpty: true })).to.eq(false);
-    });
-
-    it('returns false for non objects', () => {
-        expect(utility.isEmptyObject(1)).to.eq(true);
-        expect(utility.isEmptyObject(undefined)).to.eq(true);
-        expect(utility.isEmptyObject(null)).to.eq(true);
-        expect(utility.isEmptyObject(false)).to.eq(true);
-        expect(utility.isEmptyObject(true)).to.eq(true);
-        expect(utility.isEmptyObject('')).to.eq(true);
+        it('returns false for non objects', () => {
+            expect(utility.isEmptyObject(1)).to.eq(true);
+            expect(utility.isEmptyObject(undefined)).to.eq(true);
+            expect(utility.isEmptyObject(null)).to.eq(true);
+            expect(utility.isEmptyObject(false)).to.eq(true);
+            expect(utility.isEmptyObject(true)).to.eq(true);
+            expect(utility.isEmptyObject('')).to.eq(true);
+        });
     });
 });
