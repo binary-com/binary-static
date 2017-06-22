58ce8901c81681aaea04963523aecf38cd2a893d
negar <negar254000@gmail.com>
2017-06-22 17:02:54 +0800

    add unit test for check new release

--- src/javascript/binary/common_functions/check_new_release.js
+++ src/javascript/binary/common_functions/check_new_release.js
@@ -1,10 +1,13 @@
 const moment       = require('moment');
 const urlForStatic = require('../base/url').urlForStatic;
 
-const check_new_release = () => { // calling this method is handled by GTM tags
+// only reload if it's more than 10 minutes since the last reload
+const shouldForceReload = last_reload => !last_reload || +last_reload + (10 * 60 * 1000) < moment().valueOf();
+
+// calling this method is handled by GTM tags
+const check_new_release = () => {
     const last_reload = localStorage.getItem('new_release_reload_time');
-    // prevent reload in less than 10 minutes
-    if (last_reload && +last_reload + (10 * 60 * 1000) > moment().valueOf()) return;
+    if (!shouldForceReload(last_reload)) return false;
     localStorage.setItem('new_release_reload_time', moment().valueOf());
     const currect_hash = ($('script[src*="binary.min.js"],script[src*="binary.js"]').attr('src') || '').split('?')[1];
     const xhttp = new XMLHttpRequest();
@@ -18,8 +21,10 @@ const check_new_release = () => { // calling this method is handled by GTM tags
     };
     xhttp.open('GET', `${urlForStatic()}version?${Math.random().toString(36).slice(2)}`, true);
     xhttp.send();
+    return true;
 };
 
 module.exports = {
+    shouldForceReload: shouldForceReload,
     check_new_release: check_new_release,
 };
