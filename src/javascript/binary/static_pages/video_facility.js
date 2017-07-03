225f12f2527e9d8d7d6d4b65da642375d31ffbcc
binary-hamedanchi <hamedanchi@gmail.com>
2017-07-04 07:40:06 +0800

    Hide video facility contents from non-financial clients

--- src/javascript/binary/static_pages/video_facility.js
+++ src/javascript/binary/static_pages/video_facility.js
@@ -1,5 +1,6 @@
 const BinaryPjax         = require('../base/binary_pjax');
 const Client             = require('../base/client');
+const localize           = require('../base/localize').localize;
 const defaultRedirectUrl = require('../base/url').defaultRedirectUrl;
 const getLoginToken      = require('../common_functions/common_functions').getLoginToken;
 const DeskWidget         = require('../common_functions/attach_dom/desk_widget');
@@ -7,6 +8,11 @@ const BinarySocket       = require('../websocket_pages/socket');
 
 const VideoFacility = (() => {
     const onLoad = () => {
+        if (Client.get('loginid_array').find(obj => obj.id === Client.get('loginid')).non_financial) {
+            $('#loading').replaceWith($('<p/>', { class: 'notice-msg center-text', text: localize('Sorry, this feature is not available in your jurisdiction.') }));
+            return;
+        }
+
         BinarySocket.send({ get_account_status: 1 }).then((response) => {
             if (response.error) {
                 $('#error_message').setVisibility(1).text(response.error.message);
