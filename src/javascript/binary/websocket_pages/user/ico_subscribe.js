87b5c4bdf6b04b64bcdd2248d8ad93b5798e85b7
negar <negar254000@gmail.com>
2017-07-05 11:02:01 +0800

    update ico design

--- src/javascript/binary/websocket_pages/user/ico_subscribe.js
+++ src/javascript/binary/websocket_pages/user/ico_subscribe.js
@@ -6,12 +6,17 @@ const localize              = require('../../base/localize').localize;
 const defaultRedirectUrl    = require('../../base/url').defaultRedirectUrl;
 const jpClient              = require('../../common_functions/country_base').jpClient;
 const getDecimalPlaces      = require('../../common_functions/currency').getDecimalPlaces;
+const formatMoney           = require('../../common_functions/currency').formatMoney;
 const onlyNumericOnKeypress = require('../../common_functions/event_handler');
 const FormManager           = require('../../common_functions/form_manager');
 
 const ICOSubscribe = (() => {
     const form_id = '#frm_ico_bid';
-    let $form_error;
+    let currency,
+        $form_error,
+        $duration,
+        $price,
+        $total;
 
     const onLoad = () => {
         if (jpClient()) {
@@ -20,7 +25,7 @@ const ICOSubscribe = (() => {
         }
 
         BinarySocket.wait('website_status').then((response) => {
-            if (response.website_status.ico_status === 'closed') { // TODO: update property name and value once back-end is done
+            if (response.website_status.ico_status === 'closed') {
                 $(form_id).replaceWith($('<p/>', { class: 'notice-msg center-text', text: localize('The ICO auction is already closed.') }));
                 $('#ico_subscribe').setVisibility(1);
             } else {
@@ -30,16 +35,30 @@ const ICOSubscribe = (() => {
     };
 
     const init = () => {
-        BinarySocket.wait('balance').then(() => {
-            const currency = Client.get('currency');
-            if (!Client.get('currency') || +Client.get('balance') === 0) {
-                $('#msg_no_balance').setVisibility(1);
+        BinarySocket.wait('balance').then((response) => {
+            ICOPortfolio.onLoad();
+            currency = Client.get('currency');
+            if (currency) {
+                $('.currency').text(currency);
             } else {
-                $('#ico_subscribe').setVisibility(1);
-                ICOPortfolio.onLoad();
-                $('label[for="price"]').append(` ${currency}`);
-                $(`${form_id} input`).on('keypress', onlyNumericOnKeypress);
+                $(form_id).find('.topMenuBalance').text(formatMoney(currency, 0));
+            }
+            $duration = $('#duration');
+            $price = $('#price');
+            $total = $('#total');
+            calculateTotal();
+            $('#ico_subscribe').setVisibility(1);
+
+            const balance = response.balance.balance;
+            if (+balance === 0) {
+                $(`${form_id} input`).attr('disabled', 'disabled');
+                $(form_id)
+                    .on('submit', (evt) => { evt.preventDefault(); })
+                    .find('button').addClass('inactive');
+                $('#topup_wrapper').setVisibility(1);
+            } else {
                 const decimal_places = getDecimalPlaces(currency);
+                $form_error = $('#form_error');
                 FormManager.init(form_id, [
                     { selector: '#duration', validations: ['req', ['number', { min: 1, max: 1000000 }]], parent_node: 'parameters' },
                     { selector: '#price',    validations: ['req', ['number', { type: 'float', decimals: `1, ${decimal_places}`, min: Math.pow(10, -decimal_places), max: 999999999999999 }]] },
@@ -52,14 +71,25 @@ const ICOSubscribe = (() => {
                     { request_field: 'currency',      parent_node: 'parameters', value: currency },
                     { request_field: 'duration_unit', parent_node: 'parameters', value: 'c' },
                 ]);
-                $form_error = $('#form_error');
                 FormManager.handleSubmit({
                     form_selector       : form_id,
                     enable_button       : 1,
                     fnc_response_handler: handleResponse,
                 });
+                $(`${form_id} input`).on('keypress', onlyNumericOnKeypress)
+                    .on('input change', calculateTotal);
             }
         });
+    };
+
+    const calculateTotal = () => {
+        const duration_val = $duration.val();
+        const price_val = $price.val();
+        let total = 0;
+        if (duration_val && price_val) {
+            total = +duration_val * +price_val;
+        }
+        $total.text(formatMoney(currency, total));
     };
 
     const handleResponse = (response) => {
