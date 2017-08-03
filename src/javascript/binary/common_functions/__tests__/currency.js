b19e79841b06604fe291178d09ddf7955720f64b
binary-hamedanchi <hamedanchi@gmail.com>
2017-08-03 17:24:05 +0800

    Update tests

--- src/javascript/binary/common_functions/__tests__/currency.js
+++ src/javascript/binary/common_functions/__tests__/currency.js
@@ -1,25 +1,37 @@
-const expect        = require('chai').expect;
-const formatMoney   = require('../currency').formatMoney;
-const setCurrencies = require('../currency').setCurrencies;
+const expect         = require('chai').expect;
+const formatCurrency = require('../currency').formatCurrency;
+const formatMoney    = require('../currency').formatMoney;
+const setCurrencies  = require('../currency').setCurrencies;
 
 describe('formatMoney', () => {
-    setCurrencies({ currencies_config: { AUD: { fractional_digits: 2, type: 'fiat' }, EUR: { fractional_digits: 2, type: 'fiat' }, GBP: { fractional_digits: 2, type: 'fiat' }, JPY: { fractional_digits: 2, type: 'fiat' }, USD: { fractional_digits: 2, type: 'fiat' }, BTC: { fractional_digits: 8, type: 'crypto' } } });
-
-    it('works as expected', () => {
-        expect(formatMoney('USD', '123.55')).to.eq('$123.55');
-        expect(formatMoney('GBP', '123.55')).to.eq('£123.55');
-        expect(formatMoney('EUR', '123.55')).to.eq('€123.55');
-        expect(formatMoney('AUD', '123.55')).to.eq('A$123.55');
-        expect(formatMoney('JPY', '123.55')).to.eq('¥124');
-        expect(formatMoney('JPY', '1234.55')).to.eq('¥1,235');
-        expect(formatMoney('BTC', '0.005432110')).to.eq('₿0.00543211');
-        expect(formatMoney('BTC', '0.005432116')).to.eq('₿0.00543212');
-        expect(formatMoney('BTC', '0.00000001')).to.eq('₿0.00000001');
-        // don't remove trailing zeroes for now
-        expect(formatMoney('BTC', '0.00010000')).to.eq('₿0.00010000');
+    before(() => {
+        setCurrencies({
+            currencies_config: {
+                AUD: { fractional_digits: 2, type: 'fiat' },
+                EUR: { fractional_digits: 2, type: 'fiat' },
+                GBP: { fractional_digits: 2, type: 'fiat' },
+                JPY: { fractional_digits: 0, type: 'fiat' },
+                USD: { fractional_digits: 2, type: 'fiat' },
+                BTC: { fractional_digits: 8, type: 'crypto' },
+            }
+        });
     });
 
-    it('works for unexpected currencies', () => {
-        expect(formatMoney('WTV', '123.55')).to.eq('WTV123.55');
+    it('works as expected', () => {
+        expect(formatMoney('USD', '123.55')).to.eq(`${formatCurrency('USD')}123.55`);
+        expect(formatMoney('GBP', '123.55')).to.eq(`${formatCurrency('GBP')}123.55`);
+        expect(formatMoney('EUR', '123.55')).to.eq(`${formatCurrency('EUR')}123.55`);
+        expect(formatMoney('AUD', '123.55')).to.eq(`${formatCurrency('AUD')}123.55`);
+        expect(formatMoney('JPY', '123.55')).to.eq(`${formatCurrency('JPY')}124`);
+        expect(formatMoney('JPY', '1234.55')).to.eq(`${formatCurrency('JPY')}1,235`);
+        expect(formatMoney('BTC', '0.005432110')).to.eq(`${formatCurrency('BTC')}0.00543211`);
+        expect(formatMoney('BTC', '0.005432116')).to.eq(`${formatCurrency('BTC')}0.00543212`);
+        expect(formatMoney('BTC', '0.00000001')).to.eq(`${formatCurrency('BTC')}0.00000001`);
+        // don't remove trailing zeroes for now
+        expect(formatMoney('BTC', '0.00010000')).to.eq(`${formatCurrency('BTC')}0.00010000`);
+    });
+
+    it('works when exclude currency', () => {
+        expect(formatMoney('USD', '123.55', 1)).to.eq('123.55');
     });
 });
