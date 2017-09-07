138e80a60c818bece09266fd8e947602e40db865
kelly <kelly@binary.com>
2017-09-07 13:39:22 +0800

    add hackathon page

--- scripts/config/pages.pl
+++ scripts/config/pages.pl
@@ -119,6 +119,7 @@ sub all_pages {
         ['logged_inws',                'global/logged_in',       undef],
         ['affiliate_disclaimer',       'japan/affiliates/popup', undef, '', 'NOT-ja,en'],
         ['ico',                        'static/ico',             undef, 'Initial Coin Offering', 'ja'],
+        ['hackathon',                  'static/hackathon',       undef, 'Hackathon Competition'],
     );
 }
 
