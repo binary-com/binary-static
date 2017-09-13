b118188b05ed8eecb73fe0ff4a03ca396464ecc0
binary-hamedanchi <hamedanchi@gmail.com>
2017-09-13 11:01:41 +0800

    Validate the email address

--- src/javascript/lib/landing/signup_frame.js
+++ src/javascript/lib/landing/signup_frame.js
@@ -1,3 +1,5 @@
+var error_class = 'error-field';
+
 window.onload = function () {
     var el_email   = document.getElementById('email');
     var el_signup  = document.getElementById('signup');
@@ -12,8 +14,23 @@ window.onload = function () {
         }));
     }
 
+    var validation_set = false; // To prevent validating before submit
+
     document.getElementById('frm_verify_email').addEventListener('submit', function (evt) {
         evt.preventDefault();
+
+        if (!validateEmail(el_email.value)) {
+            if (!validation_set) {
+                ['input', 'change'].forEach(function (evt) {
+                    el_email.addEventListener(evt, function () {
+                        setValidationStyle(el_email, !validateEmail(el_email.value));
+                    });
+                });
+                setValidationStyle(el_email, !validateEmail(el_email.value));
+                validation_set = true;
+            }
+            return false;
+        }
 
         if (ws.readyState === 1) {
             sendVerifyEmail();
@@ -27,9 +44,9 @@ window.onload = function () {
     ws.onmessage = function(msg) {
         var data = JSON.parse(msg.data);
         if (data.error) {
-            el_email.classList.add('error-field');
+            el_email.classList.add(error_class);
         } else {
-            el_email.classList.remove('error-field');
+            el_email.classList.remove(error_class);
             el_signup.classList.add('invisible');
             el_success.classList.remove('invisible');
         }
@@ -41,6 +58,14 @@ window.onload = function () {
         localStorage.setItem('gclid', gclid);
     }
 };
+
+function validateEmail(email) {
+    return /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,63}$/.test(email);
+}
+
+function setValidationStyle(element, has_error) {
+    element.classList[has_error ? 'add' : 'remove'](error_class);
+}
 
 function getParamValue(url, key) {
     var regex   = new RegExp('[?&]' + key + '(=([^&#]*)|&|#|$)');
