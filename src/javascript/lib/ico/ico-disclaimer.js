d6db7e05af21e0e7d80541c409440cb51f1bc8b6
binary-hamedanchi <hamedanchi@gmail.com>
2017-09-15 09:50:36 +0800

    Get language from url

--- src/javascript/lib/ico/ico-disclaimer.js
+++ src/javascript/lib/ico/ico-disclaimer.js
@@ -4,7 +4,7 @@ window.onload = function() {
     var frm_accept_second_notice = document.getElementById('frm_accept_second_notice');
     var el_residence_list     = document.getElementById('residence_list');
 
-    var ws = new WebSocket('wss://blue.binaryws.com/websockets/v3?app_id=1&l=' + (getParamValue(window.top.location.href, 'lang') || 'en'));
+    var ws = new WebSocket('wss://blue.binaryws.com/websockets/v3?app_id=1&l=' + getLanguage());
 
     function sendResidenceList() {
         ws.send(JSON.stringify({
@@ -126,4 +126,10 @@ function getParamValue(url, key) {
     var results = regex.exec(url);
     if (!results || !results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+function getLanguage() {
+    var all_languages = [ 'en', 'de', 'es', 'fr', 'id', 'it', 'ja', 'pl', 'pt', 'ru', 'th', 'vi', 'zh_cn', 'zh_tw' ];
+    var language = window.location.href.toLowerCase().split('/').slice(3).find(function(l) { return all_languages.indexOf(l) >= 0; });
+    return language || 'en';
 }
