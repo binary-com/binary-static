6f94b77823797efbc671fadcbb42f394155e9e8a
binary-hamedanchi <hamedanchi@gmail.com>
2017-10-12 17:31:50 +0800

    Remove unused css external

--- build/cssmin.js
+++ build/cssmin.js
@@ -1,8 +1,10 @@
 module.exports = {
     all: {
         files: [
-            { src: ['src/css/external/**/*.css', global.dist + '/css/binary.css'], dest: global.dist + '/css/binary.min.css' },
-            //{dest: global.dist + '/css/binary_rtl.min.css', src: ['src/css/external/**/*.css', global.dist + '/css/binary_rtl.css']}
-        ]
-    }
+            {
+                src: [global.dist + '/css/binary.css'],
+                dest: global.dist + '/css/binary.min.css',
+            },
+        ],
+    },
 };
