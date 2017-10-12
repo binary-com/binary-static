b9836df97075e17e6baabc9d72e8f2e75a7a1f78
negar <negar254000@gmail.com>
2017-10-12 10:26:58 +0800

    change from jquery animation to css and js

---
+++ src/javascript/binary/common_functions/tab_selector.js
@@ -0,0 +1,93 @@
+const applyToAllElements = require('../base/utility').applyToAllElements;
+
+const TabSelector = (() => {
+    /**
+     * @param {String|Array} tab_ids can be the ID of single set of tabs or an array of all elements
+     * @param {Boolean} has_arrows send true if .go-left and .go-right arrows need to work with tabs
+     */
+    const init = (tab_ids, has_arrows = false) => {
+        const array_tab = Array.isArray(tab_ids) ? tab_ids : [tab_ids];
+        array_tab.forEach((tab_id) => {
+            // set initial width and margin-left of tab selector
+            changeTab(undefined, undefined, tab_id);
+
+            window.addEventListener('resize', () => {
+                changeTab(undefined, undefined, tab_id);
+            });
+        });
+        applyToAllElements('.tm-li', (element) => {
+            element.removeEventListener('click', (e) => { slideSelector(e.target.closest('ul').getAttribute('id'), e.target); });
+            element.addEventListener('click', (e) => { slideSelector(e.target.closest('ul').getAttribute('id'), e.target); });
+        });
+
+        if (has_arrows) {
+            applyToAllElements('.go-left', (element) => {
+                element.removeEventListener('click', (e) => { changeTab(e, true); });
+                element.addEventListener('click', (e) => { changeTab(e, true); });
+            });
+            applyToAllElements('.go-right', (element) => {
+                element.removeEventListener('click', (e) => { changeTab(e, false); });
+                element.addEventListener('click', (e) => { changeTab(e, false); });
+            });
+        }
+    };
+
+    const changeTab = (e, go_left, selector_id) => {
+        const selector  = selector_id || e.target.getAttribute('data-parent');
+        const el_parent = document.getElementById(selector);
+        const elements  = el_parent.getElementsByTagName('li');
+        for (let i = 0; i < elements.length - 1; i++) {
+            if (/active/.test(elements[i].classList)) {
+                if (typeof go_left === 'undefined') {
+                    slideSelector(selector, elements[i]);
+                    break;
+                }
+                let el_to_show;
+                if (go_left) {
+                    el_to_show = elements[i - 1] ? elements[i - 1] : elements[elements.length - 2];
+                } else {
+                    el_to_show = i + 1 !== elements.length - 1 && elements[i + 1] ? elements[i + 1] : elements[0];
+                }
+                slideSelector(selector, el_to_show);
+                elements[i].classList.remove('active');
+                document.getElementById(`${elements[i].getAttribute('id')}-content`).classList.add('invisible');
+                el_to_show.classList.add('active');
+                document.getElementById(`${el_to_show.getAttribute('id')}-content`).classList.remove('invisible');
+                break;
+            }
+        }
+    };
+
+    const slideSelector = (selector, el_to_show) => {
+        document.getElementById(`${selector}_selector`).setAttribute('style', `width: ${el_to_show.offsetWidth}px; margin-left: ${el_to_show.offsetLeft}px;`);
+    };
+
+    const clean = (tab_ids, has_arrows) => {
+        const array_tab = Array.isArray(tab_ids) ? tab_ids : [tab_ids];
+        array_tab.forEach((tab_id) => {
+            window.removeEventListener('resize', () => {
+                changeTab(undefined, undefined, tab_id);
+            });
+        });
+
+        applyToAllElements('.tm-li', (element) => {
+            element.removeEventListener('click', (e) => { slideSelector(e.target.closest('ul').getAttribute('id'), e.target); });
+        });
+
+        if (has_arrows) {
+            applyToAllElements('.go-left', (element) => {
+                element.removeEventListener('click', (e) => { changeTab(e, true); });
+            });
+            applyToAllElements('.go-right', (element) => {
+                element.removeEventListener('click', (e) => { changeTab(e, false); });
+            });
+        }
+    };
+
+    return {
+        init,
+        clean,
+    };
+})();
+
+module.exports = TabSelector;
--- src/javascript/binary/static_pages/home_beta.js
+++ src/javascript/binary/static_pages/home_beta.js
@@ -1,82 +1,19 @@
-const Home               = require('./home');
-const applyToAllElements = require('../base/utility').applyToAllElements;
+const Home        = require('./home');
+const TabSelector = require('../common_functions/tab_selector');
 
 const HomeBeta = (() => {
     const onLoad = () => {
         Home.onLoad();
-
-        applyToAllElements('.go-left', (element) => {
-            element.removeEventListener('click', (e) => { changeTab(e, true); });
-            element.addEventListener('click', (e) => { changeTab(e, true); });
-        });
-        applyToAllElements('.go-right', (element) => {
-            element.removeEventListener('click', (e) => { changeTab(e, false); });
-            element.addEventListener('click', (e) => { changeTab(e, false); });
-        });
-        applyToAllElements('.tm-li', (element) => {
-            element.removeEventListener('click', selectTab);
-            element.addEventListener('click', selectTab);
-        });
+        TabSelector.init(['market_tabs', 'account_tabs'], true);
     };
 
-    const changeTab = (e, go_left) => {
-        const selector  = e.target.getAttribute('data-parent');
-        const el_parent = document.getElementById(selector);
-        const elements  = el_parent.getElementsByTagName('li');
-        for (let i = 0; i < elements.length - 1; i++) {
-            if (/active/.test(elements[i].classList)) {
-                elements[i].classList.remove('active');
-                document.getElementById(`${elements[i].getAttribute('id')}-content`).classList.add('invisible');
-                let el_to_show;
-                if (go_left) {
-                    el_to_show = elements[i - 1] ? elements[i - 1] : elements[elements.length - 2];
-                } else {
-                    el_to_show = i + 1 !== elements.length - 1 && elements[i + 1] ? elements[i + 1] : elements[0];
-                }
-                el_to_show.classList.add('active');
-
-                const current_index = Array.prototype.indexOf.call(el_parent.childNodes, elements[i]);
-                const new_index     = Array.prototype.indexOf.call(el_parent.childNodes, el_to_show);
-                SlideSelector(e, selector, new_index, current_index > new_index);
-
-                document.getElementById(`${el_to_show.getAttribute('id')}-content`).classList.remove('invisible');
-                break;
-            }
-        }
-    };
-
-    const selectTab = (e) => {
-        const selector  = e.target.closest('ul').getAttribute('id');
-        const new_index = Array.prototype.indexOf.call(e.target.closest('ul').childNodes, e.target.parentNode);
-
-        const el_parent = document.getElementById(selector);
-        const elements  = el_parent.getElementsByTagName('li');
-        let current_index = 0;
-        for (let i = 0; i < elements.length; i++) {
-            if (/active/.test(elements[i].classList)) {
-                current_index = Array.prototype.indexOf.call(el_parent.childNodes, elements[i]);
-            }
-        }
-
-        SlideSelector(e, selector, new_index, current_index > new_index);
-    };
-
-    const SlideSelector = (e, selector, new_index, go_left) => {
-        if (!/img|a/i.test(e.target.nodeName)) {
-            return;
-        }
-        const selected_index = Math.floor(new_index / 2) + 1;
-        const percentage     = 100 / (document.getElementById(selector).children.length - 1);
-        const right          = { right: `${100 - (selected_index * percentage)}%` };
-        const left           = { left: `${(selected_index - 1) * percentage}%` };
-        const $selector      = $(`#${selector}_selector`);
-        $selector
-            .animate(go_left ? left : right, { duration: 300, queue: false })
-            .animate(go_left ? right : left, { duration: 500 });
+    const onUnLoad = () => {
+        TabSelector.clean(['market_tabs', 'account_tabs'], true);
     };
 
     return {
         onLoad,
+        onUnLoad,
     };
 })();
 
