632fc202484ba537c82484a2f8bdd8668061b303
Apoorv Joshi <apoorv@binary.com>
2017-11-08 10:39:14 +0800

    fix cr error

--- src/javascript/binary/websocket_pages/user/ico_subscribe.js
+++ src/javascript/binary/websocket_pages/user/ico_subscribe.js
@@ -157,10 +157,13 @@ const ICOSubscribe = (() => {
 
     const showContent = () => {
         let to_show = 'feature_not_allowed';
-        if (Client.get('landing_company_shortcode') === 'costarica'
-            && /au|ca|ch|nz|sg/.test(Client.get('residence'))
-            && !/professional_requested|professional/.test(State.getResponse('get_account_status.status'))) {
-            to_show = 'ico_professional_message';
+        if (Client.get('landing_company_shortcode') === 'costarica') {
+            if(/au|ca|ch|nz|sg/.test(Client.get('residence'))
+                && !/professional_requested|professional/.test(State.getResponse('get_account_status.status'))) {
+                to_show = 'ico_professional_message';
+            } else {
+                to_show = 'ico_subscribe';
+            }
         } else if (Client.hasCostaricaAccount()) {
             to_show = 'ico_account_message';
         } else if (Client.canOpenICO() || Client.canUpgradeVirtualToReal(State.getResponse('landing_company'))) {
