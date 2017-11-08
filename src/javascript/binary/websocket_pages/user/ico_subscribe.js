0d359248461287a3e5eaf9bd8b86768dabff1b69
amin roosta <amin.roosta@outlook.com>
2017-11-08 15:18:57 +0800

    set max token count to 10 million

--- src/javascript/binary/websocket_pages/user/ico_subscribe.js
+++ src/javascript/binary/websocket_pages/user/ico_subscribe.js
@@ -86,7 +86,7 @@ const ICOSubscribe = (() => {
                 $form_error          = $('#form_error');
 
                 FormManager.init(form_id, [
-                    { selector: '#duration', validations: ['req', ['number', { min: 25, max: 1000000 }]], parent_node: 'parameters' },
+                    { selector: '#duration', validations: ['req', ['number', { min: 25, max: 10000000 }]], parent_node: 'parameters' },
                     { selector: '#price',    validations: ['req', ['number', { type: 'float', decimals: `1, ${decimal_places}`, min: Math.pow(10, -decimal_places).toFixed(decimal_places), max: 999999999999999 }]] },
 
                     { request_field: 'buy', value: 1 },
