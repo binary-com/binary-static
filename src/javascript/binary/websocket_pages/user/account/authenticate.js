72ff0d77d4b981f0b07c01787b4989754475b6c4
amin roosta <amin.roosta@outlook.com>
2017-11-09 15:19:52 +0800

    change  View sample to Learn more

--- src/javascript/binary/websocket_pages/user/account/authenticate.js
+++ src/javascript/binary/websocket_pages/user/account/authenticate.js
@@ -20,9 +20,14 @@ const Authenticate = (() => {
                     if (!/authenticated/.test(status)) {
                         init();
                         $('#not_authenticated').setVisibility(1);
+                        let link = 'https://marketing.binary.com/authentication/2017_Authentication_Process.pdf';
                         if (Client.isAccountOfType('financial')) {
                             $('#not_authenticated_financial').setVisibility(1);
+                            link = 'https://marketing.binary.com/authentication/2017_MF_Authentication_Process.pdf';
                         }
+                        $('#not_authenticated').find('.learn_more a').attr('href', link);
+                        $('#not_authenticated').find('.learn_more').setVisibility(1);
+
                     } else if (!/age_verification/.test(status)) {
                         $('#needs_age_verification').setVisibility(1);
                     }
