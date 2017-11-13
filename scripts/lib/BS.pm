bede04060d9cc30574c08771135a7a7ef4536a41
Apoorv Joshi <apoorv@binary.com>
2017-11-13 15:56:11 +0800

    Add ICO bids option in menu

--- scripts/lib/BS.pm
+++ scripts/lib/BS.pm
@@ -191,7 +191,7 @@ sub menu {
         id         => 'topMenuProfitTable',
         url        => url_for('/user/profit_tablews'),
         text       => localize('Profit Table'),
-        class      => 'client_logged_in invisible',
+        class      => 'client_logged_in invisible ico-only-hide',
         };
 
     push @menu,
@@ -237,7 +237,7 @@ sub menu {
     push @menu,
         {
         id         => 'topMenuShop',
-        class      => 'ja-hide',
+        class      => 'ja-hide ico-only-hide',
         url        => 'https://shop.binary.com',
         text       => localize('Shop'),
         target     => '_blank'
@@ -253,6 +253,14 @@ sub menu {
 
     # push @{$menu}, $self->_main_menu_trading();
 
+    push @menu,
+        {
+        id         => 'topMenuIcoBids',
+        class      => 'ja-hide client_logged_in',
+        url        => url_for('/user/ico-subscribe'),
+        text       => localize('ICO Bids'),
+        };
+    # Link to ico-subscribe, ICO Bids.
     return \@menu;
 }
 
