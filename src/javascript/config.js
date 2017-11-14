5ccf0f550dbe77b38331abaea3d60d894f599082
negar <negar254000@gmail.com>
2017-11-14 09:31:26 +0800

    for now only use blue server

--- src/javascript/config.js
+++ src/javascript/config.js
@@ -1,4 +1,4 @@
-const Cookies = require('js-cookie');
+// const Cookies = require('js-cookie');
 
 /*
  * Configuration values needed in js codes
@@ -16,35 +16,36 @@ const getAppId = () => (
 const getSocketURL = () => {
     let server_url = window.localStorage.getItem('config.server_url');
     if (!server_url) {
-        const toGreenPercent = { real: 100, virtual: 0, logged_out: 0 }; // default percentage
-        const categoryMap    = ['real', 'virtual', 'logged_out'];
-        const percentValues  = Cookies.get('connection_setup'); // set by GTM
+        // const toGreenPercent = { real: 100, virtual: 0, logged_out: 0 }; // default percentage
+        // const categoryMap    = ['real', 'virtual', 'logged_out'];
+        // const percentValues  = Cookies.get('connection_setup'); // set by GTM
+        //
+        // // override defaults by cookie values
+        // if (percentValues && percentValues.indexOf(',') > 0) {
+        //     const cookie_percents = percentValues.split(',');
+        //     categoryMap.map((cat, idx) => {
+        //         if (cookie_percents[idx] && !isNaN(cookie_percents[idx])) {
+        //             toGreenPercent[cat] = +cookie_percents[idx].trim();
+        //         }
+        //     });
+        // }
 
-        // override defaults by cookie values
-        if (percentValues && percentValues.indexOf(',') > 0) {
-            const cookie_percents = percentValues.split(',');
-            categoryMap.map((cat, idx) => {
-                if (cookie_percents[idx] && !isNaN(cookie_percents[idx])) {
-                    toGreenPercent[cat] = +cookie_percents[idx].trim();
-                }
-            });
-        }
+        // let server = 'blue';
+        // if (!/staging\.binary\.com/i.test(window.location.hostname)) {
+        //     const loginid = window.localStorage.getItem('active_loginid');
+        //     let client_type = categoryMap[2];
+        //     if (loginid) {
+        //         client_type = /^VRT/.test(loginid) ? categoryMap[1] : categoryMap[0];
+        //     }
 
-        let server = 'blue';
-        if (!/staging\.binary\.com/i.test(window.location.hostname)) {
-            const loginid = window.localStorage.getItem('active_loginid');
-            let client_type = categoryMap[2];
-            if (loginid) {
-                client_type = /^VRT/.test(loginid) ? categoryMap[1] : categoryMap[0];
-            }
+            // const randomPercent = Math.random() * 100;
+            // if (randomPercent < toGreenPercent[client_type]) {
+            //     server = 'green';
+            // }
+        // }
 
-            const randomPercent = Math.random() * 100;
-            if (randomPercent < toGreenPercent[client_type]) {
-                server = 'green';
-            }
-        }
-
-        server_url = `${server}.binaryws.com`;
+        // server_url = `${server}.binaryws.com`;
+        server_url = 'blue.binaryws.com';
     }
     return `wss://${server_url}/websockets/v3`;
 };
