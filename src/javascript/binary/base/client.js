6663057d965edd9bfb764954522ee96caf8500af
amin roosta <amin.roosta@outlook.com>
2017-11-15 10:19:34 +0800

    only show "ICO Bids" to ico only accounts

--- src/javascript/binary/base/client.js
+++ src/javascript/binary/base/client.js
@@ -243,6 +243,7 @@ const Client = (() => {
                 const is_ico_only = get('is_ico_only');
                 if (is_ico_only) {
                     applyToAllElements('.ico-only-hide', (el) => { el.setVisibility(0); });
+                    applyToAllElements('.ico-only-show', (el) => { el.setVisibility(1); });
                 }
 
                 applyToAllElements('.client_logged_in', (el) => {
