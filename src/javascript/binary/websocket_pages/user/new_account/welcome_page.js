669b4baf76ed1210f2f987343e12ce342afb1b52
zakibinary <zaki@Zakis-MacBook-Pro-2.local>
2017-11-22 09:45:25 +0800

    check for legal allowed currency before displaying currency icons

--- src/javascript/binary/websocket_pages/user/new_account/welcome_page.js
+++ src/javascript/binary/websocket_pages/user/new_account/welcome_page.js
@@ -1,9 +1,9 @@
-const Client        = require('../../../base/client');
-const localize      = require('../../../base/localize').localize;
-const createElement = require('../../../base/utility').createElement;
-const Url           = require('../../../base/url');
-const State         = require('../../../base/storage').State;
-const BinarySocket  = require('../../../websocket_pages/socket');
+const Client             = require('../../../base/client');
+const localize           = require('../../../base/localize').localize;
+const createElement      = require('../../../base/utility').createElement;
+const Url                = require('../../../base/url');
+const State              = require('../../../base/storage').State;
+const BinarySocket       = require('../../../websocket_pages/socket');
 
 const WelcomePage = (() => {
     const onLoad = () => {
@@ -16,26 +16,34 @@ const WelcomePage = (() => {
             const landing_company   = State.getResponse('landing_company');
             const account_type      = Client.getAccountType();
             const jp_account_status = State.getResponse('get_settings.jp_account_status.status');
-            const crypto_icon       = document.getElementById('crypto_icons');
             const upgrade_btn       = document.getElementById('upgrade_btn');
             const upgrade_info      = Client.getUpgradeInfo(landing_company, jp_account_status);
             const show_welcome_msg  = upgrade_info.can_upgrade;
 
-            if(/^virtual|^real/.test(account_type)) {
-                if(crypto_icon) {
-                    crypto_icon.setVisibility(1);
+            if (/^virtual/.test(account_type)) {
+
+                const arr_landing_company = Object.keys(landing_company).map(key => landing_company[key]);
+                const allowed_currency = arr_landing_company[0].legal_allowed_currencies;
+
+                if (allowed_currency && allowed_currency.length > 0) {
+                    for (let i=0; i < allowed_currency.length; i++) {
+                        const el = document.getElementById(allowed_currency[i]);
+                        if (el) {
+                            el.classList.remove('invisible');
+                        }
+                    }
                 }
             }
 
             const setButtonLink = (url, msg) => {
-                if(upgrade_btn) {
+                if (upgrade_btn) {
                     upgrade_btn.html(createElement('span', { text: localize(msg) })).setAttribute('href', Url.urlFor(url));
                     upgrade_btn.classList.remove('button-disabled');
                 }
             };
 
             const welcome_msg = document.getElementById('welcome_container');
-            if(welcome_msg) {
+            if (welcome_msg) {
                 welcome_msg.setVisibility(1);
             }
 
