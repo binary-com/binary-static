3f9fb959fce8f2fa993ca5b4c18dfc136ab2df6c
zakibinary <zaki@Zakis-MacBook-Pro-2.local>
2017-11-28 09:28:06 +0800

    fixed wrong redirect

--- src/javascript/binary/websocket_pages/user/new_account/virtual_acc_opening.js
+++ src/javascript/binary/websocket_pages/user/new_account/virtual_acc_opening.js
@@ -112,7 +112,7 @@ const VirtualAccOpening = (() => {
                     loginid     : new_account.client_id,
                     token       : new_account.oauth_token,
                     is_virtual  : true,
-                    redirect_url: urlFor(`new_account/${jp_client ? 'landing_page' : 'account_type'}`),
+                    redirect_url: urlFor(`new_account/${jp_client ? 'landing_page' : 'welcome'}`),
                 });
             });
             return true;
