1bdf809634ef21409aa606c90ef24875bfc9715a
kelly <kelly@binary.com>
2017-12-21 14:45:44 +0800

    Fix ListNested component

---
+++ src/templates/static/legal/us_patents.jsx
@@ -0,0 +1,25 @@
+/*import React from 'react';
+
+const Patents = (items) => (
+    <ul>
+        {items.map((item) => {
+            <li><a onclick="window.open(`https://www.google.com/patents/${item.doc}`)" href="javascript:void(0);" rel="noopener noreferrer">{item.text}</a></li>
+        })}
+    </ul>
+);
+
+const USPatents = () => {
+    const items = [
+        { doc='US7206762', text=l('Betting system and method') },        
+        { doc='US8046293', text=l('Computer trading system for offering custom financial market speculations') },        
+        { doc='US8046292', text=l('Computer system and method for speculating on a financial market') }, 
+    ]
+    return (
+        <div className="static_full">
+            <h1>{it.L('US Patents')}</h1>
+            <Patents item={items} />
+        </div> 
+    )
+};
+
+export default USPatents;*/
