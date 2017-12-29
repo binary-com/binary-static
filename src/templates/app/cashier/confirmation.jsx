fdad0a83265cbd30742111f355fc6fcee7ce8b30
negar <negar254000@gmail.com>
2017-12-29 10:45:44 +0800

    for button next to button, add margin-left

--- src/templates/app/cashier/confirmation.jsx
+++ src/templates/app/cashier/confirmation.jsx
@@ -11,8 +11,7 @@ const Confirmation = () => (
                     <FormRow type='checkbox' label={it.L('Yes, I understand')} id='chk_confirm' />
                     <div className='center-text gr-centered'>
                         <button className='button' type='submit'>{it.L('Proceed')}</button>
-                        &nbsp;
-                        <a className='button-secondary' id='cancel' href='javascript:;'><span>{it.L('Cancel')}</span></a>
+                        <a className='button button-secondary' id='cancel' href='javascript:;'><span>{it.L('Cancel')}</span></a>
                     </div>
                 </form>
             </div>
