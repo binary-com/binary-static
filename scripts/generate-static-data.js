778fdb3d4bc48fb0e02a0d690ebd2a9732ee4d05
negar <negar254000@gmail.com>
2018-01-03 14:34:04 +0800

    change all stays in/goes out to stays in/out

--- scripts/generate-static-data.js
+++ scripts/generate-static-data.js
@@ -129,7 +129,7 @@ const texts = [
     'Up/Down',
     'Ends In/Out',
     'Touch/No Touch',
-    'Stays In/Goes Out',
+    'Stays In/Out',
     'Potential Payout',
     'Total Cost',
     'Potential Profit',
