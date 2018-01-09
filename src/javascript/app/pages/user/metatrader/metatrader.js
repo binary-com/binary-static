fffccb5e4189eb914feac71f5ca7e3393b9bee2a
binary-hamedanchi <hamedanchi@gmail.com>
2018-01-09 13:42:11 +0800

    Set currency of the new account

--- src/javascript/app/pages/user/metatrader/metatrader.js
+++ src/javascript/app/pages/user/metatrader/metatrader.js
@@ -6,6 +6,7 @@ const Validation       = require('../../../common/form_validation');
 const localize         = require('../../../../_common/localize').localize;
 const State            = require('../../../../_common/storage').State;
 const toTitleCase      = require('../../../../_common/string_util').toTitleCase;
+const getPropertyValue = require('../../../../_common/utility').getPropertyValue;
 
 const MetaTrader = (() => {
     const mt_companies  = MetaTraderConfig.mt_companies;
@@ -160,7 +161,7 @@ const MetaTrader = (() => {
                         const login = actions_info[action].login ?
                             actions_info[action].login(response) : accounts_info[acc_type].info.login;
                         if (!accounts_info[acc_type].info) {
-                            accounts_info[acc_type].info = { login };
+                            accounts_info[acc_type].info = { login, currency: getPropertyValue(response, ['mt5_new_account', 'currency']) };
                             MetaTraderUI.setAccountType(acc_type, true);
                             BinarySocket.send({ mt5_login_list: 1 });
                         }
