e9503f45e7f1a045ea183ee9fd4fc60b6c714421
zakibinary <zaki@Zakis-MacBook-Pro-2.local>
2018-01-19 15:27:43 +0800

    added app_2 to sections in render.js

--- scripts/render.js
+++ scripts/render.js
@@ -69,7 +69,7 @@ const getConfig = () => (
         languages       : program.branch === 'translations' ? ['ACH'] : common.languages,
         root_path       : common.root_path,
         root_url        : `/${program.dev ? 'binary-static/' : ''}${program.branch ? `${program.branch}/` : ''}`,
-        sections        : ['app', 'static'],
+        sections        : ['app', 'app_2', 'static'],
     }
 );
 
