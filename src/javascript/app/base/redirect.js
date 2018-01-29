920fc4ff39abab8506ca5c42c7b8c0986363196a
negar <negar254000@gmail.com>
2018-01-29 10:35:34 +0800

    change how we build token

--- src/javascript/app/base/redirect.js
+++ src/javascript/app/base/redirect.js
@@ -6,7 +6,7 @@ const Redirect = (() => {
         const actions_map = {
             signup                : { path: 'new_account/virtualws' },
             reset_password        : { path: 'user/reset_passwordws' },
-            payment_withdraw      : { path: 'cashier/forwardws', hash: '#withdraw' },
+            payment_withdraw      : { path: 'cashier/forwardws', query: 'action=withdraw' },
             payment_agent_withdraw: { path: 'paymentagent/withdrawws' },
         };
 
@@ -14,7 +14,7 @@ const Redirect = (() => {
         const config = actions_map[params.action];
         // need to redirect not using pjax
         window.location.href = config && params.code ?
-            Url.urlFor(config.path, `token=${params.code}${config.hash || ''}`, params.lang || '') :
+            `${Url.urlFor(config.path, config.query, params.lang || '')}#token=${params.code}` :
             defaultRedirectUrl();
     };
 
