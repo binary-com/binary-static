dfd7f3cc24cb7aeff71e298c6997a5daef77eb0d
zakibinary <zaki@Zakis-MacBook-Pro-2.local>
2018-01-30 16:07:48 +0800

    resolve conflicts

--- src/javascript/app_2/pages/trading/components/amount.jsx
+++ src/javascript/app_2/pages/trading/components/amount.jsx
@@ -3,42 +3,47 @@ import { TextField } from './form/text_field.jsx';
 import { connect } from '../store/connect';
 import Client from '../../../../app/base/client';
 
+const Currencies = ({ list }) => (
+    Object.keys(list).map((type, idx) => (
+        <React.Fragment key={idx}>
+            <optgroup key={idx} label={type}></optgroup>
+            {list[type].map(cur => (
+                <option key={cur} value={cur}>{cur}</option>
+            ))}
+        </React.Fragment>
+    ))
+);
+
 const Amount = ({
     basis,
     currency,
-    currencies,
+    currencies_list,
     amount,
-    onBasisChange,
-    onAmountChange,
-    onCurrencyChange,
+    onChange,
 }) => (
         <fieldset>
-            <select name='basis' value={basis} onChange={onBasisChange}>
+            <select name='basis' value={basis} onChange={onChange}>
                 <option value='payout'>Payout</option>
                 <option value='stake'>Stake</option>
             </select>
 
             {Client.get('currency') ?
                 <span className={`symbols ${currency.toLowerCase()}`}></span> :
-                <select name='currency' value={currency || currencies[0]} onChange={onCurrencyChange}>
-                    {currencies.map((cur, idx) => (
-                        <option key={idx} value={cur}>{cur}</option>
-                    ))}
+                <select name='currency' value={currency} onChange={onChange}>
+                    <Currencies list={currencies_list} />
                 </select>
             }
 
-            <TextField name='amount' value={amount} onChange={onAmountChange} />
+            <TextField name='amount' value={amount} onChange={onChange} />
         </fieldset>
 );
 
 export default connect(
     ({trade}) => ({
-        basis           : trade.basis,
-        currency        : trade.currency,
-        currencies      : trade.currencies,
-        amount          : trade.amount,
-        onBasisChange   : trade.handleChange,
-        onAmountChange  : trade.handleChange,
-        onCurrencyChange: trade.handleChange,
+        basis          : trade.basis,
+        currency       : trade.currency,
+        currencies_list: trade.currencies_list,
+        amount         : trade.amount,
+        onChange       : trade.handleChange,
     })
 )(Amount);
