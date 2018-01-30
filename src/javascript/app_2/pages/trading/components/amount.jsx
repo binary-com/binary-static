b90f8aa091b34b838a5cdd0c42b38157543aeafa
binary-hamedanchi <hamedanchi@gmail.com>
2018-01-30 10:16:07 +0800

    Break into child component

--- src/javascript/app_2/pages/trading/components/amount.jsx
+++ src/javascript/app_2/pages/trading/components/amount.jsx
@@ -3,6 +3,17 @@ import { TextField } from './form/text_field.jsx';
 import { connect } from '../store/connect';
 import Client from '../../../../app/base/client';
 
+const Currencies = ({ list }) => (
+    Object.keys(list).map((type, idx) => (
+        <React.Fragment key={idx}>
+            <optgroup key={idx} label={type}></optgroup>
+            {list[type].map(cur => (
+                <option key={cur} value={cur}>{cur}</option>
+            ))}
+        </React.Fragment>
+    ))
+);
+
 const Amount = ({
     basis,
     currency,
@@ -19,14 +30,7 @@ const Amount = ({
             {Client.get('currency') ?
                 <span className={`symbols ${currency.toLowerCase()}`}></span> :
                 <select name='currency' value={currency} onChange={onChange}>
-                    {Object.keys(currencies_list).map((type, idx) => (
-                        <React.Fragment key={idx}>
-                            <optgroup key={idx} label={type}></optgroup>
-                            {currencies_list[type].map(cur => (
-                                <option key={cur} value={cur}>{cur}</option>
-                            ))}
-                        </React.Fragment>
-                    ))}
+                    <Currencies list={currencies_list} />
                 </select>
             }
 
