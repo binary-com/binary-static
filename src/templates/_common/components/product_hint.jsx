30168e0d9794a00f3824456ec9af7516603226df
negar <negar254000@gmail.com>
2018-02-08 13:50:13 +0800

    add space after/before asterisk for additions

--- src/templates/_common/components/product_hint.jsx
+++ src/templates/_common/components/product_hint.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const product_hint = 'Some products are not available in all countries.';
 
 export const Asterisk = () => (
-    <span className='product-hint no-underline' data-balloon={it.L(product_hint)}>*</span>
+    <span className='product-hint no-underline' data-balloon={it.L(product_hint)}>&nbsp;*</span>
 );
 
 export const ProductHint = () => (
-    <div className='hint' id='product_hint'>*{it.L(product_hint)}</div>
+    <div className='hint' id='product_hint'>* {it.L(product_hint)}</div>
 );
