a5c6ebc88b80c15f4ec0f4d8cce7c424f6883cc7
binary-hamedanchi <mohammad@binary.com>
2018-02-13 16:00:54 +0800

    Display outdated browser message on landing pages

--- src/javascript/landing_pages/common.js
+++ src/javascript/landing_pages/common.js
@@ -283,3 +283,27 @@ function isLoggedIn () {
 
     return get('loginid') && get('token');
 }
+
+// displays notification on outdated browsers
+function outdatedBrowser() {
+    const src = '//browser-update.org/update.min.js';
+    if (document.querySelector(`script[src*="${src}"]`)) return;
+    window.$buoop = {
+        vs      : { i: 11, f: -4, o: -4, s: 9, c: -4 },
+        api     : 4,
+        l       : getLanguage().toLowerCase(),
+        url     : 'https://whatbrowser.org/',
+        noclose : true, // Do not show the 'ignore' button to close the notification
+        text    : 'Your web browser ({brow_name}) is out of date and may affect your trading experience. Proceed at your own risk. <a href="https://www.whatbrowser.org/" target="_blank">Update browser</a>',
+        reminder: 0, // show all the time
+    };
+    if (document.body) {
+        const script = document.createElement('script');
+        script.setAttribute('src', src);
+        document.body.appendChild(script);
+    }
+}
+
+window.addEventListener('load', () => { // being called before js code of each page
+    outdatedBrowser();
+});
