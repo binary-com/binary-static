bd0cc698ccef2bb7f7da09bab9b596fafade9155
binary-hamedanchi <mohammad@binary.com>
2018-02-14 11:38:56 +0800

    Fix indentation

--- src/templates/landing_pages/hackathon.jsx
+++ src/templates/landing_pages/hackathon.jsx
@@ -12,43 +12,43 @@ const Hackathon = () => (
             it.url_for('js/landing_pages/hackathon.js'),
         ]}
     >
-            <div className='navbar' id='navigation'>
-                <div className='container'>
-                    <div className='navbar-header'>
-                        <a className='navbar-brand page-scroll logo' href='#page-top' />
-                    </div>
-                    <div className='navbar-collapse'>
-                        <ul className='nav navbar-nav'>
-                            <li className='invisible'>
-                                <a href='#page-top' />
-                            </li>
-                        </ul>
-                    </div>
+        <div className='navbar' id='navigation'>
+            <div className='container'>
+                <div className='navbar-header'>
+                    <a className='navbar-brand page-scroll logo' href='#page-top' />
+                </div>
+                <div className='navbar-collapse'>
+                    <ul className='nav navbar-nav'>
+                        <li className='invisible'>
+                            <a href='#page-top' />
+                        </li>
+                    </ul>
                 </div>
             </div>
+        </div>
 
-            <section id='page-top' className='intro'>
-                <h1 className='text-uppercase'>
-                    <span className='text-bold'>Hackathon </span><span className='text-thin'>Competition</span>
-                </h1>
-                <div className='intro-bg' />
-                <div id='register' className='white-bg'>
-                    <div className='container'>
-                        <h1>Thank you for all those who participated.</h1>
-                        <h3>Stay tuned for updates on our next Hackathon competition!</h3>
-                    </div>
-                </div>
-            </section>
-
-            <footer className='text-center'>
+        <section id='page-top' className='intro'>
+            <h1 className='text-uppercase'>
+                <span className='text-bold'>Hackathon </span><span className='text-thin'>Competition</span>
+            </h1>
+            <div className='intro-bg' />
+            <div id='register' className='white-bg'>
                 <div className='container'>
-                    <p>
-                        Visit our
-                        <a className='text-bold' target='_blank' rel='noopener noreferrer' href='https://www.binary.com/en/careers.html'>career page</a>
-                         for more opportunities at Binary!
-                    </p>
+                    <h1>Thank you for all those who participated.</h1>
+                    <h3>Stay tuned for updates on our next Hackathon competition!</h3>
                 </div>
-            </footer>
+            </div>
+        </section>
+
+        <footer className='text-center'>
+            <div className='container'>
+                <p>
+                    Visit our
+                    <a className='text-bold' target='_blank' rel='noopener noreferrer' href='https://www.binary.com/en/careers.html'>career page</a>
+                     for more opportunities at Binary!
+                </p>
+            </div>
+        </footer>
     </Layout>
 );
 
