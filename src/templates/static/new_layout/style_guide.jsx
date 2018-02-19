dd8501686718ac5284b8a383d15dfe4b266dcc29
Rustem Kakimov <rustem@binary.com>
2018-02-19 13:12:57 +0800

    move dummy data out of style guide

--- src/templates/static/new_layout/style_guide.jsx
+++ src/templates/static/new_layout/style_guide.jsx
@@ -1,53 +1,7 @@
 import React from 'react';
 import Button from '../../../javascript/app_2/pages/trading/components/form/button.jsx';
 import InputField from '../../../javascript/app_2/pages/trading/components/form/input_field.jsx';
-import DataTable from '../../../javascript/app_2/pages/trading/components/data_table.jsx';
-
-// generate dummy data for the DataTable
-const transactions = Array(150).fill(0).map((_, i) => {
-    return {
-        balance_after: 10150.1300,
-        transaction_id: (10867502908 - i),
-        reference_id: (45143958928 - i),
-        transaction_time: (1441175849 - i * 100),
-        action_type: (i % 2 ? 'Sell' : 'Buy'),
-        amount: -83.2300,
-        longcode: 'Win payout if the last digit of Volatility 25 Index is 7 after 5 ticks.',
-        payout: 90.91
-    };
-});
-
-const statement_columns = [
-    {
-        title: 'Date',
-        dataIndex: 'transaction_time'
-    },
-    {
-        title: 'Ref.',
-        dataIndex: 'reference_id'
-    },
-    {
-        title: 'Potential payout',
-        dataIndex: 'payout'
-    },
-    {
-        title: 'Action',
-        dataIndex: 'action_type'
-    },
-    {
-        title: 'Description',
-        dataIndex: 'longcode'
-    },
-    {
-        title: 'Credit/Debit',
-        dataIndex: 'amount'
-    },
-    {
-        title: 'Balance (USD)',
-        dataIndex: 'balance_after'
-    }
-];
-
+import DataTable, { transactions, statement_columns } from '../../../javascript/app_2/pages/trading/components/data_table.jsx';
 
 const StyleGuide = () => (
     <div className='container'>
