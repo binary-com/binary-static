c1452cb517b69e8180084b7fdb93c81984d6e0e0
Rustem Kakimov <rustem@binary.com>
2018-02-23 14:05:49 +0800

    don't render the modal when hidden, instead of display none

--- src/javascript/app_2/pages/trading/components/fullscreen_dialog.jsx
+++ src/javascript/app_2/pages/trading/components/fullscreen_dialog.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
 const FullscreenDialog = ({ title, onClose, visible, children }) => {
+    if (!visible) return null;
+
     return (
-        <div className={`fullscreen-dialog ${visible ? '' : 'fullscreen-dialog--hidden'}`}>
+        <div className='fullscreen-dialog'>
             <h1 className='fullscreen-dialog__title'>
                 {title}
             </h1>
@@ -10,7 +12,7 @@ const FullscreenDialog = ({ title, onClose, visible, children }) => {
                 {children}
             </div>
             <div className='fullscreen-dialog__footer'>
-                <button onClick={}>close</button>
+                <button onClick={onClose}>close</button>
             </div>
         </div>
     );
