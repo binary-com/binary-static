35410632ce0ab85931ddd688023f2c3ae91f9306
kelly <kelly@binary.com>
2018-02-28 17:59:17 +0800

    Simplify dialog

--- src/javascript/app/common/attach_dom/dialog.js
+++ src/javascript/app/common/attach_dom/dialog.js
@@ -3,74 +3,51 @@ const elementTextContent = require('../../../_common/common_functions').elementT
 const getElementById     = require('../../../_common/common_functions').getElementById;
 const urlFor             = require('../../../_common/url').urlFor;
 
-const alert = (options) => (
-    new Promise((resolve) => {
-        showPopup({
-            url               : urlFor('dialog'),
-            popup_id          : options.id,
-            form_id           : '#frm_confirm',
-            content_id        : '#dialog_content',
-            additionalFunction: () => {
-                const el_dialog = getElementById(options.id);
-                const el_btn_ok = getElementById('btn_ok');
+const dialog = (() => {
+    const BaseDialog = (options, is_alert = false) => (
+        new Promise((resolve) => {
+            showPopup({
+                url               : urlFor('dialog'),
+                popup_id          : options.id,
+                form_id           : '#frm_confirm',
+                content_id        : '#dialog_content',
+                additionalFunction: () => {
+                    const el_dialog     = getElementById(options.id);
+                    const el_btn_ok     = getElementById('btn_ok');
+                    const el_btn_cancel = getElementById('btn_cancel');
 
-                elementTextContent(getElementById('dialog_message'), options.message);
+                    if (!el_dialog) return;
 
-                getElementById('btn_cancel').classList.add('invisible');
+                    elementTextContent(getElementById('dialog_message'), options.message);
 
-                el_btn_ok.addEventListener('click', () => {
-                    if (el_dialog) {
+                    if (is_alert) {
+                        el_btn_cancel.classList.add('invisible');
+                    } else {
+                        el_btn_cancel.addEventListener('click', () => {
+                            el_dialog.remove();
+                            if (typeof options.onAbort === 'function') {
+                                options.onAbort();
+                            }
+                            resolve(false);
+
+                        });
+                    }
+                    el_btn_ok.addEventListener('click', () => {
                         el_dialog.remove();
-                    }
-                    if (typeof options.onConfirm === 'function') {
-                        options.onConfirm();
-                    }
-                    resolve(true);
-                });
-            },
-        });
-    })
-);
+                        if (typeof options.onConfirm === 'function') {
+                            options.onConfirm();
+                        }
+                        resolve(true);
+                    });
+                },
+            });
+        })
+    );
 
-const confirm = (options) => (
-    new Promise((resolve) => {
-        showPopup({
-            url               : urlFor('dialog'),
-            popup_id          : options.id,
-            form_id           : '#frm_confirm',
-            content_id        : '#dialog_content',
-            additionalFunction: () => {
-                const el_dialog = getElementById(options.id);
-                const el_btn_ok = getElementById('btn_ok');
-                const el_btn_cancel = getElementById('btn_cancel');
+    return {
+        alert  : (options) => new BaseDialog(options, true),
+        confirm: (options) => new BaseDialog(options),
+    };
+})();
 
-                elementTextContent(getElementById('dialog_message'), options.message);
-
-                el_btn_cancel.addEventListener('click', () => {
-                    if (el_dialog) {
-                        el_dialog.remove();
-                    }
-                    if (typeof options.onAbort === 'function') {
-                        options.onAbort();
-                    }
-                    resolve(false);
-
-                });
-                el_btn_ok.addEventListener('click', () => {
-                    if (el_dialog) {
-                        el_dialog.remove();
-                    }
-                    if (typeof options.onConfirm === 'function') {
-                        options.onConfirm();
-                    }
-                    resolve(true);
-                });
-            },
-        });
-    })
-);
-
-module.exports = {
-    alert,
-    confirm,
-};
+module.exports = dialog;
