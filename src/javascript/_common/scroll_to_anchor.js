d7ee1fe937af642f077f41f00a359c87817ac122
Rustem Kakimov <ruskakimov@gmail.com>
2018-03-05 13:58:30 +0800

    fix eslint

--- src/javascript/_common/scroll_to_anchor.js
+++ src/javascript/_common/scroll_to_anchor.js
@@ -1,18 +1,19 @@
 const ScrollToAnchor = (() => {
     const init = () => {
-        getQueryObject(window.location.search);
+        const query = getQueryObject(window.location.search);
+
+        // scroll to the element with id === query.anchor
+        console.log(query);
     };
 
-    const getQueryObject = (query_string) => {
-        return query_string
-            .slice(1)
-            .split('&')
-            .map(pair => pair.split('='))
-            .reduce((obj, [ key, val ]) => {
-                obj[key] = val;
-                return obj;
-            }, {})
-    };
+    const getQueryObject = (query_string) => query_string
+        .slice(1)
+        .split('&')
+        .map(pair => pair.split('='))
+        .reduce((obj, [ key, val ]) => {
+            obj[key] = val;
+            return obj;
+        }, {});
 
     return {
         init,
