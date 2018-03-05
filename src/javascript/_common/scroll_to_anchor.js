ae6b289556c23ad8459ff6222fd091683548c73d
Rustem Kakimov <ruskakimov@gmail.com>
2018-03-05 13:44:03 +0800

    convert to module

--- src/javascript/_common/scroll_to_anchor.js
+++ src/javascript/_common/scroll_to_anchor.js
@@ -1,16 +1,23 @@
-const getQueryObject = (query_string) => {
-    return query_string
-        .slice(1)
-        .split('&')
-        .map(pair => pair.split('='))
-        .reduce((obj, [ key, val ]) => {
-            obj[key] = val;
-            return obj;
-        }, {})
-};
+const ScrollToAnchor = (() => {
+    const init = () => {
+        getQueryObject(window.location.search);
+    };
 
-const ScrollToAnchor = () => {
-    getQueryObject(window.location.search);
-};
+    const getQueryObject = (query_string) => {
+        return query_string
+            .slice(1)
+            .split('&')
+            .map(pair => pair.split('='))
+            .reduce((obj, [ key, val ]) => {
+                obj[key] = val;
+                return obj;
+            }, {})
+    };
+
+    return {
+        init,
+        getQueryObject,
+    };
+})();
 
 module.exports = ScrollToAnchor;
