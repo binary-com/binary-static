1b0df74afd1af4e41e497cc54925ed2540c658f2
Rustem Kakimov <ruskakimov@gmail.com>
2018-03-06 16:14:25 +0800

    add QueryString common module

---
+++ src/javascript/_common/query_string.js
@@ -0,0 +1,32 @@
+const QueryString = (() => {
+    const queryStringToObject = (query_string) => query_string
+        .slice(1)
+        .split('&')
+        .map(pair => pair.split('='))
+        .reduce((obj, [ key, val ]) => {
+            obj[key] = window.decodeURI(val);
+            return obj;
+        }, {});
+
+    const removeParamFromQueryString = (query_string, removal_key) => {
+        const query_obj = queryStringToObject(query_string);
+        const new_query_str = Object.keys(query_obj)
+            .filter(key => key !== removal_key)
+            .map(key => `${key}=${query_obj[key]}`)
+            .join('&');
+        return new_query_str !== '' ? `?${new_query_str}` : '';
+    };
+
+    const setQueryStringWithoutReload = (new_query_str) => {
+        const { pathname, hash } = window.location;
+        window.history.replaceState('', '', `${pathname}${new_query_str}${hash}`);
+    };
+
+    return {
+        queryStringToObject,
+        removeParamFromQueryString,
+        setQueryStringWithoutReload,
+    };
+})();
+
+module.exports = QueryString;
