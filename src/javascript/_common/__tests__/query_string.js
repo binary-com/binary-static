7d6e4bff6f681120881ad6ac195c1e5b1f982491
Rustem Kakimov <ruskakimov@gmail.com>
2018-03-06 16:53:37 +0800

    add query string module tests

--- src/javascript/_common/__tests__/query_string.js
+++ src/javascript/_common/__tests__/query_string.js
@@ -1,9 +1,12 @@
 const expect = require('chai').expect;
-const QueryString = require('../scroll_to_anchor');
+const QueryString = require('../query_string');
 
 describe('QueryString', () => {
     describe('.queryStringToObject', () => {
-        it('parses query with single parameter', () => {
+        it('returns empty object for empty string', () => {
+            expect(QueryString.queryStringToObject('')).to.deep.eq({});
+        });
+        it('parses query with a single parameter', () => {
             expect(QueryString.queryStringToObject('?anchor=otc')).to.deep.eq({
                 anchor: 'otc',
             });
@@ -18,5 +21,19 @@ describe('QueryString', () => {
                 param2: '42',
             });
         })
+    });
+
+    describe('.removeParamFromQueryString', () => {
+        it('works for empty query', () => {
+            expect(QueryString.removeParamFromQueryString('', 'anchor')).to.eq('');
+        });
+        it('doesn\'t modify query without specified key', () => {
+            expect(QueryString.removeParamFromQueryString('?a=123', 'anchor')).to.eq('?a=123');
+            expect(QueryString.removeParamFromQueryString('?a=123&b=test', 'anchor')).to.eq('?a=123&b=test');
+        });
+        it('removes specified key value pair', () => {
+            expect(QueryString.removeParamFromQueryString('?anchor=NZT', 'anchor').to.eq(''));
+            expect(QueryString.removeParamFromQueryString('?a=Hello%20World&b=10', 'a').to.eq('?b=10'));
+        });
     });
 });
