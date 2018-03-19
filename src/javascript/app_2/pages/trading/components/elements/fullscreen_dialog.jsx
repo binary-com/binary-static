6ff2ca13b597cc9c171f39f6cffbc39ec5181f10
Rustem Kakimov <ruskakimov@gmail.com>
2018-03-19 10:07:06 +0800

    simplify fullscreen dialog component

--- src/javascript/app_2/pages/trading/components/elements/fullscreen_dialog.jsx
+++ src/javascript/app_2/pages/trading/components/elements/fullscreen_dialog.jsx
@@ -1,57 +1,28 @@
 import React from 'react';
 import Url from '../../../../../_common/url';
 
-const scroll_lock_classname = 'no-scroll';
-const open_dialog_classname = 'fullscreen-dialog--open';
-
 class FullscreenDialog extends React.PureComponent {
-    constructor(props) {
-        super(props);
-        this.handleClose = this.handleClose.bind(this);
-    }
-
     componentDidUpdate() {
         if (this.props.visible) {
-            document.body.classList.add(scroll_lock_classname);
-            // timeout to force transition animation
-            window.setTimeout(() => {
-                this.dialogEl.classList.add(open_dialog_classname);
-            }, 0);
+            document.body.classList.add('no-scroll');
         }
         else {
-            document.body.classList.remove(scroll_lock_classname);
+            document.body.classList.remove('no-scroll');
         }
-    }
-
-    handleClose() {
-        function transitionendHandler(e) {
-            if (e.propertyName === 'transform') {
-                e.target.removeEventListener(e.type, transitionendHandler);
-                this.props.onClose();
-            }
-        }
-
-        this.dialogEl.addEventListener('transitionend', transitionendHandler.bind(this));
-        this.dialogEl.classList.remove(open_dialog_classname);
     }
 
     render() {
         const { title, visible, children } = this.props;
 
-        if (!visible) return null;
-
         return (
-            <div
-                ref={(dialogEl) => { this.dialogEl = dialogEl; }}
-                className='fullscreen-dialog'
-            >
+            <div className={`fullscreen-dialog ${visible ? 'fullscreen-dialog--open' : ''}`}>
                 <div className='fullscreen-dialog__header'>
                     <h2 className='fullscreen-dialog__title'>
                         {title}
                     </h2>
                     <div
                         className='icons btn-close fullscreen-dialog__close-btn'
-                        onClick={this.handleClose}
+                        onClick={this.props.onClose}
                     >
                         <img src={Url.urlForStatic('images/trading_app/common/close.svg')} alt='Close' />
                     </div>
