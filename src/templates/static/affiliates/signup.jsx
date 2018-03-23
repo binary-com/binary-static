41ad3b09648536f42c20fb565fae9206941e241b
Rustem Kakimov <ruskakimov@gmail.com>
2018-03-23 11:34:39 +0800

    set signup page to full_width and adjust spacings and add more stuff

--- src/templates/static/affiliates/signup.jsx
+++ src/templates/static/affiliates/signup.jsx
@@ -16,18 +16,26 @@ const Step = ({ header, text, circle_no }) => {
 };
 
 const Signup = () => (
-    <div>
-        <h1>{it.L('Binary.com Affiliate Programme')}</h1>
-        <p>
-            {it.L('Earn up to 35% commission with an award-winning binary options trading platform.')}
-        </p>
+    <div className='static_full'>
+        <div className='container'>
+            <h1>{it.L('Binary.com Affiliate Programme')}</h1>
+            <p>
+                {it.L('Earn up to 35% commission with an award-winning binary options trading platform.')}
+            </p>
 
-        <h2>{it.L('How it works')}</h2>
-        <div className='steps'>
-            {/* TODO: add link in first step */}
-            <Step circle_no='1' header={it.L('Sign up')}            text={it.L('Getting started is free and easy –– just fill out the application form and wait for our approval.')} />
-            <Step circle_no='2' header={it.L('Promote Binary.com')} text={it.L('Use your unique affiliate link and the marketing tools we provide to advertise Binary.com to your audience.')} />
-            <Step circle_no='3' header={it.L('Earn')}               text={it.L('Choose from two types of  commission plans when your referred clients trade binary options on our platform.')} />
+            <h2 className='center-text gr-padding-30'>{it.L('How it works')}</h2>
+            <div className='steps'>
+                {/* TODO: add link in first step */}
+                <Step circle_no='1' header={it.L('Sign up')}            text={it.L('Getting started is free and easy –– just fill out the application form and wait for our approval.')} />
+                <Step circle_no='2' header={it.L('Promote Binary.com')} text={it.L('Use your unique affiliate link and the marketing tools we provide to advertise Binary.com to your audience.')} />
+                <Step circle_no='3' header={it.L('Earn')}               text={it.L('Choose from two types of  commission plans when your referred clients trade binary options on our platform.')} />
+            </div>
+        </div>
+
+        <div className='fill-bg-color'>
+            <div className='container gr-padding-30'>
+                <h2>{it.L('Why you should join the Binary.com Affiliate Programme')}</h2>
+            </div>
         </div>
     </div>
 );
