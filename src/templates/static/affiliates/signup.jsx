f2c20f9e5c5a75b12a113c7c3f64b75440c9d1ae
Rustem Kakimov <ruskakimov@gmail.com>
2018-03-23 11:09:54 +0800

    start affiliate signup page

--- src/templates/static/affiliates/signup.jsx
+++ src/templates/static/affiliates/signup.jsx
@@ -2,29 +2,32 @@ import React from 'react';
 import FAQ from './faq.jsx';
 import Main from './main.jsx';
 
-const Signup = () => (
-    <div className='static_full'>
-        <h1>{it.L('Affiliate')}</h1>
+const Step = ({ header, text, circle_no }) => {
+    return (
+        <div className='step'>
+            <div className='border-bottom' />
+            <div className='circle'>{ circle_no }</div>
+            <div className='gr-padding-20 gr-gutter center-text'>
+                <div className='gr-padding-20 gr-child'><strong>{header}</strong></div>
+                <p className='no-margin gr-padding-10'>{text}</p>
+            </div>
+        </div>
+    );
+};
 
-        <div className='content-tab-container gr-padding-10'>
-            <div className='tab-menu'>
-                <div className='tab-menu-wrap container'>
-                    <ul className='tm-ul'>
-                        <li id='general_info' className='tm-li active first'><span className='menu-wrap-a'><span className='menu-wrap-b'><span className='tm-a a-active'>{it.L('Affiliate')}</span></span></span></li>
-                        <li id='faq' className='tm-li last'><a href='#faq' className='tm-a'>{it.L('FAQ')}</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div className='tab-content container'>
-                <div className='tab-content-wrapper'>
-                    <div id='general_info-content' className='toggle-content'>
-                        <Main />
-                    </div>
-                    <div id='faq-content' className='toggle-content invisible'>
-                        <FAQ />
-                    </div>
-                </div>
-            </div>
+const Signup = () => (
+    <div>
+        <h1>{it.L('Binary.com Affiliate Programme')}</h1>
+        <p>
+            {it.L('Earn up to 35% commission with an award-winning binary options trading platform.')}
+        </p>
+
+        <h2>{it.L('How it works')}</h2>
+        <div className='steps'>
+            {/* TODO: add link in first step */}
+            <Step circle_no='1' header={it.L('Sign up')}            text={it.L('Getting started is free and easy –– just fill out the application form and wait for our approval.')} />
+            <Step circle_no='2' header={it.L('Promote Binary.com')} text={it.L('Use your unique affiliate link and the marketing tools we provide to advertise Binary.com to your audience.')} />
+            <Step circle_no='3' header={it.L('Earn')}               text={it.L('Choose from two types of  commission plans when your referred clients trade binary options on our platform.')} />
         </div>
     </div>
 );
