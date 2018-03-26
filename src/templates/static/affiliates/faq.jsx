42c88741cb5d1104b9f41e7dec58ee5fff294478
Rustem Kakimov <ruskakimov@gmail.com>
2018-03-26 16:41:43 +0800

    fill and style general page

--- src/templates/static/affiliates/faq.jsx
+++ src/templates/static/affiliates/faq.jsx
@@ -3,7 +3,7 @@ import { Section, HeaderSecondary, NavButtons, ListStrong } from '../get_started
 import { List } from '../../_common/components/elements.jsx';
 
 const FAQ = () => (
-    <div className='static_full'>
+    <div className='static_full affiliates_faq'>
         <h1>{it.L('Affiliate FAQ')}</h1>
         <div className='gr-row'>
             <div className='gr-3 gr-hide-m sidebar-container'>
@@ -21,6 +21,32 @@ const FAQ = () => (
             </div>
             <div className='gr-9 gr-12-m'>
                 <Section id='general' header={it.L('General')}>
+                    <h3>{it.L('What is the Binary.com Affiliate Programme all about?')}</h3>
+                    <p>{it.L('The Binary.com Affiliate Programme allows you to benefit from referring new clients to our site.')}</p>
+                    <p>{it.L('If you\'re a broker we\'d like to work with you. You will be introducing your clients to a unique and innovative product: the Binary.com trading platform. Your clients will love the Binary.com trading platform because we offer a complete binary options trading experience tailored to the needs of an exceptionally wide range of traders.')}</p>
+                    <p>{it.L('Novice traders can use our intuitive platform to learn about trading, practise trading, and gain trading experiences. Seasoned traders can use the Binary.com platform and take full advantage of the wide range of trading and analysis tools we have to offer.')}</p>
+
+                    <h3>{it.L('Why should I become a Binary.com affiliate?')}</h3>
+                    <p>{it.L('Binary.com is a licensed and regulated binary options trading platform that’s been operating since 2000. It has:')}</p>
+                    <ul className='bullet'>
+                        <li>{it.L('An international appeal with multilingual support in English, Spanish, French, German, Portuguese, Chinese, Japanese, Italian, Thai, Polish, Russian, and Indonesian')}</li>
+                        <li>{it.L('An intuitive, web-based platform that’s instantly available to traders of all levels –– anytime, anywhere')}</li>
+                        <li>{it.L('A competitive and flexible affiliate programme that can be adapted to your needs')}</li>
+                    </ul>
+
+                    <h3>{it.L('Is there a cost for joining?')}</h3>
+                    <p>{it.L('Not at all. Joining our affiliate programme is completely free and always will be.')}</p>
+
+                    <h3>{it.L('What is the definition of a referred client?')}</h3>
+                    <p>{it.L('A client is someone who has been referred through your unique affiliate link and who has deposited money into their Binary.com account. They must fulfil the following criteria:')}</p>
+                    <ul className='bullet'>
+                        <li>{it.L('Have not previously been a Binary.com customer')}</li>
+                        <li>{it.L('Aged 18 years old and above')}</li>
+                    </ul>
+
+
+                    <h3>{it.L('Who can be a client on the Binary.com platform?')}</h3>
+                    <p>{it.L('Anyone aged 18 years old and above who is not the resident of a “restricted country” (as listed in our <a href=\'[_1]\'>Terms & Conditions</a>) can become a Binary.com client.', it.url_for('terms-and-conditions'))}</p>
                 </Section>
                 <Section id='account-management-and-tracking' header={it.L('Account management and tracking')}>
                 </Section>
