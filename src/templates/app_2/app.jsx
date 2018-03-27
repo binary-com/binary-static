c9643c5d4f6185a74e1c607a47b6a1b38ce87777
binary-hamedanchi <mohammad@binary.com>
2018-03-27 11:52:23 +0800

    Move to app

---
+++ src/templates/app_2/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Loading from '../_common/components/loading.jsx';
+
+const BinaryApp = () => (
+    <div id='binary_app'>
+        <Loading />
+    </div>
+);
+
+export default BinaryApp;
