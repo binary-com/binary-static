dcb296cf7faa7b06594c9ce2381164ae73dcba75
binary-hamedanchi <mohammad@binary.com>
2018-03-30 09:30:49 +0800

    Restrict authenticated contents

--- src/javascript/app_2/routes.js
+++ src/javascript/app_2/routes.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
+import Client from '../app/base/client';
+import { redirectToLogin } from '../app/base/login';
+
 import TradeApp  from './pages/trading/trade_app.jsx';
 import Statement from './pages/statement/statement.jsx';
 
@@ -8,17 +11,20 @@ import Statement from './pages/statement/statement.jsx';
 const routes = [
     { path: '/',          component: TradeApp, exact: true },
     { path: '/trade',     component: TradeApp },
-    { path: '/statement', component: Statement },
+    { path: '/statement', component: Statement, is_authenticated: true },
 ];
 
 const RouteWithSubRoutes = route => (
-    <Route
-        exact={route.exact}
-        path={route.path}
-        render={props => (
-            <route.component {...props} routes={route.routes} />
-        )}
-    />
+    (route.is_authenticated && !Client.isLoggedIn()) ?
+        <a href='javascript:;' onClick={redirectToLogin}>Please login to view this page.</a>
+        :
+        <Route
+            exact={route.exact}
+            path={route.path}
+            render={props => (
+                <route.component {...props} routes={route.routes} />
+            )}
+        />
 );
 
 const BinaryRoutes = () => routes.map((route, idx) => (
