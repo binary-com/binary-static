43a289f83135de74a634a0695bf38c8de1bb2d90
Rustem Kakimov <ruskakimov@gmail.com>
2018-04-02 15:59:31 +0800

    add binary dialog

--- src/javascript/_common/third_party_links.js
+++ src/javascript/_common/third_party_links.js
@@ -2,13 +2,13 @@ const localize         = require('./localize').localize;
 const State            = require('./storage').State;
 const Client           = require('../app/base/client');
 const BinarySocket     = require('../app/base/socket');
+const Dialog           = require('../app/common/attach_dom/dialog');
 
 const ThirdPartyLinks = (() => {
     const init = () => {
         if (Client.isLoggedIn()) {
-            BinarySocket.wait('authorize').then(() => {
-                const landing_company_shortcode = State.getResponse('authorize.landing_company_name');
-                if (landing_company_shortcode === 'maltainvest') {
+            BinarySocket.wait('landing_company').then(() => {
+                if (State.getResponse('landing_company.financial_company.shortcode') === 'maltainvest') {
                     document.body.addEventListener('click', clickHandler);
                 }
             });
@@ -18,16 +18,27 @@ const ThirdPartyLinks = (() => {
     const clickHandler = (e) => {
         if (!e.target) return;
         const link_el = e.target.closest('a');
-        if (link_el && isThirdPartyLink(link_el)) {
-            // TODO: replace with custom popup
-            const should_proceed = window.confirm(localize('You will be redirected to a third-party website which is not owned by Binary.com. Click OK to proceed.'));
-            if (!should_proceed) e.preventDefault();
+        if (!link_el) return;
+        // check that we are not inside third_party_redirect_dialog
+        const dialog = document.querySelector('#third_party_redirect_dialog');
+        if (dialog && dialog.contains(link_el)) return;
+
+        if (isThirdPartyLink(link_el)) {
+            e.preventDefault();
+            Dialog.confirm({
+                id: 'third_party_redirect_dialog',
+                message: 'You will be redirected to a third-party website which is not owned by Binary.com. Click OK to proceed.',
+            }).then((should_proceed) => {
+                console.log(should_proceed);
+                // manually open the link
+            });
         }
     }
 
     const isThirdPartyLink = (href) => {
         const destination = new URL(href);
-        return !/^.*\.binary\.com$/.test(destination.host)
+        return !!destination.host
+            && !/^.*\.binary\.com$/.test(destination.host)
             && window.location.host !== destination.host;
     };
 
