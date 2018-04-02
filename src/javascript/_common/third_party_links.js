cf128faacaaf07c84a3fabbc2590da5a72b3828e
Rustem Kakimov <ruskakimov@gmail.com>
2018-04-02 14:55:24 +0800

    apply only for MFSA clients

--- src/javascript/_common/third_party_links.js
+++ src/javascript/_common/third_party_links.js
@@ -1,18 +1,29 @@
-const localize = require('./localize').localize;
+const localize         = require('./localize').localize;
+const State            = require('./storage').State;
+const Client           = require('../app/base/client');
+const BinarySocket     = require('../app/base/socket');
 
 const ThirdPartyLinks = (() => {
     const init = () => {
-        document.body.addEventListener('click', (e) => {
-            if (!e.target) return;
-            const link_el = e.target.closest('a');
-            console.log(isThirdPartyLink(link_el));
-            if (link_el && isThirdPartyLink(link_el)) {
-                // TODO: replace with custom popup
-                const should_proceed = window.confirm(localize('You will be redirected to a third-party website which is not owned by Binary.com. Click OK to proceed.'));
-                if (!should_proceed) e.preventDefault();
-            }
-        });
+        if (Client.isLoggedIn()) {
+            BinarySocket.wait('authorize').then(() => {
+                const landing_company_shortcode = State.getResponse('authorize.landing_company_name');
+                if (landing_company_shortcode === 'maltainvest') {
+                    document.body.addEventListener('click', clickHandler);
+                }
+            });
+        }
     };
+
+    const clickHandler = (e) => {
+        if (!e.target) return;
+        const link_el = e.target.closest('a');
+        if (link_el && isThirdPartyLink(link_el)) {
+            // TODO: replace with custom popup
+            const should_proceed = window.confirm(localize('You will be redirected to a third-party website which is not owned by Binary.com. Click OK to proceed.'));
+            if (!should_proceed) e.preventDefault();
+        }
+    }
 
     const isThirdPartyLink = (href) => {
         const destination = new URL(href);
