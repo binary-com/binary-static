c7ea87326854a20be1faf08dc2135565dd264c22
Apoorv Joshi <apoorv@binary.com>
2018-04-09 16:09:26 +0800

    fix error not disappearing

--- src/javascript/app/pages/cashier/account_transfer.js
+++ src/javascript/app/pages/cashier/account_transfer.js
@@ -118,6 +118,7 @@ const AccountTransfer = (() => {
             const el_error = getElementById('form_error');
             elementTextContent(el_error, response.error.message);
             el_error.setVisibility(1);
+            setTimeout(() => { el_error.setVisibility(0) } , 5000);
         } else {
             BinarySocket.send({ transfer_between_accounts: 1 }).then(data => populateReceipt(response, data));
         }
