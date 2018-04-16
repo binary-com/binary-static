fc5863a6a8f113fa3e6a862e0dac3daafa2a0c18
Rustem Kakimov <ruskakimov@gmail.com>
2018-04-16 13:02:52 +0800

    remove pagination and related logic

--- src/javascript/app_2/components/elements/data_table.jsx
+++ src/javascript/app_2/components/elements/data_table.jsx
@@ -1,98 +1,5 @@
 import React from 'react';
 
-const Pagination = ({ page, total, page_size, onChange }) => {
-    const handleChange = (new_page) => {
-        if (new_page === page) return;
-        onChange(new_page, calcPagesCount());
-    };
-
-    const calcPagesCount = () => Math.ceil(total / page_size);
-
-    const handleNext = () => {
-        if (page < calcPagesCount()) {
-            handleChange(page + 1);
-        }
-    };
-
-    const handlePrev = () => {
-        if (page > 1) {
-            handleChange(page - 1);
-        }
-    };
-
-    const renderEllipsis = (id) => <li className='pagination-item pagination-ellipsis' key={`ellipsis-${id}`} />;
-
-    const renderItem = (page_num) => (
-        <li
-            className={`pagination-item ${page_num === page ? 'pagination-item-active' : ''}`}
-            key={page_num}
-            onClick={() => { handleChange(page_num); }}
-        >
-            <a>{page_num}</a>
-        </li>
-    );
-
-    const renderItemRange = (first, last) => {
-        const items = [];
-
-        for (let page_num = first; page_num <= last; page_num++) {
-            items.push(renderItem(page_num));
-        }
-        return items;
-    };
-
-    const renderItems = () => {
-        const pages_count = calcPagesCount();
-
-        if (pages_count <= 6) {
-            return renderItemRange(1, pages_count);
-        }
-        else if (page <= 4) {
-            return [
-                ...renderItemRange(1, 5),
-                renderEllipsis(2),
-            ];
-        }
-        else if (pages_count - page < 3) {
-            return [
-                renderItem(1),
-                renderEllipsis(1),
-                ...renderItemRange(pages_count - 3, pages_count),
-            ];
-        }
-        // else
-        return [
-            renderItem(1),
-            renderEllipsis(1),
-            ...renderItemRange(page - 1, page + 1),
-            renderEllipsis(2),
-        ];
-    };
-
-    return (
-        <ul className='pagination'>
-            <li
-                className={`pagination-prev ${page === 1 ? 'pagination-disabled' : ''}`}
-                onClick={handlePrev}
-            >
-                <a>&lt;</a>
-            </li>
-            {renderItems()}
-            <li
-                className={`pagination-next ${page === calcPagesCount() ? 'pagination-disabled' : ''}`}
-                onClick={handleNext}
-            >
-                <a>&gt;</a>
-            </li>
-        </ul>
-    );
-};
-
-Pagination.defaultProps = {
-    page: 1,
-};
-
-
 /* TODO:
       1. to implement sorting by column (ASC/DESC)
       2. to implement filtering per column
@@ -101,56 +8,7 @@ Pagination.defaultProps = {
 class DataTable extends React.Component {
     constructor(props) {
         super(props);
-        const { data_source, pagination, page_size } = props;
-
-        this.handlePageChange  = this.handlePageChange.bind(this);
-        this.renderPagination  = this.renderPagination.bind(this);
-        this.handlePageChange  = this.handlePageChange.bind(this);
-        this.updateDisplayData = this.updateDisplayData.bind(this);
-
-        this.state = {
-            display_data: pagination ? data_source.slice(0, page_size) : data_source,
-            page        : 1,
-        };
-    }
-
-    componentWillReceiveProps(nextProps) {
-        this.updateDisplayData(nextProps.data_source, this.state.page, nextProps.page_size);
-    }
-
-    updateDisplayData(data_source, page, page_size) {
-        const start_id = (page - 1) * page_size;
-        const end_id   = start_id + page_size;
-
-        this.setState({
-            page,
-            display_data: data_source.slice(start_id, end_id),
-        });
-    }
-
-    handlePageChange(page, pages_count) {
-        this.updateDisplayData(this.props.data_source, page, this.props.page_size);
-
-        if (!pages_count) return;
-
-        const { pages_close_to_end, onCloseToEnd } = this.props;
-        const pagesLeft = pages_count - page;
-        if (pagesLeft <= pages_close_to_end) {
-            onCloseToEnd();
-        }
-    }
-
-    renderPagination() {
-        return (
-            <div className='table-pagination'>
-                <Pagination
-                    page={this.state.page}
-                    total={this.props.data_source.length}
-                    page_size={this.props.page_size}
-                    onChange={this.handlePageChange}
-                />
-            </div>
-        );
+        this.state = { chunks: 1 };
     }
 
     renderRow(transaction, id) {
@@ -166,7 +24,9 @@ class DataTable extends React.Component {
     }
 
     renderBodyRows() {
-        return this.state.display_data.map((transaction, id) => this.renderRow(transaction, id));
+        return this.props.data_source
+            .slice(0, this.state.chunks * this.props.chunk_size)
+            .map((transaction, id) => this.renderRow(transaction, id));
     }
 
     renderHeaders() {
@@ -174,8 +34,6 @@ class DataTable extends React.Component {
     }
 
     render() {
-        const { pagination } = this.props;
-
         return (
             <div className='table-container'>
                 <table className='table'>
@@ -189,17 +47,13 @@ class DataTable extends React.Component {
                         {this.renderBodyRows()}
                     </tbody>
                 </table>
-
-                {pagination && this.renderPagination()}
             </div>
         );
     }
 }
 
 DataTable.defaultProps = {
-    pagination        : true,
-    page_size         : 10,
-    pages_close_to_end: 5,
+    chunk_size: 5,
 };
 
 export default DataTable;
