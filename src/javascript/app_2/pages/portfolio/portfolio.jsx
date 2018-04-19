aa153156f9fd87a747ab88e3816d6f55915cbc22
OskarAhl <oskar.ahlroth@gmail.com>
2018-04-19 17:54:12 +0800

    portfolio: add open_contract socket connection

--- src/javascript/app_2/pages/portfolio/portfolio.jsx
+++ src/javascript/app_2/pages/portfolio/portfolio.jsx
@@ -2,36 +2,30 @@ import React from 'react';
 import moment from 'moment';
 import Client from '../../../app/base/client';
 import BinarySocket from '../../../app/base/socket';
-import { formatMoney } from '../../../app/common/currency';
 import { localize } from '../../../_common/localize';
 import DataTable from '../../components/elements/data_table.jsx';
 
 // return transformed array
 const handlePortfolioData = (portfolio_data_arr) => {
-    const currency     = Client.get('currency');
     const formatted_data = portfolio_data_arr.map((portfolio_item) => {
-        console.log(portfolio_item);
-        const date_obj   = new Date(portfolio_item.expiry_time* 1000);
-        const moment_obj = moment.utc(date_obj);
-        const date_str   = moment_obj.format('YYYY-MM-DD');
-        const time_str   = `${moment_obj.format('HH:mm:ss')} GMT`;
-        const payout     = parseFloat(portfolio_item.payout);
-        const amount     = parseFloat(portfolio_item.amount);
-        const balance    = parseFloat(portfolio_item.balance_after);
-        const buy_price  = parseFloat(portfolio_item.buy_price);
-    
+        const date_obj       = new Date(portfolio_item.expiry_time* 1000);
+        const moment_obj     = moment.utc(date_obj);
+        const remaining_time = `${moment_obj.fromNow()} - ${moment_obj.format('h:mm:ss')}`;
+
         return {
-            ref     : portfolio_item.transaction_id,
-            type    : portfolio_item.contract_type,
-            asset   : portfolio_item.symbol,
-            contract: localize(portfolio_item.longcode.replace(/\n/g, '<br />')),
-            purchase: buy_price,
-            payout  : isNaN(payout)  ? '-' : formatMoney(currency, payout),
-            amount  : isNaN(amount)  ? '-' : formatMoney(currency, amount),
-            balance : isNaN(balance) ? '-' : formatMoney(currency, balance),
-            end_time: `${date_str}\n${time_str}`,
-            id      : portfolio_item.contract_id,
-            app_id  : portfolio_item.app_id,
+            ref       : portfolio_item.transaction_id,
+            type      : portfolio_item.contract_type,
+            asset     : portfolio_item.symbol,
+            details   : localize(portfolio_item.longcode.replace(/\n/g, '<br />')),
+            purchase  : parseFloat(portfolio_item.buy_price),
+            payout    : parseFloat(portfolio_item.payout),
+            remaining_time,
+            id        : portfolio_item.contract_id,
+            app_id    : portfolio_item.app_id,
+            indicative: {
+                amount: parseFloat(portfolio_item.amount),
+                class : '',
+            },
         };
     });
     console.log('formatted_data', formatted_data);
@@ -42,7 +36,8 @@ class Portfolio extends React.PureComponent  {
     constructor(props) {
         super(props);
 
-        this.populatePortfolioData = this.populatePortfolioData.bind(this);
+        this.getPortfolioData = this.getPortfolioData.bind(this);
+        this.updateIndicative = this.updateIndicative.bind(this);
 
         const columns = [
             {
@@ -50,7 +45,7 @@ class Portfolio extends React.PureComponent  {
                 data_index: 'ref',
             },
             {
-                title     : localize('Trade Type'),
+                title     : localize('Contract Type'),
                 data_index: 'type',
             },
             {
@@ -58,59 +53,88 @@ class Portfolio extends React.PureComponent  {
                 data_index: 'asset',
             },
             {
-                title     : localize('Contract'),
-                data_index: 'contract',
+                title     : localize('Contract Details'),
+                data_index: 'details',
             },
             {
-                title     : localize('Purchase'),
-                data_index: 'purchase',
+                title     : localize('Remaining Time (GMT)'),
+                data_index: 'remaining_time',
             },
             {
                 title     : localize('Potential Payout'),
                 data_index: 'payout',
+                renderCell: (data, data_index) => (<td key={data_index}> <span className={`symbols ${this.state.currency.toLowerCase()}`}/>{data}</td>),
             },
-            // {
-            //     title     : localize('Credit/Debit'),
-            //     data_index: 'amount',
-            //     renderCell: (data, data_index) => {
-            //         const parseStrNum = (str) => parseFloat(str.replace(',', '.'));
-            //         return <td className={`${data_index} ${(parseStrNum(data) >= 0) ? 'profit' : 'loss'}`} key={data_index}>{data}</td>;
-            //     },
-            // },
             {
-                title     : localize('End Time'),
-                data_index: 'end_time',
+                title     : localize('Purchase'),
+                data_index: 'purchase',
+                renderCell: (data, data_index) => (<td key={data_index}> <span className={`symbols ${this.state.currency.toLowerCase()}`}/>{data}</td>),
+            },
+            {
+                title     : localize('Indicative'),
+                data_index: 'indicative',
+                renderCell: (data, data_index) => {
+                    const is_nan = isNaN(data.amount);
+                    if (is_nan) {
+                        return <td key={data_index}>-</td>;
+                    }
+                    return <td key={data_index} className={data.class}> <span className={`symbols ${this.state.currency.toLowerCase()}`}/>{data.amount}</td>;
+                },
             },
         ];
+        const currency = Client.get('currency');
         this.state = {
             columns,
+            currency,
             data_source    : [],
             has_no_contract: false,
         };
     }
 
-    componentDidMount() {
-        this.populatePortfolioData();
+    componentWillMount() {
+        this.getPortfolioData();
     }
 
-    populatePortfolioData() {
+    componentWillUnmount() {
+        console.log(this.state);
+        BinarySocket.send({ forget_all: ['proposal_open_contract', 'transaction'] });
+    }
+    /* TODO:
+        1. Move socket connections to DAO
+        2. Handle errors
+        3. Handle empty portfolio
+        4. forget_all
+    */
+    getPortfolioData() {
         BinarySocket.send({ portfolio: 1 }).then((response) => {
-            // if open contracts in portfolio
-            console.log('portfolio: ', response);
+            // Handle error
             if (response.portfolio.contracts && response.portfolio.contracts.length > 0) {
                 const formatted_transactions = handlePortfolioData(response.portfolio.contracts);
                 this.setState({
                     data_source: [...this.state.data_source, ...formatted_transactions],
                 });
+                BinarySocket.send(
+                    { proposal_open_contract: 1, subscribe: 1 }, 
+                    { callback: this.updateIndicative }
+                );                
+            } else {
+            // empty portfolio
             }
-            // if empty portfolio
         });
-        console.log('getPortfolioData', this.state);
     }
-    
-    // request "proposal_open_contract"
-    //  BinarySocket.send({ proposal_open_contract: 1, subscribe: 1 }, { callback: updateIndicative });
-    // updateIndicative()
+
+    updateIndicative(data) {
+        const amount       = parseFloat(data.proposal_open_contract.bid_price);
+        const data_source  = this.state.data_source.slice();
+        
+        data_source.forEach(ds => {
+            if (ds.ref === data.proposal_open_contract.transaction_ids.buy) {
+                ds.indicative.class = data.proposal_open_contract.bid_price > ds.indicative.amount ? 'price_moved_up' : 'price_moved_down';
+                ds.indicative.amount = amount;
+            }
+        });
+        this.setState({ data_source });
+    }
 
     render() {
         return (
