3f085a3accd2b4fb1e22043a2427d5e4504b36ec
zakibinary <zaki@binary.com>
2018-04-20 15:40:42 +0800

    fixed

--- src/javascript/app/pages/user/account/settings/professional_client.js
+++ src/javascript/app/pages/user/account/settings/professional_client.js
@@ -26,7 +26,8 @@ const professionalClient = (() => {
 
     const populateProfessionalClient = (is_financial) => {
         const financial_company = State.getResponse('landing_company.financial_company.shortcode');
-        if (financial_company === 'maltainvest' && !is_financial) { // then it's not upgrading to financial
+        if (!/maltainvest/.test(financial_company) ||    // limited to these landing companies
+            (financial_company === 'maltainvest' && !is_financial)) { // then it's not upgrading to financial
             if (is_in_page) {
                 BinaryPjax.loadPreviousUrl();
             }
