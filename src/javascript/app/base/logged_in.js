367d0be3b9f7d0795bc03c623320b53927dfc8d0
binary-hamedanchi <mohammad@binary.com>
2018-05-08 10:00:53 +0800

    Refactor client

--- src/javascript/app/base/logged_in.js
+++ src/javascript/app/base/logged_in.js
@@ -8,6 +8,7 @@ const getElementById     = require('../../_common/common_functions').getElementB
 const getLanguage        = require('../../_common/language').get;
 const urlLang            = require('../../_common/language').urlLang;
 const isStorageSupported = require('../../_common/storage').isStorageSupported;
+const removeCookies      = require('../../_common/storage').removeCookies;
 const paramsHash         = require('../../_common/url').paramsHash;
 const urlFor             = require('../../_common/url').urlFor;
 const getPropertyValue   = require('../../_common/utility').getPropertyValue;
@@ -99,7 +100,7 @@ const LoggedInHandler = (() => {
             GTM.setLoginFlag();
             Client.set('session_start', parseInt(moment().valueOf() / 1000));
             // Remove cookies that were set by the old code
-            Client.cleanupCookies('email', 'login', 'loginid', 'loginid_list', 'residence');
+            removeCookies('email', 'login', 'loginid', 'loginid_list', 'residence');
         }
     };
 
