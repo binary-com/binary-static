633f12e12459dccecda0b6f1c1f808e2a0fe7d04
zakibinary <zaki@binary.com>
2018-05-15 13:39:46 +0800

    restore test.jsx

--- src/javascript/app_2/pages/trading/components/test.jsx
+++ src/javascript/app_2/pages/trading/components/test.jsx
@@ -4,22 +4,17 @@ import { connect } from '../../../store/connect';
 const Test = ({
     entries,
     json,
-    // active_symbols,
 }) =>  (
-    <React.Fragment>
-        <div style={{ fontSize: '10px', lineHeight: '15px' }}>
-            {entries.map(([k, v]) => <div key={k}><strong>{k}:</strong> {v && typeof v === 'object' ? JSON.stringify(v) : v}</div>)}
-            <br />
-            {json}
-        </div>
-    </React.Fragment>
+    <div style={{ fontSize: '10px', lineHeight: '15px' }}>
+        {entries.map(([k, v]) => <div key={k}><strong>{k}:</strong> {v && typeof v === 'object' ? JSON.stringify(v) : v}</div>)}
+        <br />
+        {json}
+    </div>
 );
 
 export default connect(
     ({trade}) => ({
-        entries       : Object.entries(trade),
-        json          : JSON.stringify(trade).replace(/(:|,)/g, '$1 '),
-        active_symbols: trade.active_symbols,
-
+        entries: Object.entries(trade),
+        json   : JSON.stringify(trade).replace(/(:|,)/g, '$1 '),
     })
 )(Test);
