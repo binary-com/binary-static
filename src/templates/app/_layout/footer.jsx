cbce59db00f86cb5c6cb4adc584bf4b21902a34e
OskarAhl <oskar.ahlroth@gmail.com>
2018-05-17 15:54:28 +0800

    add footer changes to app footer

--- src/templates/app/_layout/footer.jsx
+++ src/templates/app/_layout/footer.jsx
@@ -218,16 +218,16 @@ const FooterNormal = () => (
                                 </a>
                             </div>
                             <div className='gr-7 gr-5-m'>
-                                <a href='https://www.authorisation.mga.org.mt/verification.aspx?lang=EN&company=a5fd1edc-d072-4c26-b0cd-ab3fa0f0cc40&details=1' target='_blank' rel='noopener noreferrer'>
-                                    <img id='lga_icon_footer' className='responsive' src={it.url_for('images/pages/footer/mga-logo-footer.svg')} />
-                                </a>
-                            </div>
-                        </div>
-                        <div data-show='iom, malta, default' className='id-hide gr-row gr-row-align-center-m'>
-                            <div className='gr-9 gr-7-m'>
-                                <a href='https://www.gamstop.co.uk' target='_blank' rel='noopener noreferrer'>
-                                    <img id='gamstop_icon_footer' className='responsive' src={it.url_for('images/pages/footer/gamstop.svg')} />
-                                </a>
+                                <div className='lga-gamstop-icon-container'>
+                                    <a href='https://www.authorisation.mga.org.mt/verification.aspx?lang=EN&company=a5fd1edc-d072-4c26-b0cd-ab3fa0f0cc40&details=1' target='_blank' rel='noopener noreferrer'>
+                                        <img id='lga_icon_footer' className='responsive' src={it.url_for('images/pages/footer/mga-logo-footer.svg')} />
+                                    </a>
+                                    <div data-show='iom, malta, default' className='id-hide'>
+                                        <a href='https://www.gamstop.co.uk' target='_blank' rel='noopener noreferrer'>
+                                            <img id='gamstop_icon_footer' className='responsive' src={it.url_for('images/pages/footer/gamstop.svg')} />
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
 
