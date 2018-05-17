bfb22c427512445cc9a11102cbdae8998b99ebb3
Rustem Kakimov <ruskakimov@gmail.com>
2018-05-17 15:48:53 +0800

    add card list skeleton

---
+++ src/javascript/app_2/components/elements/card_list.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CardList = ({ data_source, Card }) => {
+    return (
+        <div className='card-list'>
+            {
+                data_source.map((transaction, id) => {
+                    <Card className='card-list__card' {...transaction} key={id} />
+                })
+            }
+        </div>
+    );
+};
+
+export default CardList;
