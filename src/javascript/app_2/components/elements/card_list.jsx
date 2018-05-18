1c12291ca8672f8270067f92c5eeb4a75c3a3355
Rustem Kakimov <ruskakimov@gmail.com>
2018-05-18 11:32:00 +0800

    fix card list render function

--- src/javascript/app_2/components/elements/card_list.jsx
+++ src/javascript/app_2/components/elements/card_list.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 
 const CardList = ({ data_source, Card }) => (
     <div className='card-list'>
-        {JSON.stringify(data_source[0])}
-        <Card {...data_source[0]} />
         {
-            data_source.map((transaction, id) => {
-                <Card className='card-list__card' {...transaction} key={id} />;
-            })
+            data_source.map((transaction, id) => (
+                <Card className='card-list__card' {...transaction} key={id} />
+            ))
         }
     </div>
 );
