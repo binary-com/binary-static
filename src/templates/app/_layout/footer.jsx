ff0cff2b9d97d1023066b6bda827df2cda42b6d5
OskarAhl <oskar.ahlroth@gmail.com>
2018-05-22 10:06:19 +0800

    increase logo column size for mobile

--- src/templates/app/_layout/footer.jsx
+++ src/templates/app/_layout/footer.jsx
@@ -212,12 +212,12 @@ const FooterNormal = () => (
                     </p>
                     <div className='gr-3 gr-12-m gr-5-p center-text' id='social-networks'>
                         <div className='gr-row gr-row-align-center-m gr-padding-10 gr-parent id-hide'>
-                            <div className='gr-5 gr-4-m'>
+                            <div className='gr-5'>
                                 <a href='https://www.gov.im/gambling/' target='_blank' rel='noopener noreferrer'>
                                     <img id='iom_icon_footer' className='responsive' src={it.url_for('images/pages/footer/isle-of-man.png')} />
                                 </a>
                             </div>
-                            <div className='gr-7 gr-5-m'>
+                            <div className='gr-7'>
                                 <div className='lga-gamstop-icon-container'>
                                     <a href='https://www.authorisation.mga.org.mt/verification.aspx?lang=EN&company=a5fd1edc-d072-4c26-b0cd-ab3fa0f0cc40&details=1' target='_blank' rel='noopener noreferrer'>
                                         <img id='lga_icon_footer' className='responsive' src={it.url_for('images/pages/footer/mga-logo-footer.svg')} />
