55c13fd4f65c482cb2dc1812ffb40bfbc0bdb4d3
OskarAhl <oskar.ahlroth@gmail.com>
2018-05-23 11:03:05 +0800

    setup basic html + component interface

--- src/javascript/app/pages/user/account/settings/two_factor_authentication.js
+++ src/javascript/app/pages/user/account/settings/two_factor_authentication.js
@@ -1,5 +1,38 @@
 const TwoFactorAuthentication = (() => {
     // two factor auth logic here
+    // loader
+    const onLoad = () => {
+        // initialize form
+        init();
+    };
+
+    const init = () => {
+        // ws - getStatus
+        // enabled state: show form
+        // disabled state:
+        // 1. get key
+        // show signup + form
+        // ws - generateKey
+        // 2. make QR
+        initQR('key');
+    };
+
+    const initQR = (key) => {
+        console.log(key);
+        handler();
+        // create QR code from key
+    };
+
+    const handler = () => {
+        // handle submit of the form
+        // 1. error - show error
+        // 2. enabled - show success + initialize enabled state (?)
+        // 3. disabled - show success + initialize disabled state (?)
+    };
+
+    return {
+        onLoad,
+    };
 })();
 
 module.exports = TwoFactorAuthentication;
