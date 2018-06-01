445966e0c80c2c9eaba764a23e05f03ea553965a
Rustem Kakimov <ruskakimov@gmail.com>
2018-06-01 16:32:22 +0800

    add vertical line (interval) between barriers

--- src/javascript/app/pages/trade/callputspread.js
+++ src/javascript/app/pages/trade/callputspread.js
@@ -1,5 +1,5 @@
 const Callputspread = (() => {
-    let el_slider;
+    let el_slider, el_interval;
 
     const getSliderPath = (x, y, width, height) => {
         const half = height / 2;
@@ -13,10 +13,23 @@ const Callputspread = (() => {
         ];
     };
 
+    const getVerticalIntervalPath = (x, y0, y1, cap_width) => {
+        const half_cap = cap_width / 2;
+        return [
+            'M', x, y0,
+            'h', cap_width,
+            'm', -half_cap, 0,
+            'v', (y1 - y0),
+            'm', -half_cap, 0,
+            'h', cap_width,
+        ];
+    };
+
     const redrawHandler = (e) => {
         window.chart = e.target;
         if (!e.target) return;
         if (el_slider) el_slider.destroy();
+        if (el_interval) el_interval.destroy();
 
         // TODO: vertical line can be series added with .addSeries and removed with .destroy
         // or SVG added with renderer
@@ -24,20 +37,39 @@ const Callputspread = (() => {
         const chart = e.target;
         // TODO: point slider to calculated Y point
         // pass from highcharts.js in params
-        console.log(chart.series);
         const points = chart.series[0].data;
         const last_point = points[points.length - 1];
 
+        const plot_end_x = chart.plotWidth + chart.plotLeft;
+
+        const [high_plot_y, low_plot_y] = chart.series
+            .find(series => series.name === 'barrier_points')
+            .data
+            .map(point => point.plotY);
+
         el_slider = chart.renderer
             .path(getSliderPath(
-                last_point.plotX + chart.plotLeft + 5,
+                chart.plotWidth + chart.plotLeft + 5,
                 last_point.plotY + chart.plotTop,
                 40,
-                15,
+                15
             ))
             .attr({
                 fill: '#FF0000',
                 'stroke-width': 0,
+            })
+            .add();
+
+        el_interval = chart.renderer
+            .path(getVerticalIntervalPath(
+                plot_end_x,
+                high_plot_y + chart.plotTop,
+                low_plot_y + chart.plotTop,
+                10
+            ))
+            .attr({
+                stroke: '#2a3052',
+                'stroke-width': 1,
             })
             .add();
     };
