03ed8874497e95da15c17ba21010bc1a1a95115c
negar <negar254000@gmail.com>
2018-06-12 14:18:47 +0800

    keep fragment

--- src/templates/app/user/profit_table.jsx
+++ src/templates/app/user/profit_table.jsx
@@ -2,23 +2,25 @@ import React from 'react';
 import Loading from '../../_common/components/loading.jsx';
 
 const ProfitTable = () => (
-    <div id='profit-table-container'>
-        <div className='page-title' id='profit-table-title'>
-            <h1>{it.L('Profit Table')}</h1>
-            <p className='notice-msg center-text invisible' id='error-msg' />
-        </div>
-        <div className='gr-padding-10 invisible' id='util_row'>
-            <div className='gr-row gr-gutter-right gr-row-align-right'>
-                <div className='label_form gr-gutter-right'>
-                    <label htmlFor='date_to'>{it.L('End at:')}</label>
-                </div>
-                <div className='gr-gutter-right'>
-                    <input type='text' id='date_to' size='20' readOnly='readonly' />
+    <React.Fragment>
+        <div id='profit-table-container'>
+            <div className='page-title' id='profit-table-title'>
+                <h1>{it.L('Profit Table')}</h1>
+                <p className='notice-msg center-text invisible' id='error-msg' />
+            </div>
+            <div className='gr-padding-10 invisible' id='util_row'>
+                <div className='gr-row gr-gutter-right gr-row-align-right'>
+                    <div className='label_form gr-gutter-right'>
+                        <label htmlFor='date_to'>{it.L('End at:')}</label>
+                    </div>
+                    <div className='gr-gutter-right'>
+                        <input type='text' id='date_to' size='20' readOnly='readonly' />
+                    </div>
                 </div>
             </div>
+            <Loading />
         </div>
-        <Loading />
-    </div>
+    </React.Fragment>
 );
 
 export default ProfitTable;
