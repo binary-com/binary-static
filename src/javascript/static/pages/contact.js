3ff59deae39bb86b4519ce5b50b08735b55792ab
negar <negar254000@gmail.com>
2018-06-13 13:33:26 +0800

    set timeout until next interval and call it then

--- src/javascript/static/pages/contact.js
+++ src/javascript/static/pages/contact.js
@@ -1,16 +1,16 @@
-const moment           = require('moment');
-const Dropdown         = require('@binary-com/binary-style').selectDropdown;
-const setExternalTimer = require('../../app/base/clock').setExternalTimer;
+const moment       = require('moment');
+const Dropdown     = require('@binary-com/binary-style').selectDropdown;
+const BinarySocket = require('../../_common/base/socket_base');
 
 const Contact = (() => {
     let $chat_button,
-        $chat_unavailable;
+        $chat_unavailable,
+        liveChatTimeout;
 
     const onLoad = () => {
         $chat_button      = $('#chat_button');
         $chat_unavailable = $('#live_chat_unavailable');
         showHideLiveChat();
-        setExternalTimer(showHideLiveChat);
 
         Dropdown('#cs_telephone_number');
         $('#cs_telephone_number').on('change.cs', function () {
@@ -19,21 +19,40 @@ const Contact = (() => {
         });
     };
 
+    // mon - fri 22 - 13 GMT
+    // sat - sun 0 - 9 GMT
     const showHideLiveChat = () => {
-        const moment_now        = moment.utc(Math.floor((window.time || 0)));
-        const hour              = moment_now.hour();
-        const is_weekday        = !/^(0|1)$/.test(moment_now.day());
-        const is_chat_available = is_weekday ? (hour >= 22 || hour < 13) : hour < 9;
-        if (is_chat_available) {
-            $chat_button.attr({ href: 'https://binary.desk.com/customer/widget/chats/new', target: '_blank' }).removeClass('button-disabled');
-        } else {
-            $chat_button.attr({ href: '', target: '' }).addClass('button-disabled');
-        }
-        $chat_unavailable.setVisibility(!is_chat_available);
+        BinarySocket.wait('time').then((response) => {
+            const moment_now        = moment.utc((response.time || 0) * 1000);
+            const hour              = moment_now.hour();
+            const day               = moment_now.day(); // 0-6 (0 for sunday and 6 for saturday)
+            const is_weekday        = !/^(0|6)$/.test(day);
+            const is_chat_available = is_weekday ? (hour >= 22 || hour < 13) : hour < 9;
+
+            let next_interval,
+                is_next_interval_tomorrow;
+            if (is_chat_available) {
+                $chat_button.attr({ href: 'https://binary.desk.com/customer/widget/chats/new', target: '_blank' }).removeClass('button-disabled');
+                $chat_unavailable.setVisibility(0);
+
+                next_interval = is_weekday ? 13 : 9;
+                is_next_interval_tomorrow = false;
+            } else {
+                $chat_button.attr({ href: '', target: '' }).addClass('button-disabled');
+                $chat_unavailable.setVisibility(1);
+
+                next_interval = /^(0|1|2|3|4)$/.test(day) ? 22 : 0;
+                is_next_interval_tomorrow = (is_weekday && hour >= 13) || (!is_weekday && hour >= 9);
+            }
+
+            const moment_interval = moment.utc().add(is_next_interval_tomorrow ? 1 : 0, 'day').hour(next_interval).minute(0);
+
+            liveChatTimeout = setTimeout(showHideLiveChat, moment_interval.diff(moment_now, 'seconds') * 1000);
+        });
     };
 
     const onUnload = () => {
-        setExternalTimer(null);
+        clearTimeout(liveChatTimeout);
     };
 
     return {
