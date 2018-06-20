848fd32e12e32721dac53a6211e60a8194f04965
kelly <kelly@binary.com>
2018-06-20 18:26:43 +0800

    hide current time row if contract has ended

--- src/javascript/app/pages/user/view_popup/view_popup.js
+++ src/javascript/app/pages/user/view_popup/view_popup.js
@@ -320,8 +320,9 @@ const ViewPopup = (() => {
     };
 
     const contractEnded = () => {
-        containerSetText('trade_details_current_title', localize(contract.status === 'sold' || (contract.sell_spot_time < contract.date_expiry) ? 'Contract Sold' : 'Contract Expiry'));
+        getElementById('trade_details_live_date').parentNode.setVisibility(0);
 
+        containerSetText('trade_details_current_title', localize(contract.status === 'sold' || (contract.sell_spot_time < contract.date_expiry) ? 'Contract Sold' : 'Contract Expiry'));
         containerSetText('trade_details_indicative_label', localize('Price'));
         if (Lookback.isLookback(contract.contract_type)) {
             containerSetText('trade_details_spot_label', localize('Close'));
