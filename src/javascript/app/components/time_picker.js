d5c8011874bd615f77cf7ab9413cb314a4763ffc
Rustem Kakimov <ruskakimov@gmail.com>
2018-07-02 11:16:16 +0800

    dont modify 55 min option (is passed to endtime if start time min is 50)

--- src/javascript/app/components/time_picker.js
+++ src/javascript/app/components/time_picker.js
@@ -34,7 +34,7 @@ const TimePicker = (() => {
             if (options.useLocalTime) options.minTime = options.minTime.local();
 
             // disable hour without minute options (55 min is the last option)
-            if (options.minTime.minutes() >= 55) {
+            if (options.minTime.minutes() > 55) {
                 options.minTime.minutes(60);
             }
             obj_config.minTime = { hour: parseInt(options.minTime.hour()), minute: parseInt(options.minTime.minute()) };
