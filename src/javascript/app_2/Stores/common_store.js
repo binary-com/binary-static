52c7f9307a008af56c84452ece3c4f50d6446a37
Morteza Tavanarad <morteza@binary.com>
2018-07-11 17:35:33 +0800

    Extended all other store classes from BaseStore class

--- src/javascript/app_2/Stores/common_store.js
+++ src/javascript/app_2/Stores/common_store.js
@@ -1,6 +1,7 @@
 import { observable } from 'mobx';
 import moment         from 'moment';
+import BaseStore      from './base_store';
 
-export default class CommonStore {
+export default class CommonStore extends BaseStore {
     @observable server_time = moment.utc();
 };
