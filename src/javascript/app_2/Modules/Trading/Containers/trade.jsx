c637931ce4807f03845c5346108572b4758e90ba
binary-hamedanchi <mohammad@binary.com>
2018-07-16 14:27:18 +0800

    Clean up

--- src/javascript/app_2/Modules/Trading/Containers/trade.jsx
+++ src/javascript/app_2/Modules/Trading/Containers/trade.jsx
@@ -8,7 +8,6 @@ import PortfolioDrawer from '../../../App/Components/Elements/portfolio_drawer.j
 import { connect }     from '../../../Stores/connect';
 
 class Trade extends React.Component {
-
     componentDidMount() {
         this.props.updateQueryString();
     }
@@ -25,12 +24,12 @@ class Trade extends React.Component {
                     <SmartCharts
                         chart_barriers={this.props.chart_barriers}
                         initial_symbol={this.props.initial_symbol}
-                        is_mobile={this.props.is_mobile}
-                        onSymbolChange={this.props.onSymbolChange}
-                        is_dark_theme={this.props.is_dark_theme}
                         is_asset_enabled={this.props.is_asset_enabled}
                         is_countdown_enabled={this.props.is_countdown_enabled}
+                        is_dark_theme={this.props.is_dark_theme}
+                        is_mobile={this.props.is_mobile}
                         is_position_default={this.props.is_position_default}
+                        onSymbolChange={this.props.onSymbolChange}
                     />
                     <Test />
                 </div>
@@ -48,30 +47,26 @@ Trade.propTypes = {
     is_asset_enabled      : PropTypes.bool,
     is_countdown_enabled  : PropTypes.bool,
     is_dark_theme         : PropTypes.bool,
-    is_position_default   : PropTypes.bool,
     is_mobile             : PropTypes.bool,
     is_portfolio_drawer_on: PropTypes.bool,
-    is_purchase_enabled   : PropTypes.bool,
+    is_position_default   : PropTypes.bool,
     is_trade_enabled      : PropTypes.bool,
     onSymbolChange        : PropTypes.func,
-    server_time           : PropTypes.object,
     updateQueryString     : PropTypes.func,
 };
 
 export default connect(
-    ({ common, modules, ui }) => ({
-        server_time           : common.server_time,
+    ({ modules, ui }) => ({
         chart_barriers        : modules.trade.chart_barriers,
         initial_symbol        : modules.trade.symbol,
-        is_purchase_enabled   : modules.trade.is_purchase_enabled,
         is_trade_enabled      : modules.trade.is_trade_enabled,
         onSymbolChange        : modules.trade.onChange,
         updateQueryString     : modules.trade.updateQueryString,
-        is_dark_theme         : ui.is_dark_mode_on,
-        is_countdown_enabled  : ui.is_chart_countdown_visible,
         is_asset_enabled      : ui.is_chart_asset_info_visible,
-        is_position_default   : ui.is_chart_layout_default,
+        is_countdown_enabled  : ui.is_chart_countdown_visible,
+        is_dark_theme         : ui.is_dark_mode_on,
         is_mobile             : ui.is_mobile,
         is_portfolio_drawer_on: ui.is_portfolio_drawer_on,
+        is_position_default   : ui.is_chart_layout_default,
     })
 )(Trade);
