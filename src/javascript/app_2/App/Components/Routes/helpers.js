761d24665caadb5f8f7c4aad0a13800aa01760ab
binary-hamedanchi <mohammad@binary.com>
2018-07-16 14:33:39 +0800

    Use matchPath to find route by its path

--- src/javascript/app_2/App/Components/Routes/helpers.js
+++ src/javascript/app_2/App/Components/Routes/helpers.js
@@ -1,8 +1,23 @@
+import { matchPath } from 'react-router';
 
 export const normalizePath = (path) => /^\//.test(path) ? path : `/${path || ''}`; // Default to '/'
 
-export const getRouteInfo = (routes, path) => routes.find(r => r.path === normalizePath(path));
+export const findRouteByPath = (path, routes_config) => {
+    let result;
+
+    routes_config.some((route_info) => {
+        if (matchPath(path, route_info)) {
+            result = route_info;
+            return true;
+        } else if (route_info.routes) {
+            result = findRouteByPath(path, route_info.routes);
+            return result;
+        }
+        return false;
+    });
+
+    return result;
+};
 
 export const isRouteVisible = (route, is_logged_in) =>
     !(route && route.is_authenticated && !is_logged_in);
-
