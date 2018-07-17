f76404a7018b16d4b5f3d6d206b1d4bafd9725d7
Rustem Kakimov <ruskakimov@gmail.com>
2018-07-17 17:28:58 +0800

    add more tests

--- src/javascript/app/common/__tests__/content_visibility.js
+++ src/javascript/app/common/__tests__/content_visibility.js
@@ -4,17 +4,31 @@ const { parseAttributeString,
 
 describe('ContentVisibility', () => {
     describe('.parseAttributeString()', () => {
-        it('works for a single included landing company', () => {
+        it('works for a single included', () => {
             expect(parseAttributeString('costarica')).to.deep.equal({
                 is_exclude: false,
                 names     : ['costarica'],
                 mt5_rules : [],
             });
         });
-        it('works for a single excluded landing company', () => {
+        it('works for two included', () => {
+            expect(parseAttributeString('mtcompany, costarica')).to.deep.equal({
+                is_exclude: false,
+                names     : ['mtcompany', 'costarica'],
+                mt5_rules : [],
+            });
+        });
+        it('works for a single excluded', () => {
             expect(parseAttributeString('-costarica')).to.deep.equal({
                 is_exclude: true,
                 names     : ['costarica'],
+                mt5_rules : [],
+            });
+        });
+        it('works for two excluded', () => {
+            expect(parseAttributeString('-default, -costarica')).to.deep.equal({
+                is_exclude: true,
+                names     : ['default', 'costarica'],
                 mt5_rules : [],
             });
         });
