79426e86dc23dffca063bd17eb065ef5ff52fb66
Rustem Kakimov <ruskakimov@gmail.com>
2018-07-18 14:19:16 +0800

    remove container from noactivitymessage

--- src/javascript/app_2/Modules/Statement/Components/no_activity_message.jsx
+++ src/javascript/app_2/Modules/Statement/Components/no_activity_message.jsx
@@ -4,14 +4,12 @@ import NoticeMessage from '../../../App/Components/Elements/notice_message.jsx';
 import { localize }  from '../../../../_common/localize';
 
 const NoActivityMessage = ({ has_selected_date }) => (
-    <div className='container'>
-        <NoticeMessage>
-            {!has_selected_date
-                ? localize('Your account has no trading activity.')
-                : localize('Your account has no trading activity for the selected period.')
-            }
-        </NoticeMessage>
-    </div>
+    <NoticeMessage>
+        {!has_selected_date
+            ? localize('Your account has no trading activity.')
+            : localize('Your account has no trading activity for the selected period.')
+        }
+    </NoticeMessage>
 );
 
 NoActivityMessage.propTypes = {
