f2d6de8770e978206055e6db5c9d70fb2408a1cc
Rustem Kakimov <ruskakimov@gmail.com>
2018-07-19 11:50:46 +0800

    add no statement message

--- src/javascript/app_2/Modules/Statement/Components/empty_statement_message.jsx
+++ src/javascript/app_2/Modules/Statement/Components/empty_statement_message.jsx
@@ -1,13 +1,21 @@
 import PropTypes     from 'prop-types';
 import React         from 'react';
 import { localize }  from '../../../../_common/localize';
+import Button        from '../../../App/Components/Form/button.jsx';
 
 const EmptyStatementMessage = ({ has_selected_date }) => (
-    <div>
-        {!has_selected_date
-            ? localize('Your account has no trading activity.')
-            : localize('Your account has no trading activity for the selected period.')
-        }
+    <div className='statement-empty'>
+        <span className='statement-empty__icon' />
+        <span className='statement-empty__title'>
+            {localize('No Statement')}
+        </span>
+        <span className='statement-empty__text'>
+            {!has_selected_date
+                ? localize('Your account has no trading activity.')
+                : localize('Your account has no trading activity for the selected period.')
+            }
+        </span>
+        <Button className='secondary orange' text={localize('Trade now')} />
     </div>
 );
 
