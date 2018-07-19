a70d97b086275a8cdeb324b2c6b36214fd4b2a47
Rustem Kakimov <ruskakimov@gmail.com>
2018-07-19 11:04:47 +0800

    make app-contents display:flex, so you can make children occupy full height (otherwise impossible since app-contents has no explicit height only min-height)

--- src/javascript/app_2/Modules/Statement/Components/empty_statement_message.jsx
+++ src/javascript/app_2/Modules/Statement/Components/empty_statement_message.jsx
@@ -3,12 +3,12 @@ import React         from 'react';
 import { localize }  from '../../../../_common/localize';
 
 const EmptyStatementMessage = ({ has_selected_date }) => (
-    <p>
+    <div>
         {!has_selected_date
             ? localize('Your account has no trading activity.')
             : localize('Your account has no trading activity for the selected period.')
         }
-    </p>
+    </div>
 );
 
 EmptyStatementMessage.propTypes = {
