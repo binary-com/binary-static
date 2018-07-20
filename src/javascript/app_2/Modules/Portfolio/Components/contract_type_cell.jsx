0b6c4bf9299e74f392ff65342202dcb843332da6
Rustem Kakimov <ruskakimov@gmail.com>
2018-07-20 15:14:18 +0800

    dont pass undefined to localize

--- src/javascript/app_2/Modules/Portfolio/Components/contract_type_cell.jsx
+++ src/javascript/app_2/Modules/Portfolio/Components/contract_type_cell.jsx
@@ -6,7 +6,7 @@ import { localize }              from '../../../../_common/localize';
 const ContractTypeCell = ({ type }) => (
     <div className='contract-type'>
         <i className={`contract-type__icon ${type.toLowerCase()}-icon--light`} />
-        {localize(contract_type_display[type])}
+        {localize(contract_type_display[type] || '')}
     </div>
 );
 
