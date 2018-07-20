0bf10fbe414c9bc880a0bc847d033549d6ae8a8f
zakibinary <zaki@binary.com>
2018-07-20 17:59:57 +0800

    moved close icon to svg component

--- src/javascript/app_2/Modules/Trading/Components/Form/Purchase/MessageBox/message_box.jsx
+++ src/javascript/app_2/Modules/Trading/Components/Form/Purchase/MessageBox/message_box.jsx
@@ -1,5 +1,6 @@
 import PropTypes            from 'prop-types';
 import React                from 'react';
+import CloseButton          from '../../../../../../App/Components/Elements/closebutton.jsx';
 import ErrorBalance         from './error_balance.jsx';
 import ErrorGeneral         from './error_general.jsx';
 import ErrorLogin           from './error_login.jsx';
@@ -29,10 +30,7 @@ const MessageBox = ({ purchase_info }) => {
             {has_error ?
                 <React.Fragment>
                     <div className='close-btn-container'>
-                        <svg className='ic-close' height='24' width='24' xmlns='http://www.w3.org/2000/svg'>
-                            <path d='M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z'/>
-                            <path d='M0 0h24v24H0z' fill='none'/>
-                        </svg>
+                        <CloseButton className='ic-close' />
                     </div>
                     {ErrorComponent}
                 </React.Fragment>
