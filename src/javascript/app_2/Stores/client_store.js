d854a49a7a65ef5944eb3e7c6095123d4146f9b2
kelly <kelly@binary.com>
2018-07-23 16:16:21 +0800

    coerces 'is_logged_in' to boolean

--- src/javascript/app_2/Stores/client_store.js
+++ src/javascript/app_2/Stores/client_store.js
@@ -4,7 +4,7 @@ import Client         from '../../_common/base/client_base';
 
 export default class ClientStore extends BaseStore {
     @observable balance;
-    @observable is_logged_in = Client.isLoggedIn();
+    @observable is_logged_in = !!Client.isLoggedIn();
     @observable loginid      = Client.get('loginid');
     @observable currency     = Client.get('currency') || '';
     @observable upgrade_info = Client.getBasicUpgradeInfo();
