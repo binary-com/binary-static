d2e5164cf9c4785a9ee2de29da9ebcf3cde6f901
binary-hamedanchi <mohammad@binary.com>
2018-07-27 11:14:24 +0800

    Add header icons

--- src/javascript/app_2/Modules/Contract/Components/details_header.jsx
+++ src/javascript/app_2/Modules/Contract/Components/details_header.jsx
@@ -1,18 +1,20 @@
-import classNames      from 'classnames';
-import { observer }    from 'mobx-react';
-import PropTypes       from 'prop-types';
-import React           from 'react';
-import { header_text } from '../../../Stores/Modules/Contract/Constants/ui';
-import { localize }    from '../../../../_common/localize';
+import classNames        from 'classnames';
+import { observer }      from 'mobx-react';
+import PropTypes         from 'prop-types';
+import React             from 'react';
+import { header_config } from '../../../Stores/Modules/Contract/Constants/ui';
+import { localize }      from '../../../../_common/localize';
 
-const DetailsHeader = ({
-    status,
-}) => {
+const DetailsHeader = ({ status }) => {
     const className = classNames('contract-header', status);
-    const title     = localize(header_text[status]);
+    const title     = localize(header_config[status].title);
+    const icon      = header_config[status].icon;
 
     return (
-        <div className={className}>{title}</div>
+        <div className={className}>
+            {icon}
+            {title}
+        </div>
     );
 };
 
--- src/javascript/app_2/Stores/Modules/Contract/Constants/ui.js
+++ src/javascript/app_2/Stores/Modules/Contract/Constants/ui.js
@@ -1,5 +1,9 @@
-export const header_text = {
-    purchased: 'Contract Purchased',
-    won      : 'Contract Won',
-    lost     : 'Contract Lost',
+import React    from 'react';
+import FlagIcon from '../../../../Modules/Contract/Components/Icons/flag.jsx';
+import TickIcon from '../../../../Modules/Contract/Components/Icons/tick.jsx';
+
+export const header_config = {
+    purchased: { title: 'Contract Purchased', icon: <TickIcon /> },
+    won      : { title: 'Contract Won',       icon: <FlagIcon /> },
+    lost     : { title: 'Contract Lost',      icon: <FlagIcon /> },
 };
