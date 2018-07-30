17980a291622d926d657aa189db35ae92aecf601
Rustem Kakimov <ruskakimov@gmail.com>
2018-07-30 13:48:13 +0800

    refactor DataTable

--- src/javascript/app_2/App/Components/Elements/data_table.jsx
+++ src/javascript/app_2/App/Components/Elements/data_table.jsx
@@ -8,6 +8,31 @@ import React                          from 'react';
       2. implement filtering per column
 */
 
+const TableCell = ({ col_index, children }) => (
+    <div className={classNames('table__cell', col_index)}>
+        {children}
+    </div>
+);
+
+const TableRow = ({ columns, row_obj = {}, is_header, is_footer }) => (
+    <div className='table__row'>
+        {columns.map(({ col_index, renderCellContent, title }) => {
+            const cell_value = row_obj[col_index] || '';
+
+            return (
+                <TableCell col_index={col_index} key={col_index}>
+                    {is_header
+                        ? title
+                        : renderCellContent
+                            ? renderCellContent({ cell_value, col_index, row_obj, is_footer })
+                            : cell_value
+                    }
+                </TableCell>
+            );
+        })}
+    </div>
+);
+
 class DataTable extends React.PureComponent {
     componentDidUpdate() {
         this.alignHeader();
@@ -16,49 +41,9 @@ class DataTable extends React.PureComponent {
     alignHeader() {
         // scrollbar inside body table can push content (depending on the browser and if mouse is plugged in)
         if (!this.props.data_source.length) return;
-
         const first_body_row   = this.el_table_body.firstChild;
-        const body_row_width   = first_body_row.offsetWidth;
-        const header_row_width = this.el_header_row.offsetWidth;
-
-        const original_padding_right = parseInt(window.getComputedStyle(first_body_row, null).getPropertyValue('padding-right'));
-        const new_padding_right      = header_row_width - body_row_width + original_padding_right;
-
-        this.el_header_row.style.paddingRight = `${new_padding_right}px`;
-    }
-
-    renderRow(row_obj, is_footer = false, id = 0) {
-        if (!row_obj) return null;
-
-        return (
-            <div className='table__row' key={id}>
-                {this.props.columns.map(({ col_index, renderCellContent }) => {
-                    const cell_value = row_obj[col_index] || '';
-
-                    return (
-                        <div className={classNames('table__cell', col_index)} key={col_index}>
-                            {renderCellContent
-                                ? renderCellContent({ cell_value, col_index, row_obj, is_footer })
-                                : cell_value
-                            }
-                        </div>
-                    );
-                })}
-            </div>
-        );
-    }
-
-    renderBodyRows() {
-        return this.props.data_source
-            .map((row_obj, id) => this.renderRow(row_obj, false, id));
-    }
-
-    renderHeaders() {
-        return this.props.columns.map(col =>
-            <div className={classNames('table__cell', col.col_index)} key={col.col_index}>
-                {col.title}
-            </div>
-        );
+        const scrollbar_offset = this.el_table_head.offsetWidth - first_body_row.offsetWidth;
+        this.el_table_head.style.paddingRight = `${scrollbar_offset}px`;
     }
 
     render() {
@@ -66,14 +51,13 @@ class DataTable extends React.PureComponent {
             children,
             onScroll,
             footer,
+            columns,
         } = this.props;
 
         return (
             <div className='table'>
-                <div className='table__head'>
-                    <div className='table__row' ref={el => { this.el_header_row = el; }}>
-                        {this.renderHeaders()}
-                    </div>
+                <div className='table__head' ref={el => { this.el_table_head = el; }}>
+                    <TableRow columns={columns} is_header />
                 </div>
                 
                 <div
@@ -81,13 +65,15 @@ class DataTable extends React.PureComponent {
                     onScroll={onScroll}
                     ref={el => { this.el_table_body = el; }}
                 >
-                    {this.renderBodyRows()}
+                    {this.props.data_source.map((row_obj, id) =>
+                        <TableRow row_obj={row_obj} columns={columns} key={id} />)
+                    }
                     {children}
                 </div>
 
                 {this.props.footer &&
                     <div className='table__foot'>
-                        {this.renderRow(footer, true)}
+                        <TableRow row_obj={footer} columns={columns} is_footer />)}
                     </div>
                 }
             </div>
