f576a1d62e586c357f35fb8c62e0c6801928d896
Rustem Kakimov <ruskakimov@gmail.com>
2018-07-30 15:00:46 +0800

    remove unused import

--- src/javascript/app_2/App/Components/Elements/DataTable/data_table.jsx
+++ src/javascript/app_2/App/Components/Elements/DataTable/data_table.jsx
@@ -1,4 +1,3 @@
-import classNames                     from 'classnames';
 import { PropTypes as MobxPropTypes } from 'mobx-react';
 import PropTypes                      from 'prop-types';
 import React                          from 'react';
