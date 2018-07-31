ddc6a3bb34199411c84b48e2b6428d444341570e
Rustem Kakimov <ruskakimov@gmail.com>
2018-07-31 15:48:06 +0800

    pass store data to portfolio drawer

--- src/javascript/app_2/App/Components/Elements/PortfolioDrawer/portfolio_drawer.jsx
+++ src/javascript/app_2/App/Components/Elements/PortfolioDrawer/portfolio_drawer.jsx
@@ -5,31 +5,53 @@ import { connect }                    from '../../../../Stores/connect';
 import { localize }                   from '../../../../../_common/localize';
 import PortfolioDrawerCard            from './portfolio_drawer_card.jsx';
 
-const PortfolioDrawer = ({ is_portfolio_drawer_on, toggleDrawer }) => (
-    <div className={classNames('portfolio-drawer', {
-        'portfolio-drawer--open': is_portfolio_drawer_on,
-    })}>
-        <div className='portfolio-drawer__header'>
-            <span className='portfolio-drawer__icon-main ic-portfolio' />
-            <span className='portfolio-drawer__title'>{localize('Portfolio Quick Menu')}</span>
-            <a
-                href='javascript:;'
-                className='portfolio-drawer__icon-close'
-                onClick={toggleDrawer}
-            />
-        </div>
-        <div className='portfolio-drawer__body'>
-            <PortfolioDrawerCard
-                type='ONETOUCH'
-                symbol='Gold/USD'
-                indicative={0.84}
-                remaining_time='00:08:17'
-                currency='USD'
-                status='price-moved-up'
-            />
-        </div>
-    </div>
-);
+class PortfolioDrawer extends React.Component {
+    render() {
+        const {
+            data,
+            error,
+            currency,
+            is_empty,
+            is_loading,
+            is_portfolio_drawer_on,
+            onMount,
+            onUnmount,
+            toggleDrawer,
+        } = this.props;
+
+        return (
+            <div className={classNames('portfolio-drawer', {
+                'portfolio-drawer--open': is_portfolio_drawer_on,
+            })}>
+                <div className='portfolio-drawer__header'>
+                    <span className='portfolio-drawer__icon-main ic-portfolio' />
+                    <span className='portfolio-drawer__title'>{localize('Portfolio Quick Menu')}</span>
+                    <a
+                        href='javascript:;'
+                        className='portfolio-drawer__icon-close'
+                        onClick={toggleDrawer}
+                    />
+                </div>
+                <div className='portfolio-drawer__body'>
+                    {
+                        data.map((portfolio_position, id) => (
+                            <PortfolioDrawerCard
+                                // type='ONETOUCH'
+                                // symbol='Gold/USD'
+                                // indicative={0.84}
+                                // remaining_time='00:08:17'
+                                // currency='USD'
+                                // status='price-moved-up'
+                                key={id}
+                                {...portfolio_position}
+                            />
+                        ))
+                    }
+                </div>
+            </div>
+        );
+    }
+};
 
 PortfolioDrawer.propTypes = {
     is_portfolio_drawer_on: PropTypes.bool,
@@ -37,7 +59,14 @@ PortfolioDrawer.propTypes = {
 };
 
 export default connect(
-    ({ ui }) => ({
+    ({ modules, client, ui }) => ({
+        data                  : modules.portfolio.data_with_remaining_time,
+        is_loading            : modules.portfolio.is_loading,
+        error                 : modules.portfolio.error,
+        is_empty              : modules.portfolio.is_empty,
+        onMount               : modules.portfolio.onMount,
+        onUnmount             : modules.portfolio.onUnmount,
+        currency              : client.currency,
         is_portfolio_drawer_on: ui.is_portfolio_drawer_on,
         toggleDrawer          : ui.togglePortfolioDrawer,
     })
