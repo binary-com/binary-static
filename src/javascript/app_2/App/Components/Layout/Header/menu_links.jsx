f86c03fe2bcef86c3d36ebb3879642e052519364
zakibinary <zaki@binary.com>
2018-08-01 20:50:52 +0800

    header fixes and refactoring

--- src/javascript/app_2/App/Components/Layout/Header/menu_links.jsx
+++ src/javascript/app_2/App/Components/Layout/Header/menu_links.jsx
@@ -13,7 +13,7 @@ const MenuLinks = ({ items }) => (
             <InkBarDiv className='menu-links'>
                 {items.map((item, idx) => (
                     <BinaryLink key={idx} to={item.link_to}>
-                        <span className={item.icon} title={item.text}>{item.text}</span>
+                        <span title={item.text}>{item.icon}{item.text}</span>
                     </BinaryLink>
                 ))}
             </InkBarDiv>
