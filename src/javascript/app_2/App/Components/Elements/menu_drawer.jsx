95f23763498bde928b9961c6838989b917bd8876
zakibinary <zaki@binary.com>
2018-08-03 15:03:05 +0800

    side drawer updates

--- src/javascript/app_2/App/Components/Elements/menu_drawer.jsx
+++ src/javascript/app_2/App/Components/Elements/menu_drawer.jsx
@@ -1,6 +1,7 @@
 import React             from 'react';
-import PerfectScrollbar  from 'react-perfect-scrollbar';
 import { DrawerItem }    from './Drawer';
+// import ToggleButton      from '../Elements/toggle_button.jsx';
+import { IconLogout }    from '../../../Assets/Header/Drawer';
 import { requestLogout } from '../../../Services';
 import Client            from '../../../../_common/base/client_base';
 import { localize }      from '../../../../_common/localize';
@@ -9,27 +10,34 @@ export const MenuDrawer = () => {
     const is_desktop = window.innerWidth > 979;
     return (
         <div className='drawer-items-container'>
-            <PerfectScrollbar>
-                <div className='list-items-container'>
-                    <DrawerItem text={localize('Manage Password')} />
-                    <DrawerItem text={localize('Useful Resources')}/>
-                    <DrawerItem text={localize('Login History')}/>
-                    <hr />
-                    <DrawerItem text={localize('Settings')} link_to='/settings' />
-                    {!is_desktop &&
-                    <React.Fragment>
-                        <DrawerItem text={localize('Purchase Confirmation')} />
-                        <DrawerItem text={localize('Purchase Lock')} />
-                        <DrawerItem text={localize('Dark Theme')} />
-                    </React.Fragment>
-                    }
-                    <hr />
-                    <DrawerItem text={localize('Contact Us')}/>
-                    {Client.isLoggedIn() &&
-                        <DrawerItem text={localize('Logout')} custom_action={requestLogout}/>
-                    }
-                </div>
-            </PerfectScrollbar>
+            <div className='list-items-container'>
+                {/* Hide menu items until pages are ready
+                <DrawerItem text={localize('Manage Password')} />
+                <DrawerItem text={localize('Useful Resources')}/>
+                <DrawerItem text={localize('Login History')}/>
+                <hr />
+                <DrawerItem text={localize('Settings')} link_to='/settings' />
+                */}
+                {!is_desktop &&
+                <React.Fragment>
+                    <DrawerItem text={localize('Purchase Confirmation')} />
+                    <DrawerItem text={localize('Purchase Lock')} />
+                    <DrawerItem text={localize('Dark Theme')} />
+                </React.Fragment>}
+                {/* Same as above
+                <hr />
+                <DrawerItem text={localize('Contact Us')}/>
+                */}
+            </div>
+            <div className='drawer-footer'>
+                {Client.isLoggedIn() &&
+                    <DrawerItem
+                        icon={<IconLogout className='drawer-icon'/>}
+                        text={localize('Logout')}
+                        custom_action={requestLogout}
+                    />
+                }
+            </div>
         </div>
     );
 };
