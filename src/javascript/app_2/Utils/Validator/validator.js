b19dab47959cef222264e19e8753490bad54b250
Morteza Tavanarad <morteza@binary.com>
2018-08-03 15:22:45 +0800

    Added Validation core

---
+++ src/javascript/app_2/Utils/Validator/validator.js
@@ -0,0 +1,93 @@
+import { pre_build_dvrs } from './declarative_validation_rules';
+import Error              from './errors';
+import { localize }       from '../../../_common/localize';
+
+
+class Validator {
+
+    constructor(input, rules) {
+        this.input       = input;
+        this.rules       = rules;
+        this.errors      = new Error();
+        this.error_count = 0;
+    }
+
+    /**
+     * Add failure and error message for given rule
+     *
+     * @param {string} attribute
+     * @param {object} rule
+     */
+    addFailure(attribute, rule) {
+        let message = rule.options.message || pre_build_dvrs[rule.name].message;
+        if (rule.name === 'length') {
+            message = localize(message, [rule.options.min === rule.options.max ? rule.options.min : `${rule.options.min}-${rule.options.max}`]);
+        } else if (rule.name === 'min') {
+            message = localize(message, [rule.options.min]);
+        } else if (rule.name === 'not_equal') {
+            message = localize(message, [localize(rule.options.name1), localize(rule.options.name2)]);
+        } else {
+            message = localize(message);
+        }
+        this.errors.add(attribute, message);
+        this.error_count++;
+    }
+    
+    /**
+     * Runs validator
+     *
+     * @return {boolean} Whether it passes; true = passes, false = fails
+     */
+    check() {
+        
+        Object.keys(this.input).forEach(attribute => {
+            if (!Object.prototype.hasOwnProperty.call(this.rules, attribute)) {
+                return;
+            }
+
+            this.rules[attribute].forEach(rule => {
+                const ruleObject = Validator.getRuleObject(rule);
+
+                if (!ruleObject.validator && typeof ruleObject.validator !== 'function') {
+                    return; 
+                }
+
+                const is_valid = ruleObject.validator(this.input[attribute], ruleObject.options);
+
+                if (!is_valid) {
+                    this.addFailure(attribute, ruleObject);
+                }
+            });
+        });
+        return !this.error_count;
+    }
+    
+    /**
+     * Determine if validation passes
+     * 
+     * @return {boolean}
+     */
+    isPassed() {
+        return this.check();
+    }
+ 
+    /**
+     * Converts the rule array to an object
+     *
+     * @param {array} rule
+     * @return {object}
+     */
+    static getRuleObject(rule) {
+        if (typeof rule === 'string') {
+            return { name: rule };
+        }
+
+        return {
+            name     : rule[0],
+            validator: rule[0] === 'custom' ? rule[1].func : pre_build_dvrs[rule[0]].func,
+            options  : rule[1] || {},
+        };
+    }
+}
+
+export default Validator;
