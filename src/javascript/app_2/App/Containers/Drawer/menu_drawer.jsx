0c0e4b0060722cb7e8a7a4f3a5e6f705b2ccc9f1
zakibinary <zaki@binary.com>
2018-08-05 20:11:54 +0800

    more ui fixes

---
+++ src/javascript/app_2/App/Containers/Drawer/menu_drawer.jsx
@@ -0,0 +1,84 @@
+import React             from 'react';
+import PropTypes         from 'prop-types';
+import { DrawerItem,
+         DrawerToggle }  from '../../Components/Elements/Drawer';
+import { IconLogout }    from '../../../Assets/Header/Drawer';
+import { requestLogout } from '../../../Services';
+import Client            from '../../../../_common/base/client_base';
+import { localize }      from '../../../../_common/localize';
+import { connect }       from '../../../Stores/connect';
+
+const MenuDrawer = ({
+    is_dark_mode,
+    is_purchase_confirmed,
+    is_purchase_locked,
+    toggleDarkMode,
+    togglePurchaseLock,
+    togglePurchaseConfirmation,
+}) => {
+    const is_desktop = window.innerWidth > 979;
+    return (
+        <div className='drawer-items-container'>
+            <div className='list-items-container'>
+                {/* Hide menu items until pages are ready
+                <DrawerItem text={localize('Manage Password')} />
+                <DrawerItem text={localize('Useful Resources')}/>
+                <DrawerItem text={localize('Login History')}/>
+                <hr />
+                <DrawerItem text={localize('Settings')} link_to='/settings' />
+                */}
+                {!is_desktop &&
+                <React.Fragment>
+                    <DrawerToggle
+                        text={localize('Purchase Confirmation')}
+                        toggle={togglePurchaseConfirmation}
+                        to_toggle={is_purchase_confirmed}
+                    />
+                    <DrawerToggle
+                        text={localize('Purchase Lock')}
+                        toggle={togglePurchaseLock}
+                        to_toggle={is_purchase_locked}
+                    />
+                    <DrawerToggle
+                        text={localize('Dark Theme')}
+                        toggle={toggleDarkMode}
+                        to_toggle={is_dark_mode}
+                    />
+                </React.Fragment>}
+                {/* Same as above
+                <hr />
+                <DrawerItem text={localize('Contact Us')}/>
+                */}
+            </div>
+            <div className='drawer-footer'>
+                {Client.isLoggedIn() &&
+                    <DrawerItem
+                        icon={<IconLogout className='drawer-icon'/>}
+                        text={localize('Logout')}
+                        custom_action={requestLogout}
+                    />
+                }
+            </div>
+        </div>
+    );
+};
+
+MenuDrawer.propTypes = {
+    is_dark_mode              : PropTypes.bool,
+    is_purchase_confirmed     : PropTypes.bool,
+    is_purchase_locked        : PropTypes.bool,
+    toggleDarkMode            : PropTypes.func,
+    togglePurchaseConfirmation: PropTypes.func,
+    togglePurchaseLock        : PropTypes.func,
+};
+
+const menu_drawer_component = connect(({ ui }) => ({
+    is_dark_mode              : ui.is_dark_mode_on,
+    is_purchase_confirmed     : ui.is_purchase_confirm_on,
+    is_purchase_locked        : ui.is_purchase_lock_on,
+    toggleDarkMode            : ui.toggleDarkMode,
+    togglePurchaseConfirmation: ui.togglePurchaseConfirmation,
+    togglePurchaseLock        : ui.togglePurchaseLock,
+}))(MenuDrawer);
+
+export { menu_drawer_component as MenuDrawer };
