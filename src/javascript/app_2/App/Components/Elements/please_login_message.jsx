d940bf5931ddeb3ba3105ef7c7294aaad862eb9d
Rustem Kakimov <ruskakimov@gmail.com>
2018-08-06 15:31:11 +0800

    style links, remove unused styles, style please login message

--- src/javascript/app_2/App/Components/Elements/please_login_message.jsx
+++ src/javascript/app_2/App/Components/Elements/please_login_message.jsx
@@ -1,12 +1,25 @@
 import React        from 'react';
 import { localize } from '../../../../_common/localize';
 
-const PleaseLoginMessage = ({ onLogin }) => (
-    <div className='login-message-wrapper'>
-        <div className='message'>
-            <a href='javascript:;' onClick={onLogin}>{localize('Please login to view this page.')}</a>
+const PleaseLoginMessage = ({ onLogin, onSignup }) => (
+    <div className='please-login'>
+        <div className='please-login__message'>
+            {localize('Please')}
+            &nbsp;
+            <a href='javascript:;' onClick={onLogin}>
+                {localize('log in')}
+            </a>
+            &nbsp;
+            {localize('or')}
+            &nbsp;
+            <a href='javascript:;' onClick={onSignup}>
+                {localize('sign up')}
+            </a>
+            &nbsp;
+            {localize('to view this page.')}
         </div>
     </div>
 );
 
 export default PleaseLoginMessage;
+
