f14f556ee84b4a12c50a64959388faf8f2239b9a
ashkan <ashkan.nemati@gmail.com>
2018-08-13 11:19:18 +0800

    remove my appid

--- src/javascript/_common/base/gtm.js
+++ src/javascript/_common/base/gtm.js
@@ -10,7 +10,7 @@ const State          = require('../storage').State;
 const getAppId       = require('../../config').getAppId;
 
 const GTM = (() => {
-    const isGtmApplicable = () => (/^(1|1098|1108)$/.test(getAppId()));
+    const isGtmApplicable = () => (/^(1|1098)$/.test(getAppId()));
 
     const gtmDataLayerInfo = (data) => {
         const data_layer_info = {
