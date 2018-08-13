193725e9fbea1151b575b56e57b6e7388c2572c3
Rustem Kakimov <ruskakimov@gmail.com>
2018-08-13 11:25:29 +0800

    break up test

--- src/javascript/app_2/Utils/Language/__tests__/fill_template.spec.js
+++ src/javascript/app_2/Utils/Language/__tests__/fill_template.spec.js
@@ -5,9 +5,13 @@ describe('fillTemplate', () => {
     it('works for string without tags', () => {
         expect(fillTemplate('hello world')).to.eql(['hello world']);
     });
-    it('works for template with one tag replaced with string', () => {
+    it('works for template with 1 tag at the end replaced with string', () => {
         expect(fillTemplate('there is no [_1]', { '1': 'spoon' })).to.eql(['there is no ', 'spoon']);
+    });
+    it('works for template with 1 tag at the start replaced with string', () => {
         expect(fillTemplate('[_1] is no spoon', { '1': 'there' })).to.eql(['there', ' is no spoon']);
+    });
+    it('works for template with 1 tag in the middle replaced with string', () => {
         expect(fillTemplate('there is [_1] spoon', { '1': 'no' })).to.eql(['there is ', 'no', ' spoon']);
     });
 });
