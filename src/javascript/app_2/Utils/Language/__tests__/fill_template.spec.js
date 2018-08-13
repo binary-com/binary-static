040a3a9bbefa78e1348ba562e1f30498af8a6b7f
Rustem Kakimov <ruskakimov@gmail.com>
2018-08-13 15:06:02 +0800

    add test with a mix of tags

--- src/javascript/app_2/Utils/Language/__tests__/fill_template.spec.js
+++ src/javascript/app_2/Utils/Language/__tests__/fill_template.spec.js
@@ -38,7 +38,7 @@ describe('fillTemplate', () => {
         expect(fillTemplate('[_1] think therefore [_1] am', { '1': 'I' }))
             .to.eql(['I think therefore I am']);
     });
-    it('works for 2 tags replaced with component', () => {
+    it('works for 2 tags replaced with components', () => {
         const img = <img src='images/avatar.jpg' />;
         expect(fillTemplate('[_1] think therefore [_1] am', { '1': img }))
             .to.eql([img, ' think therefore ', img, ' am']);
@@ -56,5 +56,12 @@ describe('fillTemplate', () => {
         expect(result)
             .to.have.lengthOf(5)
             .to.eql(['These ', <Link key={result[1].key}>violent delights</Link>, ' have ', <Link key={result[3].key}>violent ends</Link>, '.']);
+    });
+    it('works for a mix of tags replaced with strings and components', () => {
+        const Link = ({ children }) => <a className='link' href='javascript:;'>{ children }</a>;
+        const result = fillTemplate('Please [_1]log in[_2] or [_3]sign up[_2] to view [_4] page.', { '1_2': <Link />, '3_2': <Link />, '4': 'statement' });
+        expect(result)
+            .to.have.lengthOf(5)
+            .to.eql(['Please ', <Link key={result[1].key}>log in</Link>, ' or ', <Link key={result[3].key}>sign up</Link>, ' to view statement page.']);
     });
 });
