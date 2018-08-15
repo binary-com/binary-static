fb94cbc8d6e63d6e4857879a76e7134a53b3d2a7
negar <negar254000@gmail.com>
2018-08-15 16:29:34 +0800

    only show MAM account opening to costarica for now

--- src/templates/app/user/metatrader.jsx
+++ src/templates/app/user/metatrader.jsx
@@ -122,7 +122,7 @@ const Metatrader = () => (
                             <a href='javascript:;' className='act_new_account new-account center-text invisible'>
                                 <span>{it.L('Create MetaTrader 5 account')}</span>
                             </a>
-                            <a href='javascript:;' className='act_new_account_mam new-account center-text invisible'>
+                            <a href='javascript:;' className='act_new_account_mam center-text invisible'>
                                 <span>{it.L('Create MAM account')}</span>
                             </a>
                             <a href='javascript:;' className='act_cashier has-account center-text invisible'>
