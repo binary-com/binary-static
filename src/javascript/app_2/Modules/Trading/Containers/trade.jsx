8540e6f37e0a8025fe46b5bf298401c83b5d5209
zakibinary <zaki@binary.com>
2018-08-15 14:05:48 +0800

    updates for dark theme, added transition group to infobox

--- src/javascript/app_2/Modules/Trading/Containers/trade.jsx
+++ src/javascript/app_2/Modules/Trading/Containers/trade.jsx
@@ -1,13 +1,13 @@
-import PropTypes              from 'prop-types';
-import React                  from 'react';
-import { CSSTransition } from 'react-transition-group';
-import Test                   from './test.jsx';
-import FormLayout             from '../Components/Form/form_layout.jsx';
-import InfoBox                from '../../Contract/Components/InfoBox';
-import ContractDetails        from '../../Contract/Containers/contract_details.jsx';
-import SmartChart             from '../../SmartChart';
-import { connect }            from '../../../Stores/connect';
-import { getPropertyValue }   from '../../../../_common/utility';
+import PropTypes            from 'prop-types';
+import React                from 'react';
+import { CSSTransition }    from 'react-transition-group';
+import Test                 from './test.jsx';
+import FormLayout           from '../Components/Form/form_layout.jsx';
+import InfoBox              from '../../Contract/Components/InfoBox';
+import ContractDetails      from '../../Contract/Containers/contract_details.jsx';
+import SmartChart           from '../../SmartChart';
+import { connect }          from '../../../Stores/connect';
+import { getPropertyValue } from '../../../../_common/utility';
 
 class Trade extends React.Component {
     componentDidMount() {
@@ -18,7 +18,16 @@ class Trade extends React.Component {
         const contract_id = getPropertyValue(this.props.purchase_info, ['buy', 'contract_id']);
         const form_wrapper_class = this.props.is_mobile ? 'mobile-wrapper' : 'sidebar-container desktop-only';
         const InfoBoxComponent = this.props.is_contract_mode ?
-            <InfoBox contract_info={this.props.contract_info} /> : null;
+            <CSSTransition
+                in={this.props.is_contract_mode}
+                timeout={400}
+                classNames='info-box'
+                unmountOnExit
+            >
+                <InfoBox contract_info={this.props.contract_info} />
+            </CSSTransition>
+        :
+            null;
 
         return (
             <div id='trade_container' className='trade-container'>
