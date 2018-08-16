b410e5ac24d6594dada06d61f74293984ca5eb0f
Rustem Kakimov <ruskakimov@gmail.com>
2018-08-16 13:10:23 +0800

    get rid of No Portfolio/No Statement titles

--- src/javascript/app_2/Modules/Portfolio/Components/empty_portfolio_message.jsx
+++ src/javascript/app_2/Modules/Portfolio/Components/empty_portfolio_message.jsx
@@ -6,7 +6,6 @@ const EmptyPortfolioMessage = () => (
     // TODO: combine with statement component, once design is final
     <div className='portfolio-empty'>
         <IconPortfolio className='portfolio-empty__icon' />
-        <span className='portfolio-empty__title'>{localize('No Portfolio')}</span>
         <span className='portfolio-empty__text'>{localize('No open positions.')}</span>
     </div>
 );
