774c72718011b3b90004fe1d890555543a73ff21
Negar Naghshbandi <negar254000@gmail.com>
2018-08-20 15:27:10 +0800

    Revert "negar/show_mam"

--- src/templates/app/user/metatrader.jsx
+++ src/templates/app/user/metatrader.jsx
@@ -122,6 +122,7 @@ const Metatrader = () => (
                             <a href='javascript:;' className='act_new_account new-account center-text invisible'>
                                 <span>{it.L('Create MetaTrader 5 account')}</span>
                             </a>
+                            {/* toEnableMAM: add 'new-account' to className */}
                             <a href='javascript:;' className='act_new_account_mam center-text invisible'>
                                 <span>{it.L('Create MAM account')}</span>
                             </a>
