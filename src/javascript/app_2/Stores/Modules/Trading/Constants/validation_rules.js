ed61d1e1754523c10508ae1aea26452c9b17b003
Morteza Tavanarad <morteza@binary.com>
2018-08-23 18:03:05 +0800

    put validation rules in priority order

--- src/javascript/app_2/Stores/Modules/Trading/Constants/validation_rules.js
+++ src/javascript/app_2/Stores/Modules/Trading/Constants/validation_rules.js
@@ -1,17 +1,17 @@
 const validation_rules = {
     amount: [
-        ['number' , {min: 0, type: 'float'}],
         ['req'    , {message: 'The amount is a required field.'}],
+        ['number' , {min: 0, type: 'float'}],
     ],
     barrier_1: [
+        ['req'    , {condition: store => store.barrier_count, message: 'The barrier is a required field.'}],
         ['barrier', {condition: store => store.contract_expiry_type !== 'daily' && store.barrier_count}],
         ['number' , {condition: store => store.contract_expiry_type === 'daily' && store.barrier_count, type: 'float'}],
-        ['req'    , {condition: store => store.barrier_count, message: 'The barrier is a required field.'}],
     ],
     barrier_2: [
+        ['req'    , {condition: store => store.barrier_count, message: 'The barrier is a required field.'}],
         ['barrier', {condition: store => store.contract_expiry_type !== 'daily' && store.barrier_count}],
         ['number' , {condition: store => store.contract_expiry_type === 'daily' && store.barrier_count, type: 'float'}],
-        ['req'    , {condition: store => store.barrier_count, message: 'The barrier is a required field.'}],
     ],
     duration: [
         ['req'    , {message: 'The duration is a required field.'}],
