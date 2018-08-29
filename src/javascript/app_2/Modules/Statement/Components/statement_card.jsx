38b621f229191a906dca817a5e61889517a31f8e
Rustem Kakimov <ruskakimov@gmail.com>
2018-08-29 10:40:07 +0800

    remove broken link from statement cards as well

--- src/javascript/app_2/Modules/Statement/Components/statement_card.jsx
+++ src/javascript/app_2/Modules/Statement/Components/statement_card.jsx
@@ -14,36 +14,51 @@ const StatementCard = ({
     id,
     payout,
     refid,
-}) => (
-    <NavLink className={classNames('statement-card', className)} activeClassName='active' to={getContractPath(id)}>
-        <div className='statement-card__header'>
-            <span className='statement-card__date'>{date}</span>
-            <span className='statement-card__refid'>
-                {refid}
-            </span>
-        </div>
-        <div className='statement-card__body'>
-            <div className='statement-card__desc'>{desc}</div>
-            <div className='statement-card__row'>
-                <div className={`statement-card__cell statement-card__amount statement-card__amount--${action.toLowerCase()}`}>
-                    <span className='statement-card__cell-text'>
-                        {amount}
-                    </span>
-                </div>
-                <div className='statement-card__cell statement-card__payout'>
-                    <span className='statement-card__cell-text'>
-                        {payout}
-                    </span>
-                </div>
-                <div className='statement-card__cell statement-card__balance'>
-                    <span className='statement-card__cell-text'>
-                        {balance}
-                    </span>
+}) => {
+    const content = (
+        <React.Fragment>
+            <div className='statement-card__header'>
+                <span className='statement-card__date'>{date}</span>
+                <span className='statement-card__refid'>
+                    {refid}
+                </span>
+            </div>
+            <div className='statement-card__body'>
+                <div className='statement-card__desc'>{desc}</div>
+                <div className='statement-card__row'>
+                    <div className={`statement-card__cell statement-card__amount statement-card__amount--${action.toLowerCase()}`}>
+                        <span className='statement-card__cell-text'>
+                            {amount}
+                        </span>
+                    </div>
+                    <div className='statement-card__cell statement-card__payout'>
+                        <span className='statement-card__cell-text'>
+                            {payout}
+                        </span>
+                    </div>
+                    <div className='statement-card__cell statement-card__balance'>
+                        <span className='statement-card__cell-text'>
+                            {balance}
+                        </span>
+                    </div>
                 </div>
             </div>
-        </div>
-    </NavLink>
-);
+        </React.Fragment>
+    );
+
+    const class_name = classNames('statement-card', className);
+
+    return (
+        id ?
+            <NavLink className={class_name} activeClassName='active' to={getContractPath(id)}>
+                {content}
+            </NavLink>
+            :
+            <div className={class_name}>
+                {content}
+            </div>
+    );
+};
 
 StatementCard.propTypes = {
     action   : PropTypes.string,
