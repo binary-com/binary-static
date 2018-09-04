689bc6e1c3a960892d150ff6873d927689ee21c4
Rustem Kakimov <ruskakimov@gmail.com>
2018-09-04 12:50:30 +0800

    refactor var name

--- src/javascript/app_2/App/Components/Form/input_field.jsx
+++ src/javascript/app_2/App/Components/Form/input_field.jsx
@@ -42,7 +42,7 @@ const InputField = ({
             const has_zero_at_end = new RegExp(`^${signed_regex}(\\d+)?\\.(\\d+)?[0]+$`)
                 .test(e.target.value);
             
-            const scientific_notation = /e/.test(`${+e.target.value}`);
+            const is_scientific_notation = /e/.test(`${+e.target.value}`);
 
             if (max_length && fractional_digits) {
                 has_valid_length = new RegExp(`^${signed_regex}(\\d{0,${max_length}})(\\.\\d{0,${fractional_digits}})?$`)
@@ -50,7 +50,7 @@ const InputField = ({
             }
 
             if ((is_number || is_empty) && has_valid_length ) {
-                e.target.value = is_empty || is_signed || has_zero_at_end || scientific_notation
+                e.target.value = is_empty || is_signed || has_zero_at_end || is_scientific_notation
                     ? e.target.value
                     : +e.target.value;
             } else if (!is_not_completed_number) {
