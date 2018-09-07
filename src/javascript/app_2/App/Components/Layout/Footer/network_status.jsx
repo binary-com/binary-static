75dc8ceaff609c85efffa0d59bbbcf5154358389
zakibinary <zaki@binary.com>
2018-09-07 13:48:37 +0800

    fixed import and tooltip localized message

--- src/javascript/app_2/App/Components/Layout/Footer/network_status.jsx
+++ src/javascript/app_2/App/Components/Layout/Footer/network_status.jsx
@@ -1,12 +1,12 @@
 import classNames   from 'classnames';
 import PropTypes    from 'prop-types';
 import React        from 'react';
+import { localize } from '_common/localize';
 import Tooltip      from '../../Elements/tooltip.jsx';
-import { localize } from '../../../../../_common/localize';
 
 const NetworkStatus = ({ status }) => (
     <div className='network-status-wrapper'>
-        <Tooltip alignment='top' message={localize('Network status: [_1]', [(status.tooltip || 'Connecting')])}>
+        <Tooltip alignment='top' message={localize('Network status: [_1]', [(status.tooltip || 'Connecting to server')])}>
             <div className={classNames('network-status-circle', status.class)} />
         </Tooltip>
     </div>
