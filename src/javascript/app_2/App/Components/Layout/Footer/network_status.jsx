0e273d4826bc269124f77623f31267903761f33c
Zaki <31648832+zakibinary@users.noreply.github.com>
2018-09-08 21:44:19 +0800

    localize string

--- src/javascript/app_2/App/Components/Layout/Footer/network_status.jsx
+++ src/javascript/app_2/App/Components/Layout/Footer/network_status.jsx
@@ -6,7 +6,7 @@ import Tooltip      from '../../Elements/tooltip.jsx';
 
 const NetworkStatus = ({ status }) => (
     <div className='network-status-wrapper'>
-        <Tooltip alignment='top' message={localize('Network status: [_1]', [(status.tooltip || 'Connecting to server')])}>
+        <Tooltip alignment='top' message={localize('Network status: [_1]', [(status.tooltip || localize('Connecting to server'))])}>
             <div className={classNames('network-status-circle', status.class)} />
         </Tooltip>
     </div>
