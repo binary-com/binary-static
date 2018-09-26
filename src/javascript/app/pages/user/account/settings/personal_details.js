cddf166e6020e06e64df253016c05e4f5d941a77
Kelly Anyi <kelly@binary.com>
2018-09-26 14:56:36 +0800

    fix check

--- src/javascript/app/pages/user/account/settings/personal_details.js
+++ src/javascript/app/pages/user/account/settings/personal_details.js
@@ -229,10 +229,11 @@ const PersonalDetails = (() => {
                     return;
                 }
                 const get_settings    = data.get_settings;
-                const has_required_mt = (State.getResponse('landing_company.config.tax_details_required') !== 1 ?
-                    get_settings.citizen // only check Citizen if tax details are not required
+                const has_required_mt = (+State.getResponse('landing_company.config.tax_details_required') === 1 ?
+                    (get_settings.tax_residence && get_settings.tax_identification_number && get_settings.citizen)
                     :
-                    (get_settings.tax_residence && get_settings.tax_identification_number && get_settings.citizen));
+                    get_settings.citizen // only check Citizen if tax details are not required
+                );
                 if (redirect_url && has_required_mt) {
                     localStorage.removeItem('personal_details_redirect');
                     $.scrollTo($('h1#heading'), 500, { offset: -10 });
