5b7168de76f6361b7b802dae6bade5db143594fe
Mohammadreza Ghorbani <isthereanybody@gmail.com>
2018-10-16 10:31:32 +0330

    conditionally load devtools

--- src/javascript/app_2/App/app.jsx
+++ src/javascript/app_2/App/app.jsx
@@ -1,7 +1,6 @@
 import PropTypes                   from 'prop-types';
 import React                       from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import DevTools                    from 'mobx-react-devtools';
 import getBaseName                 from 'Utils/URL/base_name';
 import { MobxProvider }            from 'Stores/connect';
 import ErrorBoundary               from './Components/Elements/Errors/error_boundary.jsx';
@@ -11,6 +10,12 @@ import Footer                      from './Containers/Layout/footer.jsx';
 import Header                      from './Containers/Layout/header.jsx';
 import ThemeWrapper                from './Containers/Layout/theme_wrapper.jsx';
 import Routes                      from './Containers/Routes/routes.jsx';
+
+// Conditionally loading mobx only on development builds.
+// see https://github.com/mobxjs/mobx-react-devtools/issues/66
+const { Fragment } = React;
+const DevTools = process.env.NODE_ENV !== 'production' ? require('mobx-react-devtools').default : Fragment;
+
 
 const App = ({ root_store }) => (
     <Router basename={getBaseName()}>
