952635e4419d172b75dd8d17051d334f13db1f9b
Rustem Kakimov <ruskakimov@gmail.com>
2018-10-22 17:42:06 +0800

    interview popup + assets

--- src/templates/static/platforms.jsx
+++ src/templates/static/platforms.jsx
@@ -74,6 +74,31 @@ const DownloadApp = ({ image }) => (
 
 const Platform = () => (
     <div className='static_full'>
+
+        <div id='interview_popup_container'>
+            <div className='popup'>
+                <div className='popup__head'>
+                    <img className='popup__present_img' src={it.url_for('images/interview_popup/present.svg')} alt='$30 present' />
+                    <h1>Do you have 40 minutes for an interview?</h1>
+                </div>
+                <div className='popup__body'>
+                    <h2>Earn $30 to trade on binary.com</h2>
+                    <p>
+                    We’re looking for users of binary.com to participate in a <b>40-minute video or phone interview</b>. To qualify, just answer a few short questions. If selected, you will receive an email or a phone call from one of our researchers.
+                    </p>
+                    <div className='popup__options'>
+                        <div className='popup__secondary_options'>
+                            <a href='#' id='interview_no_thanks'>No thanks</a>
+                            <a href='#' id='interview_ask_later'>Ask me later</a>
+                        </div>
+                        <a href='#' id='interview_interested' className='button' target='_blank' rel='noopener noreferrer'>
+                            <span>I'm interested</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <div className='container'>
             <h1 className='center-text'>{it.L('Platforms')}</h1>
             <p className='center-text gr-padding-20 gr-parent'>{it.L('Explore all the trading platforms, apps, and tools we offer')}</p>
