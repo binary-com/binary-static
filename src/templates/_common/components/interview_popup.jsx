e53ded4460fc8a1c66b38235b094773bde7d7b13
Rustem Kakimov <ruskakimov@gmail.com>
2018-10-24 16:15:10 +0800

    localization

--- src/templates/_common/components/interview_popup.jsx
+++ src/templates/_common/components/interview_popup.jsx
@@ -5,23 +5,21 @@ const InterviewPopup = () => (
         <div className='popup'>
             <div className='popup__head'>
                 <img className='popup__present_img' src={it.url_for('images/interview_popup/present.svg')} alt='$30 present' />
-                <h1>Do you have 40 minutes for an interview?</h1>
+                <h1>{it.L('Do you have 40 minutes for an interview?')}</h1>
             </div>
             <div className='popup__body'>
-                <h2>Earn $30 to trade on binary.com</h2>
+                <h2>{it.L('Earn $30 to trade on [_1]', it.website_name.toLowerCase())}</h2>
                 <p>
-                    We’re looking for users of binary.com to participate in a <b>40-minute video or phone interview</b>.
-                    To qualify, just answer a few short questions.
-                    If selected, you will receive an email or a phone call from one of our researchers.
+                    {it.L('We’re looking for users of [_1] to participate in a [_2]40-minute video or phone interview[_3]. To qualify, just answer a few short questions. If selected, you will receive an email or a phone call from one of our researchers.', it.website_name.toLowerCase(), '<strong>', '</strong>')}
                 </p>
                 <div className='popup__options'>
                     <div className='popup__secondary_options'>
-                        <a href={'javascript:void(0);'} id='interview_no_thanks'>No thanks</a>
+                        <a href={'javascript:void(0);'} id='interview_no_thanks'>{it.L('No thanks')}</a>
                         <span className='popup__separator' />
-                        <a href={'javascript:void(0);'} id='interview_ask_later'>Ask me later</a>
+                        <a href={'javascript:void(0);'} id='interview_ask_later'>{it.L('Ask me later')}</a>
                     </div>
                     <a href={'javascript:void(0);'} id='interview_interested' className='button' target='_blank' rel='noopener noreferrer'>
-                        <span>I’m interested</span>
+                        <span>{it.L('I’m interested')}</span>
                     </a>
                 </div>
             </div>
