6dd3097e817e3f54a302ba2d8f6ba3b0f73bc165
Kelly Anyi <kelly@binary.com>
2018-10-31 16:07:25 +0800

    add check for tax_details_required

--- src/javascript/app/pages/user/account/settings/personal_details.js
+++ src/javascript/app/pages/user/account/settings/personal_details.js
@@ -220,8 +220,10 @@ const PersonalDetails = (() => {
                     BinaryPjax.loadPreviousUrl();
                     return;
                 }
-                const get_settings    = data.get_settings;
-                const has_required_mt = (/real_vanuatu_(standard|advanced)/.test(redirect_url) ?
+                const get_settings        = data.get_settings;
+                const is_tax_req          = +State.getResponse('landing_company.config.tax_details_required') === 1;
+                const is_for_mt_financial = /real_vanuatu_(standard|advanced)/.test(redirect_url);
+                const has_required_mt = ((is_for_mt_financial && is_tax_req) ?
                     (get_settings.tax_residence && get_settings.tax_identification_number && get_settings.citizen)
                     :
                     get_settings.citizen // only check Citizen if user selects mt volatility account
