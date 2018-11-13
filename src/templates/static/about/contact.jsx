891041afbc6857ee406e564b9cf4582b0394faeb
Kelly Anyi <kelly@binary.com>
2018-11-13 10:56:23 +0800

    reduce icon size

--- src/templates/static/about/contact.jsx
+++ src/templates/static/about/contact.jsx
@@ -26,7 +26,7 @@ const Contact = () => (
                     <div className='gr-7 gr-8-t gr-12-p gr-12-m gr-centered gr-padding-30'>
                         <h2 className='center-text'>{it.L('Can\'t find what you\'re searching for?')}</h2>
                         <div className='gr-row phone-container'>
-                            <div className='gr-col gr-4-m'>
+                            <div className='gr-col gr-4-m center-text'>
                                 <img className='responsive' src={it.url_for('images/pages/contact/contact-icon.svg')} />
                             </div>
                             <div className='gr-col gr-10-m gr-adapt number-container'>
