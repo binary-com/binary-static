a14038d8967578af2c85212f1395ecbaad84efd6
Mohammadreza Ghorbani <isthereanybody@gmail.com>
2018-11-13 10:55:26 +0330

    Use shortcode instead of loginid

--- src/javascript/app_2/App/Middlewares/is_client_allowed_to_visit.js
+++ src/javascript/app_2/App/Middlewares/is_client_allowed_to_visit.js
@@ -1,4 +1,4 @@
 import Client from '_common/base/client_base';
 
 export const isClientAllowedToVisit = () => !Client.isLoggedIn() || Client.get('is_virtual') ||
-    /^CR/.test(Client.get('loginid'));
+    Client.get('landing_company_shortcode') === 'costarica';
