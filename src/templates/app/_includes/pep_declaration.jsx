e127c3cd7659e7085f87bf6d5e61750441a3a9b6
Aaron Imming <aaron@binary.com>
2018-11-15 11:50:18 +0800

    update PEP declaration text

--- src/templates/app/_includes/pep_declaration.jsx
+++ src/templates/app/_includes/pep_declaration.jsx
@@ -11,7 +11,7 @@ const PepDeclaration = () => (
             <input id='not_pep' type='checkbox' />
             <label htmlFor='not_pep'>
                 {it.L('I acknowledge that I am not a politically exposed person (PEP).')}&nbsp;
-                <span data-balloon-length='xlarge' data-balloon={it.L('A politically exposed person (PEP) is an individual who is or has been entrusted with a prominent public function. Family members and close associates of such individuals are also considered as PEPs.<br>A PEP who has ceased to be entrusted with a prominent public function for at least 12 months no longer qualifies as a PEP.')}>
+                <span data-balloon-length='xlarge' data-balloon={it.L('A politically exposed person (PEP) is an individual who is or has been entrusted with a prominent public function. Family members and close associates of such individuals are also considered as PEPs. A PEP who has ceased to be entrusted with a prominent public function for at least 12 months no longer qualifies as a PEP.')}>
                     {it.L('What is this?')}
                 </span>
             </label>
