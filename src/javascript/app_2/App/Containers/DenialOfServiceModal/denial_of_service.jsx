c4ad6a3aed6dc5ef1517fc3bdeb15bdea8de635f
Mohammadreza Ghorbani <isthereanybody@gmail.com>
2018-11-19 11:19:13 +0330

    Refactor props

--- src/javascript/app_2/App/Containers/DenialOfServiceModal/denial_of_service.jsx
+++ src/javascript/app_2/App/Containers/DenialOfServiceModal/denial_of_service.jsx
@@ -1,10 +1,10 @@
-import React                from 'react';
-import PropTypes            from 'prop-types';
-import { localize }         from '_common/localize';
-import URL                  from '_common/url';
-import FullPageModal        from 'App/Components/Elements/FullPageModal/full_page_modal.jsx';
-import { connect }          from 'Stores/connect';
-import Localize             from 'App/Components/Elements/localize.jsx';
+import React         from 'react';
+import PropTypes     from 'prop-types';
+import { localize }  from '_common/localize';
+import URL           from '_common/url';
+import FullPageModal from 'App/Components/Elements/FullPageModal/full_page_modal.jsx';
+import { connect }   from 'Stores/connect';
+import Localize      from 'App/Components/Elements/localize.jsx';
 
 const onConfirm = async (client) => {
     await client.switchAccount(client.getAccountOfType('virtual').loginid);
@@ -28,7 +28,8 @@ const DenialOfServiceModal = ({ client, is_visible }) => (
 );
 
 DenialOfServiceModal.propTypes = {
-    show: PropTypes.bool,
+    client    : PropTypes.object,
+    is_visible: PropTypes.bool,
 };
 
 const denial_of_service = connect(
