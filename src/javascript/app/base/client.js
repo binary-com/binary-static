774b0141600f5e3f4d9062c4bb4691bf13362301
zakibinary <zaki@binary.com>
2018-11-21 14:17:57 +0800

    do not display notification message for logged out users

--- src/javascript/app/base/client.js
+++ src/javascript/app/base/client.js
@@ -51,6 +51,7 @@ const Client = (() => {
                 el.setVisibility(0);
             }, '', el_section);
             getElementById('topbar-msg').setVisibility(0);
+            getElementById('msg_notification').setVisibility(0);
             getElementById('menu-top').classList.remove('smaller-font', 'top-nav-menu');
 
             applyToAllElements('.client_logged_out', (el) => {
