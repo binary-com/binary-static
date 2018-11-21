45e0e86c531d554dc9b07b06c0d0e3d6ad3bd674
zakibinary <zaki@binary.com>
2018-11-21 11:27:04 +0800

    sorting order update

--- src/javascript/app/base/client.js
+++ src/javascript/app/base/client.js
@@ -50,11 +50,12 @@ const Client = (() => {
             applyToAllElements('#client-logged-in', (el) => {
                 el.setVisibility(0);
             }, '', el_section);
+            getElementById('topbar-msg').setVisibility(0);
+            getElementById('menu-top').classList.remove('smaller-font', 'top-nav-menu');
+
             applyToAllElements('.client_logged_out', (el) => {
                 el.setVisibility(1);
             }, '', el_section);
-            getElementById('topbar-msg').setVisibility(0);
-            getElementById('menu-top').classList.remove('smaller-font', 'top-nav-menu');
             topbar_class.add(primary_bg_color_dark);
             topbar_class.remove(secondary_bg_color);
         }
