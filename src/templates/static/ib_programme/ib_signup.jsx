948de10f1bbd8022a51a84dcb0f02da4df87fd48
zakibinary <zaki@binary.com>
2018-11-22 18:03:59 +0800

    split forex and metals into forex in ib-signup page

--- src/templates/static/ib_programme/ib_signup.jsx
+++ src/templates/static/ib_programme/ib_signup.jsx
@@ -54,11 +54,12 @@ const Signup = () => (
                         ],
                         tbody: [
                             [
-                                { text: `${it.L('Forex')}*`, attributes: { rowSpan: 2 } },
+                                { text: `${it.L('Forex & Metals')}*`, attributes: { rowSpan: 1 } },
                                 { text: it.L('Standard') },
                                 { text: it.L('[_1] per lot', '10') },
                             ],
                             [
+                                { text: `${it.L('Forex')}*`, attributes: { rowSpan: 1 } },
                                 { text: it.L('Advanced') },
                                 { text: it.L('[_1] per lot', '5') },
                             ],
@@ -67,6 +68,7 @@ const Signup = () => (
                                 { text: it.L('Standard') },
                                 { text: it.L('[_1] per lot', '0.3%') },
                             ],
+                            [],
                             [
                                 { text: `${it.L('Volatility Indices')}***`, attributes: { rowSpan: 8 } },
                                 { text: it.L('Vol 10') },
