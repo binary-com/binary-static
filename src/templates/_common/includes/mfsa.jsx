9aa83f670670e1c634ec72bb36fdd12fe5781cb0
Aaron Imming <aaron@binary.com>
2018-11-23 15:34:19 +0800

    Restore original MFSA component and remove style

--- src/templates/_common/includes/mfsa.jsx
+++ src/templates/_common/includes/mfsa.jsx
@@ -7,8 +7,7 @@ const MFSA = () => {
     return (
         <div style={style} className='container mfsa_message'>
             <div className='notice-msg center-text gr-parent gr-child'>
-                <p>{it.L('In the EU, financial binary options are only available to professional investors.')}&nbsp;<a className='client_logged_in invisible' href={it.url_for('user/settings/professional')}>{it.L('Apply now as a professional investor')}</a>
-                </p>
+                <p>{it.L('In the EU, financial binary options are only available to professional investors.')}</p>
             </div>
         </div>
     );
