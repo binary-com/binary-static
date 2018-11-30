5f8447f98757e88bdd1f5bc4cf31be4a5d2624b1
zakibinary <zaki@binary.com>
2018-11-30 10:37:15 +0800

    add switch to binary routes

--- src/javascript/app_2/App/Components/Routes/binary_routes.jsx
+++ src/javascript/app_2/App/Components/Routes/binary_routes.jsx
@@ -1,9 +1,16 @@
 import React              from 'react';
+import { Switch }         from 'react-router-dom';
 import RouteWithSubRoutes from './route_with_sub_routes.jsx';
 import getRoutesConfig    from '../../Constants/routes_config';
 
-const BinaryRoutes = () => getRoutesConfig().map((route, idx) => (
-    <RouteWithSubRoutes key={idx} {...route} />
-));
+const BinaryRoutes = () => (
+    <Switch>
+        {
+            getRoutesConfig().map((route, idx) => (
+                <RouteWithSubRoutes key={idx} {...route} />
+            ))
+        }
+    </Switch>
+);
 
 export default BinaryRoutes;
