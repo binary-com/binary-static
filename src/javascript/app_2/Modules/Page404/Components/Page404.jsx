a4ec654eddb3056c56f74b7c1b8dccad7e424de8
zakibinary <zaki@binary.com>
2018-11-30 11:34:00 +0800

    added route link to trade button

--- src/javascript/app_2/Modules/Page404/Components/Page404.jsx
+++ src/javascript/app_2/Modules/Page404/Components/Page404.jsx
@@ -1,8 +1,10 @@
-import React        from 'react';
-import { localize } from '_common/localize';
-import { Icon404 }  from './Icon404.jsx';
-import Button       from '../../../App/Components/Form/button.jsx';
-import ErrorBox     from '../../../App/Components/Elements/ErrorBox';
+import React          from 'react';
+import { Link }       from 'react-router-dom';
+import { routes }     from 'Constants/index';
+import { localize }   from '_common/localize';
+import { Icon404 }    from './Icon404.jsx';
+import Button         from '../../../App/Components/Form/button.jsx';
+import ErrorBox       from '../../../App/Components/Elements/ErrorBox';
 
 const Page404 = () => (
     <div className='page-error-container'>
@@ -11,11 +13,13 @@ const Page404 = () => (
             icon={<Icon404 />}
             message={localize('Sorry, we couldn\'t find the page you are looking for.')}
         >
-            <Button
-                className='secondary orange'
-                has_effect
-                text={localize('Go to trade page')}
-            />
+            <Link to={routes.trade}>
+                <Button
+                    className='secondary orange'
+                    has_effect
+                    text={localize('Go to trade page')}
+                />
+            </Link>
         </ErrorBox>
     </div>
 );
