d007a7649e0d0f3fa303baa62b74acb71d00ad7e
nazaninreihani <nazanin.reyh@gmail.com>
2018-12-05 11:36:48 -0500

    add format_response test

---
+++ src/javascript/app_2/Stores/Modules/Portfolio/Helpers/__tests__/format_response.js
@@ -0,0 +1,28 @@
+import { expect }                  from 'chai';
+import React                       from 'react';
+import { formatPortfolioPosition } from '../format_response';
+
+describe('formatPortfolioPosition', () => {
+    const portfolio_pos = {
+        contract_id   : 1234,
+        transaction_id: 5678,
+        contract_type : 'ASIANU',
+        longcode      : 'test \n test \n test',
+        payout        : 3500.1,
+        buy_price     : 2500.5,
+        expiry_time   : 123456789,
+    };
+
+    it('should return an object with values in object passed as argumebt', () => {
+        expect(formatPortfolioPosition(portfolio_pos)).to.eql({
+            reference  : +5678,
+            type       : 'ASIANU',
+            details    :'test <br /> test <br /> test',
+            payout     : 3500.1,
+            purchase   : 2500.5,
+            expiry_time: 123456789,
+            id         : 1234,
+            indicative : 0,
+        });
+    });
+});
