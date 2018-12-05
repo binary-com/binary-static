bb02bf952a4b68e2c11ac45de317a28250928c6c
nazaninreihani <nazanin.reyh@gmail.com>
2018-12-05 18:00:31 -0500

    add the logic to not call localize repeatedly

--- src/javascript/app_2/Stores/Modules/Contract/Helpers/details.js
+++ src/javascript/app_2/Stores/Modules/Contract/Helpers/details.js
@@ -5,6 +5,30 @@ import Money                      from 'App/Components/Elements/money.jsx';
 import { getContractTypeDisplay } from 'Constants/contract';
 import { toGMTFormat }            from 'Utils/Date';
 
+const detailsInfoProperties = (() => {
+    let details_info_properties;
+
+    const initDetailsInfoProperties = () => ({
+        contract_type : localize('Contract Type'),
+        start_time    : localize('Start Time'),
+        entry_spot    : localize('Entry Spot'),
+        purchase_price: localize('Purchase Price'),
+        end_time      : localize('End Time'),
+        exit_spot     : localize('Exit Spot'),
+        exit_spot_time: localize('Exit Spot Time'),
+        payout        : localize('Payout'),
+    });
+
+    return {
+        get: () => {
+            if (!details_info_properties) {
+                details_info_properties = initDetailsInfoProperties();
+            }
+            return details_info_properties;
+        },
+    };
+})();
+
 export const getDetailsInfo = (contract_info) => {
     const {
         buy_price,
@@ -15,19 +39,20 @@ export const getDetailsInfo = (contract_info) => {
         sell_time,
     } = contract_info;
 
+    const details_info_properties = detailsInfoProperties.get();
+
     // if a forward starting contract was sold before starting
     // API will still send entry spot when start time is passed
     // we will hide it from our side
     const is_sold_before_start = sell_time && +sell_time < +date_start;
     const txt_start_time       = date_start && toGMTFormat(+date_start * 1000);
     const txt_entry_spot       = entry_spot && !is_sold_before_start ? addComma(entry_spot) : '-';
-
-    // TODO: don't localize on every call
+    
     return {
-        [localize('Contract Type')] : getContractTypeDisplay()[contract_type],
-        [localize('Start Time')]    : txt_start_time,
-        [localize('Entry Spot')]    : txt_entry_spot,
-        [localize('Purchase Price')]: <Money amount={buy_price} currency={currency} />,
+        [details_info_properties.contract_type] : getContractTypeDisplay()[contract_type],
+        [details_info_properties.start_time]    : txt_start_time,
+        [details_info_properties.entry_spot]    : txt_entry_spot,
+        [details_info_properties.purchase_price]: <Money amount={buy_price} currency={currency} />,
     };
 };
 
@@ -42,16 +67,17 @@ export const getDetailsExpiry = (store) => {
         is_user_sold,
     } = store;
 
-    // TODO: don't localize on every call
+    const details_info_properties = detailsInfoProperties.get();
+
     // for user sold contracts sell spot can get updated when the next tick becomes available
     // so we only show end time instead of any spot information
     return {
         ...(is_user_sold ? {
-            [localize('End Time')]: contract_info.date_expiry && toGMTFormat(+contract_info.date_expiry * 1000),
+            [details_info_properties.end_time]: contract_info.date_expiry && toGMTFormat(+contract_info.date_expiry * 1000),
         } : {
-            [localize('Exit Spot')]     : end_spot ? addComma(end_spot) : '-',
-            [localize('Exit Spot Time')]: end_spot_time ? toGMTFormat(+end_spot_time * 1000) : '-',
+            [details_info_properties.exit_spot]     : end_spot ? addComma(end_spot) : '-',
+            [details_info_properties.exit_spot_time]: end_spot_time ? toGMTFormat(+end_spot_time * 1000) : '-',
         }),
-        [localize('Payout')]: <Money amount={indicative_price} currency={contract_info.currency} />,
+        [details_info_properties.payout]: <Money amount={indicative_price} currency={contract_info.currency} />,
     };
 };
