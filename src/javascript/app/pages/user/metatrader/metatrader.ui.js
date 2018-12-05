d9bb785c36ee30b3625cad81236d357e54d5546b
zakibinary <zaki@binary.com>
2018-12-06 00:42:22 +0800

    filter vanuatu_advanced and labuan_standard

--- src/javascript/app/pages/user/metatrader/metatrader.ui.js
+++ src/javascript/app/pages/user/metatrader/metatrader.ui.js
@@ -449,6 +449,9 @@ const MetaTraderUI = (() => {
         Object.keys(accounts_info)
             .filter(acc_type => !accounts_info[acc_type].is_demo && accounts_info[acc_type].mt5_account_type !== 'mamm') // toEnableMAM: remove second check
             .forEach((acc_type) => {
+                if (/labuan_standard|vanuatu_advanced/.test(acc_type)) {
+                    return;
+                }
                 count++;
                 const $acc  = $acc_template.clone();
                 const type  = acc_type.split('_').slice(1).join('_');
