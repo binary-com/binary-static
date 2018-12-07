b7a8d38bf1f7a709af02f2d49d3168f4a4595f15
Amam Mustofa <mustofa@binary.com>
2018-12-07 09:36:34 +0800

    change optimize container id to tag manager container id

--- src/templates/_common/includes/tracker.jsx
+++ src/templates/_common/includes/tracker.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Tracker = () => (
     <React.Fragment>
         <style dangerouslySetInnerHTML={{__html: `.async-hide { opacity: 0 }`}}></style>
-        <script data-cfasync="false" dangerouslySetInnerHTML={{ __html: `(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};(a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;})(window,document.documentElement,'async-hide','dataLayer',4000,{'GTM-KPSSR9K':true});`}}></script>
+        <script data-cfasync="false" dangerouslySetInnerHTML={{ __html: `(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};(a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;})(window,document.documentElement,'async-hide','dataLayer',4000,{'GTM-MZWFF7':true});`}}></script>
     </React.Fragment>
 );
 export default Tracker;
