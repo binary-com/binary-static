569c8a9be6c3ce79805ec8e778825408bb35219c
Amam Mustofa <mustofa@binary.com>
2018-12-17 17:20:36 +0800

    add test

---
+++ src/javascript/app_2/Services/Helpers/__tests__/process_data.js
@@ -0,0 +1,22 @@
+import { expect } from 'chai';
+import { getOauthAppsObject } from '../process_data.js';
+import BinarySocket from '_common/base/socket_base';
+
+describe('getOauthAppsObject', () => {
+    it('Expects default oauth object when there is no arguments', () => {
+        expect(getOauthAppsObject()).to.eql({ 2: 'Binary.com Autoexpiry' });
+    })
+
+    it('Expects correct value when arguments passed', () => {
+            const response = new Promise(async (resolve, reject) => {
+                await BinarySocket.send({ oauth_apps: 1 }).then(result => {
+                    resolve(result);
+                }).catch(error => {
+                    reject(error);
+                });
+            }) 
+            response.then(result => {
+                expect(Object.size(getOauthAppsObject(result))).to.be.above(1);
+            });
+    })
+})
