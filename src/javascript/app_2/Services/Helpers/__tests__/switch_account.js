0262d6fa13557a472e7f2f98d2e8590a7805df02
Amam Mustofa <mustofa@binary.com>
2018-12-18 17:27:39 +0800

    add more test

--- src/javascript/app_2/Services/Helpers/__tests__/switch_account.js
+++ src/javascript/app_2/Services/Helpers/__tests__/switch_account.js
@@ -1,14 +1,17 @@
 import { expect } from 'chai';
-import 'mock-local-storage'
 import { switchAccount } from '../switch_account.js';
 import Client from '_common/base/client_base';
-import 'mock-local-storage'
+import 'mock-local-storage';
 
 describe('switchAccount', () => {
+
+    it('expects empty if no arguments passed', () => {
+        expect(switchAccount()).to.be.empty;
+    })
+
     it('expects item set on the session storage', () => {
         switchAccount('2FZQzLdbhLpjLzf');
 
-        expect(sessionStorage.getItem('active_tab')).to.eql('1');
         expect(Client.get('cashier_confirmed')).to.eql(0);
         expect(Client.get('accepted_bch')).to.eql(0);
         expect(Client.get('loginid')).to.eql('2FZQzLdbhLpjLzf');
