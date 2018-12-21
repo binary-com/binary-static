6860f4dc78b97aa0e13290250eff75d2318e5849
nazaninreihani <nazanin.reyh@gmail.com>
2018-12-20 20:58:10 -0500

    route_with_sub_routes test added

---
+++ src/javascript/app_2/App/Components/Routes/__tests__/route_with_sub_routes.spec.js
@@ -0,0 +1,21 @@
+import React                  from 'react';
+import { expect }             from 'chai';
+import { configure, shallow } from 'enzyme';
+import Adapter                from 'enzyme-adapter-react-16';
+import RouteWithSubRoutes     from '../route_with_sub_routes.jsx';
+import { Redirect }           from "react-router-dom";
+
+configure({ adapter: new Adapter() });
+
+describe('<RouteWithSubRoutes />', () => {
+    it('should render one <RouteWithSubRoutes /> component', () => {
+        const wrapper = shallow(<RouteWithSubRoutes />);
+        expect(wrapper).to.have.length(1);
+    });
+    it('should have props as passed as route (component is redirect)', () => {
+        const route = { path: '/', component: Redirect, title: '', exact: true, to: '/root' };
+        const wrapper = shallow(<RouteWithSubRoutes {...route} />);
+        expect(wrapper.prop('exact')).to.equal(true);
+        expect(wrapper.prop('path')).to.equal('/');
+    });
+});
