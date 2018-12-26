aa365574cdca4b4d51086fec921d234c5f177b4f
Aaron Imming <aaron@binary.com>
2018-12-26 15:56:15 +0800

    Only split array for tax_residence setting

--- src/javascript/app/pages/user/account/settings/personal_details.js
+++ src/javascript/app/pages/user/account/settings/personal_details.js
@@ -118,11 +118,18 @@ const PersonalDetails = (() => {
                     if (element_key.type === 'checkbox') {
                         element_key.checked = !!get_settings[key];
                     } else if (!should_update_value) { // for all non (checkbox|select|text) elements
-                        const localized_text = get_settings[key] ? get_settings[key]
-                            .split(',')
-                            .map((value) => (document.querySelector(`#${key} option[value="${value}"]`) || {}).innerText || value)
-                            .join(', ') : '-';
-                        CommonFunctions.elementInnerHtml(element_key, localized_text);
+                        const getOptionText = (value) =>  (document.querySelector(`#${key} option[value="${value}"]`) || {}).innerText || value;
+                        let localized_text;
+                        if (key === 'tax_residence') { // Resolve comma-separated country codes to country names
+                            localized_text = get_settings[key] ? get_settings[key]
+                                .split(',')
+                                .map((value) => getOptionText(value))
+                                .join(', ') : '';
+                        } else {
+                            localized_text = getOptionText(get_settings[key]);
+                        }
+                        CommonFunctions.elementInnerHtml(element_key, localized_text || '-');
+                        
                     }
                     if (should_update_value || should_show_label) {
                         // if should show label, set the value of the non-label so that it doesn't count as missing information
