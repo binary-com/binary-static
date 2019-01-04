0c4dd7cd48c8f8529ff2f4735e2f01a0e9b75873
Aaron Imming <aaron@binary.com>
2019-01-04 09:41:59 +0800

    Maintain data-balloon-length attribute for consistent tooltip size

--- src/javascript/app/pages/trade/event.js
+++ src/javascript/app/pages/trade/event.js
@@ -314,13 +314,13 @@ const TradingEvents = (() => {
             }
             const id        = this.getAttribute('data-purchase-id');
             const ask_price = this.getAttribute('data-ask-price');
+            const params    = { buy: id, price: ask_price, passthrough: {} };
 
-            const params = { buy: id, price: ask_price, passthrough: {} };
             Object.keys(this.attributes).forEach(function(attr) {
                 if (attr && this.attributes[attr] && this.attributes[attr].name) {
-                    if (/^data-balloon/.test(this.attributes[attr].name)) {
+                    if (/^data-balloon$/.test(this.attributes[attr].name)) { // Force removal of attribute for Safari
                         this.removeAttribute(this.attributes[attr].name);
-                    } else {
+                    } else if (!/data-balloon/.test(this.attributes[attr].name)) { // Do not send tooltip data
                         const m = this.attributes[attr].name.match(/data-(.+)/);
                         if (m && m[1] && m[1] !== 'purchase-id' && m[1] !== 'passthrough') {
                             params.passthrough[m[1]] = this.attributes[attr].value;
