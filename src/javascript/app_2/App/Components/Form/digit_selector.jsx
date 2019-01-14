a776b1b7a35d8ff77718c98a3c47b7d8f6795876
negar <negar254000@gmail.com>
2019-01-14 15:18:20 +0800

    change from class

--- src/javascript/app_2/App/Components/Form/digit_selector.jsx
+++ src/javascript/app_2/App/Components/Form/digit_selector.jsx
@@ -1,35 +1,32 @@
 import PropTypes from 'prop-types';
 import React     from 'react';
 
-class DigitSelector extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleSelect = this.handleSelect.bind(this);
-    }
-
-    handleSelect(item) {
-        if (+item.target.getAttribute('data-value') !== this.props.selected_digit) {
-            this.props.onChange({ target: { name: this.props.name, value: +item.target.getAttribute('data-value') } });
+const DigitSelector = ({
+    name,
+    onChange,
+    selected_digit,
+}) => {
+    const handleSelect = (item) => {
+        if (+item.target.getAttribute('data-value') !== selected_digit) {
+            onChange({ target: { name, value: +item.target.getAttribute('data-value') } });
         }
-    }
+    };
 
-    render() {
-        return (
-            <div className='digit-selector center-text'>
-                {[...Array(10).keys()].map(i =>
-                    <div
-                        key={i}
-                        className={`digit-selection${this.props.selected_digit === i ? ' selected' : ''}`}
-                        data-value={i}
-                        onClick={this.handleSelect}
-                    >
-                        {i}
-                    </div>
-                )}
-            </div>
-        );
-    }
-}
+    return (
+        <div className='digit-selector center-text'>
+            {[...Array(10).keys()].map(i =>
+                <div
+                    key={i}
+                    className={`digit-selection${selected_digit === i ? ' selected' : ''}`}
+                    data-value={i}
+                    onClick={handleSelect}
+                >
+                    {i}
+                </div>
+            )}
+        </div>
+    );
+};
 
 DigitSelector.propTypes = {
     name          : PropTypes.string,
