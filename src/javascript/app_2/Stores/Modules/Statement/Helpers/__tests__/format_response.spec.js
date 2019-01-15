f0c961f4b94ab3942d68e5e4b5e9cf0a4901ec6b
brandon <26451183+ahkuma@users.noreply.github.com>
2019-01-15 14:01:03 +0800

    update tests

--- src/javascript/app_2/Stores/Modules/Statement/Helpers/__tests__/format_response.spec.js
+++ src/javascript/app_2/Stores/Modules/Statement/Helpers/__tests__/format_response.spec.js
@@ -2,31 +2,38 @@ import { expect }                       from 'chai';
 import { formatStatementTransaction }   from '../format_response.js';
 
 describe('formatStatementTransaction', () => {
+    const constants = {
+        id: 1234,
+        action_type: 'Buy',
+    }
+
+    const currency = 'USD';
     let transaction = {
-        action_type     : 'Buy',
+        action_type     : constant.action_type,
         transaction_time: 123456789,
-        transaction_id  : 1234,
+        transaction_id  : constant.id,
         payout          : 1000,
         amount          : 2000,
         balance_after   : 3000,
         longcode        : 'test \n test \n test',
-        contract_id     : 1234,
-        app_id          : 1234,
+        contract_id     : constant.id,
+        app_id          : constant.id,
     };
-    const currency = 'USD';
+
+    let expected_result = {
+        action  : constant.action_type,
+        date    : '1973-11-29\n21:33:09 GMT',
+        refid   : constant.id,
+        payout  : '1,000.00',
+        amount  : '2,000.00',
+        balance : '3,000.00',
+        desc    : 'test <br /> test <br /> test',
+        id      : constant.id,
+        app_id  : constant.id,
+    }
 
     it('should return an object with values of object passed as argument', () => {
-        expect(formatStatementTransaction(transaction, currency)).to.eql({
-            action  : 'Buy',
-            date    : '1973-11-29\n21:33:09 GMT',
-            refid   : 1234,
-            payout  : '1,000.00',
-            amount  : '2,000.00',
-            balance : '3,000.00',
-            desc    : 'test <br /> test <br /> test',
-            id      : 1234,
-            app_id  : 1234,
-        });
+        expect(formatStatementTransaction(transaction, currency)).to.eql(expected_result);
     });
 
     it('should return payout, amount and balance as -', () => {
@@ -34,17 +41,11 @@ describe('formatStatementTransaction', () => {
         transaction.amount = NaN;
         transaction.balance_after = NaN;
 
-        expect(formatStatementTransaction(transaction, currency)).to.eql({
-            action  : 'Buy',
-            date    : '1973-11-29\n21:33:09 GMT',
-            refid   : 1234,
-            payout  : '-',
-            amount  : '-',
-            balance : '-',
-            desc    : 'test <br /> test <br /> test',
-            id      : 1234,
-            app_id  : 1234,
-        })
+        expected_result.payout = '-';
+        expected_result.amount = '-';
+        expected_result.balance = '-';
+
+        expect(formatStatementTransaction(transaction, currency)).to.eql(expected_result);
     });
 
 });
--- src/javascript/app_2/Utils/URL/__tests__/url_helper.spec.js
+++ src/javascript/app_2/Utils/URL/__tests__/url_helper.spec.js
@@ -1,17 +1,14 @@
 import { expect }   from 'chai';
+import { setURL }                       from '../../tests_common.js';
 import URLHelper    from '../url_helper';
 
 describe('URLHelper', () => {
-    let url;
-    beforeEach('Setting up url and params', () => {
-        url = 'https://www.binary.com/en/trading.html?currency=USD&market=forex';
-    });
+    // We don't use setQueryParam because we are testing it.
+    const url = setURL(`${Url.websiteUrl()}en/home.html`) + '?currency=USD&market=forex';
 
     describe('.getQueryParams', () => {
-        let params;
-        beforeEach('Setting up params', () => {
-            params = URLHelper.getQueryParams(url);
-        });
+        const params = URLHelper.getQueryParams(url);
+
         it('should return query parameters', () => {
             expect(params.get('currency')).to.eql('USD');
             expect(params.get('market')).to.eql('forex');
@@ -22,21 +19,19 @@ describe('URLHelper', () => {
     });
 
     describe('.setQueryParam', () => {
-        let params;
-        beforeEach('Setting up params', () => {
-            params = {
-                'currency': 'MYR',
-                'market': 'forex',
-            }
-        });
+        const params = {
+            'currency': 'JPY',
+            'market': 'forex',
+        };
+
         it('should return an object', () => {
             expect(URLHelper.setQueryParam(params, url)).to.be.a('object');
         });
-        it('should return an object with key "currency" to the value of "MYR"', () => {
-            expect(URLHelper.setQueryParam(params, url).searchParams.get('currency')).to.eql('MYR');
+        it('should return an object currency key with the value of params.currency ', () => {
+            expect(URLHelper.setQueryParam(params, url).searchParams.get('currency')).to.eql(params.currency);
         });
-        it('should return an object with key "market" to the value of "forex"', () => {
-            expect(URLHelper.setQueryParam(params, url).searchParams.get('market')).to.eql('forex');
+        it('should return an object market key witht he value of params.market', () => {
+            expect(URLHelper.setQueryParam(params, url).searchParams.get('market')).to.eql(params.currency);
         });
 
     });
