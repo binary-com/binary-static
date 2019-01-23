3be01b7677dd296bf80c92467db686ad9a2a13f0
Amam Mustofa <mustofa@binary.com>
2019-01-23 16:29:45 +0800

    handling status notif display

--- src/javascript/app/base/footer.js
+++ src/javascript/app/base/footer.js
@@ -31,65 +31,77 @@ const Footer = (() => {
     };
 
     // by default elevio is 8px above bottom of page, and scrollup is 18px above elevio
-    const adjustNotifElevioAndScrollup = (elevio_height = 8, scrollup_height = 18, status_height = 8) => {
+    const adjustElevioAndScrollup = (elevio_height = 8, scrollup_height = 18) => {
         const $elevio_button = $('#_elev_io ._6byvm');
         const $scrollup = $('#scrollup');
-        const $status_notification = $('#status_notification');
-        
+
         $elevio_button.attr('style', `bottom: ${elevio_height}px !important`);
-        $status_notification.attr('style', `bottom: ${status_height}px !important`);
         $scrollup.attr('style', `bottom: ${$elevio_button.height() + scrollup_height}px`);
     };
 
     const clearDialogMessage = () => {
         const $dialog_notification = $('#dialog_notification');
         const el_footer = getElementById('footer');
+        const $status_notification = $('#status_notification');
+        const gap_to_notification = 8;
 
+        $status_notification.css('bottom', `${gap_to_notification}px`);
         el_footer.style.paddingBottom = '0px';
         $dialog_notification.slideUp(200);
-        adjustNotifElevioAndScrollup();
+        adjustElevioAndScrollup();
     };
 
     const displayDialogMessage = () => {
         BinarySocket.wait('website_status', 'authorize', 'landing_company').then(() => {
             if (isEuCountry()) {
                 const $dialog_notification = $('#dialog_notification');
+                const $status_notification = $('#status_notification');
                 const el_dialog_notification_accept = getElementById('dialog_notification_accept');
                 const el_footer = getElementById('footer');
                 const gap_dialog_to_elevio = 30;
                 const gap_elevio_to_scrollup = 10;
+                const gap_to_notification = 8;
+
+                if ($status_notification.css('display') !== 'none') {
+                    $status_notification.css('bottom', `${$dialog_notification.height()}px`);
+                }
 
                 $dialog_notification.css('display', 'flex');
                 el_footer.style.paddingBottom = `${$dialog_notification.height()}px`;
-                adjustNotifElevioAndScrollup($dialog_notification.height() + gap_dialog_to_elevio,
-                    $dialog_notification.height() + gap_dialog_to_elevio + gap_elevio_to_scrollup,
-                    $dialog_notification.height() + gap_dialog_to_elevio);
+                adjustElevioAndScrollup($dialog_notification.height() + gap_dialog_to_elevio,
+                    $dialog_notification.height() + gap_dialog_to_elevio + gap_elevio_to_scrollup);
 
                 el_dialog_notification_accept
                     .addEventListener('click', () => {
-                        adjustNotifElevioAndScrollup();
+                        adjustElevioAndScrollup();
                         $dialog_notification.slideUp(200);
                         el_footer.style.paddingBottom = '0px';
+                        $status_notification.css('bottom', `${gap_to_notification}px`);
                         Cookies.set('CookieConsent', 1);
                     });
                 window.addEventListener('resize', () => {
-                    adjustNotifElevioAndScrollup($dialog_notification.height() + gap_dialog_to_elevio,
+                    adjustElevioAndScrollup($dialog_notification.height() + gap_dialog_to_elevio,
                         $dialog_notification.height() + gap_dialog_to_elevio + gap_elevio_to_scrollup);
                 });
             }
         });
     };
-    
+
     const displayNotification = (message) => {
         BinarySocket.wait('time').then((response) => {
             const notification_storage = LocalStore.getObject('status_notification');
+            const $dialog_notification = $('#dialog_notification');
+            const $status_notification = $('#status_notification');
             const time_difference = (parseInt(response.time) - (parseInt(notification_storage.close_time) || 0));
             const required_difference = 30 * 60;
-            
+
             if (time_difference > required_difference || notification_storage.message !== message) {
                 const $status_message_text = $('#status_notification_text');
                 const $close_icon = $('#status_notification_close');
-                const $status_notification = $('#status_notification');
+
+                if ($dialog_notification.css('display') !== 'none') {
+                    $status_notification.css('bottom', `${$dialog_notification.height()}px`);
+                }
 
                 $status_notification.css('display', 'flex');
                 $status_message_text.html(message);
