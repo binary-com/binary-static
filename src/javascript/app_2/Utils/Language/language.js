5be193e3dc2c4d94130b3d53ad67b830bf4fce85
brandon <26451183+ahkuma@users.noreply.github.com>
2019-01-25 11:28:28 +0800

    much simpler linterfix

--- src/javascript/app_2/Utils/Language/language.js
+++ src/javascript/app_2/Utils/Language/language.js
@@ -6,16 +6,17 @@ export const getAllowedLanguages = () => {
     const exclude_languages = [
         'ACH',
     ];
+    // TODO Change language_list to const when design is ready.
+    let language_list = Object.keys(getAll())
+        .filter(key => !(exclude_languages.includes(key)))
+        .reduce((obj, key) => {
+            obj[key] = getAll()[key];
+            return obj;
+        }, {});
 
-    // const language_list = Object.keys(getAll())
-    //     .filter(key => !(exclude_languages.includes(key)))
-    //     .reduce((obj, key) => {
-    //         obj[key] = getAll()[key];
-    //         return obj;
-    //     }, {});
-
-    // TODO uncomment language_list and replace ['en'] when design is ready.
-    return ['en'];
+    // TODO Remove this one line below when design is ready.
+    language_list = ['en'];
+    return language_list;
 };
 
 export const getURL = lang => urlFor(lang);
