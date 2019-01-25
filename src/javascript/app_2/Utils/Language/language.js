6035f4f9cb2970449e84ffbdb0353a27e62b6e43
brandon <26451183+ahkuma@users.noreply.github.com>
2019-01-25 11:15:46 +0800

    fix linter error

--- src/javascript/app_2/Utils/Language/language.js
+++ src/javascript/app_2/Utils/Language/language.js
@@ -6,14 +6,15 @@ export const getAllowedLanguages = () => {
     const exclude_languages = [
         'ACH',
     ];
-    const language_list = Object.keys(getAll())
-        .filter(key => !(exclude_languages.includes(key)))
-        .reduce((obj, key) => {
-            obj[key] = getAll()[key];
-            return obj;
-        }, {});
 
-    // Replace this with language_list when design is ready.
+    // const language_list = Object.keys(getAll())
+    //     .filter(key => !(exclude_languages.includes(key)))
+    //     .reduce((obj, key) => {
+    //         obj[key] = getAll()[key];
+    //         return obj;
+    //     }, {});
+
+    // TODO uncomment language_list and replace ['en'] when design is ready.
     return ['en'];
 };
 
