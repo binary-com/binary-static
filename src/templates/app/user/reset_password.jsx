ead06314844686645aa07de18267754680e18e02
brandon <26451183+ahkuma@users.noreply.github.com>
2019-01-29 16:50:32 +0800

    Code Improvement : Value to lowercase and options localized

--- src/templates/app/user/reset_password.jsx
+++ src/templates/app/user/reset_password.jsx
@@ -15,7 +15,7 @@ const ResetPassword = () => (
             <div className='gr-parent gr-padding-10' id='container_reset_password'>
                 <form id='frm_reset_password'>
                     <Fieldset legend={it.L('Details')}>
-                        <FormRow type='radio' id='have_real_account' options={ [{ value: 'Yes', label: 'Yes' }, { value: 'No', label: 'No' }] } default_option={'No'} label={it.L('Do you have a real money account?')} />
+                        <FormRow type='radio' id='have_real_account' options={ [{ value: 'yes', label: it.L('Yes') }, { value: 'no', label: L.it('No') }] } default_option={'no'} label={it.L('Do you have a real money account?')} />
                         <FormRow type='text' id='date_of_birth' label={it.L('Date of Birth')} row_class='invisible' row_id='dob_field' attributes={{ size: 12, readOnly: 'readonly' }} />
                         <FormRow type='password' id='new_password' label={it.L('New Password')} hint={it.L('Minimum of six lower and uppercase letters with numbers')} />
                         <FormRow type='password' id='repeat_password' label={it.L('Confirm New Password')} />
