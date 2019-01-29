cee5af2e6536c9d9c1ea801762f7326b77ae3b6e
brandon <26451183+ahkuma@users.noreply.github.com>
2019-01-29 11:23:01 +0800

    Make have_real_money field required

--- src/javascript/app/pages/user/reset_password.js
+++ src/javascript/app/pages/user/reset_password.js
@@ -43,10 +43,10 @@ const ResetPassword = (() => {
 
         const form_id = '#frm_reset_password';
         FormManager.init(form_id, [
-            { selector: '#new_password',    validations: ['req', 'password'], re_check_field: '#repeat_password' },
-            { selector: '#repeat_password', validations: ['req', ['compare', { to: '#new_password' }]], exclude_request: 1 },
-            { selector: '#date_of_birth',   validations: ['req'] },
-
+            { selector: '#have_real_account',  validations: ['req'], exclude_request: 1 },
+            { selector: '#new_password',       validations: ['req', 'password'], re_check_field: '#repeat_password' },
+            { selector: '#repeat_password',    validations: ['req', ['compare', { to: '#new_password' }]], exclude_request: 1 },
+            { selector: '#date_of_birth',      validations: ['req'] },
             { request_field: 'reset_password', value: 1 },
         ], true);
 
