7355615d4621b710e7e4f3926b5f4a71f2bd2fd1
nazaninreihani <nazanin.reyh@gmail.com>
2019-02-04 10:14:21 -0500

    hide positions drawer in logout

--- src/javascript/app_2/App/Components/Elements/PositionsDrawer/positions_drawer.jsx
+++ src/javascript/app_2/App/Components/Elements/PositionsDrawer/positions_drawer.jsx
@@ -9,7 +9,7 @@ import EmptyPortfolioMessage          from 'Modules/Portfolio/Components/empty_p
 import { connect }                    from 'Stores/connect';
 import PositionsDrawerCard            from './positions_drawer_card.jsx';
 
-class PositionsDrawer extends React.PureComponent {
+class PositionsDrawer extends React.Component {
     componentDidMount()    {
         this.props.onMount();
     }
