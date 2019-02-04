953eaa6402f318ed78fd04242bb8e83cb775b0da
nazaninreihani <nazanin.reyh@gmail.com>
2019-02-04 17:32:46 -0500

    fix tests

--- src/javascript/app_2/App/Components/Elements/__tests__/toggle_button.spec.js
+++ src/javascript/app_2/App/Components/Elements/__tests__/toggle_button.spec.js
@@ -2,33 +2,33 @@ import React                  from 'react';
 import { expect }             from 'chai';
 import { configure, shallow } from 'enzyme';
 import Adapter                from 'enzyme-adapter-react-16';
-import ToggleButton           from '../toggle_button.jsx';
+import SwitchButton           from '../switch_button.jsx';
 
 configure({ adapter: new Adapter() });
 
-describe('ToggleButton', () => {
-    it('should render one <ToggleButton /> component', () => {
-        const wrapper = shallow(<ToggleButton />);
+describe('SwitchButton', () => {
+    it('should render one <SwitchButton /> component', () => {
+        const wrapper = shallow(<SwitchButton />);
         expect(wrapper).to.have.length(1);
     });
     it('should have class equal to style passed in props', () => {
-        const wrapper = shallow(<ToggleButton style='red' />);
+        const wrapper = shallow(<SwitchButton style='red' />);
         expect(wrapper.hasClass('red')).to.be.true;
     });
-    it('should have toggle-button class if style is not passed', () => {
-        const wrapper = shallow(<ToggleButton />);
-        expect(wrapper.hasClass('toggle-button')).to.be.true;
+    it('should have switch-button class if style is not passed', () => {
+        const wrapper = shallow(<SwitchButton />);
+        expect(wrapper.hasClass('switch-button')).to.be.true;
     });
     it('should have class .toggled if toggled is passed true in props', () => {
-        const wrapper = shallow(<ToggleButton toggled={true} />);
+        const wrapper = shallow(<SwitchButton toggled={true} />);
         expect(wrapper.find('.toggled').exists()).to.be.true;
     });
     it('should not have .toggled if toggled is passed false in props', () => {
-        const wrapper = shallow(<ToggleButton toggled={false} />);
+        const wrapper = shallow(<SwitchButton toggled={false} />);
         expect(wrapper.find('.toggled').exists()).to.be.false;
     });
     it('should not have .toggled if toggled is not passed in props', () => {
-        const wrapper = shallow(<ToggleButton />);
+        const wrapper = shallow(<SwitchButton />);
         expect(wrapper.find('.toggled').exists()).to.be.false;
     });
 });
