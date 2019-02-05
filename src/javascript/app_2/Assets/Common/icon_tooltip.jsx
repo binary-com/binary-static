d293ced9630c677eda994c7337a2f5c9bf3b6a15
negar <negar254000@gmail.com>
2019-02-05 14:27:30 +0800

    fix (i) color in dark theme and rename component

--- src/javascript/app_2/Assets/Common/icon_tooltip.jsx
+++ src/javascript/app_2/Assets/Common/icon_tooltip.jsx
@@ -1,14 +1,14 @@
 import PropTypes from 'prop-types';
 import React     from 'react';
 
-const IconTooltipLight = ({ className }) => (
+const IconTooltip = ({ className }) => (
     <svg className={className} xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 16 16'>
         <g fill='none' fillRule='evenodd'><circle cx='8' cy='8' r='7.5' stroke='rgba(0, 0, 0, 0.8)' /><g fill='rgba(0, 0, 0, 0.8)' transform='translate(6.5 4)'><circle cx='1.5' cy='1.25' r='1' /><rect width='1' height='5' x='1' y='3' rx='.5' /></g></g>
     </svg>
 );
 
-IconTooltipLight.propTypes = {
+IconTooltip.propTypes = {
     className: PropTypes.string,
 };
 
-export { IconTooltipLight };
+export { IconTooltip };
