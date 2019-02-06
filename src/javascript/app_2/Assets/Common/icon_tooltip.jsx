c2ff941284a52611f4db9ba9f49e2ee68706ca75
negar <negar254000@gmail.com>
2019-02-06 10:39:28 +0800

    fix explanation style

--- src/javascript/app_2/Assets/Common/icon_tooltip.jsx
+++ src/javascript/app_2/Assets/Common/icon_tooltip.jsx
@@ -3,7 +3,7 @@ import React     from 'react';
 
 const IconTooltip = ({ className }) => (
     <svg className={className} xmlns='http://www.w3.org/2000/svg' width='16' height='16' viewBox='0 0 16 16'>
-        <g fill='none' fillRule='evenodd'><circle cx='8' cy='8' r='7.5' stroke='rgba(0, 0, 0, 0.8)' /><g fill='rgba(0, 0, 0, 0.8)' transform='translate(6.5 4)'><circle cx='1.5' cy='1.25' r='1' /><rect width='1' height='5' x='1' y='3' rx='.5' /></g></g>
+        <path fill='rgba(0, 0, 0, 0.8)' fillRule='evenodd' d='M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16zM8 6a.75.75 0 1 0 0-1.5A.75.75 0 0 0 8 6zm0 1a.5.5 0 0 0-.5.5v4a.5.5 0 1 0 1 0v-4A.5.5 0 0 0 8 7z' />
     </svg>
 );
 
