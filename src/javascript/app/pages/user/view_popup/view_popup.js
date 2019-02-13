c12dfbf176615a6b1628631d9e99ace8bc05cd6b
Mohammadreza Ghorbani <isthereanybody@gmail.com>
2019-02-13 12:19:40 +0330

    Update Localizations

--- src/javascript/app/pages/user/view_popup/view_popup.js
+++ src/javascript/app/pages/user/view_popup/view_popup.js
@@ -120,8 +120,8 @@ const ViewPopup = (() => {
             PUTSPREAD   : localize('Put Spread'),
             TICKHIGH    : localize('High Tick'),
             TICKLOW     : localize('Low Tick'),
-            RUNHIGH     : localize('Run High'),
-            RUNLOW      : localize('Run Low'),
+            RUNHIGH     : localize('Run Ups'),
+            RUNLOW      : localize('Run Downs'),
         });
 
         return {
