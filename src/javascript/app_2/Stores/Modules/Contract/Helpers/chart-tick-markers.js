b244a3ba2fcad032241088b4f31fc49b80ae8f4a
OskarAhl <oskar@binary.com>
2019-03-05 11:58:03 +0800

    refactor stream handling to always open stream

--- src/javascript/app_2/Stores/Modules/Contract/Helpers/chart-tick-markers.js
+++ src/javascript/app_2/Stores/Modules/Contract/Helpers/chart-tick-markers.js
@@ -5,9 +5,14 @@ import { MARKER_TYPES_CONFIG } from '../../SmartChart/Constants/markers';
 let has_been_initialized = false;
 let middle_ticks_handler;
 
+// 1. add stream
+// 2. Cache ticks until contract has entry_tick_time
+// 3. Filter cached ticks
+// 4. Add cached ticks to chart
+// 5. If entry_tick_time not in cached ticks send history request
+// 6. Wait for history request, populate markers
 export const createChartTickMarkers = (SmartChartStore, contract_info) => {
-    if (!has_been_initialized && contract_info.entry_tick_time) {
-        // console.log(contract_info.entry_tick_time);
+    if (!has_been_initialized) {
         middle_ticks_handler = getMiddleTicksHandler(SmartChartStore, contract_info);
         has_been_initialized = true;
 
@@ -17,13 +22,16 @@ export const createChartTickMarkers = (SmartChartStore, contract_info) => {
             middle_ticks_handler.addMarkerFromHistory(contract_info);
         }
     }
+
+    if (has_been_initialized && contract_info.entry_tick_time) {
+        middle_ticks_handler.updateContract(contract_info);
+    }
+
     if (contract_info.exit_tick_time && middle_ticks_handler) {
         middle_ticks_handler.addExitTick(contract_info);
         middle_ticks_handler.forgetStreamHistory(contract_info);
     }
-    // if (contract_info.is_path_dependent && is_running_contract && middle_ticks_handler && contract_info.barrier) {
-    //     middle_ticks_handler.handlePathDependentMarkers(contract_info);
-    // }
+
 };
 
 export const cleanUpTickMarkers = (contract_info) => {
@@ -34,14 +42,15 @@ export const cleanUpTickMarkers = (contract_info) => {
     has_been_initialized = false;
 };
 
-const getMiddleTicksHandler = (SmartChartStore, contract_info) => {
+const getMiddleTicksHandler = (SmartChartStore, contract) => {
     let on_going_tick_idx = 0;
     let is_finished_before_end = false;
     let draw_timeout = 0;
     let has_stream = false;
-    let path_dependent_barrier, first_tick_time, is_getting_history;
+    let should_cache_ticks = false;
     const added_ticks = [];
-    const stream_ticks = [];
+    let stream_ticks = [];
+    let contract_info = { ...contract };
 
     const ticks_history_req = {
         ticks_history: contract_info.underlying,
@@ -67,7 +76,6 @@ const getMiddleTicksHandler = (SmartChartStore, contract_info) => {
             if (is_entry) marker_config = createMarkerSpotEntry(tick, idx);
             if (is_exit) {
                 marker_config = createMarkerSpotExit(ci);
-                // console.log(added_ticks);
             }
             if (is_middle) {
                 marker_config = createMarkerSpotMiddle(tick, idx);
@@ -82,7 +90,7 @@ const getMiddleTicksHandler = (SmartChartStore, contract_info) => {
 
     const onTick = (tick) => {
         if (isContractTick(tick, contract_info) && !is_finished_before_end) {
-            if (path_dependent_barrier && +tick.price >= path_dependent_barrier) {
+            if (contract_info.is_path_dependent && +tick.price >= +contract_info.barrier) {
                 is_finished_before_end = true;
                 return;
             }
@@ -95,10 +103,8 @@ const getMiddleTicksHandler = (SmartChartStore, contract_info) => {
         }
     };
 
-    const onCompletedContract = (data) => {
-        const { prices, times } = data.history;
-        let contract_ticks = combinePriceTime(prices, times);
-        contract_ticks = [...contract_ticks, ...stream_ticks]
+    const addTicks = (ticks_arr) => {
+        let contract_ticks = [...ticks_arr]
             .sort((a, b) => +a.time - b.time)
             .filter((tick) => +tick.time >= +contract_info.entry_tick_time);
         contract_ticks = unique(contract_ticks, 'time');
@@ -113,27 +119,34 @@ const getMiddleTicksHandler = (SmartChartStore, contract_info) => {
         });
     };
 
+    const onCompletedContract = (data) => {
+        const { prices, times } = data.history;
+        const contract_ticks = combinePriceTime(prices, times);
+        addTicks([...contract_ticks, ...stream_ticks]);
+        should_cache_ticks = false;
+    };
+
     const onRunningContract = (data) => {
         if (data.tick) {
             const tick = { price: data.tick.quote, time: data.tick.epoch };
-            if (!first_tick_time) {
-                first_tick_time = tick.time;
-            }
             stream_ticks.push(tick);
-            if (!is_getting_history) onTick(tick, contract_info);
+
+            if (!should_cache_ticks) onTick(tick, contract_info);
         }
         if (data.history) {
             const { prices, times } = data.history;
-            combinePriceTime(prices, times).forEach(tick => onTick(tick));
+            const combined = combinePriceTime(prices, times);
+            stream_ticks = [...stream_ticks, ...combined];
         }
     };
     
     return {
-        addMarkerFromStreamHistory: (start) => {
+        addMarkerFromStreamHistory: () => {
             draw_timeout = 1500;
             has_stream = true;
-
-            WS.sendRequest({ ...ticks_history_req, start }).then((data) => onCompletedContract(data));
+            if (!contract_info.entry_tick_time) {
+                should_cache_ticks = true;
+            }
             WS.subscribeTicksHistory({ ...ticks_history_req, subscribe: 1 }, onRunningContract, false);
         },
         forgetStreamHistory: () =>{
@@ -142,12 +155,23 @@ const getMiddleTicksHandler = (SmartChartStore, contract_info) => {
             WS.forget('ticks_history', onRunningContract, ticks_history_req);
         },
         addMarkerFromHistory: () =>
-            WS.sendRequest(ticks_history_req).then((data) => onCompletedContract(data)),
-        handlePathDependentMarkers: (ci) => {
-            path_dependent_barrier = ci.barrier;
-        },
+            WS.sendRequest({ ...ticks_history_req }).then((data) => onCompletedContract(data)),
         addExitTick: (ci) => {
             addTickToChart({ price: ci.exit_tick, time: ci.exit_tick_time }, ci.tick_count, ci);
+        },
+        updateContract: (ci) => {
+            if (!has_stream) return;
+
+            contract_info = { ...ci };
+            stream_ticks = stream_ticks.filter((t) => t.time >= ci.entry_tick_time);
+
+            if (stream_ticks.length > 0) {
+                addTicks([...stream_ticks]);
+                should_cache_ticks = false;
+            } else {
+                WS.sendRequest({ ...ticks_history_req, start: ci.entry_tick_time })
+                    .then((data) => onCompletedContract(data));
+            }
         },
     };
 };
