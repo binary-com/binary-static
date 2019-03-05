adb0a1bbb9182dde34e22a9c771eb087eede6cbc
OskarAhl <oskar@binary.com>
2019-03-05 14:34:10 +0800

    Add entry spot component

---
+++ src/javascript/app_2/Modules/SmartChart/Components/Markers/marker-spot-entry.jsx
@@ -0,0 +1,8 @@
+import { observer } from 'mobx-react';
+import React        from 'react';
+
+const MarkerSpotEntry = () => (
+    <div className='chart-spot__spot-entry' />
+);
+
+export default observer(MarkerSpotEntry);
