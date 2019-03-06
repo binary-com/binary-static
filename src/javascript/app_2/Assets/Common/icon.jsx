638d56df6e2dccd834620ad600cfc8708c7c8469
Mahdi <m.pourziaei@gmail.com>
2019-03-06 15:28:52 +0800

    + Separated Arrow and Arrow Bold into their own components + Added `Icon` component + Added `Icons` constant to call icons with `Icon` component + Updated indices

---
+++ src/javascript/app_2/Assets/Common/icon.jsx
@@ -0,0 +1,31 @@
+import React     from 'react';
+import PropTypes from 'prop-types';
+
+class Icon extends React.PureComponent {
+    render() {
+        const options = {
+            category     : this.props.category,
+            className    : this.props.className,
+            classNamePath: this.props.classNamePath,
+            is_disabled  : this.props.is_disabled,
+            onClick      : this.props.onClick,
+            onMouseEnter : this.props.onMouseEnter,
+            onMouseLeave : this.props.onMouseLeave,
+            type         : this.props.type,
+        };
+
+        return this.props.icon(options);
+    }
+}
+
+Icon.propTypes = {
+    category     : PropTypes.string,
+    className    : PropTypes.string,
+    classNamePath: PropTypes.string,
+    icon         : PropTypes.func,
+    is_disabled  : PropTypes.bool,
+    onClick      : PropTypes.func,
+    type         : PropTypes.string,
+};
+
+export { Icon };
---
+++ src/javascript/app_2/Constants/icons.js
@@ -0,0 +1,55 @@
+import { IconArrow } from 'Assets/Common/icon-arrow.jsx';
+import { IconArrowBold } from 'Assets/Common/icon-arrow-bold.jsx';
+import { IconBack } from 'Assets/Common/icon-back.jsx';
+import { IconCalendar } from 'Assets/Common/icon-calendar.jsx';
+import { IconCalendarToday } from 'Assets/Common/icon-calendar-today.jsx';
+import { IconChevronDoubleLeft } from 'Assets/Common/icon-chevron-double-left.jsx';
+import { IconChevronDoubleRight } from 'Assets/Common/icon-chevron-double-right.jsx';
+import { IconChevronLeft } from 'Assets/Common/icon-chevron-left.jsx';
+import { IconChevronRight } from 'Assets/Common/icon-chevron-right.jsx';
+import { IconClear } from 'Assets/Common/icon-clear.jsx';
+import { IconClock } from 'Assets/Common/icon-clock.jsx';
+import { IconClose } from 'Assets/Common/icon-close.jsx';
+import { IconCountryFlag } from 'Assets/Common/icon-country-flag.jsx';
+import { IconError } from 'Assets/Common/icon-error.jsx';
+import { IconExclamation } from 'Assets/Common/icon-exclamation.jsx';
+import { IconFlag } from 'Assets/Common/icon-flag.jsx';
+import { IconInfoBlue } from 'Assets/Common/icon-info-blue.jsx';
+import { IconInfoOutline } from 'Assets/Common/icon-info-outline.jsx';
+import { IconMinimize } from 'Assets/Common/icon-minimize.jsx';
+import { IconMinus } from 'Assets/Common/icon-minus.jsx';
+import { IconPlus } from 'Assets/Common/icon-plus.jsx';
+import { IconQuestion } from 'Assets/Common/icon-question.jsx';
+import { IconRedDot } from 'Assets/Common/icon-red-dot.jsx';
+import { IconSuccess } from 'Assets/Common/icon-success.jsx';
+import { IconWarning } from 'Assets/Common/icon-warning.jsx';
+
+const ICONS = {
+    ARROW               : IconArrow,
+    ARROW_BOLD          : IconArrowBold,
+    BACK                : IconBack,
+    CALENDAR_TODAY      : IconCalendarToday,
+    CALENDAR            : IconCalendar,
+    CHEVRON_DOUBLE_LEFT : IconChevronDoubleLeft,
+    CHEVRON_DOUBLE_RIGHT: IconChevronDoubleRight,
+    CHEVRON_LEFT        : IconChevronLeft,
+    CHEVRON_RIGHT       : IconChevronRight,
+    CLEAR               : IconClear,
+    CLOCK               : IconClock,
+    CLOSE               : IconClose,
+    COUNTRY_FLAG        : IconCountryFlag,
+    ERROR               : IconError,
+    EXCLAMATION         : IconExclamation,
+    FLAG                : IconFlag,
+    INFO_BLUE           : IconInfoBlue,
+    INFO_OUTLINE        : IconInfoOutline,
+    MINIMIZE            : IconMinimize,
+    MINUS               : IconMinus,
+    PLUS                : IconPlus,
+    QUESTION            : IconQuestion,
+    RED_DOT             : IconRedDot,
+    SUCCESS             : IconSuccess,
+    WARNING             : IconWarning,
+};
+
+export { ICONS };
