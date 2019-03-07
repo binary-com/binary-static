9c025d2ef6351b81d8e25a2246806d3144879988
Mahdi <m.pourziaei@gmail.com>
2019-03-07 11:31:50 +0800

    Added other icon components from assets to `Icons` constant

--- src/javascript/app_2/Constants/icons.js
+++ src/javascript/app_2/Constants/icons.js
@@ -24,32 +24,101 @@ import { IconRedDot } from 'Assets/Common/icon-red-dot.jsx';
 import { IconSuccess } from 'Assets/Common/icon-success.jsx';
 import { IconWarning } from 'Assets/Common/icon-warning.jsx';
 
+import IconEntrySpot from 'Assets/Contract/icon-entry-spot.jsx';
+import IconContractFlag from 'Assets/Contract/icon-flag.jsx';
+import IconTick from 'Assets/Contract/icon-tick.jsx';
+
+import { IconMaximize } from 'Assets/Footer/icon-maximize.jsx';
+import { IconPositions } from 'Assets/Footer/icon-positions.jsx';
+import { IconSettings } from 'Assets/Footer/icon-settings.jsx';
+
+import { IconAccountsCurrency } from 'Assets/Header/AccountsCurrency/icon_accounts_currency.jsx';
+import { IconLogout } from 'Assets/Header/Drawer/icon-logout.jsx';
+import { IconBell } from 'Assets/Header/NavBar/icon-bell.jsx';
+import { IconCashier } from 'Assets/Header/NavBar/icon-cashier.jsx';
+import { IconHamburger } from 'Assets/Header/NavBar/icon-hamburger.jsx';
+import { IconPortfolio } from 'Assets/Header/NavBar/icon-portfolio.jsx';
+import { IconStatement } from 'Assets/Header/NavBar/icon-statement.jsx';
+import { IconTrade } from 'Assets/Header/NavBar/icon-trade.jsx';
+
+import { IconBuy } from 'Assets/Statement/icon-buy.jsx';
+import { IconDeposit } from 'Assets/Statement/icon-deposit.jsx';
+import { IconPayout } from 'Assets/Statement/icon-payout.jsx';
+import { IconSell } from 'Assets/Statement/icon-sell.jsx';
+import { IconWallet } from 'Assets/Statement/icon-wallet.jsx';
+import { IconWithdrawal } from 'Assets/Statement/icon-withdrawal.jsx';
+
+import { IconBarrierDown } from 'Assets/Trading/Barriers/icon-barrier-down.jsx';
+import { IconBarrierUp } from 'Assets/Trading/Barriers/icon-barrier-up.jsx';
+import { IconTradeCategory } from 'Assets/Trading/Categories/icon-trade-categories.jsx';
+import { IconTradeType } from 'Assets/Trading/Types/icon-trade-types.jsx';
+import { IconLock } from 'Assets/Trading/icon-lock.jsx';
+import { IconPriceMove } from 'Assets/Trading/icon-price-move.jsx';
+
 const ICONS = {
-    ARROW               : IconArrow,
-    ARROW_BOLD          : IconArrowBold,
-    BACK                : IconBack,
-    CALENDAR_TODAY      : IconCalendarToday,
-    CALENDAR            : IconCalendar,
-    CHEVRON_DOUBLE_LEFT : IconChevronDoubleLeft,
-    CHEVRON_DOUBLE_RIGHT: IconChevronDoubleRight,
-    CHEVRON_LEFT        : IconChevronLeft,
-    CHEVRON_RIGHT       : IconChevronRight,
-    CLEAR               : IconClear,
-    CLOCK               : IconClock,
-    CLOSE               : IconClose,
-    COUNTRY_FLAG        : IconCountryFlag,
-    ERROR               : IconError,
-    EXCLAMATION         : IconExclamation,
-    FLAG                : IconFlag,
-    INFO_BLUE           : IconInfoBlue,
-    INFO_OUTLINE        : IconInfoOutline,
-    MINIMIZE            : IconMinimize,
-    MINUS               : IconMinus,
-    PLUS                : IconPlus,
-    QUESTION            : IconQuestion,
-    RED_DOT             : IconRedDot,
-    SUCCESS             : IconSuccess,
-    WARNING             : IconWarning,
+    COMMON: {
+        ARROW               : IconArrow,
+        ARROW_BOLD          : IconArrowBold,
+        BACK                : IconBack,
+        CALENDAR_TODAY      : IconCalendarToday,
+        CALENDAR            : IconCalendar,
+        CHEVRON_DOUBLE_LEFT : IconChevronDoubleLeft,
+        CHEVRON_DOUBLE_RIGHT: IconChevronDoubleRight,
+        CHEVRON_LEFT        : IconChevronLeft,
+        CHEVRON_RIGHT       : IconChevronRight,
+        CLEAR               : IconClear,
+        CLOCK               : IconClock,
+        CLOSE               : IconClose,
+        COUNTRY_FLAG        : IconCountryFlag,
+        ERROR               : IconError,
+        EXCLAMATION         : IconExclamation,
+        FLAG                : IconFlag,
+        INFO_BLUE           : IconInfoBlue,
+        INFO_OUTLINE        : IconInfoOutline,
+        MINIMIZE            : IconMinimize,
+        MINUS               : IconMinus,
+        PLUS                : IconPlus,
+        QUESTION            : IconQuestion,
+        RED_DOT             : IconRedDot,
+        SUCCESS             : IconSuccess,
+        WARNING             : IconWarning,
+    },
+    CONTRACT: {
+        ENTRY_SPOT: IconEntrySpot,
+        FLAG      : IconContractFlag,
+        TICK      : IconTick,
+    },
+    FOOTER: {
+        MAXIMIZE : IconMaximize,
+        POSITIONS: IconPositions,
+        SETTINGS : IconSettings,
+    },
+    HEADER: {
+        ACCOUNTS_CURRENCY: IconAccountsCurrency,
+        LOGOUT           : IconLogout,
+        BELL             : IconBell,
+        CASHIER          : IconCashier,
+        HAMBURGER        : IconHamburger,
+        PORTFOLIO        : IconPortfolio,
+        STATEMENT        : IconStatement,
+        TRADE            : IconTrade,
+    },
+    STATEMENT: {
+        BUY       : IconBuy,
+        DEPOSIT   : IconDeposit,
+        PAYOUT    : IconPayout,
+        SELL      : IconSell,
+        WALLET    : IconWallet,
+        WITHDRAWAL: IconWithdrawal,
+    },
+    TRADING: {
+        BARRIER_DOWN  : IconBarrierDown,
+        BARRIER_UP    : IconBarrierUp,
+        TRADE_CATEGORY: IconTradeCategory,
+        TRADE_TYPES   : IconTradeType,
+        LOCK          : IconLock,
+        PRICE_MOVE    : IconPriceMove,
+    },
 };
 
 export { ICONS };
