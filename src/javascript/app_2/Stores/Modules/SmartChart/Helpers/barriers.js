dd68538a436193bae221e2c9d2f0a3699e515a8a
Mahdi <m.pourziaei@gmail.com>
2019-03-11 11:56:28 +0800

    Fixed for not considering `0` values

--- src/javascript/app_2/Stores/Modules/SmartChart/Helpers/barriers.js
+++ src/javascript/app_2/Stores/Modules/SmartChart/Helpers/barriers.js
@@ -5,7 +5,7 @@ import { CONTRACT_SHADES } from '../Constants/barriers';
 export const isBarrierSupported = (contract_type) => contract_type in CONTRACT_SHADES;
 
 export const barriersToString = (is_relative, ...barriers_list) => barriers_list
-    .filter(barrier => barrier)
+    .filter(barrier => barrier !== undefined && barrier !== null)
     .map(barrier => `${is_relative && !/^[+-]/.test(barrier) ? '+' : ''}${barrier}`);
 
 export const barriersObjectToArray = (barriers) => (
