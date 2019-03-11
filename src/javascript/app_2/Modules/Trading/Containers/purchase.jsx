55697a27dafef07610392f177dfcdd0b247bcc1d
zakibinary <zaki@binary.com>
2019-03-11 14:42:15 +0800

    fixed bug with popconfirm

--- src/javascript/app_2/Modules/Trading/Containers/purchase.jsx
+++ src/javascript/app_2/Modules/Trading/Containers/purchase.jsx
@@ -35,32 +35,32 @@ const Purchase = ({
         const is_purchase_error = (!isEmptyObject(purchase_info) && purchase_info.echo_req.buy === info.id);
 
         const purchase_button = (
-            <div className='btn-purchase__shadow-wrapper'>
-                <Button
-                    is_disabled={is_contract_mode || is_disabled}
-                    id={`purchase_${type}`}
-                    className='btn--primary btn-purchase'
-                    has_effect
-                    onClick={() => { onClickPurchase(info.id, info.stake, type); }}
-                >
-                    <React.Fragment>
-                        <div className='btn-purchase__effect-main' />
-                        <div className='btn-purchase__effect-detail' />
-                        <div className='btn-purchase__content'>
-                            <div className='btn-purchase__trade-type'>
-                                <IconTradeType type={type.toLowerCase()} className='btn-purchase__trade-type-icon' />
-                                <span className='btn-purchase__trade-type-text'>{localize('[_1]', trade_types[type])}</span>
-                            </div>
+            <Button
+                is_disabled={is_contract_mode || is_disabled}
+                id={`purchase_${type}`}
+                className='btn--primary btn-purchase'
+                has_effect
+                onClick={() => {
+                    onClickPurchase(info.id, info.stake, type);
+                }}
+            >
+                <React.Fragment>
+                    <div className='btn-purchase__effect-main' />
+                    <div className='btn-purchase__effect-detail' />
+                    <div className='btn-purchase__content'>
+                        <div className='btn-purchase__trade-type'>
+                            <IconTradeType type={type.toLowerCase()} className='btn-purchase__trade-type-icon' />
+                            <span className='btn-purchase__trade-type-text'>{localize('[_1]', trade_types[type])}</span>
                         </div>
-                        <div className='btn-purchase__info'>
-                            <div className='btn-purchase__return'>{is_disabled ? '---,-' : info.returns}</div>
-                            <div className='btn-purchase__profit'>
-                                {is_disabled ? '--,--' : <Money amount={info.profit} currency={currency} className='btn-purchase__currency' />}
-                            </div>
+                    </div>
+                    <div className='btn-purchase__info'>
+                        <div className='btn-purchase__return'>{is_disabled ? '---,-' : info.returns}</div>
+                        <div className='btn-purchase__profit'>
+                            {is_disabled ? '--,--' : <Money amount={info.profit} currency={currency} className='btn-purchase__currency' />}
                         </div>
-                    </React.Fragment>
-                </Button>
-            </div>
+                    </div>
+                </React.Fragment>
+            </Button>
         );
 
         return (
@@ -90,18 +90,21 @@ const Purchase = ({
                         has_increased={info.has_increased}
                         is_visible={!is_contract_mode}
                     />
-                    {is_purchase_confirm_on ?
-                        <PopConfirm
-                            alignment='left'
-                            cancel_text={localize('Cancel')}
-                            confirm_text={localize('Purchase')}
-                            message={localize('Are you sure you want to purchase this contract?')}
-                        >
-                            {purchase_button}
-                        </PopConfirm>
-                        :
-                        purchase_button
-                    }
+                    <div className='btn-purchase__shadow-wrapper'>
+                        {
+                            is_purchase_confirm_on ?
+                                <PopConfirm
+                                    alignment='left'
+                                    cancel_text={localize('Cancel')}
+                                    confirm_text={localize('Purchase')}
+                                    message={localize('Are you sure you want to purchase this contract?')}
+                                >
+                                    {purchase_button}
+                                </PopConfirm>
+                                :
+                                purchase_button
+                        }
+                    </div>
                 </React.Fragment>
             </Fieldset>
         );
