ab9ef4583966d08a2ca0b8aa2fea46b6b9f5813b
nazanin <reihani>
2019-03-13 10:21:04 -0400

    Change text for empty asset index table

--- src/templates/app/resources/asset_index.jsx
+++ src/templates/app/resources/asset_index.jsx
@@ -7,7 +7,7 @@ const AssetIndex = () => (
             <p id='errorMsg' className='error-msg invisible' />
             <div id='asset-index' className='has-tabs gr-parent' />
             <p className='notice-msg center-text invisible' id='empty-asset-index'>
-                {it.L('Asset index is unavailable at this time.')}
+                {it.L('Asset index is unavailable in this country. If you have an active [_1] account, please log in for full access.', it.website_name)}
             </p>
         </div>
     </div>
