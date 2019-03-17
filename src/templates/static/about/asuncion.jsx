b987f56079657c72dfcfd9b31ac890826c094ab5
arash <arash@arashs-MacBook-Pro.local>
2019-03-17 11:35:11 +0330

    Opitmize asuncion pictures

--- src/templates/static/about/asuncion.jsx
+++ src/templates/static/about/asuncion.jsx
@@ -13,7 +13,7 @@ const Asuncion = () =>  (
                 <div className='gr-12 gr-padding-20 gr-centered'>
                     <div className='gr-row'>
                         <div className='gr-5 gr-12-p gr-12-m'>
-                            <img className='responsive' src={it.url_for('images/pages/careers/asuncion/introduction-asuncion.jpg')} />
+                            <img className='responsive' src={it.url_for('images/pages/careers/asuncion/introduction-asuncion@3.jpg')} />
                         </div>
                         <div className='gr-7 gr-12-p gr-12-m'>
                             <p className='no-margin-top'>{it.L('Asunción is the capital city of Paraguay and one of the oldest settlements in South America. In the past, colonial expeditions departed from Asunción to establish other important cities, leading to its nickname as the ‘Mother of Cities’.')}</p>
@@ -33,15 +33,15 @@ const Asuncion = () =>  (
                         <ImageSlider
                             images={[
                                 {
-                                    url    : 'images/pages/careers/asuncion/asuncion-city.jpg',
+                                    url    : 'images/pages/careers/asuncion/asuncion-city@3.jpg',
                                     caption: it.L('Spanish and Guaraní are the two official languages of the country. Most Paraguayans are comfortably bilingual, although Guaraní is more widely spoken outside urban areas. In the capital, Spanish is the dominant language but you can easily find English speakers too.'),
                                 },
                                 {
-                                    url    : 'images/pages/careers/asuncion/asuncion-food.jpg',
+                                    url    : 'images/pages/careers/asuncion/asuncion-food@3.jpg',
                                     caption: it.L('Paraguayan cuisine mostly consists of meat, mandioca, corn, milk, and cheese. Must-tries include chipá, a bagel-shaped bread; mbejú, a type of pancake made from mandioca and cheese; and tereré, a cold-brewed tea that’s an integral part of everyday life. Paraguayans are generally happy and relaxed; get-togethers with family and friends are the norm on weekends, especially over a hearty barbecue.'),
                                 },
                                 {
-                                    url    : 'images/pages/careers/asuncion/asuncion-university.jpg',
+                                    url    : 'images/pages/careers/asuncion/asuncion-university@3.jpg',
                                     caption: it.L('Asunción has one of the lowest costs of living in the world, being ranked 386th out of 441 cities by Numbeo, the world’s largest database of crowdsourced consumer data. International schools with a bilingual curriculum (Spanish and English) are popular among foreigners with children. You’ll also have easy access to quality healthcare with well-equipped private hospitals staffed with English-speaking doctors.'),
                                 },
                             ]}
@@ -53,18 +53,29 @@ const Asuncion = () =>  (
         <div className='misc'>
             <div className='container gr-row gr-padding-30 center-text'>
                 <div className='gr-12 gr-padding-20 center-text'>
-                    <h2>{it.L('Can\'t miss in Malta')}</h2>
+                    <h2>{it.L('Can\'t miss in Asunción')}</h2>
                 </div>
                 <BoxOverlayImage
-                    title={it.L('Natural wonders')}
-                    text={it.L('Malta\'s natural attractions are absolutely breathtaking, from its sheer cliffs and lush valleys to its golden sandy beaches and underwater structures in clear blue seas.')}
-                    img_src={it.url_for('images/pages/careers/asuncion/asuncion-fascinating-culture@2.jpg')}
+                    title={it.L('Friendly locals')}
+                    text={it.L('Feel right at home with the extremely warm and friendly locals who always receive new faces with open arms.')}
+                    img_src={it.url_for('images/pages/careers/asuncion/asuncion-friendly-locals@3.jpg')}
                 />
                 <BoxOverlayImage
                     alignment='right'
-                    title={it.L('Festivals galore')}
-                    text={it.L('Malta celebrates a large number of religious festivities and cultural events every year, filling the streets with carts laden with traditional sweets and delicacies.')}
-                    img_src={it.url_for('images/pages/careers/asuncion/asuncion-fascinating-culture.jpg')}
+                    title={it.L('Affordable cost of living')}
+                    text={it.L('Paraguay’s cost of living is among the lowest in South America and allows you to enjoy a high quality of life.')}
+                    img_src={it.url_for('images/pages/careers/asuncion/asuncion-affordable-cost-of-living@3.jpg')}
+                />
+                <BoxOverlayImage
+                    title={it.L('Natural wonders')}
+                    text={it.L('Experience the great outdoors like never before with incredible lakes, waterfalls, mountains, caverns, and more.')}
+                    img_src={it.url_for('images/pages/careers/asuncion/asuncion-natural-wonders@3.jpg')}
+                />
+                <BoxOverlayImage
+                    alignment='right'
+                    title={it.L('Fascinating culture')}
+                    text={it.L('Paraguay’s unique cultural melange is a direct result of the intermarriage between Spanish settlers and the native Guaranís.')}
+                    img_src={it.url_for('images/pages/careers/asuncion/asuncion-fascinating-culture@3.jpg')}
                 />
             </div>
         </div>
