d9f00350a497ec9828e2d141ee5226d3ca5db50a
zakibinary <zaki@binary.com>
2019-03-21 11:45:05 +0800

    refactoring

--- src/javascript/app_2/Stores/Modules/SmartChart/smart-chart-store.js
+++ src/javascript/app_2/Stores/Modules/SmartChart/smart-chart-store.js
@@ -94,8 +94,11 @@ export default class SmartChartStore extends BaseStore {
     // --------- Tick Contracts ---------
     @action.bound
     setTickChartView(scroll_to_left_epoch) {
-        this.updateChartType('mountain');
-        this.updateEpochScrollToOffset(3);
+        if (this.granularity !== 0) {
+            this.updateGranularity(0);
+            this.updateChartType('mountain');
+        }
+        this.updateEpochScrollToOffset(1);
         this.updateChartZoom(100);
         this.updateEpochScrollToValue(scroll_to_left_epoch);
     }
