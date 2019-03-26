1d887272a74f54fdadd67183355fd612c185c5d9
Mohammadreza Ghorbani <isthereanybody@gmail.com>
2019-03-26 14:48:36 +0430

    Add assets

--- src/javascript/app_2/App/Containers/SettingsDialog/settings-chart.jsx
+++ src/javascript/app_2/App/Containers/SettingsDialog/settings-chart.jsx
@@ -1,19 +1,32 @@
-import PropTypes       from 'prop-types';
-import React           from 'react';
-import { localize }    from '_common/localize';
-import { connect }     from 'Stores/connect';
-import Localize        from 'App/Components/Elements/localize.jsx';
+import PropTypes                     from 'prop-types';
+import React                         from 'react';
+import { localize }                  from '_common/localize';
+import { connect }                   from 'Stores/connect';
+import Localize                      from 'App/Components/Elements/localize.jsx';
 import MediaItem, {
     MediaHeading,
     MediaIcon,
     MediaDescription,
-}                      from 'App/Components/Elements/Media';
-import Checkbox        from 'App/Components/Form/Checkbox';
-import RadioGroup      from 'App/Components/Form/Radio';
+}                                    from 'App/Components/Elements/Media';
+import Checkbox                      from 'App/Components/Form/Checkbox';
+import RadioGroup                    from 'App/Components/Form/Radio';
+import OHLCDisabledLight             from 'Images/app_2/settings/OHLC-disabled.svg';
+import OHLCEnabledLight              from 'Images/app_2/settings/OHLC-enabled.svg';
+import ChartPositionEnabledLight     from 'Images/app_2/settings/bottom.svg';
+import OHLCDisabledDark              from 'Images/app_2/settings/dark/OHLC-disabled.svg';
+import OHLCEnabledDark               from 'Images/app_2/settings/dark/OHLC-enabled.svg';
+import ChartPositionEnabledDark      from 'Images/app_2/settings/dark/bottom.svg';
+import IntervalDurationDisabledDark  from 'Images/app_2/settings/dark/interval-disabled.svg';
+import IntervalDurationEnabledDark   from 'Images/app_2/settings/dark/interval-enabled.svg';
+import ChartPositionDisabledDark     from 'Images/app_2/settings/dark/left.svg';
+import IntervalDurationDisabledLight from 'Images/app_2/settings/interval-disabled.svg';
+import IntervalDurationEnabledLight  from 'Images/app_2/settings/interval-enabled.svg';
+import ChartPositionDisabledLight    from 'Images/app_2/settings/left.svg';
 
 const ChartSettings = ({
     is_asset_visible,
     is_countdown_visible,
+    is_dark_mode,
     is_layout_default,
     toggleAsset,
     toggleCountdown,
@@ -25,7 +38,11 @@ const ChartSettings = ({
                 <Localize str='Toolbar position' />
             </MediaHeading>
             <MediaDescription>
-                <MediaIcon />
+                <MediaIcon
+                    disabled={is_dark_mode ? ChartPositionDisabledDark : ChartPositionDisabledLight}
+                    enabled={is_dark_mode ? ChartPositionEnabledDark : ChartPositionEnabledLight}
+                    is_enabled={is_layout_default}
+                />
                 <div className='media__form'>
                     <p><Localize str='Change chart control positions' /></p>
                     <RadioGroup
@@ -50,7 +67,11 @@ const ChartSettings = ({
                 <Localize str='Open-High-Low-Close (OHLC) information' />
             </MediaHeading>
             <MediaDescription>
-                <MediaIcon />
+                <MediaIcon
+                    disabled={is_dark_mode ? OHLCDisabledDark : OHLCDisabledLight }
+                    enabled={is_dark_mode ? OHLCEnabledDark : OHLCEnabledLight}
+                    is_enabled={is_asset_visible}
+                />
                 <div className='media__form'>
                     <Checkbox
                         value={is_asset_visible}
@@ -65,7 +86,11 @@ const ChartSettings = ({
                 <Localize str='Interval duration' />
             </MediaHeading>
             <MediaDescription>
-                <MediaIcon />
+                <MediaIcon
+                    disabled={is_dark_mode ? IntervalDurationDisabledDark : IntervalDurationDisabledLight}
+                    enabled={is_dark_mode ? IntervalDurationEnabledDark : IntervalDurationEnabledLight}
+                    is_enabled={is_countdown_visible}
+                />
                 <div className='media__form'>
                     <Checkbox
                         value={is_countdown_visible}
@@ -81,6 +106,7 @@ const ChartSettings = ({
 ChartSettings.propTypes = {
     is_asset_visible    : PropTypes.bool,
     is_countdown_visible: PropTypes.bool,
+    is_dark_mode        : PropTypes.bool,
     is_layout_default   : PropTypes.bool,
     toggleAsset         : PropTypes.func,
     toggleCountdown     : PropTypes.func,
@@ -89,9 +115,10 @@ ChartSettings.propTypes = {
 
 export default connect(({ ui }) => (
     {
-        is_layout_default   : ui.is_chart_layout_default,
         is_asset_visible    : ui.is_chart_asset_info_visible,
         is_countdown_visible: ui.is_chart_countdown_visible,
+        is_dark_mode        : ui.is_dark_mode_on,
+        is_layout_default   : ui.is_chart_layout_default,
         toggleAsset         : ui.toggleChartAssetInfo,
         toggleCountdown     : ui.toggleChartCountdown,
         toggleLayout        : ui.toggleChartLayout,
--- src/javascript/app_2/App/Containers/SettingsDialog/settings-purchase.jsx
+++ src/javascript/app_2/App/Containers/SettingsDialog/settings-purchase.jsx
@@ -1,16 +1,25 @@
-import PropTypes              from 'prop-types';
-import React                  from 'react';
-import { localize }           from '_common/localize';
+import PropTypes                from 'prop-types';
+import React                    from 'react';
+import { localize }             from '_common/localize';
 import MediaItem, {
     MediaDescription,
     MediaHeading,
     MediaIcon,
-}                             from 'App/Components/Elements/Media';
-import Localize               from 'App/Components/Elements/localize.jsx';
-import Checkbox               from 'App/Components/Form/Checkbox';
-import { connect }            from 'Stores/connect';
+}                                from 'App/Components/Elements/Media';
+import Localize                  from 'App/Components/Elements/localize.jsx';
+import Checkbox                  from 'App/Components/Form/Checkbox';
+import ConfirmationDisabledLight from 'Images/app_2/settings/confirmation-disabled.svg';
+import ConfirmationEnabledLight  from 'Images/app_2/settings/confirmation-enabled.svg';
+import ConfirmationDisabledDark  from 'Images/app_2/settings/dark/confirmation-disabled.svg';
+import ConfirmationEnabledDark   from 'Images/app_2/settings/dark/confirmation-enabled.svg';
+import LockDisabledDark          from 'Images/app_2/settings/dark/lock-disabled.svg';
+import LockEnabledDark           from 'Images/app_2/settings/dark/lock-enabled.svg';
+import LockDisabledLight         from 'Images/app_2/settings/lock-disabled.svg';
+import LockEnabledLight          from 'Images/app_2/settings/lock-enabled.svg';
+import { connect }               from 'Stores/connect';
 
 const PurchaseSettings = ({
+    is_dark_mode,
     is_purchase_confirmed,
     is_purchase_locked,
     togglePurchaseConfirmation,
@@ -22,7 +31,11 @@ const PurchaseSettings = ({
                 <Localize str='Purchase confirmation' />
             </MediaHeading>
             <MediaDescription>
-                <MediaIcon />
+                <MediaIcon
+                    disabled={is_dark_mode ? ConfirmationDisabledDark : ConfirmationDisabledLight }
+                    enabled={is_dark_mode ? ConfirmationEnabledDark : ConfirmationEnabledLight }
+                    is_enabled={is_purchase_confirmed}
+                />
                 <div className='media__form'>
                     <Checkbox
                         value={is_purchase_confirmed}
@@ -37,7 +50,11 @@ const PurchaseSettings = ({
                 <Localize str='Purchase lock' />
             </MediaHeading>
             <MediaDescription>
-                <MediaIcon />
+                <MediaIcon
+                    disabled={is_dark_mode ? LockDisabledDark : LockDisabledLight}
+                    enabled={is_dark_mode ? LockEnabledDark : LockEnabledLight}
+                    is_enabled={is_purchase_locked}
+                />
                 <div className='media__form'>
                     <Checkbox
                         value={is_purchase_locked}
@@ -51,6 +68,7 @@ const PurchaseSettings = ({
 );
 
 PurchaseSettings.propTypes = {
+    is_dark_mode              : PropTypes.bool,
     is_purchase_confirmed     : PropTypes.bool,
     is_purchase_locked        : PropTypes.bool,
     togglePurchaseConfirmation: PropTypes.func,
@@ -59,6 +77,7 @@ PurchaseSettings.propTypes = {
 
 export default connect(({ ui }) => (
     {
+        is_dark_mode              : ui.is_dark_mode_on,
         is_purchase_confirmed     : ui.is_purchase_confirm_on,
         is_purchase_locked        : ui.is_purchase_lock_on,
         togglePurchaseConfirmation: ui.togglePurchaseConfirmation,
