95f0e1627d34e90ce251a563d647e73ea46f8aab
Amam Mustofa <mustofa@binary.com>
2019-04-04 18:40:57 +0800

    return the dom directly

--- src/templates/app_2/app.jsx
+++ src/templates/app_2/app.jsx
@@ -4,20 +4,16 @@ import Head        from './head.jsx';
 import GTMNoScript from './includes/google/gtm_no_script.jsx';
 import Loading     from './components/loading.jsx';
 
-const BinaryApp = () => {
-    const loading = true;
-
-    return (
-        <html>
-            <Head />
-            <body className={classNames('body', it.language, 'theme')}>
-                <GTMNoScript />
-                { loading && <Loading /> }
-                <div id='binary_app' className='binary-app' />
-                <div id='modal_root' className='modal-root' />
-            </body>
-        </html>
-    );
-};
+const BinaryApp = () => (
+    <html>
+        <Head />
+        <body className={classNames('body', it.language, 'theme')}>
+            <GTMNoScript />
+            <Loading />
+            <div id='binary_app' className='binary-app' />
+            <div id='modal_root' className='modal-root' />
+        </body>
+    </html>
+);
 
 export default BinaryApp;
