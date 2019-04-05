a97ab8623ac65e4e2a0d68f3985d2b4a3195e39c
Kelly Anyi <kelly@binary.com>
2019-04-05 12:30:36 +0200

    Revert "Merge pull request #5285 from binary-com/revert-5248-install_pwa_notification"
    This reverts commit e39900e06d324aa36d676a95d2548ecfae5907af, reversing
    changes made to c49ebc60b3fd55e1b0962a1d9ee27067e3f83f6e.

---
+++ src/javascript/app_2/App/Containers/Layout/install-pwa.jsx
@@ -0,0 +1,46 @@
+import PropTypes    from 'prop-types';
+import React        from 'react';
+import { connect }  from 'Stores/connect';
+import { localize } from '_common/localize';
+import Button       from '../../Components/Form/button.jsx';
+
+const InstallPWA = ({
+    pwa_prompt_event,
+    removePWAPromptEvent,
+}) => {
+    const showPrompt = () => {
+        if (pwa_prompt_event) {
+            pwa_prompt_event.prompt();
+            pwa_prompt_event.userChoice
+                .then(choice_result => {
+                    if (choice_result.outcome === 'accepted') {
+                        removePWAPromptEvent();
+                    }
+                });
+        }
+    };
+
+    return (
+        <React.Fragment>
+            {localize('Want quick and easy access to world markets?')}
+            <Button
+                className='btn--secondary notification-bar__button'
+                has_effect
+                text={localize('Get the Binary.com app')}
+                onClick={showPrompt}
+            />
+        </React.Fragment>
+    );
+};
+
+InstallPWA.propTypes = {
+    pwa_prompt_event    : PropTypes.object,
+    removePWAPromptEvent: PropTypes.func,
+};
+
+export default connect(
+    ({ ui }) => ({
+        pwa_prompt_event    : ui.pwa_prompt_event,
+        removePWAPromptEvent: ui.removePWAPromptEvent,
+    })
+)(InstallPWA);
