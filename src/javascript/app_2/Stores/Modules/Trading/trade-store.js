c2ff16c1b6a14a5961d2ffad90c00288b3ddf518
Amam Mustofa <mustofa@binary.com>
2019-04-12 12:07:26 +0800

    add more loading

--- src/javascript/app_2/Stores/Modules/Trading/trade-store.js
+++ src/javascript/app_2/Stores/Modules/Trading/trade-store.js
@@ -523,7 +523,7 @@ export default class TradeStore extends BaseStore {
         });
         this.updateQueryString();
         this.onSwitchAccount(this.accountSwitcherListener);
-        this.root_store.ui.setAppLoading(false);
+        // this.root_store.ui.setAppLoading(false);
     }
 
     @action.bound
