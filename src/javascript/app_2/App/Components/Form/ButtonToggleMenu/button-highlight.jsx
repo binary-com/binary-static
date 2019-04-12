4abfead3b8faad5a3405ee981d1fe5f591caf682
zakibinary <zaki@binary.com>
2019-04-12 09:11:57 +0800

    update border_size constant in button highlight

--- src/javascript/app_2/App/Components/Form/ButtonToggleMenu/button-highlight.jsx
+++ src/javascript/app_2/App/Components/Form/ButtonToggleMenu/button-highlight.jsx
@@ -2,13 +2,14 @@ import PropTypes from 'prop-types';
 import React     from 'react';
 
 const Highlight = ({ left, width }) => {
+    const border_radius_size = '4px';
     const highlight_style = {
         width,
         transform                : `translate3d(${left}px, 0, 0)`,
-        'borderTopLeftRadius'    : (left === 0) ? '4px' : '0',
-        'borderTopRightRadius'   : (left === 0) ? '0' : '4px',
-        'borderBottomLeftRadius' : (left === 0) ? '4px' : '0',
-        'borderBottomRightRadius': (left === 0) ? '0' : '4px',
+        'borderTopLeftRadius'    : (left === 0) ? border_radius_size : 0,
+        'borderTopRightRadius'   : (left === 0) ? 0 : border_radius_size ,
+        'borderBottomLeftRadius' : (left === 0) ? border_radius_size  : 0,
+        'borderBottomRightRadius': (left === 0) ? 0 : border_radius_size ,
     };
 
     return (
