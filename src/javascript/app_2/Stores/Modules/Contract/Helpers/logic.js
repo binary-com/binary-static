b8e00127fd20b8301748bff8d45d4a3ac5e144fd
zakibinary <zaki@binary.com>
2019-04-18 11:15:20 +0800

    added comments

--- src/javascript/app_2/Stores/Modules/Contract/Helpers/logic.js
+++ src/javascript/app_2/Stores/Modules/Contract/Helpers/logic.js
@@ -13,6 +13,7 @@ const hour_to_granularity_map = [
 export const getChartType = (start_time, expiry_time) => {
     const beginning_time = start_time || ServerTime.get().unix();
     const duration = moment.duration(moment.unix(expiry_time).diff(moment.unix(beginning_time))).asHours();
+    // use line chart if duration is less than 1 hour
     return (duration < 1) ? 'mountain' : 'candle';
 };
 
