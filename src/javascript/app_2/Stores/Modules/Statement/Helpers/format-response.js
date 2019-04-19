100fcc9d8269340f94490806c846b82469b9969d
Mahdi <m.pourziaei@gmail.com>
2019-04-19 17:25:17 +0800

    Corrected statement helper function of previous commit

--- src/javascript/app_2/Stores/Modules/Statement/Helpers/format-response.js
+++ src/javascript/app_2/Stores/Modules/Statement/Helpers/format-response.js
@@ -13,17 +13,16 @@ export const formatStatementTransaction = (transaction, currency) => {
     const should_exclude_currency = true;
 
     return {
-        action        : localize(toTitleCase(transaction.action_type) /* localize-ignore */), // handled in static_strings_app_2.js: 'Buy', 'Sell', 'Deposit', 'Withdrawal'
-        date          : `${date_str}\n${time_str}`,
-        refid         : transaction.transaction_id,
-        payout        : isNaN(payout)  ? '-' : formatMoney(currency, payout,  should_exclude_currency),
-        amount        : isNaN(amount)  ? '-' : formatMoney(currency, amount,  should_exclude_currency),
-        balance       : isNaN(balance) ? '-' : formatMoney(currency, balance, should_exclude_currency),
-        desc          : transaction.longcode.replace(/\n/g, '<br />'),
-        id            : transaction.contract_id,
-        app_id        : transaction.app_id,
-        transaction_id: transaction.transaction_id,
-        shortcode     : ['buy', 'sell'].includes(transaction.action_type) ? transaction.shortcode : null,
-        action_type   : transaction.action_type,
+        action     : localize(toTitleCase(transaction.action_type) /* localize-ignore */), // handled in static_strings_app_2.js: 'Buy', 'Sell', 'Deposit', 'Withdrawal'
+        date       : `${date_str}\n${time_str}`,
+        refid      : transaction.transaction_id,
+        payout     : isNaN(payout) ? '-' : formatMoney(currency, payout, should_exclude_currency),
+        amount     : isNaN(amount) ? '-' : formatMoney(currency, amount, should_exclude_currency),
+        balance    : isNaN(balance) ? '-' : formatMoney(currency, balance, should_exclude_currency),
+        desc       : transaction.longcode.replace(/\n/g, '<br />'),
+        id         : transaction.contract_id,
+        app_id     : transaction.app_id,
+        shortcode  : ['buy', 'sell'].includes(transaction.action_type) ? transaction.shortcode : null,
+        action_type: transaction.action_type,
     };
 };
