9e8362d4512cbe179b7e504cedbc9c09f44fcc80
brandon <26451183+ahkuma@users.noreply.github.com>
2019-04-22 17:15:29 +0800

    refactor tooltip-2

--- src/javascript/app_2/App/Components/Elements/tooltip-2.jsx
+++ src/javascript/app_2/App/Components/Elements/tooltip-2.jsx
@@ -2,62 +2,46 @@ import React      from 'react';
 import ReactDOM   from 'react-dom';
 import classNames from 'classnames';
 
-class Tooltip2 extends React.Component {
-    state = {
-        should_show_tooltip: false,
-    }
-
+class Tooltip2 extends React.PureComponent {
     constructor(props) {
         super(props);
+        this.state = { should_show_tooltip: false };
         this.item_reference = React.createRef();
         this.item_coordinates = {};
     }
 
     onMouseEnter = () => {
-        this.setState({
-            should_show_tooltip: true,
-        });
+        this.setState({ should_show_tooltip: true });
         this.item_coordinates = this.item_reference.current.getBoundingClientRect();
     }
 
     onMouseLeave = () => {
-        this.setState({
-            should_show_tooltip: false,
-        });
+        this.setState({ should_show_tooltip: false });
     }
 
     createPositionStyle = alignment => {
-        let style;
         switch (alignment) {
-            case 'top':
-                style = {
-                    left  : this.item_coordinates.x,
-                    bottom: `calc(100% - ${this.item_coordinates.top}px)`,
-                };
-                break;
-            case 'right':
-                style = {
-                    left: this.item_coordinates.x + this.item_coordinates.width,
-                    top : this.item_coordinates.y,
-                };
-                break;
-            case 'bottom':
-                style = {
-                    left: this.item_coordinates.x,
-                    top : this.item_coordinates.y + this.item_coordinates.height,
-                };
-                break;
-            case 'left':
-                style = {
-                    right: `calc(100% - ${this.item_coordinates.left}px)`,
-                    top  : this.item_coordinates.y,
-                };
-                break;
-            default:
-                break;
+            case 'top': return {
+                left  : this.item_coordinates.x,
+                bottom: `calc(100% - ${this.item_coordinates.top}px)`,
+            };
+            case 'right': return {
+                left: this.item_coordinates.x + this.item_coordinates.width,
+                top : this.item_coordinates.y,
+            };
+            case 'bottom': return {
+                left: this.item_coordinates.x,
+                top : this.item_coordinates.y + this.item_coordinates.height,
+            };
+            case 'left': return {
+                right: `calc(100% - ${this.item_coordinates.left}px)`,
+                top  : this.item_coordinates.y,
+            };
+            default: return {
+                left: this.item_coordinates.x,
+                top : this.item_coordinates.y,
+            };
         }
-
-        return style;
     }
 
     createTooltipElement = (
@@ -68,18 +52,15 @@ class Tooltip2 extends React.Component {
     ) => (
         <span
             style={style}
-            className={
-                classNames(
-                    'tooltip-2',
-                    `tooltip-2--${alignment}`,
-                    { 'tooltip-2--show': should_show_tooltip },
-                )}
+            className={classNames(
+                'tooltip-2',
+                `tooltip-2--${alignment}`,
+                { 'tooltip-2--show': should_show_tooltip },
+            )}
         >
-            <span className={
-                classNames(
-                    'tooltip-2-arrow',
-                    `tooltip-2-arrow--${alignment}`,
-                )}
+            <span className={classNames(
+                'tooltip-2-arrow',
+                `tooltip-2-arrow--${alignment}`)}
             />
             { message }
         </span>
@@ -93,10 +74,12 @@ class Tooltip2 extends React.Component {
         } = this.props;
 
         const tooltip = ReactDOM.createPortal(
-            this.createTooltipElement(alignment,
+            this.createTooltipElement(
+                alignment,
                 this.createPositionStyle(alignment),
                 this.state.should_show_tooltip,
-                message),
+                message
+            ),
             document.getElementById('binary_app'),
         );
 
