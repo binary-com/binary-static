f7156e6f83d1b252b56cceab3c53c8d7cba29b5d
brandon <26451183+ahkuma@users.noreply.github.com>
2019-04-23 16:38:04 +0800

    refactor tooltip-2

--- src/javascript/app_2/App/Components/Elements/Tooltip2/tooltip-2.jsx
+++ src/javascript/app_2/App/Components/Elements/Tooltip2/tooltip-2.jsx
@@ -4,43 +4,17 @@ import TooltipBubble from './tooltip-bubble.jsx';
 class Tooltip2 extends React.PureComponent {
     constructor(props) {
         super(props);
-        this.state = { should_show_tooltip: false };
-        this.item_reference = React.createRef();
-        this.item_coordinates = {};
+        this.state = { should_show_tooltip_bubble: false };
+        this.tooltip_trigger_ref = React.createRef();
     }
 
     onMouseEnter = () => {
-        this.setState({ should_show_tooltip: true });
-        this.item_coordinates = this.item_reference.current.getBoundingClientRect();
+        this.setState({ should_show_tooltip_bubble: true });
+        this.tooltip_trigger_rect = this.tooltip_trigger_ref.current.getBoundingClientRect();
     }
 
     onMouseLeave = () => {
-        this.setState({ should_show_tooltip: false });
-    }
-
-    createPositionStyle = alignment => {
-        switch (alignment) {
-            case 'top': return {
-                left  : this.item_coordinates.x,
-                bottom: `calc(100% - ${this.item_coordinates.top}px)`,
-            };
-            case 'right': return {
-                left: this.item_coordinates.x + this.item_coordinates.width,
-                top : this.item_coordinates.y,
-            };
-            case 'bottom': return {
-                left: this.item_coordinates.x,
-                top : this.item_coordinates.y + this.item_coordinates.height,
-            };
-            case 'left': return {
-                right: `calc(100% - ${this.item_coordinates.left}px)`,
-                top  : this.item_coordinates.y,
-            };
-            default: return {
-                left: this.item_coordinates.x,
-                top : this.item_coordinates.y,
-            };
-        }
+        this.setState({ should_show_tooltip_bubble: false });
     }
 
     render() {
@@ -56,16 +30,16 @@ class Tooltip2 extends React.PureComponent {
                 onMouseLeave={this.onMouseLeave}
             >
                 <div
-                    ref={this.item_reference}
+                    ref={this.tooltip_trigger_ref}
                 >
                     { children }
                 </div>
 
-                { this.state.should_show_tooltip &&
+                { this.state.should_show_tooltip_bubble &&
                     <TooltipBubble
                         alignment={alignment}
-                        style={this.createPositionStyle(alignment)}
                         message={message}
+                        tooltip_trigger_rect={this.tooltip_trigger_rect}
                     />
                 }
             </div>
--- src/javascript/app_2/App/Components/Elements/Tooltip2/tooltip-bubble.jsx
+++ src/javascript/app_2/App/Components/Elements/Tooltip2/tooltip-bubble.jsx
@@ -1,27 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import classNames from 'classnames';
+import React               from 'react';
+import ReactDOM            from 'react-dom';
+import classNames          from 'classnames';
 
-const TooltipBubble = ({
-    alignment,
-    style,
-    message,
-}) => ReactDOM.createPortal(
-    <span
-        style={style}
-        className={classNames(
-            'tooltip-2',
-            `tooltip-2--${alignment}`,
-        )}
-    >
-        <span className={classNames(
-            'tooltip-2-arrow',
-            `tooltip-2-arrow--${alignment}`,
-        )}
-        />
-        { message }
-    </span>,
-    document.getElementById('binary_app')
-);
+class TooltipBubble extends React.PureComponent {
+    getBubblePositionStyle = (alignment, tooltip_trigger_rect) => {
+        switch (alignment) {
+            case 'top': return {
+                left  : tooltip_trigger_rect.x,
+                bottom: `calc(100% - ${tooltip_trigger_rect.top}px)`,
+            };
+            case 'right': return {
+                left: tooltip_trigger_rect.x + tooltip_trigger_rect.width,
+                top : tooltip_trigger_rect.y,
+            };
+            case 'bottom': return {
+                left: tooltip_trigger_rect.x,
+                top : tooltip_trigger_rect.y + tooltip_trigger_rect.height,
+            };
+            case 'left': return {
+                right: `calc(100% - ${tooltip_trigger_rect.left}px)`,
+                top  : tooltip_trigger_rect.y,
+            };
+            default: return {
+                left: tooltip_trigger_rect.x,
+                top : tooltip_trigger_rect.y,
+            };
+        }
+    }
+
+    render() {
+        const {
+            alignment,
+            message,
+            tooltip_trigger_rect,
+        } = this.props;
+
+        return ReactDOM.createPortal(
+            <span
+                style={this.getBubblePositionStyle(
+                    alignment,
+                    tooltip_trigger_rect,
+                )}
+                className={classNames(
+                    'tooltip-2',
+                    `tooltip-2--${alignment}`,
+                )}
+            >
+                <span className={classNames(
+                    'tooltip-2-arrow',
+                    `tooltip-2-arrow--${alignment}`,
+                )}
+                />
+                { message }
+            </span>,
+            document.getElementById('binary_app')
+        );
+    }
+}
 
 export default TooltipBubble;
