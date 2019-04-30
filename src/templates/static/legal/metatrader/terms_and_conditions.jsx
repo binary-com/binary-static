0b8945c0fcb7f5e0829603279e2fc755baab41c0
Nazanin Reihani <nazaninreihani@users.noreply.github.com>
2019-04-30 10:41:41 +0800

    Update src/templates/static/legal/metatrader/terms_and_conditions.jsx
    Co-Authored-By: Khalidxx <FXTRyio@gmail.com>

--- src/templates/static/legal/metatrader/terms_and_conditions.jsx
+++ src/templates/static/legal/metatrader/terms_and_conditions.jsx
@@ -194,7 +194,7 @@ const TermsAndConditions = () => (
             <li>{it.L('Margin Requirement and calculation')}
                 <p>
                     <ol>
-                        <li>{it.L('Margin Requirement is the amount that the Client needs to have in their account prior to entering into a trade and it is a percentage of the value  of that trade. Margin Requirement will continue to increase/decrease in accordance with the volume and direction of the Open Positions.')}</li>
+                        <li>{it.L('Margin Requirement is the amount that the Client needs to have in their account prior to entering into a trade and it is a percentage of the value of that trade. Margin Requirement will continue to increase/decrease in accordance with the volume and direction of the Open Positions.')}</li>
                         <li>{it.L('The Company may modify Margin Requirements for any Open Positions or new Orders at its sole discretion. Formulas for Margin Requirements published on the website are indicative only and may be changed by the Company at its discretion at any time, due to changing market conditions or other factors.')}</li>
                         <li>{it.L('Whilst the Company endeavours to close out the Client\'s Open Positions if and when the Margin Level for their MT5 Real Account reaches or falls below the Stop Out Level')}</li>
                         <li>{it.L('The Company does not guarantee that the Client\'s Open Positions will be closed when the Margin Level for their MT5 Real Account reaches the Stop Out Level.')}</li>
