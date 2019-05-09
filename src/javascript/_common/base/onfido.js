090c8d17842666ec27c0dd64d30b0f4eed7694e4
Mahdi <m.pourziaei@gmail.com>
2019-05-09 16:07:41 +0800

    Removed Onfido code that belongs in Backend

--- src/javascript/_common/base/onfido.js
+++ src/javascript/_common/base/onfido.js
@@ -1,73 +1,10 @@
-/* eslint-disable no-console */
 const OnfidoSDK    = require('onfido-sdk-ui');
-const websiteUrl   = require('../url').websiteUrl;
 
 const Onfido = (() => {
-    let api_key, applicant_id, sdk_token, onfidoOut;
-    const api_base_url = 'https://api.onfido.com/v2';
+    let onfidoOut, sdk_token;
 
-    const getApiUrlFor = api_path => {
-        if (api_path[0] === '/') {
-            return api_base_url + api_path;
-        }
-
-        return `${api_base_url}/${api_path}`;
-    };
-
-    const init = async (applicant) => {
-        api_key = await simulateCallForApiKey();
-        console.log(api_key);
-        createApplicant(applicant);
-    };
-
-    const sendRequest = (method, url, params = null) => (
-        new Promise((resolve, reject) => {
-            const xhttp           = new XMLHttpRequest();
-
-            xhttp.overrideMimeType('application/json');
-            xhttp.open(method, url, true);
-            xhttp.setRequestHeader('Authorization', `Token token=${api_key}`);
-
-            xhttp.onreadystatechange = function () {
-                if (xhttp.readyState === 4) {
-                    if (xhttp.status >= 200 && xhttp.status < 400) {
-                        resolve(JSON.parse(xhttp.responseText));
-                    } else {
-                        reject(new Error({
-                            statusText: xhttp.statusText,
-                            response  : JSON.parse(xhttp.responseText || null),
-                        }));
-                    }
-                }
-            };
-
-            xhttp.send(method === 'POST' ? params : null);
-        })
-    );
-
-    const createApplicant = ({ first, last }) => {
-        const params = `first_name=${first}&last_name=${last}`;
-
-        sendRequest('POST', getApiUrlFor('/applicants'), params)
-            .then(res => {
-                console.log(res);
-                applicant_id = res.id;
-                generateSdkToken();
-            });
-    };
-
-    const generateSdkToken = () => {
-        const params = `applicant_id=${applicant_id}&referrer=${websiteUrl()}/*`;
-
-        sendRequest('POST', getApiUrlFor('/sdk_token'), params)
-            .then(res => {
-                console.log(res);
-                sdk_token = res.token;
-                runOnfido();
-            });
-    };
-
-    const runOnfido = () => {
+    const init = async () => {
+        sdk_token = await simulateCallForApiKey();
         onfidoOut = OnfidoSDK.init({
             containerId: 'onfido',
             // language   : 'en', // Currently only 'en' and 'es' are available - custom language objects can be passed
@@ -81,8 +18,11 @@ const Onfido = (() => {
     };
 
     const handleComplete = data => {
+        // eslint-disable-next-line no-console
         console.log('Process complete');
+        // eslint-disable-next-line no-console
         console.log(data);
+        // TODO remove the console logs above and follow comment instructions below
 
         // Call backend to create a check (https://github.com/onfido/onfido-sdk-ui#1-creating-a-check).
         // When creating a `facial_similarity` check, you can specify the variant
@@ -94,7 +34,7 @@ const Onfido = (() => {
     const changeOnfidoOptions = options => { onfidoOut.setOptions(options); };
 
     const simulateCallForApiKey = () => new Promise(resolve => {
-        setTimeout(() => resolve('test_SZLtr4bamjGfSjtnOJEGbyZCGoMIEj26'), Math.random() * 1000);
+        setTimeout(() => resolve('eyJhbGciOiJIUzI1NiJ9.eyJwYXlsb2FkIjoicEVUV3NIUTJJdVljZ2FJSnl4dkdseEYxTmtXS0NFdVlqZXZhYlAyUVl4ZVA5clU1UTJZV2toZGRycUY5XG5NTFdxeGFJZENSdEEwUlRObnR1eUhyYXRmVGZCNzJKcmNsZGMyYzNoUkNZa1pZbnNSVE5TMUVKcGlnMnBcbjYzUm9FUm53XG4iLCJ1dWlkIjoiSHhzYTA5dXZrQTgiLCJleHAiOjE1NTY3ODMzNTZ9.PWEAP1f5nnu0VnyTIoKW4ylf_ktjUht9zua4kqbCQRo'), Math.random() * 1000);
     });
 
     return {
