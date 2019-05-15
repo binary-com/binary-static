2e0ed24dbbe6f7db3f4fdf9655f8060ae1f8242d
Mahdi <m.pourziaei@gmail.com>
2019-05-15 14:38:09 +0800

    Modified middleware for POC response to include necessary properties

--- src/javascript/app/common/request_middleware.js
+++ src/javascript/app/common/request_middleware.js
@@ -3,24 +3,34 @@ const addComma             = require('../../_common/base/currency_base').addComm
 
 const changePocNumbersToString = (response) => {
     const {
+        barrier,
         current_spot,
+        entry_spot,
+        entry_tick,
         exit_tick,
         sell_price,
+        sell_spot,
+        profit_percentage,
     } = response.proposal_open_contract;
 
     return new Promise((resolve) => {
         getUnderlyingPipSize(response.proposal_open_contract.underlying).then(pip_size => {
-            const toString = (property) => (
-                property || property === 0 ? addComma(property, pip_size) : undefined
+            const toString = (property, decimal_places = pip_size) => (
+                property || property === 0 ? addComma(property, decimal_places) : undefined
             );
 
             resolve($.extend({}, {
                 ...response,
                 proposal_open_contract: {
                     ...response.proposal_open_contract,
-                    sell_price  : toString(sell_price),
-                    current_spot: toString(current_spot),
-                    exit_tick   : toString(exit_tick),
+                    barrier          : barrier.toString(),
+                    sell_price       : toString(sell_price),
+                    sell_spot        : toString(sell_spot),
+                    current_spot     : toString(current_spot),
+                    entry_spot       : toString(entry_spot),
+                    entry_tick       : toString(entry_tick),
+                    exit_tick        : toString(exit_tick),
+                    profit_percentage: toString(profit_percentage, 2),
                 },
             }));
         });
