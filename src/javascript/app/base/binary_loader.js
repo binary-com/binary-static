f2da4e6fe3e2db73458b0f6af330b23f2f69f287
Khalid Ibrahim <fxtryio@gmail.com>
2019-05-21 11:18:56 +0800

    without title variable

--- src/javascript/app/base/binary_loader.js
+++ src/javascript/app/base/binary_loader.js
@@ -149,8 +149,7 @@ const BinaryLoader = (() => {
             return;
         }
 
-        const title = content.getElementsByTagName('h1')[0];
-        const div_container = createElement('div', { class: 'logged_out_title_container', html: title || '' });
+        const div_container = createElement('div', { class: 'logged_out_title_container', html: content.getElementsByTagName('h1')[0] || '' });
         const div_notice    = createElement('p', { class: 'center-text notice-msg', html: localized_message });
 
         div_container.appendChild(div_notice);
