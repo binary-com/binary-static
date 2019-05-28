4bdc663462d5c140227a3188922b1ed0ad1f7ba6
Amam Mustofa <mustofa@binary.com>
2019-05-28 16:24:15 +0800

    statistics done

--- src/templates/static/affiliate_ib_landing.jsx
+++ src/templates/static/affiliate_ib_landing.jsx
@@ -2,33 +2,31 @@ import React from 'react';
 
 const AffiliateIbLanding = () => (
     // suggestion hero
-    <React.Fragment>
+    <div className='affiliate-ib-landing'>
         <section id='page_top' className='hero'>
             <h1>Build a rewarding and long-term business relationship with an industry pioneer</h1>
             <button><span className='btn-text'>Become our partner</span></button>
         </section>
 
         <section className='statistics'>
-            <div className='container'>
-                <article className='statistics-item'>
-                    <h2>40K+</h2>
-                    <h4>Partners</h4>
-                </article>
-                <article className='statistics-item'>
-                    <h2>$12M+</h2>
-                    <h4>Partner earnings</h4>
-                </article>
-                <article className='statistics-item'>
-                    <h2>150+</h2>
-                    <h4>Countries</h4>
-                </article>
-                <article className='statistics-item'>
-                    <h2>1M+</h2>
-                    <h4>Clients</h4>
-                </article>
-            </div>
+            <article className='statistics-item'>
+                <h1>40K+</h1>
+                <h3>Partners</h3>
+            </article>
+            <article className='statistics-item'>
+                <h1>$12M+</h1>
+                <h3>Partner earnings</h3>
+            </article>
+            <article className='statistics-item'>
+                <h1>150+</h1>
+                <h3>Countries</h3>
+            </article>
+            <article className='statistics-item'>
+                <h1>1M+</h1>
+                <h3>Clients</h3>
+            </article>
         </section>
-    </React.Fragment>
+    </div>
 );
 
 export default AffiliateIbLanding;
