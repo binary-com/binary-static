c791a6c8be366ca5bc6d737aa755b8128cf82305
brandon <26451183+ahkuma@users.noreply.github.com>
2019-05-30 14:36:25 +0800

    fix table mobile

--- src/templates/static/affiliate_ib_landing.jsx
+++ src/templates/static/affiliate_ib_landing.jsx
@@ -167,7 +167,7 @@ const AffiliateIbLanding = () => (
                         <div className='gr-12 gr-padding-20 white-bg-color'>
                             <p className='no-margin'>{it.L(`Earn daily commissions on the trading activities of the clients you refer to the ${it.website_name} MetaTrader 5 (MT5) platform. Your total commissions will be credited into your account by the end of the day.`)}</p>
 
-                            <div className='gr-10 gr-parent gr-centered'>
+                            <div className='gr-10 gr-12-m gr-parent gr-centered'>
                                 <h3 className='secondary-color'>{it.L('Commision structure')}</h3>
                                 
                                 <h3>{it.L('Synthetic Indices Account')}</h3>
