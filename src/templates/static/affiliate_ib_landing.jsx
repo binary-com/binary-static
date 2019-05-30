8f6cde91f86a9a967dcfc4ceb66eff8f56a5a7b3
brandon <26451183+ahkuma@users.noreply.github.com>
2019-05-30 14:03:18 +0800

    how it works is now responsive

--- src/templates/static/affiliate_ib_landing.jsx
+++ src/templates/static/affiliate_ib_landing.jsx
@@ -275,34 +275,34 @@ const AffiliateIbLanding = () => (
             <div className='container center-text gr-padding-20'>
                 <h2>{it.L('How it works')}</h2>
                 <div className='gr-row'>
-                    <div className='gr-1' />
-                    <div className='gr-2 gr-centered'>
-                        <img src={it.url_for('images/pages/affiliates_ib_landing/icons/signup.svg')} />
-                    </div>
-                    <div className='gr-1 gr-centered align-self-center'>
-                        <img src={it.url_for('images/pages/affiliates_ib_landing/icons/circle_arrow.svg')} />
-                    </div>
-                    <div className='gr-3 gr-centered'>
-                        <img src={it.url_for('images/pages/affiliates_ib_landing/icons/introduce.svg')} />
-                    </div>
-                    <div className='gr-1 gr-centered align-self-center'>
-                        <img src={it.url_for('images/pages/affiliates_ib_landing/icons/circle_arrow.svg')} />
-                    </div>
-                    <div className='gr-2 gr-centered'>
-                        <img src={it.url_for('images/pages/affiliates_ib_landing/icons/earn.svg')} />
-                    </div>
-                    <div className='gr-1' />
-                </div>
-                <div className='gr-row'>
-                    <div className='gr-4'>
+                    <div className='gr-4 gr-12-m gr-centered'>
+                        <div className='gr-row'>
+                            <div className='gr-2 gr-hide-m gr-hide-p' />
+                            <div className='gr-8 gr-12-m gr-12-p'>
+                                <img src={it.url_for('images/pages/affiliates_ib_landing/icons/signup.svg')} />
+                            </div>
+                            <div className='gr-2 gr-hide-m gr-hide-p align-self-center'>
+                                <img src={it.url_for('images/pages/affiliates_ib_landing/icons/circle_arrow.svg')} />
+                            </div>
+                        </div>
                         <h3>{it.L('Sign up')}</h3>
                         <p>{it.L('Choose your preferred programme, complete the application form, and receive your affiliate link upon approval')}</p>
                     </div>
-                    <div className='gr-4'>
+                    <div className='gr-4 gr-12-m gr-centered'>
+                        <div className='gr-row'>
+                            <div className='gr-1 gr-hide-m gr-hide-p' />
+                            <div className='gr-10 gr-12-m gr-12-p'>
+                                <img src={it.url_for('images/pages/affiliates_ib_landing/icons/introduce.svg')} />
+                            </div>
+                            <div className='gr-1 gr-hide-m gr-hide-p align-self-center'>
+                                <img src={it.url_for('images/pages/affiliates_ib_landing/icons/circle_arrow.svg')} />
+                            </div>
+                        </div>
                         <h3>{it.L(`Introduce ${it.website_name}`)}</h3>
                         <p>{it.L(`Spread the word to your audience. Use your unique referral link and our tried-and-tested referral tools to drive traffic to ${it.website_name}.`)}</p>
                     </div>
-                    <div className='gr-4'>
+                    <div className='gr-4 gr-12-m gr-centered'>
+                        <img src={it.url_for('images/pages/affiliates_ib_landing/icons/earn.svg')} />
                         <h3>{it.L('Earn')}</h3>
                         <p>{it.L('Refer new clients to us. Receive commissions based on your chosen partnership programme.')}</p>
                     </div>
