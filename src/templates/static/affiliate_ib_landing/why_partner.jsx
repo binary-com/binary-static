b61e8401075541e0e488a1101191828fe9026623
brandon <26451183+ahkuma@users.noreply.github.com>
2019-05-31 17:52:43 +0800

    refator why-partner

--- src/templates/static/affiliate_ib_landing/why_partner.jsx
+++ src/templates/static/affiliate_ib_landing/why_partner.jsx
@@ -1,49 +1,60 @@
 import React from 'react';
 
-const WhyPartner = () => (
-    <section className='why-partner primary-bg-color'>
-        <div className='container center-text full-height gr-padding-30'>
-            <h2 className='center-text color-white'>{it.L('Why partner with us')}</h2>
-            <div className='gr-row gr-padding-20'>
-                <div className='gr-4 gr-padding-10'>
-                    <img src={it.url_for('images/pages/affiliates_ib_landing/icons/commission.svg')} alt='Generous commissions' className='gr-centered' />
-                    <h4 className='secondary-color'>{it.L('Generous commissions')}</h4>
-                </div>
-                <div className='gr-4 gr-padding-10'>
-                    <img src={it.url_for('images/pages/affiliates_ib_landing/icons/conversion.svg')} alt='High conversions' className='gr-centered' />
-                    <h4 className='secondary-color'>{it.L('High conversions')}</h4>
-                </div>
-                <div className='gr-4 gr-padding-10'>
-                    <img src={it.url_for('images/pages/affiliates_ib_landing/icons/payment.svg')} alt='On-time payments' className='gr-centered' />
-                    <h4 className='secondary-color'>{it.L('On-time payments')}</h4>
-                </div>
-                <div className='gr-4 gr-padding-10'>
-                    <img src={it.url_for('images/pages/affiliates_ib_landing/icons/no-hidden-fees.svg')} alt='No hidden fees' className='gr-centered' />
-                    <h4 className='secondary-color'>{it.L('No hidden fees')}</h4>
-                </div>
-                <div className='gr-4 gr-padding-10'>
-                    <img src={it.url_for('images/pages/affiliates_ib_landing/icons/contact.svg')} alt='Customer-centric partnership' className='gr-centered' />
-                    <h4 className='secondary-color'>{it.L('Customer-centric partnership')}</h4>
-                </div>
-                <div className='gr-4 gr-padding-10'>
-                    <img src={it.url_for('images/pages/affiliates_ib_landing/icons/diversify-income.svg')} alt='Multiple income opportunities' className='gr-centered' />
-                    <h4 className='secondary-color'>{it.L('Multiple income opportunities')}</h4>
-                </div>
-                <div className='gr-4 gr-padding-10'>
-                    <img src={it.url_for('images/pages/affiliates_ib_landing/icons/marketing.svg')} alt='Advanced referral tools' className='gr-centered' />
-                    <h4 className='secondary-color'>{it.L('Advanced referral tools')}</h4>
-                </div>
-                <div className='gr-4 gr-padding-10'>
-                    <img src={it.url_for('images/pages/affiliates_ib_landing/icons/support-faq.svg')} alt='International support' className='gr-centered' />
-                    <h4 className='secondary-color'>{it.L('International support')}</h4>
-                </div>
-                <div className='gr-4 gr-padding-10'>
-                    <img src={it.url_for('images/pages/affiliates_ib_landing/icons/globe.svg')} alt='Multilingual platforms' className='gr-centered' />
-                    <h4 className='secondary-color'>{it.L('Multilingual platforms')}</h4>
+const WhyPartner = () => {
+    const reasons = [
+        {
+            image      : it.url_for('images/pages/affiliates_ib_landing/icons/commission.svg'),
+            description: it.L('Generous commissions'),
+        },
+        {
+            image      : it.url_for('images/pages/affiliates_ib_landing/icons/conversion.svg'),
+            description: it.L('High conversions'),
+        },
+        {
+            image      : it.url_for('images/pages/affiliates_ib_landing/icons/payment.svg'),
+            description: it.L('On-time payments'),
+        },
+        {
+            image      : it.url_for('images/pages/affiliates_ib_landing/icons/no-hidden-fees.svg'),
+            description: it.L('No hidden fees'),
+        },
+        {
+            image      : it.url_for('images/pages/affiliates_ib_landing/icons/contact.svg'),
+            description: it.L('Customer-centric partnership'),
+        },
+        {
+            image      : it.url_for('images/pages/affiliates_ib_landing/icons/diversify-income.svg'),
+            description: it.L('Multiple income opportunities'),
+        },
+        {
+            image      : it.url_for('images/pages/affiliates_ib_landing/icons/marketing.svg'),
+            description: it.L('Advanced referral tools'),
+        },
+        {
+            image      : it.url_for('images/pages/affiliates_ib_landing/icons/support-faq.svg'),
+            description: it.L('International support'),
+        },
+        {
+            image      : it.url_for('images/pages/affiliates_ib_landing/icons/globe.svg'),
+            description: it.L('Multilingual platforms'),
+        },
+    ];
+
+    return (
+        <section id='why-partner' className='primary-bg-color'>
+            <div className='container center-text gr-padding-30'>
+                <h2 className='center-text color-white'>{it.L('Why partner with us')}</h2>
+                <div className='gr-row gr-padding-20'>
+                    { reasons.map((reason, index) => (
+                        <div key={index} className='gr-4 gr-padding-10'>
+                            <img src={reason.image} alt='Multilingual platforms' className='gr-centered' />
+                            <h4 className='secondary-color'>{reason.description}</h4>
+                        </div>
+                    ))}
                 </div>
             </div>
-        </div>
-    </section>
-);
+        </section>
+    );
+};
 
 export default WhyPartner;
