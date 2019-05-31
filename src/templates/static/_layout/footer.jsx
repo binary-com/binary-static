2da0338b3f4f9128c1c06b1f43a1d06d013c8d2a
brandon <26451183+ahkuma@users.noreply.github.com>
2019-05-31 14:00:49 +0800

    update header and title

--- src/templates/static/_layout/footer.jsx
+++ src/templates/static/_layout/footer.jsx
@@ -103,7 +103,7 @@ const Footer = () => (
                             <FooterColumn
                                 header={it.L('Partner With Us')}
                                 items={[
-                                    { text: it.L('Affiliate and IB Programme'), href: it.url_for('affiliate-ib') },
+                                    { text: it.L('Affiliate and IB Programmes'), href: it.url_for('affiliate-ib') },
                                     { text: it.L('API'),                        href: 'https://developers.binary.com', target: '_blank' },
                                     { text: it.L('Binary Shop'),                href: 'https://shop.binary.com',       target: '_blank' },
                                     /* { text: it.L('Charitable Activities'),   href: it.url_for('charity') }, */
