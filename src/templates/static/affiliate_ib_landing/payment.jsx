b5afcb4de4494907e3fc86fb9e7db3204272fd54
brandon <26451183+ahkuma@users.noreply.github.com>
2019-06-03 15:01:31 +0800

    improvements

--- src/templates/static/affiliate_ib_landing/payment.jsx
+++ src/templates/static/affiliate_ib_landing/payment.jsx
@@ -2,9 +2,9 @@ import React       from 'react';
 import PaymentLogo from '../../_common/components/payment_logo.jsx';
 
 const Payment = () => (
-    <section id='payment'>
+    <section id='payment' className='center-text gr-padding-30'>
         <h2>{it.L('Receive your earnings through your favourite payment method')}</h2>
-        <div className='container center-text gr-padding-30'>
+        <div className='container'>
             <div className='gr-row gr-row-align-center'>
                 <PaymentLogo />
             </div>
