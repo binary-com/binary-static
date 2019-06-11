b5d00630f2188f99167944d3aee01088ea8f702a
brandon <26451183+ahkuma@users.noreply.github.com>
2019-06-11 15:41:48 +0800

    static_pages : bookmarking for type of partners is working time to refactor :skull:

--- src/javascript/static/pages/static_pages.js
+++ src/javascript/static/pages/static_pages.js
@@ -7,6 +7,7 @@ const handleHash   = require('../../_common/utility').handleHash;
 const BinaryPjax   = require('../../app/base/binary_pjax');
 const Client       = require('../../app/base/client');
 const Header       = require('../../app/base/header');
+const Url          = require('../../../javascript/_common/url.js');
 
 module.exports = {
     OpenPositions: {
@@ -40,7 +41,28 @@ module.exports = {
         },
     },
     AffiliatesIb: {
-        onLoad  : () => { $('.has-tabs').tabs(); TabSelector.onLoad(); },
+        onLoad: () => {
+            $('.has-tabs').tabs();
+
+            const navigation_tabs = $('.has-tabs').children('ul').find('li');
+
+            // If query string not set, set it.
+            const params_hash = Url.paramsHash();
+            if (params_hash.tabs === undefined) {
+                Url.updateParamsWithoutReload({ tabs: navigation_tabs.first().find('a').attr('href').replace('#', '') }, true);
+            }
+            // If query string set, then open the correct tab.
+            navigation_tabs.each((index, element) => {
+                $(element).on('click', () => Url.updateParamsWithoutReload({ tabs: $(element).find('a').attr('href').replace('#', '') }, true));
+            });
+
+            navigation_tabs.each((index, element) => {
+                const id = $(element).find('a').attr('href').replace('#', '');
+                $('.has-tabs').tabs({ active: params_hash.tabs === id ? index : 0 });
+            });
+
+            TabSelector.onLoad();
+        },
         onUnload: () => { TabSelector.onUnload(); },
     },
     AffiliatesFAQ: {
