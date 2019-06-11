eaa11c15db11f8d346d7eafe11096387c23a3f90
brandon <26451183+ahkuma@users.noreply.github.com>
2019-06-11 16:32:45 +0800

    refactor affiliate_landing tab

--- src/javascript/static/pages/static_pages.js
+++ src/javascript/static/pages/static_pages.js
@@ -1,13 +1,11 @@
 const tabListener  = require('@binary-com/binary-style').tabListener;
 const ImageSlider  = require('../../_common/image_slider');
 const MenuSelector = require('../../_common/menu_selector');
-const TabSelector  = require('../../_common/tab_selector');
 const Scroll       = require('../../_common/scroll');
 const handleHash   = require('../../_common/utility').handleHash;
 const BinaryPjax   = require('../../app/base/binary_pjax');
 const Client       = require('../../app/base/client');
 const Header       = require('../../app/base/header');
-const Url          = require('../../../javascript/_common/url.js');
 
 module.exports = {
     OpenPositions: {
@@ -39,31 +37,6 @@ module.exports = {
                 Header.upgradeMessageVisibility();
             }
         },
-    },
-    AffiliatesIb: {
-        onLoad: () => {
-            $('.has-tabs').tabs();
-
-            const navigation_tabs = $('.has-tabs').children('ul').find('li');
-
-            // If query string not set, set it.
-            const params_hash = Url.paramsHash();
-            if (params_hash.tabs === undefined) {
-                Url.updateParamsWithoutReload({ tabs: navigation_tabs.first().find('a').attr('href').replace('#', '') }, true);
-            }
-            // If query string set, then open the correct tab.
-            navigation_tabs.each((index, element) => {
-                $(element).on('click', () => Url.updateParamsWithoutReload({ tabs: $(element).find('a').attr('href').replace('#', '') }, true));
-            });
-
-            navigation_tabs.each((index, element) => {
-                const id = $(element).find('a').attr('href').replace('#', '');
-                $('.has-tabs').tabs({ active: params_hash.tabs === id ? index : 0 });
-            });
-
-            TabSelector.onLoad();
-        },
-        onUnload: () => { TabSelector.onUnload(); },
     },
     AffiliatesFAQ: {
         onLoad  : () => { MenuSelector.init(['general', 'account-management-and-tracking', 'referral-tools', 'support']); },
