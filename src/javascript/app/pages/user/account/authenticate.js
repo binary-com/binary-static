697748ccf6f62f2b9418b9fae6000b5262e68694
Amam Mustofa <mustofa@binary.com>
2019-06-19 18:39:19 +0800

    add onfido messages and call socket

--- src/javascript/app/pages/user/account/authenticate.js
+++ src/javascript/app/pages/user/account/authenticate.js
@@ -1,13 +1,43 @@
-const Onfido = require('../../../../_common/base/onfido');
+const Onfido = require('onfido-sdk-ui');
+const BinarySocket = require('../../../base/socket');
+const getLanguage     = require('../../../../_common/language').get;
 
 const Authenticate = (() => {
+    let onfido;
+
+    const simulateCallForApiKey = () => new Promise((resolve, reject) => {
+        BinarySocket.send({
+            service_token: 1,
+            service      : 'onfido',
+            referrer     : 'https://www.binary.com/',
+        }).then((response) => {
+            if (response.error) reject(Error(response.error.message));
+            resolve(response.service_token.token);
+        });
+    });
+
+    const onfidoInit = async () => {
+        try {
+            const sdk_token = await simulateCallForApiKey();
+            onfido = Onfido.init({
+                containerId: 'onfido',
+                language   : getLanguage().toLowerCase(),
+                token      : sdk_token,
+                useModal   : false,
+                onComplete : handleComplete,
+            });
+        } catch (err) {
+            $('#error_occured').setVisibility(1);
+        }
+    };
+
+    const handleComplete = () => {
+        onfido.tearDown();
+        $('#upload_complete').setVisibility(1);
+    };
 
     const onLoad = () => {
-        const user = {
-            first: 'Me',
-            last : 'po',
-        };
-        Onfido.init(user);
+        onfidoInit();
     };
 
     return {
--- src/templates/app/user/authenticate.jsx
+++ src/templates/app/user/authenticate.jsx
@@ -2,30 +2,29 @@ import React from 'react';
 
 const Authenticate = () => (
     <React.Fragment>
-        <h1>{it.L('Onfido Demo')}</h1>
         <div id='onfido' />
 
         <div id='upload_complete' className='center-text gr-padding-20 invisible'>
-            <img className='gr-padding-30' src={it.url_for('images/pages/authenticate/letter.svg')} />
-            <h1 className='gr-padding-20'>{it.L('Thank you for uploading your documents!')}</h1>
+            <img className='gr-padding-20' src={it.url_for('images/pages/authenticate/letter.svg')} />
+            <h1 className='gr-padding-10'>{it.L('Thank you for uploading your documents!')}</h1>
             <p>{it.L('We will send an email after verifying your documents')}</p>
         </div>
 
         <div id='error_occured' className='center-text gr-padding-20 invisible'>
-            <img className='gr-padding-30' src={it.url_for('images/pages/authenticate/clock.svg')} />
-            <h1 className='gr-padding-20'>{it.L('Sorry,')}</h1>
+            <img className='gr-padding-20' src={it.url_for('images/pages/authenticate/clock.svg')} />
+            <h1 className='gr-padding-10'>{it.L('Sorry,')}</h1>
             <p>{it.L('there was a connection error. Please try again later.')}</p>
         </div>
 
         <div id='unverified' className='center-text gr-padding-20 invisible'>
-            <img className='gr-padding-30' src={it.url_for('images/pages/authenticate/invalid.svg')} />
-            <h1 className='gr-padding-20'>{it.L('Sorry, the authentication was not successful.')}</h1>
+            <img className='gr-padding-20' src={it.url_for('images/pages/authenticate/invalid.svg')} />
+            <h1 className='gr-padding-10'>{it.L('Sorry, the authentication was not successful.')}</h1>
             <p>{it.L('Don\'t worry, we will send you an email to assist you further.')}</p>
         </div>
 
         <div id='verified' className='center-text gr-padding-20 invisible'>
-            <img className='gr-padding-30' src={it.url_for('images/pages/authenticate/valid.svg')} />
-            <h1 className='gr-padding-20'>{it.L('You have been successfully verified')}</h1>
+            <img className='gr-padding-20' src={it.url_for('images/pages/authenticate/valid.svg')} />
+            <h1 className='gr-padding-10'>{it.L('You have been successfully verified')}</h1>
         </div>
     </React.Fragment>
 );
