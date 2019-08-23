a2d80407d3be01e18c2b19489411d8ac9ae39c43
mustofa-binary <mustofa@regentmarkets.com>
2019-08-23 15:14:55 +0800

    hide authentication if its not needed

--- src/javascript/app/pages/user/account/settings.js
+++ src/javascript/app/pages/user/account/settings.js
@@ -9,8 +9,15 @@ const Settings = (() => {
             $('.real').setVisibility(!Client.get('is_virtual'));
 
             const status = State.getResponse('get_account_status.status') || [];
+            const authentication = State.getResponse('get_account_status.authentication') || {};
             if (!/social_signup/.test(status)) {
                 $('#change_password').setVisibility(1);
+            }
+
+            if (authentication.identity.status === 'verified' && authentication.document.status === 'verified') {
+                $('#authenticate').setVisibility(1);
+            } else if (!authentication.needs_verification.length) {
+                $('#authenticate').setVisibility(0);
             }
 
             // Professional Client menu should only be shown to maltainvest accounts.
