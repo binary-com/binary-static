1522fd46881b5eddc6eab0e31e1e624f2ee06df1
Brandon <52795236+brandon-binary@users.noreply.github.com>
2019-09-12 14:49:10 +0800

    mobile : removed resource and redirect trade button to mt5 for MF clients

--- src/templates/app/_layout/mobile_menu.jsx
+++ src/templates/app/_layout/mobile_menu.jsx
@@ -14,6 +14,7 @@ const MobileMenu = () => (
                 <Li
                     text={it.L('Trade')}
                     href={it.url_for('trading')}
+                    dataShow='-maltainvest'
                     className='ico-only-hide client_logged_in invisible'
                     subitems={[
                         { text: it.L('SmartTrader'),  href: it.url_for('trading'), className: 'no-capitalize' },
@@ -22,6 +23,7 @@ const MobileMenu = () => (
                         { text: it.L('MetaTrader 5'), href: it.url_for('user/metatrader'), className: 'invisible mt_visibility' },
                     ]}
                 />
+                <Li text={it.L('Trade')}        href={it.url_for('user/metatrader')}     className={'ico-only-hide client_logged_in invisible'} dataShow='maltainvest' />
                 <Li text={it.L('Portfolio')}    href={it.url_for('user/portfoliows')}    className='ico-only-hide client_logged_in invisible' />
                 <Li text={it.L('Profit Table')} href={it.url_for('user/profit_tablews')} className='ico-only-hide client_logged_in invisible' />
                 <Li text={it.L('Statement')}    href={it.url_for('user/statementws')}    className='client_logged_in invisible' />
@@ -30,6 +32,7 @@ const MobileMenu = () => (
                     text={it.L('Resources')}
                     href={it.url_for('resources')}
                     className='ico-only-hide client_logged_in invisible'
+                    dataShow='-maltainvest'
                     subitems={[
                         { text: it.L('Asset Index'),       href: it.url_for('resources/asset_indexws') },
                         { text: it.L('Trading Times'),     href: it.url_for('resources/market_timesws') },
