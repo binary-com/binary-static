26cf2340471e62c095690256bc41b3a000211dbb
negar <negar254000@gmail.com>
2019-09-26 11:26:34 +0330

    change vanuatu to svg but handle old accounts being vanuatu

--- src/javascript/_common/base/gtm.js
+++ src/javascript/_common/base/gtm.js
@@ -84,7 +84,11 @@ const GTM = (() => {
             BinarySocket.wait('mt5_login_list').then((response) => {
                 (response.mt5_login_list || []).forEach((obj) => {
                     const acc_type = (ClientBase.getMT5AccountType(obj.group) || '')
-                        .replace('real_vanuatu', 'financial').replace('vanuatu_', '').replace(/svg/, 'gaming'); // i.e. financial_cent, demo_cent, demo_gaming, real_gaming
+                        .replace('real_vanuatu', 'financial')
+                        .replace('real_svg', 'financial')
+                        .replace('vanuatu_', '')
+                        .replace('svg_', '')
+                        .replace(/svg/, 'gaming'); // i.e. financial_cent, demo_cent, demo_gaming, real_gaming
                     if (acc_type) {
                         data[`mt5_${acc_type}_id`] = obj.login;
                     }
