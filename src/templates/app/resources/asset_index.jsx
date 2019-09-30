db01ecbdba6ddd4eff8f000c6f019558351dfc65
Brandon <52795236+brandon-binary@users.noreply.github.com>
2019-09-30 09:23:43 +0800

    hide trading times and asset index page, show error message instead

--- src/templates/app/resources/asset_index.jsx
+++ src/templates/app/resources/asset_index.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 
 const AssetIndex = () => (
-    <div id='asset_index_wrapper' className='gr-centered gr-12-p gr-12-m'>
-        <h1>{it.L('Asset Index')}</h1>
-        <div className='gr-padding-10'>
-            <p id='errorMsg' className='error-msg invisible' />
-            <div id='asset-index' className='has-tabs gr-parent' />
-            <p className='notice-msg center-text invisible' id='empty-asset-index'>
-                {it.L('Asset index is unavailable in this country. If you have an active [_1] account, please [_2]log in[_3] for full access.', it.website_name, '<a id=\'empty-asset-index-btn-login\' href=\'javascript:;\'>', '</a>')}
-            </p>
+    <React.Fragment>
+        <div id='asset_index_wrapper' className='gr-centered gr-12-p gr-12-m' data-show='-maltainvest'>
+            <h1>{it.L('Asset Index')}</h1>
+            <div className='gr-padding-10'>
+                <p id='errorMsg' className='error-msg invisible' />
+                <div id='asset-index' className='has-tabs gr-parent' />
+                <p className='notice-msg center-text invisible' id='empty-asset-index'>
+                    {it.L('Asset index is unavailable in this country. If you have an active [_1] account, please [_2]log in[_3] for full access.', it.website_name, '<a id=\'empty-asset-index-btn-login\' href=\'javascript:;\'>', '</a>')}
+                </p>
+            </div>
         </div>
-    </div>
+        <p className='center-text notice-msg' data-show='maltainvest' id='trading_error_msg'>{it.L('Sorry, but binary options trading is not available in your financial account.')}</p>
+    </React.Fragment>
 );
 
 export default AssetIndex;
--- src/templates/app/resources/trading_times.jsx
+++ src/templates/app/resources/trading_times.jsx
@@ -2,20 +2,23 @@ import React from 'react';
 
 const TradingTimes = () => (
     <React.Fragment>
-        <h1>{it.L('Trading Times')}</h1>
-        <p id='trading-date-container' className='invisible'>
-            <label htmlFor='trading-date'>{it.L('Date')}: </label>
-            <input type='text' id='trading-date' readOnly='readonly' size='20' />
-        </p>
-        <p id='trading-date-notice' className='invisible'>{it.L('All times are in GMT (Greenwich Mean Time).')}</p>
-
-        <div className='gr-padding-10'>
-            <p className='error-msg invisible' id='errorMsg' />
-            <div id='trading-times' className='has-tabs gr-parent' />
-            <p className='notice-msg center-text invisible' id='empty-trading-times'>
-                {it.L('Trading times is unavailable in this country. If you have an active [_1] account, please [_2]log in[_3] for full access.', it.website_name, '<a id=\'empty-trading-times-btn-login\' href=\'javascript:;\'>', '</a>')}
+        <div data-show='-maltainvest'>
+            <h1>{it.L('Trading Times')}</h1>
+            <p id='trading-date-container' className='invisible'>
+                <label htmlFor='trading-date'>{it.L('Date')}: </label>
+                <input type='text' id='trading-date' readOnly='readonly' size='20' />
             </p>
+            <p id='trading-date-notice' className='invisible'>{it.L('All times are in GMT (Greenwich Mean Time).')}</p>
+
+            <div className='gr-padding-10'>
+                <p className='error-msg invisible' id='errorMsg' />
+                <div id='trading-times' className='has-tabs gr-parent' />
+                <p className='notice-msg center-text invisible' id='empty-trading-times'>
+                    {it.L('Trading times is unavailable in this country. If you have an active [_1] account, please [_2]log in[_3] for full access.', it.website_name, '<a id=\'empty-trading-times-btn-login\' href=\'javascript:;\'>', '</a>')}
+                </p>
+            </div>
         </div>
+        <p className='center-text notice-msg' data-show='maltainvest' id='trading_error_msg'>{it.L('Sorry, but binary options trading is not available in your financial account.')}</p>
     </React.Fragment>
 );
 
