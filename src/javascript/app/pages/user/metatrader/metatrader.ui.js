c84ba544b687da397e019b868feb16b0b42fe503
Brandon <52795236+brandon-binary@users.noreply.github.com>
2019-10-01 14:03:48 +0800

    fixed duplicated advanced

--- src/javascript/app/pages/user/metatrader/metatrader.ui.js
+++ src/javascript/app/pages/user/metatrader/metatrader.ui.js
@@ -468,7 +468,7 @@ const MetaTraderUI = (() => {
             .filter(acc_type => !accounts_info[acc_type].is_demo && accounts_info[acc_type].mt5_account_type !== 'mamm') // toEnableMAM: remove second check
             .forEach((acc_type) => {
                 // toEnableVanuatuAdvanced: remove vanuatu_advanced from regex below
-                if (/labuan_standard|vanuatu_advanced|maltainvest_advanced/.test(acc_type)) {
+                if (/svg_advanced|labuan_standard|vanuatu_advanced|maltainvest_advanced/.test(acc_type)) {
                     return;
                 }
                 count++;
