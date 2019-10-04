6f3e2b3cd78e3fef1447dc3ace9e9f7c843db807
negar <negar254000@gmail.com>
2019-10-04 09:31:26 +0800

    allow adding synthetic indices account for svg

--- src/javascript/app/pages/user/metatrader/metatrader.js
+++ src/javascript/app/pages/user/metatrader/metatrader.js
@@ -97,7 +97,6 @@ const MetaTrader = (() => {
                 Object.keys(mt_companies).forEach((company) => {
                     Object.keys(mt_companies[company]).forEach((acc_type) => {
                         mt_company[company] = State.getResponse(`landing_company.mt_${company}_company.${MetaTraderConfig.getMTFinancialAccountType(acc_type)}.shortcode`);
-                        // if have vanuatu, don't add svg anymore
                         if (mt_company[company]) {
                             addAccount(company, vanuatu_standard_demo_account, vanuatu_standard_real_account);
                         }
@@ -115,7 +114,8 @@ const MetaTrader = (() => {
             const is_demo          = /^demo_/.test(acc_type);
             const type             = is_demo ? 'demo' : 'real';
 
-            if (!((vanuatu_standard_demo_account || vanuatu_standard_real_account) && /svg/.test(mt_company[company]))) {
+            // if have vanuatu, don't add svg anymore unless it's for volatility, meaning has mt5_account_type value
+            if (!((vanuatu_standard_demo_account || vanuatu_standard_real_account) && /svg/.test(mt_company[company]) && mt5_account_type)) {
                 accounts_info[`${type}_${mt_company[company]}${mt5_account_type ? `_${mt5_account_type}` : ''}`] = {
                     is_demo,
                     mt5_account_type,
