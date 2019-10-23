f1976eb08b4f648db3d5912cee56e03e0ecc7cda
negar <negar254000@gmail.com>
2019-10-23 15:31:02 +0800

    update translations to run on master instead

--- scripts/render.js
+++ scripts/render.js
@@ -70,7 +70,7 @@ const getConfig = () => (
         add_translations: false,
         branch          : program.branch,
         dist_path       : Path.join(common.root_path, 'dist', (program.branch || '')),
-        languages       : program.branch === 'translations' ? ['ACH'] : common.languages,
+        languages       : is_translation ? ['ACH'] : common.languages,
         root_path       : common.root_path,
         root_url        : `/${program.dev && !fs.existsSync(Path.join(common.root_path, 'scripts', 'CNAME')) ? 'binary-static/' : ''}${program.branch ? `${program.branch}/` : ''}`,
     }
