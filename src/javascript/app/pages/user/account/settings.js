2ded7c631f89fd90a6f0b205de27f2b669db91b1
Mahdi Aryayi <mahdi.aryayi@firstsource.io>
2019-12-18 11:34:51 +0330

    Removed is_high_risk

--- src/javascript/app/pages/user/account/settings.js
+++ src/javascript/app/pages/user/account/settings.js
@@ -16,11 +16,10 @@ const Settings = (() => {
             }
 
             // Disabling Authentication button for SVG accounts
-            const is_authenticated                 = State.getResponse('get_account_status.status').includes('authenticated');
+            const is_authenticated                 = status.includes('authenticated');
             const is_client_prompt_to_authenticate = State.getResponse('get_account_status.prompt_client_to_authenticate');
-            const is_high_risk                     = Client.getRiskAssessment();
             const is_svg                           = Client.get('landing_company_shortcode') === 'svg';
-            if (is_svg && !is_high_risk && !is_client_prompt_to_authenticate && !is_authenticated) {
+            if (is_svg && !is_client_prompt_to_authenticate && !is_authenticated) {
                 $('#authenticate a')
                     .attr('href', '#')
                     .on('click',  () => {
