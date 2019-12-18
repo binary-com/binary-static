5cf3b9812e3b36020163edee933620064aee1ad5
Mahdi <m.pourziaei@gmail.com>
2019-12-18 11:56:52 +0800

    Code cleanup

--- src/templates/app/cashier/dp2p.jsx
+++ src/templates/app/cashier/dp2p.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 
-// eslint-disable-next-line arrow-body-style
-const DP2P = () => {
-
-    return (
-        <React.Fragment>
-            <div className='invisible' id='message_cashier_unavailable'>
-                <p className='notice-msg center-text'>{it.L('Sorry, this feature is not available in your jurisdiction.')}</p>
-            </div>
-            <div id='binary-dp2p' />
-        </React.Fragment>
-    );
-};
+const DP2P = () => (
+    <React.Fragment>
+        <div className='invisible' id='message_cashier_unavailable'>
+            <p className='notice-msg center-text'>{it.L('Sorry, this feature is not available in your jurisdiction.')}</p>
+        </div>
+        <div id='binary-dp2p' />
+    </React.Fragment>
+);
 
 export default DP2P;
