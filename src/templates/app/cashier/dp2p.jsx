37bc2aa1b6980f8e763cb20a7f72c394165ab685
mustofa binary <mustofa@regentmarkets.com>
2020-01-28 12:11:09 +0800

    mobile message

--- src/templates/app/cashier/dp2p.jsx
+++ src/templates/app/cashier/dp2p.jsx
@@ -5,7 +5,10 @@ const DP2P = () => (
         <div className='invisible' id='message_cashier_unavailable'>
             <p className='notice-msg center-text'>{it.L('Sorry, this feature is not available in your jurisdiction.')}</p>
         </div>
-        <div id='binary-dp2p' />
+        <div className='invisible show_mobile' id='message_mobile_view'>
+            <p className='notice-msg center-text'>{it.L('Sorry, mobile version is not available yet.')}</p>
+        </div>
+        <div id='binary-dp2p' className='gr-hide-m' />
     </React.Fragment>
 );
 
