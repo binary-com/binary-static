3f30730ece1721746f30795afc80a7803ede043a
Salar Hafezi <salar@firstsource.io>
2020-02-24 11:17:59 +0330

    fix: replace Llc with LLC

--- src/templates/static/legal/regulation.jsx
+++ src/templates/static/legal/regulation.jsx
@@ -429,9 +429,9 @@ const Regulation = () => {
                 </Box>
             </div>
 
-            <Box header={it.L('Binary (SVG) Llc')} >
-                <p>{it.L('Binary (SVG) Llc, Hinds Buildings, Kingstown, St. Vincent and the Grenadines; company number 25299 BC 2019.')}</p>
-                <p>{it.L('Clients from the rest of the world (excluding certain countries such as the USA, Canada, and Hong Kong) will have their account opened with Binary (SVG) Llc.')}</p>
+            <Box header={it.L('Binary (SVG) LLC')} >
+                <p>{it.L('Binary (SVG) LLC, Hinds Buildings, Kingstown, St. Vincent and the Grenadines; company number 25299 BC 2019.')}</p>
+                <p>{it.L('Clients from the rest of the world (excluding certain countries such as the USA, Canada, and Hong Kong) will have their account opened with Binary (SVG) LLC.')}</p>
             </Box>
 
             <Box header={it.L('Binary (V) Ltd')} >
