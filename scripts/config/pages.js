c5ae63318246c8a7c72b0505bc3d281f9f4b832d
Behnaz <behnaz@Behnaz-MacBook-pro.local>
2020-04-15 14:20:48 +0430

    in footer change binary to binary.com

--- scripts/config/pages.js
+++ scripts/config/pages.js
@@ -74,7 +74,7 @@ module.exports = [
 
     ['about-us',                                'static/about/index',                          'full_width', 'About Us'],
     ['asuncion',                                'static/about/asuncion',                       'full_width', 'Careers - Asunción'],
-    ['binary-in-numbers',                       'static/about/binary_in_numbers',              'default',    'Binary in Numbers'],
+    ['binary-in-numbers',                       'static/about/binary_in_numbers',              'default',    'Binary.com in Numbers'],
     ['careers',                                 'static/about/careers',                        'full_width', 'Careers'],
     ['careers/privacy-policy',                  'static/about/job_applicant_policy',           'full_width', 'Job Applicant Privacy Policy'],
     ['contact',                                 'static/about/contact',                        'full_width', 'Contact Us'],
