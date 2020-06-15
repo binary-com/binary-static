888b704e8f4ef2e54e0843951a207525239871fd
Behnaz <behnaz@firstsource.tech>
2020-06-15 11:15:35 +0430

    edit code to remove trailing space

--- src/templates/static/affiliate_ib_landing/type_of_partnership.jsx
+++ src/templates/static/affiliate_ib_landing/type_of_partnership.jsx
@@ -197,7 +197,7 @@ const TypesOfPartnership = () => (
                                         [
                                             { text: it.L('Volatility 50 (1s) Index') },
                                             { text: it.L('7.50') },
-                                        ],                       
+                                        ],                   
                                         [
                                             { text: it.L('Volatility 25 Index') },
                                             { text: it.L('3.50') },
@@ -205,7 +205,7 @@ const TypesOfPartnership = () => (
                                         [
                                             { text: it.L('Volatility 25 (1s) Index') },
                                             { text: it.L('3.50') },
-                                        ],                              
+                                        ],                        
                                         [
                                             { text: it.L('Volatility 10 Index') },
                                             { text: it.L('1.50') },
@@ -213,7 +213,7 @@ const TypesOfPartnership = () => (
                                         [
                                             { text: it.L('Volatility 10 (1s) Index') },
                                             { text: it.L('1.50') },
-                                        ],                               
+                                        ],                       
                                         [
                                             { text: it.L('HF Volatility 100 Index') },
                                             { text: it.L('15.00') },
