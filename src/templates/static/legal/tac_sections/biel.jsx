0becec6ac893a9d7fbb3c3113e4b55fe812778d7
Behnaz <behnaz@firstsource.tech>
2020-06-22 21:18:34 +0430

    rename Binary Investments (Europe) to Deriv

--- src/templates/static/legal/tac_sections/biel.jsx
+++ src/templates/static/legal/tac_sections/biel.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Biel = () => (
     <div data-show='eucountry'>
-        <h2 data-anchor='supplementary-terms-and-conditions'>{it.L('Supplementary terms and conditions for Financial Products offered by Binary Investments (Europe) Ltd')}</h2>
-        <p>{it.L('These supplementary terms and conditions (these "ST&Cs") apply solely to the client of Binary Investments (Europe) Ltd ("BIEL") and will govern the relationship between clients and BIEL when trading Financial Products (as defined in the Key information documents [_1]here[_2]) with BIEL.', `<a href=${it.url_for('regulation#key_information_documents')}>`, '</a>')}</p>
+        <h2 data-anchor='supplementary-terms-and-conditions'>{it.L('Supplementary terms and conditions for Financial Products offered by Deriv Investments (Europe) Ltd')}</h2>
+        <p>{it.L('These supplementary terms and conditions (these "ST&Cs") apply solely to the client of Deriv Investments (Europe) Ltd ("BIEL") and will govern the relationship between clients and BIEL when trading Financial Products (as defined in the Key information documents [_1]here[_2]) with BIEL.', `<a href=${it.url_for('regulation#key_information_documents')}>`, '</a>')}</p>
         <p>{it.L('These ST&Cs form part of, and need to be read together with, the general terms and conditions relating to the use of [_1].', it.website_name)}</p>
         <p>{it.L('Clients should read these ST&Cs carefully as well as the Summary order execution policy and Summary conflicts policy, the applicable Contract details together with the Risk disclosure notice and any other documents that BIEL has supplied or in the future supplies to clients.')}</p>
         <p>{it.L('These ST&Cs begin to apply on the date BIEL opens the client\'s account, and, for any new versions after that, on the date BIEL notifies the client.')}</p>
@@ -15,7 +15,7 @@ const Biel = () => (
         <p>{it.L('In providing these services, BIEL is bound by the Act, any applicable regulations, by-laws, licence conditions, guidelines, exchange requirements, and other provisions or market practices (the "Rules"). In the event of conflict between these ST&Cs and the Rules, the latter should prevail.')}</p>
 
         <h2 data-anchor='restrictions'>{it.L('Restrictions')}</h2>
-        <p>{it.L('Residents of countries that have been identified by the FATF as having strategic deficiencies shall be restricted from opening an account with Binary Investments (Europe) Ltd.')}</p>
+        <p>{it.L('Residents of countries that have been identified by the FATF as having strategic deficiencies shall be restricted from opening an account with Deriv Investments (Europe) Ltd.')}</p>
 
         <h2 data-anchor='client-classification'>{it.L('Client classification')}</h2>
         <p>{it.L('Under the Rules, clients may be categorised into one of the following three categories:')}</p>
