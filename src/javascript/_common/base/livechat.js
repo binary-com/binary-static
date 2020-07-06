3e5e4295bb4c011256622ea5d2b73a5b7dc8135f
oskar-binary <oskar@binary.com>
2020-07-06 16:46:43 +0800

    reset the widget for logged out users

--- src/javascript/_common/base/livechat.js
+++ src/javascript/_common/base/livechat.js
@@ -1,10 +1,12 @@
 const ClientBase = require('./client_base');
 
 const LiveChat = (() => {
-
     const init = () => {
         if (window.LiveChatWidget) {
             window.LiveChatWidget.on('visibility_changed', ({ visibility }) => {
+                // only visible to CS
+                let session_variables = { loginid: '', landing_company_shortcode: '', currency: '', residence: '' };
+
                 if (visibility === 'maximized' && ClientBase.isLoggedIn()) {
                     const email = ClientBase.get('email');
                     const fullname = ClientBase.get('fullname');
@@ -12,13 +14,12 @@ const LiveChat = (() => {
                     if (email) window.LiveChatWidget.call('set_customer_email', email);
                     if (fullname) window.LiveChatWidget.call('set_customer_name', fullname);
 
-                    // only visible to CS
                     const loginid = ClientBase.get('loginid');
                     const landing_company_shortcode = ClientBase.get('landing_company_shortcode');
                     const currency = ClientBase.get('currency');
                     const residence = ClientBase.get('residence');
 
-                    const session_variables = {
+                    session_variables = {
                         ...loginid && { loginid },
                         ...landing_company_shortcode && { landing_company_shortcode },
                         ...currency && { currency },
@@ -26,6 +27,12 @@ const LiveChat = (() => {
                     };
 
                     window.LiveChatWidget.call('set_session_variables', session_variables);
+                }
+                if (visibility === 'maximized' && !ClientBase.isLoggedIn()) {
+                    window.LiveChatWidget.call('set_customer_email', ' ');
+                    window.LiveChatWidget.call('set_customer_name', ' ');
+                    window.LiveChatWidget.call('set_session_variables', session_variables);
+
                 }
             });
         }
