244b5c01c185f0ebf0cd4b4cc5878b3b3d774c8d
behnaz-fs <43137011+behnaz-fs@users.noreply.github.com>
2020-07-07 13:03:13 +0430

    remove extra spaces around view membership

--- src/templates/static/legal/regulation.jsx
+++ src/templates/static/legal/regulation.jsx
@@ -546,7 +546,7 @@ const Regulation = () => {
             </Box>
 
             <Box header={it.L('The Financial Commission')} last>
-                <p>{it.L('We are registered with the Financial Commission, an international independent organisation dedicated to resolving disputes within the financial services industry ( [_1]view membership[_2] ).', `<a href=${it.url_for('download/regulation/Binary_membership.pdf')} target="_blank">`, '</a>')}</p>
+                <p>{it.L('We are registered with the Financial Commission, an international independent organisation dedicated to resolving disputes within the financial services industry ([_1]view membership[_2]).', `<a href=${it.url_for('download/regulation/Binary_membership.pdf')} target="_blank">`, '</a>')}</p>
                 <p>{it.L('The Financial Commission offers compensation up to 20,000 EUR on a case-to-case basis, unbiased assessment of disputes, and fast and effective dispute resolution free of charge.')}</p>
                 <RegulatorText />
                 <RegulatorImage padding='4' padding_m='8' href='https://financialcommission.org/' image='images/pages/regulation/financial-commission.svg' />
