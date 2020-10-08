fbe3d3eda471906813669b3a2013e39cafbd04d2
Akmal Djumakhodjaev <71861519+akmal-binary@users.noreply.github.com>
2020-10-08 10:41:14 +0800

    Updated IB info (#6228)
    Co-authored-by: Akmal <akmal@Akmals-MacBook-Pro.local>

--- src/templates/static/affiliate_ib_landing/type_of_partnership.jsx
+++ src/templates/static/affiliate_ib_landing/type_of_partnership.jsx
@@ -268,12 +268,16 @@ const TypesOfPartnership = () => (
                                             { text: it.L('Forex*') },
                                             { text: it.L('5') },
                                         ],
+                                        [
+                                            { text: it.L('Cryptocurrencies**') },
+                                            { text: it.L('0.2%') },
+                                        ],
                                     ],
                                 }}
                             />
 
                             <p className='text-align-left'>*{it.L('Represents the amount in base currency per round trade. For example, a round trade of 1 lot of EUR/USD would pay 10 EUR on financial accounts. A round trade of 1 lot of USD/CAD would pay 5 USD on financial STP accounts.')}</p>
-                            <p className='text-align-left'>**{it.L('Cryptocurrencies commission per round trade. For example, a round trade of 1 lot of BTC/USD with spot price of 10,000 USD will pay 30 USD on financial accounts.')}</p>
+                            <p className='text-align-left'>**{it.L('Cryptocurrencies commission per round trade. For example, a round trade of 1 lot of BTC/USD with spot price of 10,000 USD will pay 30 USD on financial accounts and 20 USD on financial STP accounts.')}</p>
                         </div>
 
                         <a href={it.affiliate_signup_url} className='button' target='_blank' rel='noopener noreferrer'>
