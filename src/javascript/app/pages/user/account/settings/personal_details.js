613c0a26197e2cbfbe0649587000e372c10b085e
Negar Naghshbandi <negar254000@gmail.com>
2020-11-17 16:29:33 +0800

    negar/update_changeable_fields (#6275)
    * feat: use the new immutable field from API
    
    * fix: only add items to validations that are changeable
    
    * fix: update get_settings_data on receiving new get_settings response

--- src/javascript/app/pages/user/account/settings/personal_details.js
+++ src/javascript/app/pages/user/account/settings/personal_details.js
@@ -6,7 +6,6 @@ const Header           = require('../../../../base/header');
 const BinarySocket     = require('../../../../base/socket');
 const FormManager      = require('../../../../common/form_manager');
 const DatePicker       = require('../../../../components/date_picker');
-const ClientBase       = require('../../../../../_common/base/client_base');
 const CommonFunctions  = require('../../../../../_common/common_functions');
 const localize         = require('../../../../../_common/localize').localize;
 const State            = require('../../../../../_common/storage').State;
@@ -27,18 +26,18 @@ const PersonalDetails = (() => {
         is_fully_authenticated,
         residence,
         get_settings_data,
-        changeable_fields,
         mt_acct_type,
         is_mt_tax_required,
+        validations,
         $tax_residence;
 
     const init = () => {
         editable_fields   = {};
         get_settings_data = {};
-        changeable_fields = [];
         is_virtual        = Client.get('is_virtual');
         residence         = Client.get('residence');
         mt_acct_type      = getHashValue('mt5_redirect');
+        validations       = [];
         // demo and synthetic mt accounts do not require tax info
         is_mt_tax_required = /real/.test(mt_acct_type) && mt_acct_type.split('_').length > 2 && +State.getResponse('landing_company.config.tax_details_required') === 1;
     };
@@ -68,40 +67,27 @@ const PersonalDetails = (() => {
     };
 
     const showHideMissingDetails = () => {
-        const validations = getValidations();
         const has_missing_field = validations.find((validation) => /req/.test(validation.validations) && $(validation.selector).val() === '');
         $('#missing_details_notice').setVisibility(!!has_missing_field);
     };
 
-    const populateChangeableFields = () => {
-        if (is_fully_authenticated) return;
-
-        const loginid         = Client.get('loginid');
-        const landing_company = State.getResponse('landing_company');
-        const changeable      = ClientBase.getLandingCompanyValue(loginid, landing_company, 'changeable_fields');
-        if (changeable && changeable.only_before_auth) {
-            changeable_fields = changeable_fields.concat(changeable.only_before_auth);
-        }
-    };
-
     /**
-     * Remove labels and static fields and replace them with input when fields are changeable.
+     * Remove inputs and replace them with labels and static fields when fields are immutable.
      *
-     * @param {get_settings} to prepopulate some of the values.
+     * @param get_settings to prepopulate some of the values.
      */
     const displayChangeableFields = (get_settings) => {
-        if (is_fully_authenticated) return;
-        changeable_fields.forEach(field => {
-            CommonFunctions.getElementById(`row_${field}`).setVisibility(1);
-            CommonFunctions.getElementById(`row_lbl_${field}`).setVisibility(0);
+        get_settings.immutable_fields.forEach(field => {
+            CommonFunctions.getElementById(`row_${field}`).setVisibility(0);
+            CommonFunctions.getElementById(`row_lbl_${field}`).setVisibility(1);
         });
 
-        if (name_fields.some(key => changeable_fields.includes(key))) {
-            CommonFunctions.getElementById('row_name').setVisibility(0);
-            name_fields.forEach(field => CommonFunctions.getElementById(field).setVisibility(1));
+        if (name_fields.some(key => get_settings.immutable_fields.includes(key))) {
+            CommonFunctions.getElementById('row_name').setVisibility(1);
+            name_fields.forEach(field => CommonFunctions.getElementById(field).setVisibility(0));
         }
 
-        if (changeable_fields.includes('date_of_birth')) {
+        if (!get_settings.immutable_fields.includes('date_of_birth')) {
             $('#date_of_birth').setVisibility(1);
 
             DatePicker.init({
@@ -114,20 +100,22 @@ const PersonalDetails = (() => {
             });
         }
 
-        if (changeable_fields.includes('place_of_birth') || changeable_fields.includes('citizen')) {
+        const is_changeable_citizen = !get_settings.immutable_fields.includes('citizen');
+        const is_changeable_pob     = !get_settings.immutable_fields.includes('place_of_birth');
+        if (is_changeable_pob || is_changeable_citizen) {
             const $options       = $('<div/>');
             const residence_list = State.getResponse('residence_list');
             residence_list.forEach((res) => {
                 $options.append(CommonFunctions.makeOption({ text: res.text, value: res.value }));
             });
             $options.prepend($('<option/>', { value: '', text: localize('Please select') }));
-            if (changeable_fields.includes('place_of_birth')) {
+            if (is_changeable_pob) {
                 $('#place_of_birth')
                     .html($options.html())
                     .val(get_settings.place_of_birth);
             }
 
-            if (changeable_fields.includes('citizen')) {
+            if (is_changeable_citizen) {
                 $('#citizen')
                     .html($options.html())
                     .val(get_settings.citizen);
@@ -146,13 +134,13 @@ const PersonalDetails = (() => {
             get_settings.name = `${(get_settings.salutation || '')} ${(get_settings.first_name || '')} ${(get_settings.last_name || '')}`;
         }
 
-        if (get_settings.place_of_birth && !changeable_fields.includes('place_of_birth') && residence_list) {
+        if (get_settings.place_of_birth && get_settings.immutable_fields.includes('place_of_birth') && residence_list) {
             get_settings.place_of_birth =
                 (residence_list.find(obj => obj.value === get_settings.place_of_birth) || {}).text ||
                 get_settings.place_of_birth;
         }
 
-        if (get_settings.citizen && !changeable_fields.includes('citizen') && residence_list) {
+        if (get_settings.citizen && get_settings.immutable_fields.includes('citizen') && residence_list) {
             get_settings.citizen =
                 (residence_list.find(obj => obj.value === get_settings.citizen) || {}).text ||
                 get_settings.citizen;
@@ -163,19 +151,19 @@ const PersonalDetails = (() => {
         if (is_virtual) {
             $(real_acc_elements).remove();
         } else {
+            displayChangeableFields(data);
             $(real_acc_elements).setVisibility(1);
             showHideTaxMessage();
             CommonFunctions.getElementById('tax_information_form').setVisibility(shouldShowTax(get_settings));
             if (is_fully_authenticated) {
                 $(real_acc_auth_elements).setVisibility(1);
-            } else {
-                displayChangeableFields(data);
             }
         }
 
         $(form_id).setVisibility(1);
         $('#loading').remove();
-        FormManager.init(form_id, getValidations());
+        setValidations();
+        FormManager.init(form_id, validations);
         FormManager.handleSubmit({
             form_selector       : form_id,
             obj_request         : { set_settings: 1 },
@@ -186,22 +174,14 @@ const PersonalDetails = (() => {
         showHideMissingDetails();
     };
 
-    const show_label_if_any_value = ['account_opening_reason', 'citizen', 'place_of_birth', 'date_of_birth', 'first_name', 'last_name', 'salutation'];
-    const force_update_fields     = ['tax_residence', 'tax_identification_number'];
-
     const displayGetSettingsData = (get_settings) => {
-        const show_label = [...show_label_if_any_value];
-        if (is_fully_authenticated) {
-            show_label.push('tax_residence', 'tax_identification_number');
-        }
         Object.keys(get_settings).forEach((key) => {
-            // If there are changeable fields, show input instead of labels instead.
-            const has_label         = show_label.includes(key) &&
-                (!is_fully_authenticated ? !changeable_fields.includes(key) : true);
-            const force_update      = force_update_fields.concat(changeable_fields).includes(key);
+            // If field is immutable and value was set by client, show label instead of input
+            const has_label         = get_settings.immutable_fields.includes(key);
             const should_show_label = has_label && get_settings[key];
-            const element_id        = `${should_show_label ? 'lbl_' : ''}${key}`;
-            const element_key       = CommonFunctions.getElementById(element_id);
+
+            const element_id  = `${should_show_label ? 'lbl_' : ''}${key}`;
+            const element_key = document.getElementById(element_id);
             if (!element_key) return;
 
             editable_fields[key] = (get_settings[key] !== null ? get_settings[key] : '');
@@ -233,7 +213,7 @@ const PersonalDetails = (() => {
                 if (should_show_label) { // If we show label, (input) row should be hidden
                     CommonFunctions.getElementById(`row_${key}`).setVisibility(0);
                 }
-                if (force_update) { // Force pushing values, used for (API-)expected values
+                if (has_label) { // Force pushing values, used for (API-)expected values
                     $element.attr({ 'data-force': true, 'data-value': el_value });
                 }
                 // Update data-value on change for inputs
@@ -276,8 +256,7 @@ const PersonalDetails = (() => {
         (Client.isAccountOfType('financial') && Client.shouldCompleteTax()) ||
         is_mt_tax_required;
 
-    const getValidations = () => {
-        let validations;
+    const setValidations = () => {
         if (is_virtual) {
             validations = [
                 { selector: '#email_consent' },
@@ -289,6 +268,9 @@ const PersonalDetails = (() => {
             const is_tax_req   = isTaxReq();
 
             validations = [
+                { selector: '#salutation',             validations: ['req'] },
+                { selector: '#first_name',             validations: ['req', 'letter_symbol', ['length', { min: 2, max: 50 }]] },
+                { selector: '#last_name',              validations: ['req', 'letter_symbol', ['length', { min: 2, max: 50 }]] },
                 { selector: '#address_line_1',         validations: ['req', 'address'] },
                 { selector: '#address_line_2',         validations: ['address'] },
                 { selector: '#address_city',           validations: ['req', 'letter_symbol'] },
@@ -316,28 +298,15 @@ const PersonalDetails = (() => {
                 { selector: '#chk_tax_id', validations: is_financial ? [['req', { hide_asterisk: true, message: localize('Please confirm that all the information above is true and complete.') }]] : '', exclude_request: 1 },
             ];
 
-            // Push validations for changeable fields.
-            changeable_fields.forEach(key => {
-                const selector = `#${key}`;
-
-                // First name and last name validations
-                if (['first_name', 'last_name'].includes(key)) {
-                    validations.push({
-                        selector,
-                        validations: ['req', 'letter_symbol', ['length', { min: 2, max: 50 }]],
-                    });
+            // loop backwards since we are removing array items
+            for (let i = validations.length - 1; i >= 0; i--) {
+                // if field is immutable, no need to validate or send it to API
+                if (!validations[i].exclude_request &&
+                    get_settings_data.immutable_fields.includes(validations[i].selector.slice(1))) {
+                    validations.splice(i, 1);
                 }
-
-                // Required Without special treatment
-                if (['salutation'].includes(key)) {
-                    validations.push({
-                        selector,
-                        validations: ['req'],
-                    });
-                }
-            });
+            }
         }
-        return validations;
     };
 
     const setDetailsResponse = (response) => {
@@ -364,6 +333,7 @@ const PersonalDetails = (() => {
                     return;
                 }
                 const get_settings    = data.get_settings;
+                get_settings_data = get_settings;
                 const has_required_mt = is_mt_tax_required ?
                     (get_settings.tax_residence && get_settings.tax_identification_number && get_settings.citizen)
                     :
@@ -380,9 +350,7 @@ const PersonalDetails = (() => {
                     getDetailsResponse(get_settings);
 
                     // Re-populate changeable fields based on incoming data
-                    if (!is_fully_authenticated) {
-                        displayChangeableFields(get_settings);
-                    }
+                    displayChangeableFields(get_settings);
                     showFormMessage(localize('Your settings have been updated successfully.'), true);
                 }
             });
@@ -499,15 +467,7 @@ const PersonalDetails = (() => {
             get_settings_data = State.getResponse('get_settings');
             is_fully_authenticated = checkStatus(account_status , 'authenticated');
 
-            if (!residence) {
-                displayResidenceList();
-            } else if (is_fully_authenticated) {
-                displayResidenceList();
-                name_fields.forEach(field => CommonFunctions.getElementById(`row_${field}`).classList.add('invisible'));
-            } else {
-                populateChangeableFields();
-                displayResidenceList();
-            }
+            displayResidenceList();
         });
     };
 
