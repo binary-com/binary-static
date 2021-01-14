49fdb7b49f11c051f214925585f1d6fe9b1392ad
Mahdi Aryayi <55829032+mahdiaryayi-fs@users.noreply.github.com>
2021-01-14 10:05:31 +0330

    Fixed footer CFD Risk number (#6333)
    Co-authored-by: Mahdi Aryayi <mahdi.aryayi@firstsource.io>
    Co-authored-by: Negar Naghshbandi <negar254000@gmail.com>

--- src/templates/_common/_layout/footer.jsx
+++ src/templates/_common/_layout/footer.jsx
@@ -225,7 +225,7 @@ const Footer = () => (
                                 {it.L('Trading binary options may not be suitable for everyone, so please ensure that you fully understand the risks involved. Your losses can exceed your initial deposit and you do not own or have any interest in the underlying asset.')}
                             </p>
                             <p className='eu-only invisible'>
-                                {it.L('CFDs are complex instruments and come with a high risk of losing money rapidly due to leverage. [_1] of retail investor accounts lose money when trading CFDs with Deriv Investments (Europe) Limited. You should consider whether you understand how CFDs work and whether you can afford to take the high risk of losing your money.', '74%')}
+                                {it.L('CFDs are complex instruments and come with a high risk of losing money rapidly due to leverage. [_1] of retail investor accounts lose money when trading CFDs with Deriv Investments (Europe) Limited. You should consider whether you understand how CFDs work and whether you can afford to take the high risk of losing your money.', '71%')}
                             </p>
                         </div>
                     </div>
